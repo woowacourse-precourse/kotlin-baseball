1c4397175e1eabc9d6415fbb10ec22a498545a3f
jaemin-Yoo <iamjm29@gmail.com>
2022-11-03 03:38:24 +0900

    fix: 볼, 스트라이크 개수 출력 및 게임 종료

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 private val numberRegex = "[1-9]{3}".toRegex()
+private const val MAX_SIZE = 3
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
     val answer = getRandomNumber()
@@ -15,15 +17,20 @@ fun main() {
             throw IllegalStateException()
         }
 
-    } while (getStrikeCount(answer, input) < 3)
+        val ballCount = getBallCount(answer, input)
+        val strikeCount = getStrikeCount(answer, input)
+        printCount(ballCount, strikeCount)
+    } while (strikeCount < MAX_SIZE)
+
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
 }
 
 fun isValidNumber(n: String): Boolean =
-    numberRegex.matches(n) && n.toSet().count() == 3
+    numberRegex.matches(n) && n.toSet().count() == MAX_SIZE
 
 fun getRandomNumber(): String {
     val numbers = mutableListOf<Int>()
-    while (numbers.size < 3) {
+    while (numbers.size < MAX_SIZE) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
         if (numbers.contains(randomNumber).not()) {
             numbers.add(randomNumber)
@@ -38,11 +45,24 @@ fun getBallCount(answer: String, input: String): Int =
 
 fun getStrikeCount(answer: String, input: String): Int {
     var count = 0
-    for (i in 0 until 3) {
+    for (i in 0 until MAX_SIZE) {
         if (input[i] == answer[i]) {
             count += 1
         }
     }
 
     return count
+}
+
+fun printCount(ballCount: Int, strikeCount: Int) {
+    val result = if (ballCount > 0 && strikeCount > 0) {
+        "${ballCount}볼 ${strikeCount}스트라이크"
+    } else if (ballCount == 0 && strikeCount > 0) {
+        "${strikeCount}스트라이크"
+    } else if (ballCount > 0 && strikeCount == 0) {
+        "${ballCount}볼"
+    } else {
+        "낫싱"
+    }
+    println(result)
 }
