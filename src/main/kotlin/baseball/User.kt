2e1c87d8c6bd739c21ab3bbdb67850eb47fd3a0c
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-11-03 03:57:37 +0900

    chore : 사용자 입력을 처리하기 위한 클래스 분리 extension 함수 구현

---
+++ src/main/kotlin/baseball/User.kt
@@ -0,0 +1,28 @@
+package baseball
+
+import camp.nextstep.edu.missionutils.Console
+import javax.swing.text.html.HTMLDocument.HTMLReader.CharacterAction
+
+object User {
+    fun guessNumber(): ArrayList<Int>? {
+        val userInput = Console.readLine()
+
+        try {
+            BaseballGameReferee.checkException(userInput)
+        } catch (error: IllegalArgumentException) {
+            error.printStackTrace()
+            return null
+        }
+        return userInput.toBaseballNumbers()
+    }
+
+    fun String.toBaseballNumbers() : ArrayList<Int>{
+        val guessedNumbers=ArrayList<Int>()
+
+        this.forEach {  eachNum ->
+            guessedNumbers.add(Character.getNumericValue(eachNum))
+        }
+        return guessedNumbers
+    }
+
+}
