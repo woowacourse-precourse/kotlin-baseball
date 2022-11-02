e84cfb9b4d6de98f40811b3e32bc9df389aff515
lgy15 <lgy159@naver.com>
2022-11-02 16:02:16 +0900

    docs: 기능 구현 목록 정리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,6 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
+
 fun main() {
-    TODO("프로그램 구현")
 }
---
+++ src/main/kotlin/baseball/ComputerNumberCreate.kt
@@ -0,0 +1,9 @@
+package baseball
+
+class ComputerNumberCreate {
+    val minRangeNum = 1
+    val maxRangeNum = 9
+    val numRange = 3
+
+    //val answerNumber: List<Int>
+}
