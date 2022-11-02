9019589a983140bcde9c7e09fdd9072a64d975cf
kyounggudev <homin222@naver.com>
2022-11-02 22:24:00 +0900

    feat : exceptionTest() - 입력 예외처리 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,11 +1,16 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import java.lang.IllegalArgumentException
 
 
 fun main() {
     baseballGame.initGame()
     baseballGame.setOpponentNumber()
+
+    val input = readLine()
+    input?.let { exceptionTest(it) }
+
 }
 
 object baseballGame {
@@ -23,5 +28,16 @@ object baseballGame {
                 opponentNumber.add(randomNumber)
             }
         }
+    }
+}
+
+fun exceptionTest(input: String) {
+    val regex = "[1-9][1-9][1-9]".toRegex()
+    require(input.matches(regex)) {
+        throw IllegalArgumentException()
+    }
+
+    require(input[0] != input[1] && input[0] != input[2] && input[1] != input[2]) {
+        throw IllegalArgumentException()
     }
 }
