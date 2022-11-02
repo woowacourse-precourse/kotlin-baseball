934a1db6133f0e9da2b75451c83c63dff8adc5a9
lgy15 <lgy159@naver.com>
2022-11-02 19:09:18 +0900

    feat: 스트라이크, 볼 count 구현

--- src/main/kotlin/baseball/Constant.kt
+++ src/main/kotlin/baseball/Constant.kt
@@ -5,5 +5,7 @@ class Constant {
         const val minRangeNum = 1
         const val maxRangeNum = 9
         const val numRange = 3
+        const val startBallCount = 0
+        const val startStrikeCount = 0
     }
 }
--- src/main/kotlin/baseball/PlayBaseballGame.kt
+++ src/main/kotlin/baseball/PlayBaseballGame.kt
@@ -1,11 +1,35 @@
 package baseball
 
+import baseball.Constant.Companion.startBallCount
+import baseball.Constant.Companion.startStrikeCount
+
 class PlayBaseballGame {
     val computerNum = ComputerNumberCreate().computerNumber
     val playerNum = PlayerNumber().toListPlayerNumber
     init {
+        println("숫자를 입력해주세요 : ")
         println(computerNum)
         println(playerNum)
+    }
+
+    fun countBall(computerNum: List<Int>, playerNum: List<Int>): Int {
+        var ball = startBallCount
+        for (index in 0..playerNum.size) {
+            if (playerNum[index] != computerNum[index] && computerNum.contains(playerNum[index])){
+                ball += 1
+            }
+        }
+        return ball
+    }
+
+    fun countStrike(computerNum: List<Int>, playerNum: List<Int>): Int {
+        var strike = startStrikeCount
+        for (index in 0..playerNum.size) {
+            if (playerNum[index] == computerNum[index]){
+                strike += 1
+            }
+        }
+        return strike
     }
 
 }
