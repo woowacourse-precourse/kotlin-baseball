67af7986b2ace2f056751b0cd77ab6731b53d847
username <ieejo716@naver.com>
2022-11-02 22:08:05 +0900

    [FEAT] : 숫자를 입력 받고 올바른 형식인지 검사, 올바르지 않다면 애플리케이션 종료

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -11,10 +11,23 @@ fun startBaseball() {
     while (true) {
         val computerNumbers = mutableListOf<Int>()
         inputComputerNumbers(computerNumbers)
+        val userNumbers = inputUserNumbers()
     }
 }
 
+fun inputUserNumbers(): String {
+    val input = readLine()!!
+    //예외 사항 : 정수 인지* , 길이가 3인지 , 중복된 숫자는 없는지
+    val userNumbers = input.map {
+        it.digitToInt()
+    }
 
+    if (userNumbers.size != userNumbers.toSet().size || userNumbers.size != 3) {
+        throw IllegalArgumentException()
+    }
+
+    return input
+}
 
 fun inputComputerNumbers(numbers: MutableList<Int>) {
     while (numbers.size < 3) {
