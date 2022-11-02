84535ed1e763a0809301af869cd9a89d3c2bf51f
choiwoosuk <wooseokchoi0508@gmail.com>
2022-11-02 23:22:18 +0900

    Feat: 컴퓨터의 3자리 난수 생성 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,8 +1,22 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     printStartGame()
 }
-fun printStartGame(){
+
+fun printStartGame() {
     println("숫자 야구 게임을 시작합니다.")
+}
+
+fun generateComputerNumber(): List<Int> {
+    val computerNumber = mutableListOf<Int>()
+    while (computerNumber.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computerNumber.contains(randomNumber)) {
+            computerNumber.add(randomNumber)
+        }
+    }
+    return computerNumber
 }
