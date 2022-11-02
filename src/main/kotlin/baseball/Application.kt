5e8d6df3a5c79e5e1d33e7b792124a898b3ae0a2
kyounggudev <homin222@naver.com>
2022-11-03 00:05:17 +0900

    feat: restartGame() - 게임 재시작 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -95,3 +95,14 @@ fun restartException(answer : String){
     }
 }
 
+fun restartGame() : Boolean{
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    val answer = readLine()
+    answer?.let { restartException(it) }
+    when(answer){
+        "1" -> true
+        "2" -> false
+    }
+    return true
+}
