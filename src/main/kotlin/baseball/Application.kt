5658cb3923b1db20f6f0e97310c2e0fcb3f62462
sjh970603 <sjh970603@naver.com>
2022-11-02 21:06:03 +0900

    숫자야구게임 게임 새로 시작 밑 종료 while-switch문 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,4 +2,20 @@ package baseball
 
 fun main() {
     TODO("프로그램 구현")
+
+
+    while (true)
+    {
+        var select = readLine()?.toInt()
+
+        when(select)
+        {
+            1 -> println("임시 1번내용")
+            2 -> println("임시 2번내용")
+        }
+    }
+
+
+
+
 }
