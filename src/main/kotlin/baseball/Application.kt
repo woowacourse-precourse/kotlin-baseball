3107a6e1f27b17f55d12305291c42779451eb815
username <ieejo716@naver.com>
2022-11-02 21:33:43 +0900

    [FEAT] : 랜덤으로 중복 없는 3자리 숫자 추출

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,7 +1,27 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     gameStartText()
+    startBaseball()
+}
+
+fun startBaseball() {
+    while (true) {
+        val numberList = mutableListOf<Int>()
+        inputNumbers(numberList)
+
+    }
+}
+
+fun inputNumbers(numberList: MutableList<Int>) {
+    while (numberList.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!numberList.contains(randomNumber)) {
+            numberList.add(randomNumber)
+        }
+    }
 }
 
 fun gameStartText() = println("숫자 야구 게임을 시작합니다.")
