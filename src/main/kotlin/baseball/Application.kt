13f783cf23ad197c71a57cff0c125d9f77843f7d
kjyook <106497893+kjyook@users.noreply.github.com>
2022-11-03 00:30:08 +0900

    컴퓨터의 숫자를 랜덤으로 만든다

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,14 @@
 package baseball
 
+import kotlin.random.Random
+
 fun main() {
-    TODO("프로그램 구현")
+
+    val computer = mutableListOf<Int>()     //사용자가 맞춰야 할 컴퓨터의 숫자이다
+    while (computer.size < 3) {
+        val randomNumber = Random.nextInt(1, 10)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
 }
