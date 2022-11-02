8c9804fd9b127b64a536d76fc493acfe55178ff5
username <ieejo716@naver.com>
2022-11-02 22:43:48 +0900

    [FEAT] : 입력한 수에 대한 결과를 볼, 스트라이크 개수, 낫싱으로 표시

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -12,21 +12,50 @@ fun startBaseball() {
         val computerNumbers = mutableListOf<Int>()
         inputComputerNumbers(computerNumbers)
         val userNumbers = inputUserNumbers()
+        checkUserNumbers(userNumbers, computerNumbers)
     }
 }
 
-fun inputUserNumbers(): String {
+fun checkUserNumbers(
+    userNumbers: MutableList<Int>,
+    computerNumbers: MutableList<Int>
+) {
+    //strike check
+    val strikeCount = userNumbers.filterIndexed { idx, userNum ->
+        userNum == computerNumbers[idx]
+    }.size
+    // ball check
+    val ballCount = userNumbers.filterIndexed { idx, userNum ->
+        computerNumbers.contains(userNum) &&
+                userNum != computerNumbers[idx]
+    }.size
+    printUserCount(strikeCount, ballCount)
+}
+
+fun printUserCount(strikeCount: Int, ballCount: Int) {
+    if (strikeCount == 3) {
+        println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    } else if (strikeCount > 0 && ballCount == 0) {
+        println("$strikeCount 스트라이크")
+    } else if (strikeCount == 0 && ballCount > 0) {
+        println("$ballCount 볼")
+    } else if (strikeCount == 0 && ballCount == 0) {
+        println("낫싱")
+    }
+}
+
+fun inputUserNumbers(): MutableList<Int> {
     val input = readLine()!!
     //예외 사항 : 정수 인지* , 길이가 3인지 , 중복된 숫자는 없는지
     val userNumbers = input.map {
         it.digitToInt()
-    }
+    }.toMutableList()
 
     if (userNumbers.size != userNumbers.toSet().size || userNumbers.size != 3) {
         throw IllegalArgumentException()
     }
 
-    return input
+    return userNumbers
 }
 
 fun inputComputerNumbers(numbers: MutableList<Int>) {
@@ -39,4 +68,3 @@ fun inputComputerNumbers(numbers: MutableList<Int>) {
 }
 
 fun gameStartText() = println("숫자 야구 게임을 시작합니다.")
-
