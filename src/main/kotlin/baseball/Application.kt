581999f8053cda0d9c65cee86b79db7cd4979eb9
SeungHyeon Shim <tladhehd@naver.com>
2022-11-02 20:07:49 +0900

    랜덤값 추출

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,25 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
+fun getRandomNumber() :String { // 문자형으로 3자리 수 반환
+    var randomNum=""
+    var num =0
+    while (randomNum.length<3){
+        num = Randoms.pickNumberInRange(1, 9) // 1~9까지
+        if (!randomNum.contains(num.toString())){
+            randomNum+=num.toString()
+        }
+    }
+    return randomNum
+}
+
+
 fun startgame(){ //게임 시작 문구 출력 (최초 한번만 실행)
     println("숫자 야구 게임을 시작합니다.")
 }
 
 fun main() {
     startgame()
+    getRandomNumber()
 }
