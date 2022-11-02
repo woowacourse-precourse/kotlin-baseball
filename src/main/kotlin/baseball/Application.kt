bbba6f206952b891058e59befa734a84d9a7e9d7
kyounggudev <homin222@naver.com>
2022-11-02 21:10:43 +0900

    feat : setOpponentNumber() - 랜덤숫자 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     baseballGame.initGame()
-
+    baseballGame.setOpponentNumber()
 }
 
 object baseballGame {
@@ -16,5 +16,12 @@ object baseballGame {
         opponentNumber.clear()
     }
 
-
+    fun setOpponentNumber() {
+        while (opponentNumber.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!opponentNumber.contains(randomNumber)) {
+                opponentNumber.add(randomNumber)
+            }
+        }
+    }
 }
