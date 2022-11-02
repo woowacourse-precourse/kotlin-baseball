d3317d5d045de30460500e103a44989034c8bfbb
lgy15 <lgy159@naver.com>
2022-11-02 19:13:54 +0900

    볼, 스트라이크 출력 구현

--- src/main/kotlin/baseball/PlayBaseballGame.kt
+++ src/main/kotlin/baseball/PlayBaseballGame.kt
@@ -6,10 +6,24 @@ import baseball.Constant.Companion.startStrikeCount
 class PlayBaseballGame {
     val computerNum = ComputerNumberCreate().computerNumber
     val playerNum = PlayerNumber().toListPlayerNumber
+    val countStrike = countStrike(computerNum, playerNum)
+    val countBall = countBall(computerNum, playerNum)
+
     init {
+        inputNumPrint()
+        ballCountPrint(countBall)
+        strikeCountPrint(countStrike)
+    }
+    fun ballCountPrint(ballCount:Int){
+        if (ballCount > 0) print("${ballCount}볼 ")
+    }
+
+    fun strikeCountPrint(strikeCount:Int){
+        if (strikeCount > 0) print("${strikeCount}스트라이크")
+    }
+
+    private fun inputNumPrint(){
         println("숫자를 입력해주세요 : ")
-        println(computerNum)
-        println(playerNum)
     }
 
     fun countBall(computerNum: List<Int>, playerNum: List<Int>): Int {
