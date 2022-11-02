71530001f53fa3a687d16c42c7c1ce04dc07e2cd
zqwz <zqwz@naver.com>
2022-11-03 04:24:22 +0900

    feat: 컴퓨터가 서로 다른 임의의 수 세 가지를 선택하는 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,11 +1,25 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 const val START_GAME = "숫자 야구 게임을 시작합니다."
 fun main() {
     startGame()
 
 }
 
-fun startGame(){
+fun startGame() {
     println(START_GAME)
+    val answer = pickRandomNumber()
+}
+
+fun pickRandomNumber(): String {
+    val computerNumber = mutableListOf<Int>()
+    while (computerNumber.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computerNumber.contains(randomNumber)) {
+            computerNumber.add(randomNumber)
+        }
+    }
+    return computerNumber.joinToString("")
 }
