ecda4894eed49f902da0fb1d12d6b6d64ddd4955
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-03 16:42:50 +0900

    test:기능 4번 테스트 코드 추가

--- src/test/kotlin/baseball/BallsTest.kt
+++ src/test/kotlin/baseball/BallsTest.kt
@@ -1,15 +1,38 @@
 package baseball
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.lang.IllegalArgumentException
 
 class BallsTest {
 
+    val com = Balls("123")
     @Test
     fun `입력받은 ballList 길이 예외 테스트`() {
         Assertions.assertThrows(IllegalArgumentException::class.java, {
             val Balls = Balls("3456")
         })
+    }
+
+    @Test
+    fun `balls이 스트라이크인지 확인`() {
+        val ball = Ball(1, 1)
+        val result = "Strike"
+        assertThat(com.playGame(ball)).isEqualTo(result)
+    }
+
+    @Test
+    fun `balls이 볼인지 확인`() {
+        val ball = Ball(2, 1)
+        val result = "Ball"
+        assertThat(com.playGame(ball)).isEqualTo(result)
+    }
+
+    @Test
+    fun `balls이 낫싱인지 확인`() {
+        val ball = Ball(4, 1)
+        val result = "Nothing"
+        assertThat(com.playGame(ball)).isEqualTo(result)
     }
 }
