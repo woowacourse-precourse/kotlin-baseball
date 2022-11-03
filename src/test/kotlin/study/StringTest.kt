b3eed753ea574ffa62d297d1c1890b8936688ad4
[alsswl] <[alswl4223@naver.com]>
2022-11-03 12:38:34 +0900

    commit test

--- src/test/kotlin/study/StringTest.kt
+++ src/test/kotlin/study/StringTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.assertThrows
 
 class StringTest {
     @Test
-    //commit test2
+    //commit test
     fun `split 메서드로 주어진 값을 구분`() {
         val input = "1,2"
         val result = input.split(",")
