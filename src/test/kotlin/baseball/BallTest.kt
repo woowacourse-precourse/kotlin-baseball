bde4136d3f505e5683346d17f9fc33595ed6624d
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-03 13:37:17 +0900

    test:기능 2번 테스트 코드 추가

--- src/test/kotlin/baseball/BallTest.kt
+++ src/test/kotlin/baseball/BallTest.kt
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test
 
 class BallTest {
 
+    val ball = Ball(4, 1)
 
     @Test
     fun ball_number_valid_Test() {
-        var ball = Ball(4, 1)
         val result = 4
         assertThat(ball.number).isEqualTo(result)
     }
@@ -19,5 +19,26 @@ class BallTest {
         Assertions.assertThrows(IllegalArgumentException::class.java, {
             val ball = Ball(0,1)
         })
+    }
+
+    @Test
+    fun `ball이 스트라이크인지 확인`() {
+        val userBall = Ball(4, 1)
+        val result = "Strike"
+        assertThat(ball.playGame(userBall)).isEqualTo(result)
+    }
+
+    @Test
+    fun `ball이 볼인지 확인`() {
+        val userBall = Ball(4, 2)
+        val result = "Ball"
+        assertThat(ball.playGame(userBall)).isEqualTo(result)
+    }
+
+    @Test
+    fun `ball이 낫싱인지 확인`() {
+        val userBall = Ball(2, 1)
+        val result = "Nothing"
+        assertThat(ball.playGame(userBall)).isEqualTo(result)
     }
 }
