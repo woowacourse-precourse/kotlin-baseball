804228a1f89f70e1878e8efb2adff2f8398a75fe
swjw1110 <swjw1110@naver.com>
2022-11-03 23:57:28 +0900

    feat: add logics to check validation fo input
    added logic to the gmae:
    - check is input number valid
    - check is ending number input valid

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -13,3 +13,10 @@ fun generateRandomNumber(): String {
     }
     return randomNumberSet.joinToString("")
 }
+
+fun checkInputNumberValid(input: String): Boolean {
+    val inputNumberSet = input.toSet()
+    return Regex("[1-9]{3}").matches(input) && inputNumberSet.size == 3
+}
+
+fun checkEndingNumberValid(input: String): Boolean = input == "1" || input == "2"
