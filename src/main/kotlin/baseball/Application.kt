5d363ab50c2fd4d531a63c750be87e3a6897b7d9
권용민 <briandr@naver.com>
2022-11-03 16:07:44 +0900

    feat(Application.kt): Printer 기능 목록 구현
    Printer의 모든 기능 목록 각각을 메소드로 구현
    시작 메시지 출력 : showStartMessage
    숫자 입력 메시지 출력 : showInputMessage
    스트라이크 메시지 출력 : showStrikeMessage
    볼 메시지 출력 : showBallMessage
    낫싱 메시지 출력 : showNothingMessage
    볼&스트라이크 메시지 출력 : showBallStrikeMessage
    게임종료&다시시작 메시지 출력 : showEndMessage

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,35 @@
 package baseball
 
 fun main() {
-    TODO("프로그램 구현")
+}
+
+class Printer() {
+    fun showStartMessage() {
+        println("숫자 야구 게임을 시작합니다.")
+    }
+
+    fun showInputMessage() {
+        print("숫자를 입력해주세요 : ")
+    }
+
+    fun showStrikeMessage(strikeCount: Int) {
+        println("${strikeCount}스트라이크")
+    }
+
+    fun showBallMessage(ballCount: Int) {
+        println("${ballCount}볼")
+    }
+
+    fun showNothingMessage() {
+        println("낫싱")
+    }
+
+    fun showBallStrikeMessage(ballCount: Int, strikeCount: Int) {
+        println("${ballCount}볼 ${strikeCount}스트라이크")
+    }
+
+    fun showEndMessage() {
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    }
 }
