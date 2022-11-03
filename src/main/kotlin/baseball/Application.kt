953b89c405210fddccc5f82ca4d96b97b7c3aaa6
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-03 10:01:16 +0900

    feat: Add getting Computer Random Number Method

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,23 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.*
 import java.util.*
 
 fun main() {
+    println("숫자 야구 게임을 시작합니다.")
+    val computerNumber = getComputerNumber()
 
+}
+
+private fun getComputerNumber(): String {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer.joinToString("")
 }
 
 private fun validateInputString(inputString: String, isNewGameCheckInputString: Boolean) {
