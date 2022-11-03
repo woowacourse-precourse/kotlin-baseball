6986e07b38ad26e1c55825172cca6eb5e15a2312
JunHyeok0205 <wnsdlfgur@naver.com>
2022-11-03 13:42:07 +0900

    사용자가 압력할 3개의 숫자 지정, 배열 저장후 예외처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,23 @@
 package baseball
 
 fun main() {
-    TODO("프로그램 구현")
+
+    println("숫자 야구 게임을 시작합니다.")
+    print("숫자를 입력해주세요 : ")
+    print(readNum())
+
 }
+
+fun readNum(): MutableList<String> {
+    val number = readLine()!!
+    val numberArray = number.split("") as MutableList<String>
+    numberArray.removeAt(0)
+    numberArray.removeAt(numberArray.size-1)
+
+    if (number.length == 3 && numberArray[0] != numberArray[1] && numberArray[1] != numberArray[2] && numberArray[0] != numberArray[2]) {
+        return numberArray
+    }
+    else throw IllegalArgumentException()
+
+}
+
