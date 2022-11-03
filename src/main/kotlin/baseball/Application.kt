8eea08b4dfcbe066fe4b25ee08bf2703129b6b81
HIBER97 <hibar9710@gmail.com>
2022-11-03 17:55:06 +0900

    2022. 11. 3. PM 5:54 --------------------------------------- ADD - 컴퓨터 랜덤 숫자 설정 함수 - 게임 시작 함수 - 비교 함수 - 비교 예외 상황 함수 작성 --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - ---------------------------------------

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,18 +1,71 @@
 package baseball
 
+import java.util.Scanner
+import kotlin.random.Random
 
-fun Get_Cpu_Num (Target: List<Int>): Int {
+val Range = (1..9)
+val Input_List = mutableListOf<Int>()
 
+//0 = S , 1 = B , 2 = Null
+var Compare_List = mutableListOf<Int>(0,0,0)
+
+val Result = mutableListOf<Int>()
+var Cpu_Num = mutableListOf<Int>()
+
+//컴퓨터 숫자 선택
+fun Get_Cpu_Num (): List<Int> {
+
+    for (i in 0..3)
+        Cpu_Num = mutableListOf(Range.random())
+
+    return Cpu_Num.toList()
 }
 
-fun Game_Start (Target: List<Int>): Int {
+//게임 시작 하나한 비교
+fun Game_Start (List: List<Int>): List<Int> {
+    List.forEach {i ->
+        for (j in 0..3) {
+            Compare_Num(List[i], Input_List[j],i,j)
+        }
+    }
+    Compare_Exception(Compare_List)
 
+    return Result.toList()
 }
 
-fun Compare_Num (Target: Int): Int {
+//비교 함수
+fun Compare_Num (Cpu_Num: Int, Input_Num : Int, Cpu_idx : Int, Input_idx : Int): List<Int> {
+    if (Cpu_Num == Input_Num || Cpu_idx == Input_idx)
+        //S
+        Compare_List[0]++
+    else if (Cpu_Num == Input_Num || Cpu_idx != Input_idx)
+        //B
+        Compare_List[1]++
 
+
+    return Compare_List.toList()
 }
 
-fun main() {
-    print("야구 시작")
+//비교 예외
+fun Compare_Exception (Compare_List : List<Int>) {
+    if (Compare_List[0] == 0 || Compare_List[1] == 0)
+        return Nothing
+}
+
+fun main() = with(Scanner(System.`in`)){
+    print("숫자 야구 게임을 시작합니다.")
+    while (true){
+        print("숫자를 입력해주세요 :")
+
+        //상대방 번호 설정
+        Get_Cpu_Num()
+
+        //사용자 입력
+        for (i in 0..3)
+            println(Input_List[i])
+
+        Game_Start(Input_List)
+
+
+    }
 }
