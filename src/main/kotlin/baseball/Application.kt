1c3e90940e8884139a56815c6f8dc9d6f9e228b7
Jongwon-0518 <77ha@naver.com>
2022-11-04 00:25:03 +0900

    feat: Application.kt - 임의의 수 설정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,6 +2,7 @@ package baseball
 
 // 기능 목록
 // 1. "숫자 야구 게임을 시작합니다." 출력, 게임 함수 실행
+// 2. 임의의 수 설정
 // 2. "숫자를 입력해주세요:" 출력 후, 3자리 숫자 입력 받기.
 // 3. 숫자가 서로 다른 3자리 수가 아니거나 각 자릿수에 0이 있으면, IllegalArgumentException 발생 후 종료
 // 4. 입력 받은 숫자와 임의의 수 3개를 비교하는 함수를 이용해 비교하고 결과 출력
@@ -15,5 +16,14 @@ fun main() {
 }
 
 fun game() {
+    var computer_num = computer_random_num()
 
+}
+
+fun computer_random_num(): MutableSet<Int> {
+    val num_set = mutableSetOf<Int>()
+    while (num_set.size<=2) {
+        num_set.add((1..9).random())
+    }
+    return num_set
 }
