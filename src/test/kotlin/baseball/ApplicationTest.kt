89da9de3eed7269c4c01778f2b0bcbd96869ba77
choiwoosuk <wooseokchoi0508@gmail.com>
2022-11-03 22:43:04 +0900

    Test: 스트라이크와 볼의 최종 개수를 테스트하는 코드 추가

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -69,6 +69,18 @@ class ApplicationTest : NsTest() {
         assertThat(countStrike(userNumber,computerNumber)).isEqualTo(1)
     }
 
+    @Test
+    fun `최종 스트라이크,볼 개수 반환 테스트`() {
+        val userNumber= listOf(1,2,3)
+        val computerNumber= listOf(1,3,4)
+        var ball= countBall(userNumber,computerNumber)
+        val strike= countStrike(userNumber,computerNumber)
+        ball= getFinalNumberOfBallBySubtractStrikeFromBall(ball,strike)
+        assertThat(ball).isEqualTo(1)
+        assertThat(strike).isEqualTo(1)
+    }
+
+
     override fun runMain() {
         main()
     }
