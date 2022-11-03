f144ebfcd5db2c7117b420e915d4ef2a2710faab
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-11-04 04:04:44 +0900

    feat: 게임 시작 문구 출력 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,7 +3,12 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
+    printGameStart()
     val computerNumber = generateComputerNumber()
+}
+
+fun printGameStart(){
+    println("숫자 야구 게임을 시작합니다.")
 }
 
 fun generateComputerNumber(): List<Int> {
