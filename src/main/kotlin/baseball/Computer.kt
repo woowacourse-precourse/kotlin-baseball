fc0420cbb3d6488852b10d7ef18f2a0c6594c048
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-03 22:49:43 +0900

    feat : 컴퓨터에 랜덤값 할당 로직 추가 서로 다른 3자리로 할당 될 수 있게 로직 구성

---
+++ src/main/kotlin/baseball/Computer.kt
@@ -0,0 +1,15 @@
+package baseball
+
+import camp.nextstep.edu.missionutils.Randoms
+
+object Computer {
+    val correctNumber = mutableListOf<Int>()
+    fun getCorrectRandomNumber(){
+        while (correctNumber.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!correctNumber.contains(randomNumber)) {
+                correctNumber.add(randomNumber)
+            }
+        }
+    }
+}
--- src/main/kotlin/baseball/Game.kt
+++ src/main/kotlin/baseball/Game.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 object Game {
     var input = ""
     fun gameStart(){
+        Computer.getCorrectRandomNumber()
         while(true){
             input = Console.readLine()
         }
