6e5a35d8843cacc670264f5ab44b7521260921f2
jwson <sonjungwoo9@gmail.com>
2022-11-03 17:55:54 +0900

    [feat] 3 스트라이크가 될 때까지 반복하도록 반복문 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -74,11 +74,13 @@ fun main() {
 
     println("정답$computer")
 
-    var user = inputUserNumber()
+    var ball = 0
+    var strike = 0
 
-
-    var ball = checkBall(computer,user)
-    var strike = checkStrike(computer,user)
-
-    printHintMessage(ball,strike)
+    while(strike != 3) {
+        var user = inputUserNumber()
+        ball = checkBall(computer, user)
+        strike = checkStrike(computer, user)
+        printHintMessage(ball, strike)
+    }
 }
