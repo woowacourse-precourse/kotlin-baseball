648c628e311e0b7f4c4f03d5bee2a006b998183d
SeungHyeon Shim <tladhehd@naver.com>
2022-11-03 13:21:47 +0900

    사용자 값 입력 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -14,25 +14,28 @@ fun getRandomNumber() :String { // 랜덤으로 숫자를 뽑아 3자리 수 문
     return randomNum
 }
 
-fun checkNumber() { // 잘못 입력했을 시 IllegalArgumentException을 발생시킨 후 애플리케이션 종료
-
+fun inputNumber() : String{ // 잘못 입력했을 시 IllegalArgumentException을 발생시킨 후 애플리케이션 종료
     print("숫자를 입력해주세요 : ")
     var user = readLine()
-
+    return user.toString()
 }
-
 
 fun startgame(){ //게임 시작 문구 출력 (최초 한번만 실행)
     println("숫자 야구 게임을 시작합니다.")
 }
 
+
+
+
+
 fun main() {
     var randomNumber = ""
 
-    startgame()
+    startgame() // 최초 한번만 동작하므로 while문 밖 위치
+
     randomNumber=getRandomNumber() // 숫자가 무엇인지 보여줄 필요는 없으므로 메서드 호출만
     println(randomNumber) // 임시 확인용 후에 제거 예정
-    checkNumber()
+    inputNumber()
 
 
 }
