d39cc75251a6295b2e2fece010d6b7c845d83748
hansh0101 <hansh0101@naver.com>
2022-11-03 15:30:36 +0900

    feat: implement feature #1
    Feature #1 to host number baseball game

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,49 @@
 package baseball
 
 fun main() {
-    TODO("프로그램 구현")
+    printWelcomeMessage()
+
+    var intentForNewBaseball = true
+    while (intentForNewBaseball) {
+        playNewBaseball()
+        intentForNewBaseball = getIntentForNewBaseball()
+    }
+}
+
+private fun printWelcomeMessage() {
+    println("숫자 야구 게임을 시작합니다.")
+}
+
+private fun playNewBaseball() {
+    while (true) {
+        TODO()
+        if (getBallCount() == 3) {
+            printGameFinishedMessage()
+            break
+        }
+    }
+}
+
+private fun getBallCount(): Int {
+    TODO()
+}
+
+private fun printGameFinishedMessage() {
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+}
+
+private fun getIntentForNewBaseball(): Boolean {
+    printNewGameMessage()
+    val input = readLine()
+    if (input == "1") {
+        return true
+    }
+    if (input == "2") {
+        return false
+    }
+    return getIntentForNewBaseball()
+}
+
+private fun printNewGameMessage() {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 }
