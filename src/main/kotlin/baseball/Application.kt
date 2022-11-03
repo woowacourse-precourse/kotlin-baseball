72a5c20ab9aeb88d8db70968d41a249c02eb68c9
HIBER97 <hibar9710@gmail.com>
2022-11-03 21:26:07 +0900

    2022. 11. 3. PM 9:25 --------------------------------------- ADD - - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - 알고리즘 수정중 - ---------------------------------------

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -7,7 +7,7 @@ val Range = (1..9)
 val Input_List = mutableListOf<Int>()
 
 //0 = S , 1 = B , 2 = Null
-var Compare_List = mutableListOf<Int>(0,0,0)
+var Compare_List = mutableListOf<Int>(0,0)
 
 val Result = mutableListOf<Int>()
 var Cpu_Num = mutableListOf<Int>()
@@ -28,7 +28,9 @@ fun Game_Start (List: List<Int>): List<Int> {
             Compare_Num(List[i], Input_List[j],i,j)
         }
     }
-    Compare_Exception(Compare_List)
+    if(Compare_Exception(Compare_List))
+        //print("낫싱")
+        Result[3] = 1
 
     return Result.toList()
 }
@@ -47,9 +49,8 @@ fun Compare_Num (Cpu_Num: Int, Input_Num : Int, Cpu_idx : Int, Input_idx : Int):
 }
 
 //비교 예외
-fun Compare_Exception (Compare_List : List<Int>) {
-    if (Compare_List[0] == 0 || Compare_List[1] == 0)
-        return Nothing
+fun Compare_Exception (Compare_List : List<Int>): Boolean {
+    return Compare_List[0] == 0 || Compare_List[1] == 0
 }
 
 fun main() = with(Scanner(System.`in`)){
