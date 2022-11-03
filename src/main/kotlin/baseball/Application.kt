908a9c941114bba00bee72b4c0ed85f4e994ddde
choiwoosuk <wooseokchoi0508@gmail.com>
2022-11-03 21:51:41 +0900

    Feat: 볼 개수 세는 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -63,3 +63,13 @@ fun convertCharacterListToIntList(input: List<Char>): List<Int> {
     }
 }
 
+fun countBall(userNumber:List<Int>,computerNumber:List<Int>):Int{
+    var ball=0
+    userNumber.forEach{num->
+        if(computerNumber.contains(num)) ball++
+    }
+    return ball
+}
+
+
+
