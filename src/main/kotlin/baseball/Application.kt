1883123434f36da8eae5d1ad703e480a12392f5c
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-03 09:42:38 +0900

    feat: Add Validate inputString Method

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,54 @@
 package baseball
 
+import java.util.*
+
 fun main() {
-    TODO("프로그램 구현")
+
 }
+
+private fun validateInputString(inputString: String, isNewGameCheckInputString: Boolean) {
+    if (isNewGameCheckInputString && isContinuedNewGameCheckInputError(inputString))
+        throw IllegalArgumentException("User New Game Continue Check Input Error")
+
+    if (!isNewGameCheckInputString && isPredictionInputError(inputString))
+        throw IllegalArgumentException("User Prediction Input Error")
+}
+
+private fun isContinuedNewGameCheckInputError(inputString: String): Boolean {
+    if (isNotInteger(inputString))
+        return true
+
+    if (inputString == "1" || inputString == "2")
+        return false
+
+    return true
+}
+
+private fun isPredictionInputError(inputString: String): Boolean {
+    if (isNotInteger(inputString))
+        return true
+    if (containsZeroDigit(inputString))
+        return true
+    if (inputString.length != 3)
+        return true
+
+    val numberCheckList: MutableList<Boolean> = ArrayList(Collections.nCopies(10, false))
+    for (currentDigit in inputString) {
+        if (numberCheckList[currentDigit.code - '0'.code])
+            return true
+
+        numberCheckList[currentDigit.code - '0'.code] = true
+    }
+    return false
+}
+
+private fun isNotInteger(string: String): Boolean {
+    return try {
+        string.toInt()
+        false
+    } catch (e: NumberFormatException) {
+        true
+    }
+}
+
+private fun containsZeroDigit(string: String): Boolean = string.contains("0")
