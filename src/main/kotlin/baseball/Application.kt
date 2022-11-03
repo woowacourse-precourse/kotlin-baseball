56f08671eda47c859fe640235991208a37330593
swjw1110 <swjw1110@naver.com>
2022-11-04 00:20:16 +0900

    feat: make logic of main()
    features to be fixes:
    - remove unused function
    - reformat ball/strike string

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,38 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
+    println("숫자 야구 게임을 시작합니다.")
+    while (true){
+        val robotAnswer = generateRandomNumber()
 
+        print("숫자를 입력해주세요 : ")
+        val playerInput = Console.readLine()
+
+        if (checkInputNumberValid(playerInput).not()){
+            throw java.lang.IllegalArgumentException("Invalid Input")
+        }
+
+        val ballScore = findBallScore(playerInput, robotAnswer)
+        val strikeScore = findStrikeScore(playerInput, robotAnswer)
+        println(makeFormattedResult(ballScore, strikeScore))
+
+        if (strikeScore == 3){
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+            val endingInput = Console.readLine()
+
+            if (checkEndingNumberValid(endingInput).not()){
+                throw java.lang.IllegalArgumentException("Invalid Input")
+            }
+
+            if (endingInput == "2"){
+                break
+            }
+        }
+    }
 }
 
 fun makeFormattedResult(ball: Int, strike: Int): String = "${ball}볼 ${strike}스트라이크"
