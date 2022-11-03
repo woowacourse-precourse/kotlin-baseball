691dd34eb1f741b4674b450df6b1bfae2a58c597
JEGHTNER <jeheon0717@naver.com>
2022-11-03 16:59:06 +0900

    fun ballOrStrike

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 fun gameStart() {
     println("숫자 야구 게임을 시작합니다.")
 }
+
 fun strikeRandNumCreate(): MutableList<Int> {
     val computer = mutableListOf<Int>()
     while (computer.size < 3) {
@@ -15,6 +16,7 @@ fun strikeRandNumCreate(): MutableList<Int> {
     }
     return computer
 }
+
 fun inputNum(): String {
     print("숫자를 입력해주세요 : ")
     val userNum = camp.nextstep.edu.missionutils.Console.readLine()
@@ -22,6 +24,7 @@ fun inputNum(): String {
 
     return userNum
 }
+
 fun checkInputValid(user_num: String): Boolean {
     if (user_num.length != 3)
         return false
@@ -32,7 +35,28 @@ fun checkInputValid(user_num: String): Boolean {
     }
     return true
 }
-
+fun ballOrStrike(randNum:MutableList<Int>, inputNum : String){
+    var ball : Int = 0
+    var strike : Int = 0
+    for (i in inputNum){
+        if(randNum.contains(i.digitToInt()))
+            strike++
+        else
+            ball++
+        }
+    if(ball != 0 && ball != 3)
+        print("${ball}볼 ")
+    else if(ball == 3)
+        println("낫싱")
+    if(strike != 0 && strike != 3)
+        println("${strike}스트라이크")
+    else if(strike == 3)
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+}
 fun main() {
-
+    gameStart()
+    val randNum = strikeRandNumCreate()
+    println(randNum)
+    val userNum=inputNum()
+    ballOrStrike(randNum, userNum)
 }
