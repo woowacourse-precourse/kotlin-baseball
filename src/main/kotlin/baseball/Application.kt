27574bc0364d638c59b971555c8976e9e407c228
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-04 00:26:18 +0900

    feat: 서로 다른 3자리 숫자 랜덤 생성 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
+
 fun main() {
-    TODO("프로그램 구현")
+    val computer = createRandomNumber()
+}
+
+fun createRandomNumber(): MutableList<Int> {
+    val computer: MutableList<Int> = mutableListOf()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains((randomNumber))) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
