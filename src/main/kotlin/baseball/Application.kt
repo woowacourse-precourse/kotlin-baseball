fd2a1b0361f1ffd4d13ab842719b83562ae64f4e
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-04 02:57:45 +0900

    feat: 입력사항이 요구사항에 맞는지 확인하는 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -20,9 +20,31 @@ fun createRandomNumber(): MutableList<Int> {
     return computer
 }
 
-fun inputUserNumber() {
+fun inputUserNumber(): List<Int> {
     print("숫자를 입력해주세요 : ")
     val user = readLine().toString()
+    if(isNumber(user)){
+       return checkNumber(user)
+    }
+    return checkNumber(user)
+}
+
+fun checkNumber(user:String): List<Int> {
+    val userNumber = user.map{ it.code - '0'.code }
+
+    if(userNumber.size != 3){
+        println("3자리 숫자만 입력 가능합니다. 다시 입력해주세요.")
+        inputUserNumber()
+    }
+    else if(userNumber.contains(0)){
+        println("0은 포함할 수 없습니다. 다시 입력해주세요.")
+        inputUserNumber()
+    }
+    else if(userNumber.size != userNumber.distinct().count()){
+        println("숫자를 중복할 수 없습니다. 다시 입력해주세요.")
+        inputUserNumber()
+    }
+    return userNumber
 }
 
 fun isNumber(user:String): Boolean {
