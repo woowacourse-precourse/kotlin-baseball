a0edb842b18f38a0197c16581359f6fa625093c6
lgy15 <lgy159@naver.com>
2022-11-03 18:57:44 +0900

    refator: computerNumberCreate 변수 명 변경, comment

--- src/main/kotlin/baseball/ComputerNumberCreate.kt
+++ src/main/kotlin/baseball/ComputerNumberCreate.kt
@@ -8,19 +8,19 @@ import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 class ComputerNumberCreate() {
     val computerNumber = makeNum()
 
-    private fun randomNum() = pickNumberInRange(minRangeNumber, maxRangeNumber)
+    private fun randomNum() = pickNumberInRange(minRangeNumber, maxRangeNumber) // 1 ~ 9 랜덤 숫자 생성
 
     private fun appendNum(randomNum: Int, numList: MutableList<Int>) {
-        if (!numList.contains(randomNum)) {
+        if (!numList.contains(randomNum)) { // 중복된 숫자가 아니면 add
             numList.add(randomNum)
         }
     }
 
     private fun makeNum(): MutableList<Int> {
-        val makeThreeNum = mutableListOf<Int>()
-        while (makeThreeNum.size < numberRange) {
-            appendNum(randomNum(), makeThreeNum)
+        val computerNumber = mutableListOf<Int>()
+        while (computerNumber.size < numberRange) {
+            appendNum(randomNum(), computerNumber)
         }
-        return makeThreeNum
+        return computerNumber
     }
 }
