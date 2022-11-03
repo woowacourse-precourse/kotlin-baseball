ca3bdfb3f8887c9804d72de29ea6537bd0454397
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-03 23:21:55 +0900

    chore : 랜덤 숫자 할당전 할당받을 리스트 초기화

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 object Computer {
     val correctNumber = mutableListOf<Int>()
     fun getCorrectRandomNumber(){
+        correctNumber.clear()
         while (correctNumber.size < 3) {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
             if (!correctNumber.contains(randomNumber)) {
