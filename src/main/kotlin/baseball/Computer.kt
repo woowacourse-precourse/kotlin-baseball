6b367c37acee93181fcbd9628c06e553153fefe1
EmilyCh0 <seoyeon3089@naver.com>
2022-11-03 23:26:26 +0900

    feat(Computer): 서로 다른 임의의 수 3개 선택해 정답 생성

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -1,10 +1,18 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Computer {
-    private lateinit var answer: List<Int>
+    private var answer = arrayListOf<Int>()
 
     fun generateAnswer() {
-
+        answer.clear()
+        while (answer.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber)
+            }
+        }
     }
 
     fun compareGuessAndAnswer(userGuess: List<Int>): Boolean {
