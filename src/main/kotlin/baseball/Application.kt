be2cca87f3ac20f4d8b5c7b5c0b4f0fe33b1fa67
zqwz <zqwz@naver.com>
2022-11-03 15:45:56 +0900

    feat: 게임을 다시 시작할지 물어보는 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,9 +2,13 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
+import java.lang.IllegalArgumentException
 
 const val START_GAME = "숫자 야구 게임을 시작합니다."
 const val END_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+const val RESTART_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+const val RESTART = "1"
+const val END = "2"
 const val STRIKE = "스트라이크"
 const val BALL = "볼"
 const val NOTHING = "낫싱"
@@ -26,6 +30,15 @@ fun startGame() {
         }
     }
     println(END_GAME)
+    restartGame()
+}
+fun restartGame(){
+    println(RESTART_GAME)
+    when (userInput()){
+        RESTART -> startGame()
+        END -> return
+        else -> throw IllegalArgumentException()
+    }
 }
 
 fun checkAnswer(input: String, answer: String): Boolean {
