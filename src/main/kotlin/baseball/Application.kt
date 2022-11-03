550e8fcd1f31615a86b425a0a32d917daed8c86a
jwson <sonjungwoo9@gmail.com>
2022-11-03 16:54:37 +0900

    [feat] 컴퓨터 정답 생성 function 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,10 +1,24 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun printStartMessage(){
     println("숫자 야구 게임을 시작합니다")
 }
 
+fun createComputerAnswer(){
+    val computer = mutableListOf<Int>()
+    while (computer.count() < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    println(computer)
+}
 
 fun main() {
     printStartMessage()
+    createComputerAnswer()
+
 }
