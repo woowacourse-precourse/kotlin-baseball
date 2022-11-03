f21ebb4660679d18a94facb8076359af46371dd2
JunHyeok0205 <wnsdlfgur@naver.com>
2022-11-03 15:08:04 +0900

    1번 입력한 숫자 3개 예외처리 수정 / 2번 컴퓨터 랜덤값 추출, 배열로 저장

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,23 +1,50 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
 
     println("숫자 야구 게임을 시작합니다.")
     print("숫자를 입력해주세요 : ")
-    print(readNum())
+
+    val playerNumber = readNum()
+    val computerNumber = computerNum()
+    println(playerNumber.toString())
+    computerNumber
+    println(computerNumber)
+
+
+
+
 
 }
 
 fun readNum(): MutableList<String> {
     val number = readLine()!!
     val numberArray = number.split("") as MutableList<String>
+    val newArray = mutableListOf<String>()
     numberArray.removeAt(0)
-    numberArray.removeAt(numberArray.size-1)
+    numberArray.removeAt(numberArray.size - 1)
 
-    if (number.length == 3 && numberArray[0] != numberArray[1] && numberArray[1] != numberArray[2] && numberArray[0] != numberArray[2]) {
-        return numberArray
+    if (number.length != 3) {
+        throw IllegalArgumentException()
     }
-    else throw IllegalArgumentException()
-
+    for(i in 0..2) {
+        if (newArray.contains(numberArray[i])) {
+            throw IllegalArgumentException()
+        }
+        newArray.add(numberArray[i])
+    }
+    return newArray
 }
 
+fun computerNum(): MutableList<String> {
+    val computer = mutableListOf<String>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9).toString()
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
