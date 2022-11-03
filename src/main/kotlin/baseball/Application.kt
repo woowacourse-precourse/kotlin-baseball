3b394903521355e49275edcf4f774106fe8cd89f
Jongwon-0518 <77ha@naver.com>
2022-11-04 02:06:19 +0900

    feat: Application.kt - 입력 받은 숫자와 임의의 수 3개를 비교하는 함수를 이용해 비교하고 결과 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -6,7 +6,7 @@ package baseball
 // 3. "숫자를 입력해주세요:" 출력 후, 3자리 숫자 입력 받기.
 // 4. 숫자가 서로 다른 3자리 수가 아니거나 각 자릿수에 0이 있으면, IllegalArgumentException 발생 후 종료
 // 5. 입력 받은 숫자와 임의의 수 3개를 비교하는 함수를 이용해 비교하고 결과 출력
-// 6. 4번 기능 반복
+// 6. 5번 기능 반복
 // 7. 임의의 수와 입력 받은 수가 일치하면 기능 종료 및 재시작 여부 확인
 // 8. 재시작 여부에 따라 다시 함수 실행
 
@@ -19,24 +19,33 @@ fun game() {
     val computerNum = computerRandomNum()
 
     print("숫자를 입력해주세요 : ")
-    val num = readLine()
+    val inputNum = readLine()!!
 
-    rightInput(num)
+    rightInput(inputNum)
+
+    val strikeBall = compare(computerNum, inputNum.toInt())
+
+    if (strikeBall[1] > 0) {
+        print("${strikeBall[1]}볼 ")
+    }
+    if (strikeBall[0] > 0) {
+        print("${strikeBall[0]}스트라이크")
+    }
 
 }
 
-fun computerRandomNum(): MutableSet<Int> {
+fun computerRandomNum(): List<Int> {
     val numSet = mutableSetOf<Int>()
     while (numSet.size<=2) {
         numSet.add((1..9).random())
     }
-    return numSet
+    return numSet.toList()
 }
 
-fun rightInput(inputStr: String?) {
+fun rightInput(inputStr: String) {
     val inputSet = mutableSetOf<Char>()
 
-    for (i in 0 until inputStr!!.length) {
+    for (i in 0 until inputStr.length) {
         if (inputStr[i] != '0') {
             inputSet.add(inputStr[i])
         }
@@ -45,4 +54,25 @@ fun rightInput(inputStr: String?) {
     if (inputSet.size != 3) {
         throw IllegalArgumentException()
     }
+}
+
+fun compare(computerNum:List<Int>, inputNum:Int): ArrayList<Int> {
+    var inputNumb = inputNum
+    val strikeBall = arrayListOf(0, 0)
+    val inputNumList = arrayListOf<Int>()
+
+    inputNumList.add(inputNumb/100)
+    inputNumb %= 100
+    inputNumList.add(inputNumb/10)
+    inputNumb %= 10
+    inputNumList.add(inputNumb)
+
+    for (i in 0 until 3) {
+        if (inputNumList[i] == computerNum[i]) {
+            strikeBall[0] += 1
+        } else if (inputNumList[i] in computerNum) {
+            strikeBall[1] += 1
+        }
+    }
+    return strikeBall
 }
