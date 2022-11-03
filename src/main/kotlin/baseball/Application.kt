0ad198b0dee1c6834a72abe074f90bbae231cca0
zqwz <zqwz@naver.com>
2022-11-03 15:19:48 +0900

    feat: 볼과 스트라이크의 개수를 세는 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,16 +4,36 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 const val START_GAME = "숫자 야구 게임을 시작합니다."
+const val DIGIT_END = 2
+const val DIGIT_START = 0
 fun main() {
+    println(START_GAME)
     startGame()
 }
 
 fun startGame() {
-    println(START_GAME)
     val answer = pickRandomNumber()
+    println(answer)
     while (true) {
         val input = userInput()
+        println(checkHint(input, answer))
     }
+}
+
+fun checkHint(input: String, answer: String) {
+    var ballCount = 0
+    var strikeCount = 0
+
+    for (index in DIGIT_START..DIGIT_END) {
+        if (answer.contains(input[index]) && input[index] == answer[index]) {
+            strikeCount++
+        }
+        if (answer.contains(input[index]) && input[index] != answer[index]) {
+            ballCount++
+        }
+    }
+    println("$ballCount $strikeCount")
+
 }
 
 fun userInput(): String {
