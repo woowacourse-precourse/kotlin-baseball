1098be8741dffcb77a76c4ea59be2ae899bb3cdb
Yerin <myheaven_b@naver.com>
2022-11-04 02:34:04 +0900

    feat: add function getComputerNumber

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,24 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
+
 fun main() {
-    TODO("프로그램 구현")
+    var flag = 1
+    var computer: List<Int>
+
+    while (flag == 1) {
+        computer = getComputerNumber()
+    }
+}
+
+fun getComputerNumber(): List<Int>{
+    val computer: MutableList<Int> = mutableListOf()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
