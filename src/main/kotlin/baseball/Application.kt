04c65204f6a364000ffc862f45e91a0055947a3a
swjw1110 <swjw1110@naver.com>
2022-11-04 00:31:35 +0900

    refactor: delete/change invalid logics
    couple of logics changed:
    - delete answering logic that provide ball and strike simultaniously
    - change formatting logic

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,13 +5,13 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    while (true){
+    while (true) {
         val robotAnswer = generateRandomNumber()
 
         print("숫자를 입력해주세요 : ")
         val playerInput = Console.readLine()
 
-        if (checkInputNumberValid(playerInput).not()){
+        if (checkInputNumberValid(playerInput).not()) {
             throw java.lang.IllegalArgumentException("Invalid Input")
         }
 
@@ -19,36 +19,37 @@ fun main() {
         val strikeScore = findStrikeScore(playerInput, robotAnswer)
         println(makeFormattedResult(ballScore, strikeScore))
 
-        if (strikeScore == 3){
+        if (strikeScore == 3) {
             println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
             println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
             val endingInput = Console.readLine()
 
-            if (checkEndingNumberValid(endingInput).not()){
+            if (checkEndingNumberValid(endingInput).not()) {
                 throw java.lang.IllegalArgumentException("Invalid Input")
             }
 
-            if (endingInput == "2"){
+            if (endingInput == "2") {
                 break
             }
         }
     }
 }
 
-fun makeFormattedResult(ball: Int, strike: Int): String = "${ball}볼 ${strike}스트라이크"
+fun makeFormattedResult(ball: Int, strike: Int): String {
+    val resultStringList = mutableListOf<String>()
+    if (ball != 0) {
+        resultStringList.add("${ball}볼")
+    }
+    if (strike != 0) {
+        resultStringList.add("${strike}스트라이크")
+    }
+    return resultStringList.joinToString(" ")
+}
 
 fun findStrikeScore(input: String, answer: String): Int = (input.indices).filter { input[it] == answer[it] }.size
 
-
 fun findBallScore(input: String, answer: String): Int =
     (input.indices).filter { input[it] != answer[it] && input[it] in answer }.size
-
-fun matchAnswer(input: String, answer: String): String {
-    val ballValue = findBallScore(input, answer)
-    val strikeValue = findStrikeScore(input, answer)
-
-    return makeFormattedResult(ballValue, strikeValue)
-}
 
 fun generateRandomNumber(): String {
     val randomNumberSet = mutableSetOf<Int>()
