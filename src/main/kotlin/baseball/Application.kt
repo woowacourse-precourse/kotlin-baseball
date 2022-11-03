7374cccbf952bf4a5a6e3de3dc35423c22c90005
JunHyeok0205 <wnsdlfgur@naver.com>
2022-11-03 20:32:44 +0900

    feat(스트라이크, 볼, 낫싱을 프린트하는 함수 생성)

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,38 +4,34 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
 
+    val computerNumList = computerNum()
     println("숫자 야구 게임을 시작합니다.")
-    print("숫자를 입력해주세요 : ")
 
-    val playerNumber = readNum()
-    val computerNumber = computerNum()
-    println(playerNumber.toString())
-    computerNumber
-    println(computerNumber)
-
-
-
+    ballCount(readNum(), computerNumList)
 
 
 }
 
 fun readNum(): MutableList<String> {
-    val number = readLine()!!
-    val numberArray = number.split("") as MutableList<String>
-    val newArray = mutableListOf<String>()
-    numberArray.removeAt(0)
-    numberArray.removeAt(numberArray.size - 1)
+        print("숫자를 입력하세요 : ")
+        val computerNumList = computerNum()
+        val number = readLine()!!
+        val numberArray = number.split("") as MutableList<String>
+        val newArray = mutableListOf<String>()
+        numberArray.removeAt(0)
+        numberArray.removeAt(numberArray.size - 1)
 
-    if (number.length != 3) {
-        throw IllegalArgumentException()
-    }
-    for(i in 0..2) {
-        if (newArray.contains(numberArray[i])) {
+        if (number.length != 3) {
             throw IllegalArgumentException()
         }
-        newArray.add(numberArray[i])
-    }
-    return newArray
+
+        for (i in 0..2) {
+            if (newArray.contains(numberArray[i])) {
+                throw IllegalArgumentException()
+            }
+            newArray.add(numberArray[i])
+        }
+       return newArray
 }
 
 fun computerNum(): MutableList<String> {
@@ -47,4 +43,34 @@ fun computerNum(): MutableList<String> {
         }
     }
     return computer
+}
+
+fun ballCount(playerList: MutableList<String>, computerList: MutableList<String>){
+
+    var ball = 0
+    var strike = 0
+
+    for (i in 0..2) {
+        if (playerList[i] == computerList[i]) {
+            strike++
+        }
+        else if (playerList[i].contains(computerList[i])) {
+            ball++
+        }
+    }
+
+
+    if (strike == 0 && ball == 0) {
+        println("낫싱")
+    }
+    else if (ball == 0) {
+        println("${strike}스트라이크")
+    }
+    else if (strike == 0) {
+        println("${ball}볼")
+    }
+    else println("${strike}스트라이크 ${ball}볼")
+
+
+//    return strike.toString()
 }
