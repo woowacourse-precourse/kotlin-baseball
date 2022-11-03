4b5c9c09819fa0f904fcb2690acbe46dece333c0
JEGHTNER <jeheon0717@naver.com>
2022-11-03 16:37:03 +0900

    fun strikeRandNumCreate

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,6 +5,16 @@ import camp.nextstep.edu.missionutils.Randoms
 fun gameStart() {
     println("숫자 야구 게임을 시작합니다.")
 }
+fun strikeRandNumCreate(): MutableList<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
 
 fun main() {
 
