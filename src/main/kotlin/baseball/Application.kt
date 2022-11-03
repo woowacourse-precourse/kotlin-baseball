ee10f4084027bce505da554d12585e9481f491f3
swjw1110 <swjw1110@naver.com>
2022-11-03 23:52:32 +0900

    feat: add number generate logic
    added logic:
    - generate random number

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,15 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+
+}
+
+fun generateRandomNumber(): String {
+    val randomNumberSet = mutableSetOf<Int>()
+    while (randomNumberSet.size != 3) {
+        randomNumberSet.add(Randoms.pickNumberInRange(1, 9))
+    }
+    return randomNumberSet.joinToString("")
 }
