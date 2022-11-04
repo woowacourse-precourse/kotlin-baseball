508f275ebf1519f855d118098686e76805740c96
cyjadela <adelacho@naver.com>
2022-11-04 14:15:27 +0900

    style: computerNum -> computerNumList 변수명 변경

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,11 +8,11 @@ fun main() {
     println("숫자 야구 게임을 시작합니다.")
 
     while (chooseGameNum == 1) {
-        val computerNum = mutableListOf<Int>()
-        while (computerNum.size < 3) {
+        val computerNumList = mutableListOf<Int>()
+        while (computerNumList.size < 3) {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
-            if (!computerNum.contains(randomNumber)) {
-                computerNum.add(randomNumber)
+            if (!computerNumList.contains(randomNumber)) {
+                computerNumList.add(randomNumber)
             }
         }
 
