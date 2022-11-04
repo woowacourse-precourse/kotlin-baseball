81b467f9f5be7ffd44b42f99bceedd32f5eeb77e
JunJangE <wnswkd486@gmail.com>
2022-11-04 23:06:21 +0900

    feat: 2번 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,22 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
+    var computer: String = ""
+    computer = randomNum()
+
+}
+
+fun randomNum(): String {
+    var computer: String = ""
+    while (computer.length < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber.toString())) {
+            computer += randomNumber
+        }
+    }
+    return computer
 
 }
