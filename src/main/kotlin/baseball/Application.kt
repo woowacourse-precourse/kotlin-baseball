d6fb54e80f6fb081ea3576a138dbd9ed806e50ac
zqwz <zqwz@naver.com>
2022-11-04 11:00:59 +0900

    feat: 예외사항 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -7,11 +7,13 @@ import java.lang.IllegalArgumentException
 const val START_GAME = "숫자 야구 게임을 시작합니다."
 const val END_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
 const val RESTART_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+const val INPUT = "숫자를 입력해주세요 : "
 const val RESTART = "1"
 const val END = "2"
 const val STRIKE = "스트라이크"
 const val BALL = "볼"
 const val NOTHING = "낫싱"
+const val DIGIT_LENGTH = 3
 const val DIGIT_END = 2
 const val DIGIT_START = 0
 fun main() {
@@ -75,6 +77,11 @@ fun printHint(ballCount: Int, strikeCount: Int) {
 }
 
 fun userInput(): String {
+    print(INPUT)
+    val input = Console.readLine()
+    if(input.length != DIGIT_LENGTH){
+        throw IllegalArgumentException()
+    }
     return Console.readLine()
 }
 
