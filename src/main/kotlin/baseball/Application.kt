553b7aa27f714b78c84a35c9a64707aaa9da7802
dltkd1395 <dltkd13956@naver.com>
2022-11-04 22:29:11 +0900

    feat(Application) : 낫싱/스트라이크/볼 출력 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -14,9 +14,25 @@ fun main() {
     val baseBall = BaseBall(computerNumbers, playerNumbers)
     val strike = baseBall.getStrikeCount()
     val ball = baseBall.getBallCount()
+    getResultMessage(ball, strike)
 
     PlayerNumber().exitGame()
 
+}
+
+fun getResultMessage(ball: Int, strike: Int) {
+    if (strike == 0 && ball == 0) {
+        println("낫싱")
+    }
+    if (strike > 0 && ball > 0) {
+        println("${ball}볼 ${strike}스트라이크")
+    }
+    if (strike > 0 && ball == 0) {
+        println("${strike}스트라이크")
+    }
+    if (ball > 0 && strike == 0) {
+        println("${ball}볼")
+    }
 }
 
 fun isNumberException(number: List<Int>): Boolean {
