9e41dceedb216ab946f59a948f0c3cae06ebb949
Grandelwald <artjr97@naver.com>
2022-11-05 03:42:30 +0900

    InputView.kt 입력에 필요한 함수 생성

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -1,5 +1,12 @@
 package baseball.view
 
 object InputView {
+    fun inputExpectingNumber(): List<Int> {
+        println("숫자를 입력해주세요 : ")
+        return readln().split("").map { it.toInt() }
+    }
 
+    fun inputRestartingDecideNumber()  {
+        TODO("입력한 숫자에 따라서 재시작, 종료 결정하는 함수 새어")
+    }
 }
