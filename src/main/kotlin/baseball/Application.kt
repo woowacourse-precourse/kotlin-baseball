e363549c5e313f47385676863eb55a26c02bf77b
lh99j <grand2181@gmail.com>
2022-11-04 17:00:45 +0900

    feat : 게임 종료 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -91,4 +91,13 @@ fun isInValidNum(userNum: String): Boolean {
 fun baseballGameEnd(){
     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    var inputNum = readLine()!!.toInt()
+
+    if(inputNum == 1){
+        baseballGameStart()
+    }else if(inputNum == 2){
+        System.exit(-1)
+    }else
+        throw IllegalArgumentException("Error")
+
 }
