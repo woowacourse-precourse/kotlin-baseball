489b9c13c1e3d878351f5505eb64604249491973
EmilyCh0 <seoyeon3089@naver.com>
2022-11-04 23:06:11 +0900

    feat(User): 숫자 입력 받기

--- src/main/kotlin/baseball/User.kt
+++ src/main/kotlin/baseball/User.kt
@@ -1,8 +1,12 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+
 class User {
 
     fun getGuessInput(): List<Int> {
-        TODO()
+        val input = Console.readLine()
+        // TODO 예외 사항 체크
+        return input.toList().map { it.code.plus(-48) }
     }
 }
