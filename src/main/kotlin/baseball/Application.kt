9e6301c8a1cd3435fada7394420d6896e1896b58
ChunBaee <mljung10@naver.com>
2022-11-05 03:27:29 +0900

    feat: 코드 작성 간 사용할 상수 선언

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,12 @@
 package baseball
 
+const val INPUT_LENGTH_STANDARD = 3
+const val GAME_CLEAR = 3
+const val USER_WANT_PLAY_AGAIN = 1
+const val USER_DONT_WANT_PLAY_AGAIN = 2
+const val MIN_NUMBER = 1
+const val MAX_NUMBER = 9
+
 fun main() {
     TODO("프로그램 구현")
 }
