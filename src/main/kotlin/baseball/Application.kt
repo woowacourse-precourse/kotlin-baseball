822caf11f66233a5f3da5516d9b89860b64c9710
lh99j <grand2181@gmail.com>
2022-11-04 14:10:07 +0900

    feat : 게임 시작을 위한 메세지 출력과 컴퓨터 숫자를 배정하는 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,24 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    baseballGameStart()
+}
+
+fun baseballGameStart(){
+    val computerNum = setComputerNum()
+}
+
+fun setComputerNum() : List<Int>{
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
+    return computer
 }
