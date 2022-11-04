77aa9b01652fbe91947ce9ae7dc8ac77087ae3c8
dpwns1234 <dpwns1234@naver.com>
2022-11-04 19:09:13 +0900

    docs: 기능 구현 목록 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,11 @@
 package baseball
 
-fun main() {
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
+import camp.nextstep.edu.missionutils.Console
 
+fun main() {
+    val random = pickNumberInRange(1, 9)
+    println(random)
+    val input = Console.readLine()
+    println(input.get(2))
 }
