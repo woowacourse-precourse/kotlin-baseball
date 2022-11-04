d5661b47e8420e215fee34a89f95ca5a476f75bc
tmdgh1592 <tmdgh1592@naver.com>
2022-11-04 12:16:08 +0900

    test: add test code of functions for Hint
    - Write test code that validates hint results from computer and player

---
+++ src/test/kotlin/baseball/hint/BaseballHintTest.kt
@@ -0,0 +1,134 @@
+package baseball.hint
+
+import domain.Computer
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import utils.InputUtil
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+internal class BaseballHintTest {
+
+    private lateinit var outputStreamCaptor: ByteArrayOutputStream
+
+    @BeforeEach
+    fun setUp() {
+        this.outputStreamCaptor = ByteArrayOutputStream()
+    }
+
+    @DisplayName("3스트라이크")
+    @Test
+    fun `컴퓨터와 플레이어 정답 상황의 힌트 출력 예시`() {
+        System.setIn(ByteArrayInputStream("123".toByteArray()))
+        System.setOut(PrintStream(outputStreamCaptor))
+
+        val computer = Computer(listOf(1, 2, 3))
+        val player = InputUtil.enterAnswer()
+
+        val baseballHint: Hint = BaseballHint(computer, player)
+        baseballHint.print()
+
+        val output = outputStreamCaptor.toString()
+        assertThat(output).contains("3스트라이크")
+    }
+
+    @DisplayName("2볼 1스트라이크")
+    @Test
+    fun `컴퓨터와 플레이어 2볼 1스트라이크 힌트 출력 예시`() {
+        System.setIn(ByteArrayInputStream("123".toByteArray()))
+        System.setOut(PrintStream(outputStreamCaptor))
+
+        val computer = Computer(listOf(1, 3, 2))
+        val player = InputUtil.enterAnswer()
+
+        val baseballHint: Hint = BaseballHint(computer, player)
+        baseballHint.print()
+
+        val output = outputStreamCaptor.toString()
+        assertThat(output).contains("2볼 1스트라이크")
+    }
+
+    @DisplayName("1볼 1스트라이크")
+    @Test
+    fun `컴퓨터와 플레이어 1볼 1스트라이크 힌트 출력 예시`() {
+        System.setIn(ByteArrayInputStream("143".toByteArray()))
+        System.setOut(PrintStream(outputStreamCaptor))
+
+        val computer = Computer(listOf(1, 3, 2))
+        val player = InputUtil.enterAnswer()
+
+        val baseballHint: Hint = BaseballHint(computer, player)
+        baseballHint.print()
+
+        val output = outputStreamCaptor.toString()
+        assertThat(output).contains("1볼 1스트라이크")
+    }
+
+    @DisplayName("1스트라이크")
+    @Test
+    fun `컴퓨터와 플레이어 1스트라이크 힌트 출력 예시`() {
+        System.setIn(ByteArrayInputStream("145".toByteArray()))
+        System.setOut(PrintStream(outputStreamCaptor))
+
+        val computer = Computer(listOf(1, 3, 2))
+        val player = InputUtil.enterAnswer()
+
+        val baseballHint: Hint = BaseballHint(computer, player)
+        baseballHint.print()
+
+        val output = outputStreamCaptor.toString()
+        assertThat(output).contains("1스트라이크")
+    }
+
+    @DisplayName("1볼")
+    @Test
+    fun `컴퓨터와 플레이어 1볼 힌트 출력 예시`() {
+        System.setIn(ByteArrayInputStream("981".toByteArray()))
+        System.setOut(PrintStream(outputStreamCaptor))
+
+        val computer = Computer(listOf(1, 3, 2))
+        val player = InputUtil.enterAnswer()
+
+        val baseballHint: Hint = BaseballHint(computer, player)
+        baseballHint.print()
+
+        val output = outputStreamCaptor.toString()
+        assertThat(output).contains("1볼")
+    }
+
+    @DisplayName("2볼")
+    @Test
+    fun `컴퓨터와 플레이어 2볼 힌트 출력 예시`() {
+        System.setIn(ByteArrayInputStream("923".toByteArray()))
+        System.setOut(PrintStream(outputStreamCaptor))
+
+        val computer = Computer(listOf(1, 3, 2))
+        val player = InputUtil.enterAnswer()
+
+        val baseballHint: Hint = BaseballHint(computer, player)
+        baseballHint.print()
+
+        val output = outputStreamCaptor.toString()
+        assertThat(output).contains("2볼")
+    }
+
+    @DisplayName("3볼")
+    @Test
+    fun `컴퓨터와 플레이어 3볼 힌트 출력 예시`() {
+        System.setIn(ByteArrayInputStream("213".toByteArray()))
+        System.setOut(PrintStream(outputStreamCaptor))
+
+        val computer = Computer(listOf(1, 3, 2))
+        val player = InputUtil.enterAnswer()
+
+        val baseballHint: Hint = BaseballHint(computer, player)
+        baseballHint.print()
+
+        val output = outputStreamCaptor.toString()
+        assertThat(output).contains("3볼")
+    }
+
+}
