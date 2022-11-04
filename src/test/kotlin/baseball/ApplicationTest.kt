541408b53a3d817cb1174b99b69c8c478fe84f0f
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-11-04 19:31:01 +0900

    test: 예외 테스트 코드 추가

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -25,6 +25,8 @@ class ApplicationTest : NsTest() {
     fun `예외 테스트`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("1234") }
+            assertThrows<IllegalArgumentException> { runException("abc") }
+            assertThrows<IllegalArgumentException> { runException("112") }
         }
     }
 
