da7ac75d0d2ef204537a9653c66ddcbae29c612f
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-11-05 00:04:54 +0900

    feat<main> 컴퓨터생성함수추가
    서로 다른 1- 9 사이의 수 3개 생성
    
    기능목록 첫번째 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,25 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+
+    generateComputer()
+
 }
+
+//1. 서로 다른 1- 9 사이의 수 3개 생성
+fun generateComputer(): List<Int>{
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
+return computer
+}
+
+
+
