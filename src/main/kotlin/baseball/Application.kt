d4bc89e199dccda34500a0fb521cde20be6e7234
dltkd1395 <dltkd13956@naver.com>
2022-11-04 23:31:47 +0900

    refactor(OutputView, Application) : 출력관련 클래스 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,7 +1,9 @@
 package baseball
 
+import baseball.view.OutputView
+
 fun main() {
-    printGameStart()
+    OutputView().printGameStart()
 
     do {
         val computerNumbers = ComputerNumber().getRandomNumbers()
@@ -9,7 +11,7 @@ fun main() {
             throw IllegalArgumentException()
         }
         do {
-            printInputNumber()
+            OutputView().printInputNumber()
             val playerNumbers = PlayerNumber().inputPlayerNumber()
             if (!isNumberException(playerNumbers)) {
                 throw IllegalArgumentException()
@@ -17,43 +19,15 @@ fun main() {
             val baseBall = BaseBall(computerNumbers, playerNumbers)
             val strike = baseBall.getStrikeCount()
             val ball = baseBall.getBallCount()
-            getResultMessage(ball, strike)
+            OutputView().printResult(ball, strike)
         } while (!isThreeStrike(strike))
-        printGameOver()
+        OutputView().printGameOver()
     } while (!PlayerNumber().exitGame())
 
 }
 
 fun isThreeStrike(strike: Int): Boolean {
     return strike == 3
-}
-
-fun printGameStart() {
-    println("숫자 야구 게임을 시작합니다.")
-}
-
-fun printInputNumber() {
-    print("숫자를 입력해주세요 : ")
-}
-
-fun printGameOver() {
-    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-}
-
-fun getResultMessage(ball: Int, strike: Int) {
-    if (strike == 0 && ball == 0) {
-        println("낫싱")
-    }
-    if (strike > 0 && ball > 0) {
-        println("${ball}볼 ${strike}스트라이크")
-    }
-    if (strike > 0 && ball == 0) {
-        println("${strike}스트라이크")
-    }
-    if (ball > 0 && strike == 0) {
-        println("${ball}볼")
-    }
 }
 
 fun isNumberException(number: List<Int>): Boolean {
