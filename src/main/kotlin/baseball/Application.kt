14d5974aa41fddd355d2a2f969a4363e834b0cbc
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-11-04 19:17:46 +0900

    feat: 사용자가 잘못된 값을 입력하지 않는지 검사 기능 추가
    사용자가 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력하지 않은 경우 IllegalArgumentException을 발생시키는 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -26,7 +26,9 @@ fun generateComputerNumber(): List<Int> {
 
 fun getUserNumber(): List<Int> {
     print("숫자를 입력해주세요 : ")
-    val userInput = readLine()
+    val userInput = readLine()?.toList()
+
+    checkException(userInput)
 
     val userNumber = mutableListOf<Int>()
     userInput?.forEach { c ->
@@ -34,4 +36,20 @@ fun getUserNumber(): List<Int> {
     }
 
     return userNumber
+}
+
+fun checkException(userInput: List<Char>?) {
+    if (userInput?.size != 3) {
+        throw IllegalArgumentException("3개의 숫자를 입력하세요")
+    }
+
+    userInput.forEach { c ->
+        if (c.code < 48 || c.code > 57) {
+            throw IllegalArgumentException("숫자만 입력하세요")
+        }
+    }
+
+    if (userInput.distinct() != userInput) {
+        throw java.lang.IllegalArgumentException("서로 다른 수를 입력하세요")
+    }
 }
