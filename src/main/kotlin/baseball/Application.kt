b6fe9cd7490e108dd2c4fab74e4d571e1b2f01e7
krrong <khk9664@naver.com>
2022-11-05 07:38:52 +0900

    feat[function]: implement makeNumber function

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,27 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    startGame()
+}
+
+fun startGame(){
+    println("숫자 야구 게임을 시작합니다.")
+    val computerNumber = makeNumber()
+}
+fun countBall(){}
+fun countStrike(){}
+fun printResult(){}
+fun isGameOver(){}
+fun makeNumber(): String{
+    var computer = ""
+    while (computer.length < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9).toString()
+        if (!computer.contains(randomNumber)) {
+            computer += randomNumber
+        }
+    }
+
+    return computer
 }
