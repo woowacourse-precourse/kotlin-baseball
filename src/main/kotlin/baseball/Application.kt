ad162a6e52370aa294b1666678809366ec9271f5
krrong <khk9664@naver.com>
2022-11-05 07:47:05 +0900

    feat[function]: implement countBall function

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -9,11 +9,15 @@ fun main() {
 fun startGame(){
     println("숫자 야구 게임을 시작합니다.")
     val computerNumber = makeNumber()
+    print(computerNumber)
+
+//    while(true){
+        print("숫자를 입력해주세요 : ")
+        val inputNumber = camp.nextstep.edu.missionutils.Console.readLine()
+        val ballCount = countBall(computerNumber, inputNumber)
+//    }
 }
-fun countBall(){}
-fun countStrike(){}
-fun printResult(){}
-fun isGameOver(){}
+
 fun makeNumber(): String{
     var computer = ""
     while (computer.length < 3) {
@@ -22,6 +26,19 @@ fun makeNumber(): String{
             computer += randomNumber
         }
     }
-
     return computer
 }
+
+fun countBall(computerNumber: String, inputNumber: String): Int{
+    var ballCnt = 0
+    for(n in computerNumber){
+        if(inputNumber.contains(n)){
+            ballCnt++
+        }
+    }
+    return ballCnt
+}
+
+fun countStrike(){}
+fun printResult(){}
+fun isGameOver(){}
