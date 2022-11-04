fbcd71c144fc0da421b51fd30a1d1dbc5c314a82
JunJangE <wnswkd486@gmail.com>
2022-11-04 23:24:15 +0900

    feat: 8번 기능 추가
    반복문을 통해 사용자가 야구 게임을 진행하고 3개의 숫자를 모두 맞추면 게임이 종료됨

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -6,12 +6,19 @@ fun main() {
     println("숫자 야구 게임을 시작합니다.")
     var computer: String = ""
     computer = randomNum()
-    print("숫자를 입력해주세요 : ")
-    val userInput = readLine()!!
-    println(userInput)
-    userWrongTypeAnswer(userInput)
-    userWrongSizeAnswer(userInput)
-    println(userAnswerCount(computer, userInput))
+
+    while (true) {
+        print("숫자를 입력해주세요 : ")
+        val userInput = readLine()!!
+        println(userInput)
+        userWrongTypeAnswer(userInput)
+        userWrongSizeAnswer(userInput)
+        println(userAnswerCount(computer, userInput))
+        if (userCorrectAnswer(computer, userInput)) {
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            break
+        }
+    }
 }
 
 fun randomNum(): String {
@@ -24,6 +31,7 @@ fun randomNum(): String {
     }
     return computer
 }
+
 fun userWrongTypeAnswer(userAnswer: String) {
     try {
         Integer.parseInt(userAnswer)
@@ -31,21 +39,22 @@ fun userWrongTypeAnswer(userAnswer: String) {
         throw IllegalArgumentException("게임 종료")
     }
 }
+
 fun userWrongSizeAnswer(userAnswer: String) {
     if (userAnswer.length != 3) throw IllegalArgumentException("게임 종료")
 }
+
 fun userAnswerCount(answer: String, userAnswer: String): String {
     val score = mutableListOf<Int>(0, 0, 0) // 스트라이크/볼/아웃
 
     answer.forEachIndexed { index, c ->
-
         if (c == userAnswer[index]) score[0] += 1
         else if (userAnswer.contains(c)) score[1] += 1
         else score[2] += 1
-
     }
     return userAnswerCountPrint(score)
 }
+
 fun userAnswerCountPrint(score: MutableList<Int>): String {
     var userAnswerCountString = ""
     if (score[1] != 0) userAnswerCountString += "${score[1]}볼"
@@ -56,4 +65,8 @@ fun userAnswerCountPrint(score: MutableList<Int>): String {
     if (score[2] == 3) userAnswerCountString += "낫싱"
 
     return userAnswerCountString
+}
+
+fun userCorrectAnswer(answer: String, userAnswer: String): Boolean {
+    return answer == userAnswer
 }
