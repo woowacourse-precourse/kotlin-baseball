fa702411f3dc88fa532c2428a7ff10f1ad6c2522
Doyeop Kim <dygames@naver.com>
2022-11-04 23:25:14 +0900

    FEATURE: 사용자로부터 숫자 입력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,6 +3,17 @@ package baseball
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
+fun getUserInput(): String {
+    val input = Console.readLine()
+    if (input == "1" || input == "2")
+        return input
+    if (input.length != 3)
+        throw IllegalArgumentException()
+    if (!input.all { char -> char.isDigit() })
+        throw IllegalArgumentException()
+    return input
+}
+
 fun main() {
     TODO("프로그램 구현")
 }
