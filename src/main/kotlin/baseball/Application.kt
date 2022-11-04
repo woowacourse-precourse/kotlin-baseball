d42e86e1f3289f8cb364eea5a054d9173b154019
cyjadela <adelacho@naver.com>
2022-11-04 14:46:17 +0900

    feat: strike,ball,낫싱 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -43,6 +43,22 @@ fun main() {
                 }
             }
         }
+
+        if (ball == 0) {
+            if (strike == 0)
+                println("낫싱")
+            else
+                println("${strike}스트라이크")
+        }
+
+        if (strike == 0) {
+            if (ball != 0) {
+                println("${ball}볼")
+            }
+        }
+
+        if (ball != 0 && strike != 0)
+            println("${ball}볼 ${strike}스트라이크")
     }
 
 }
