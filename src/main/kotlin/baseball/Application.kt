3903771b2c08ca30fa476879470d9781d7cb1274
ChunBaee <mljung10@naver.com>
2022-11-05 03:29:42 +0900

    feat: 숫자야구 게임 시작 문구 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,5 +8,10 @@ const val MIN_NUMBER = 1
 const val MAX_NUMBER = 9
 
 fun main() {
-    TODO("프로그램 구현")
+    startGameAtFirst()
+}
+
+private fun startGameAtFirst() {
+    println("숫자 야구 게임을 시작합니다.")
+
 }
