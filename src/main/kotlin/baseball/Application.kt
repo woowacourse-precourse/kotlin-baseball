f3148e4962ef905810fd90ef4fd1a3d34f5da0be
ChoHJin <cjc9739@naver.com>
2022-11-05 01:39:13 +0900

    feat: 숫자 입력 기능 구현
    컴퓨터의 숫자는 랜덤변수를 받아 리스트에 저장하고,
    사용자의 숫자는 직접 입력받아 리스트에 저장하는 기능을 구현함.
    
    Closes #1

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,52 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+fun comInput(): MutableList<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) computer.add(randomNumber)
+    }
+    return computer
+}
+
+fun userInput() : MutableList<Int> {
+    val user = mutableListOf<Int>()
+
+    while(user.size < 3) {
+        print("숫자를 입력해주세요 : ")
+        val userNumber = Console.readLine()!!
+
+        if(userNumber.length == 3) {
+            for(i in userNumber.indices) {
+                if(userNumber[i] - '0' in 1..9) {
+                    user.add(userNumber[i] - '0')
+                }
+            }
+        } else {
+            throw IllegalArgumentException()
+        }
+
+    }
+    return user
+}
 fun main() {
-    TODO("프로그램 구현")
+    val comNumbers = comInput()
+    val userNumbers = userInput()
+
+    println("com : $comNumbers")
+    println("user : $userNumbers")
+//    while(true) {
+//        var gameStartControl = readLine()!!.toInt()
+//
+//        if(gameStartControl == 1) {
+//            continue
+//        } else if (gameStartControl == 0){
+//            break
+//        } else { println("다시 입력해 주세요") }
+//    }
+
+
 }
