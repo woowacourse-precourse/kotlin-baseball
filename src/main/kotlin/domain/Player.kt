2e53f177da4a7a336f0e2ab5d5e19c916a1400f2
tmdgh1592 <tmdgh1592@naver.com>
2022-11-04 13:07:28 +0900

    refactor: remove common code for Player and Computer by adding Role abstract class

--- src/main/kotlin/domain/Player.kt
+++ src/main/kotlin/domain/Player.kt
@@ -2,8 +2,4 @@ package domain
 
 class Player(
     private val numbers: List<Int> = listOf()
-) {
-    fun find(index: Int): Int = numbers[index]
-    fun contains(number: Int): Boolean = numbers.contains(number)
-    fun isEquals(index: Int, number: Int): Boolean = (find(index) == number)
-}
+) : Role()
---
+++ src/main/kotlin/domain/Role.kt
@@ -0,0 +1,9 @@
+package domain
+
+abstract class Role(
+    private val numbers: List<Int> = listOf()
+) {
+    fun find(index: Int): Int = numbers[index]
+    fun contains(number: Int): Boolean = numbers.contains(number)
+    fun isEquals(index: Int, number: Int): Boolean = (find(index) == number)
+}
