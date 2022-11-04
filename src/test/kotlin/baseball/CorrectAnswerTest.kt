b39bfdaa938da668beeb0adad9a79022a3fa8d64
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-05 02:40:06 +0900

    [Test] 모두 볼일 때 볼 3개를 반환하는 테스트 추가

--- src/test/kotlin/baseball/CorrectAnswerTest.kt
+++ src/test/kotlin/baseball/CorrectAnswerTest.kt
@@ -5,9 +5,17 @@ import org.junit.jupiter.api.Test
 
 class CorrectAnswerTest {
     @Test
-    fun `모두 스트라이크일 때 점수가 3인지 확인`() {
+    fun `모두 스트라이크일 때 스트라이크 3개 반환`() {
         val round = BaseballGame("123")
         val checkStrike = round.strike("123")
         assertThat(3).isEqualTo(checkStrike)
     }
+
+    @Test
+    fun `모두 볼일 때 볼 3개 반환`() {
+        val round = BaseballGame("123")
+        val checkBall = round.ball("321")
+        assertThat(3).isEqualTo(checkBall)
+    }
+
 }
