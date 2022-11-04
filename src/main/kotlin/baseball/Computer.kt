2b83ee024e178ab21a8f010bb13fa3a20f0fb94f
choiwoosuk <wooseokchoi0508@gmail.com>
2022-11-04 21:44:42 +0900

    refactor(Computer): 3을 const val 로 변경

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -7,12 +7,16 @@ class Computer() {
 
     fun generateComputerNumber(): List<Int> {
         val computerNumber = mutableListOf<Int>()
-        while (computerNumber.size < 3) {
+        while (computerNumber.size < NUMBER_OF_DIGITS) {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber)
             }
         }
         return computerNumber
+    }
+
+    companion object{
+        const val NUMBER_OF_DIGITS = 3
     }
 }
