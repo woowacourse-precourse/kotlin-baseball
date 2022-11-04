054c0fab0690c2f5a3d9ad6f9cc48550bcc2d280
ese111 <feung94@gmail.com>
2022-11-04 23:46:04 +0900

    feat: 중복된 값인지 체크하기위한 함수 기능 구현

--- src/main/kotlin/baseball/view/Input.kt
+++ src/main/kotlin/baseball/view/Input.kt
@@ -9,6 +9,10 @@ fun inputUserValue(): List<Int> {
 
 fun checkInput(input: String): List<Int> {
     val list = mutableListOf<Int>()
+    if (!checkInputLength(input)) {
+        throw IllegalArgumentException("3개만 입력할 수 있습니다.")
+    }
+
     return list
 }
 
@@ -18,3 +22,13 @@ fun checkInputLength(input: String): Boolean {
     }
     return true
 }
+
+fun checkInputElement(input: String): Boolean {
+    val firstElement = input[0]
+    val afterCheckList = input.filter { firstElement != it }
+    if (afterCheckList.length < 2) {
+        throw IllegalArgumentException("중복한 입력은 할 수 없습니다.")
+    }
+    return true
+}
+
