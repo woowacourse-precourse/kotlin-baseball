95b77fe0c2e25e6ed96a5ddf94d70d65007caf1c
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-05 00:46:13 +0900

    feat:기능 9번 출력된 결과에 따라 성공여부를 판단하는 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,7 +3,18 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    println(comRandomNum())
+    var comNum = comRandomNum()
+
+    while(true) {
+        val userNum = inputUserNum()
+
+        val num = compareUserAndCom(comNum, userNum!!)
+
+        when(num) {
+            1 -> comNum = comRandomNum()
+            2 -> break
+        }
+    }
 }
 
 fun comRandomNum() : String {
@@ -31,8 +42,7 @@ fun compareUserAndCom(com : String, user : String) : Int {
     if(result.equals("3스트라이크")) {
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        val num = readLine()!!.toInt()
-        return num
+        return readLine()!!.toInt()
     }
     return 0
 }
