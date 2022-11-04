337aaf387f51a5b4c82090459f7794a792f4115a
cyjadela <adelacho@naver.com>
2022-11-04 14:48:24 +0900

    feat: 게임 진행 여부 확인

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -61,4 +61,7 @@ fun main() {
             println("${ball}볼 ${strike}스트라이크")
     }
 
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    chooseGameNum = Console.readLine().toInt()
+
 }
