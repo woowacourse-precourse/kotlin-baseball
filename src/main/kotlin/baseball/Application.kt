425405a36041a6e0d5f56805abac4fbcee9f6007
cyjadela <adelacho@naver.com>
2022-11-04 14:06:33 +0900

    feat: random 숫자 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    var chooseGameNum = 1
+    println("숫자 야구 게임을 시작합니다.")
+
+    while (chooseGameNum == 1) {
+        val computerNum = mutableListOf<Int>()
+        while (computerNum.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!computerNum.contains(randomNumber)) {
+                computerNum.add(randomNumber)
+            }
+        }
+    }
+
 }
