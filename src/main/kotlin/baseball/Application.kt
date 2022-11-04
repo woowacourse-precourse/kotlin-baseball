b9e5114f8742ab85c1197c3566714dbc4f926391
dev-worthwhile <dev.lboram@gmail.com>
2022-11-04 15:41:42 +0900

    feat: create computer's number (기능1 - 맞힐 랜덤 숫자를 생성)

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,20 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     TODO("프로그램 구현")
+}
+
+
+private fun createComputerNum() : List<Int> {
+    val computerNum = mutableListOf<Int>()
+    while (computerNum.size < 3) {  //순서대로 백의 자리, 십의 자리, 일의 자리
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computerNum.contains(randomNumber)) {
+            computerNum.add(randomNumber)
+        }
+    }
+
+    return computerNum
 }
