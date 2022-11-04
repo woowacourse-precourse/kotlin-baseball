fbcc77bc7f72035b26b634c07f495ac633f478c2
EmilyCh0 <seoyeon3089@naver.com>
2022-11-05 00:42:03 +0900

    fix(User): 숫자 입력 받기 문구 출력

--- src/main/kotlin/baseball/User.kt
+++ src/main/kotlin/baseball/User.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 class User {
 
     fun getGuessInput(): List<Int> {
+        print("숫자를 입력해주세요 : ")
         val input = Console.readLine()
         // TODO 예외 사항 체크
         return input.toList().map { it.code.plus(-48) }
