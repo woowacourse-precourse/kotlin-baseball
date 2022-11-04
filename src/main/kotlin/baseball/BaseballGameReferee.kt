04a6ddd19c2a0ed50dc136d028c3af84a779eb22
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-11-05 04:28:33 +0900

    feat(BaseballGameReferee) : 게임 시작 및 진행을 하는 함수 구현

--- src/main/kotlin/baseball/BaseballGameReferee.kt
+++ src/main/kotlin/baseball/BaseballGameReferee.kt
@@ -1,8 +1,61 @@
 package baseball
 
-object BaseballGameReferee {
-    fun decideEachTurn(userInput: ArrayList<Int>) {
+import camp.nextstep.edu.missionutils.Console
 
+object BaseballGameReferee {
+    fun processBaseballGame() {
+        Computer.generateNewRandomNumbers()
+        WordPrinter.printGameStart()
+
+        while (true) {
+            val guessedNumbers = User.guessNumber() ?: return
+            lateinit var curGameStatus: GameStatus
+
+            try {
+                curGameStatus = decideEachTurn(guessedNumbers)
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+                return
+            }
+            when (curGameStatus) {
+                GameStatus.TERMINATE -> break
+                GameStatus.NEW_GAME -> Computer.generateNewRandomNumbers()
+                else -> continue
+            }
+        }
+    }
+
+    private fun decideEachTurn(userInput: ArrayList<Int>): GameStatus {
+        val (strikeCount, ballCount) = arrayOf(calcStrikeCount(userInput), calcBallCount(userInput))
+
+        if (strikeCount + ballCount == 0) {
+            WordPrinter.printNothing()
+            return GameStatus.CONTINUE
+        }
+
+        when {
+            strikeCount == 3 -> {
+                WordPrinter.printStrikeCount(strikeCount)
+                WordPrinter.printGameEnd()
+                return checkPlayAgain()
+            }
+            ballCount > 0 && strikeCount > 0 -> {
+                WordPrinter.printBallCount(ballCount)
+                WordPrinter.printStrikeCount(strikeCount)
+            }
+            ballCount > 0 -> WordPrinter.printBallCount(ballCount)
+            strikeCount > 0 -> WordPrinter.printStrikeCount(strikeCount)
+        }
+        WordPrinter.printNewLine()
+        return GameStatus.CONTINUE
+    }
+
+    private fun checkPlayAgain(): GameStatus {
+        return when (Console.readLine()) {
+            "1" -> GameStatus.NEW_GAME
+            "2" -> GameStatus.TERMINATE
+            else -> throw IllegalArgumentException("잘못된 입력입니다.")
+        }
     }
 
     fun calcStrikeCount(userInput: ArrayList<Int>): Int {
