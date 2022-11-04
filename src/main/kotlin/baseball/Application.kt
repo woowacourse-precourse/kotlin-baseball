9181ea216e4706c0fc17165b4e2f40362d334887
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-11-05 00:13:27 +0900

    feat<main> : 대이터입력받는함수추가
    사용자로부터 숫자를 입력받는 함수추가
    
    기능목록 두번째 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,10 +5,10 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
 
     generateComputer()
+    getInputFormUser()
 
 }
 
-//1. 서로 다른 1- 9 사이의 수 3개 생성
 fun generateComputer(): List<Int>{
     val computer = mutableListOf<Int>()
     while (computer.size < 3) {
@@ -21,5 +21,11 @@ fun generateComputer(): List<Int>{
 return computer
 }
 
+//2. 사용자로부터 입력받기
 
+fun getInputFormUser():Int{
+    print("숫자를 입력해주세요 : ")
+    var inputNumber = readLine()!!.toInt()
+    return inputNumber
+}
 
