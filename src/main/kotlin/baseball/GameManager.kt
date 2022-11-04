d4cd3373805ec8f22a63c7a8a36235c58fc83ae0
choiwoosuk <wooseokchoi0508@gmail.com>
2022-11-05 00:14:48 +0900

    refactor(): Game클래스 생성 및 GameManager 수정
    효율적인 코드 수정을 위해 안내문구를 출력하고 게임을 시작하는 코드들을 Game클래스에 넣고, GameManager는 숫자야구 규칙과 계산 관련된 코드들만 있도록 수정함.

--- src/main/kotlin/baseball/GameManager.kt
+++ src/main/kotlin/baseball/GameManager.kt
@@ -1,15 +1,6 @@
 package baseball
 
 class GameManager {
-
-    fun printStartGame() {
-        println(START_GAME_MESSAGE)
-    }
-
-    fun printEnterNumber() {
-        print(ENTER_NUMBER_MESSAGE)
-    }
-
     fun countBall(userNumber: List<Int>, computerNumber: List<Int>): Int {
         var ball = 0
         userNumber.forEach { num ->
@@ -32,29 +23,7 @@ class GameManager {
         return strike == ALL_STRIKE_NUMBER
     }
 
-    fun printThreeStrike() {
-        println(THREE_STRIKE_MESSAGE)
-    }
-
-    fun printNothing(ball: Int, strike: Int) {
-        if (ball == 0 && strike == 0) print(NOTHING_MESSAGE)
-    }
-
-    fun printBall(ball: Int) {
-        if (ball != 0) print("$ball" + BALL_MESSAGE)
-    }
-
-    fun printStrike(strike: Int) {
-        if (strike != 0) print("$strike" + STRIKE_MESSAGE)
-    }
-
     companion object {
-        const val START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다."
-        const val ENTER_NUMBER_MESSAGE = "숫자를 입력해주세요 : "
-        const val THREE_STRIKE_MESSAGE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임종료"
-        const val NOTHING_MESSAGE = "낫싱"
-        const val STRIKE_MESSAGE = "스트라이크"
-        const val BALL_MESSAGE = "볼 "
         const val ALL_STRIKE_NUMBER = 3
     }
 }
