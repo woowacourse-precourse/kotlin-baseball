cf8c1008d798834aad61b3f2521f97691002a1b1
sujin-kk <sjoo0002@gmail.com>
2022-11-04 19:40:00 +0900

    feat(validator): 사용자 입력값에 중복을 확인한 뒤  exception 낸다.

--- src/main/kotlin/baseball/validator/InputValidator.kt
+++ src/main/kotlin/baseball/validator/InputValidator.kt
@@ -4,21 +4,23 @@ import baseball.common.MAX_NUMBER_SIZE
 
 
 class InputValidator {
-    fun checkCorrectInput(input: String) {
-
+    fun checkInput(input: String) {
+        (isDigit(input = input) && isCorrectLength(input = input) && isNoDuplicate(input = input)).let { isCorrectInput ->
+            if (!isCorrectInput)
+                throw java.lang.IllegalArgumentException()
+        }
     }
 
-    fun isDigit(input: String): Boolean {
+    private fun isDigit(input: String): Boolean {
         return input.all {
             it.isDigit()
             it.code != 48
         }
     }
 
-    fun isCorrectLength(input: String): Boolean =
+    private fun isCorrectLength(input: String): Boolean =
         input.length == MAX_NUMBER_SIZE
 
-
-
-
+    private fun isNoDuplicate(input: String): Boolean =
+        input.toSet().size == MAX_NUMBER_SIZE
 }
