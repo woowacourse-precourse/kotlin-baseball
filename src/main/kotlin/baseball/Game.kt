139adf220f39fd9cfeae93fb61758c1b41b43777
EmilyCh0 <seoyeon3089@naver.com>
2022-11-05 10:53:17 +0900

    feat(Game): 게임 재시작/종료 확인

--- src/main/kotlin/baseball/Game.kt
+++ src/main/kotlin/baseball/Game.kt
@@ -3,13 +3,23 @@ package baseball
 class Game {
     private val computer = Computer()
     private val user = User()
+
     init {
         println("숫자 야구 게임을 시작합니다.")
     }
+
     fun start() {
         computer.generateAnswer()
         do {
             val userGuess = user.getGuessInput()
         } while (computer.compareGuessAndAnswer(userGuess))
+
+        if (isRestart()) {
+            start()
+        }
+    }
+
+    private fun isRestart(): Boolean {
+        TODO("유저 입력 받아서 true/false 리턴")
     }
 }
