735eeb23bb712904a0ad23522031d482db8daa2e
tmdgh1592 <tmdgh1592@naver.com>
2022-11-05 19:18:11 +0900

    refactor: Refactoring the Player and Computer classes into a sealed class called Role.

--- src/main/kotlin/model/Role.kt
+++ src/main/kotlin/model/Role.kt
@@ -1,23 +1,25 @@
 package model
 
 import utils.Constants
-import utils.ExceptionHandler.validateDuplicatedOrException
-import utils.ExceptionHandler.validateListLengthOrException
+import utils.ExceptionHandler
 
-abstract class Role(
-    internal val numbers: List<Int>
+sealed class Role(
+    protected val numbers: List<Int>
 ) {
 
     init {
         validate()
     }
 
-    fun validate() {
-        validateListLengthOrException(numbers, Constants.DIGIT_LENGTH)
-        validateDuplicatedOrException(numbers)
+    private fun validate() {
+        ExceptionHandler.validateListLengthOrException(numbers, Constants.DIGIT_LENGTH)
+        ExceptionHandler.validateDuplicatedOrException(numbers)
     }
 
     fun find(index: Int): Int = numbers[index]
     fun contains(number: Int): Boolean = numbers.contains(number)
     fun isEquals(index: Int, number: Int): Boolean = (find(index) == number)
 }
+
+class Player(numbers: List<Int>) : Role(numbers)
+class Computer(numbers: List<Int>) : Role(numbers)
