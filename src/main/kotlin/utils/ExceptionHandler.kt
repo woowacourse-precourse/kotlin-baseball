1b6e203d1bd294ffdd34bb8b5d2ca90f3af86ac5
tmdgh1592 <tmdgh1592@naver.com>
2022-11-05 13:18:15 +0900

    refactor: add exception handler
    - Separate the existing exception handling method into a separate object class.

---
+++ src/main/kotlin/utils/ExceptionHandler.kt
@@ -0,0 +1,31 @@
+package utils
+
+object ExceptionHandler {
+    fun validateLengthOrException(answer: String, expectedLength: Int) {
+        if (answer.length != expectedLength) throw IllegalArgumentException(Constants.LENGTH_MISMATCH_EXCEPTION)
+    }
+
+    fun validateDigitOrException(ch: Char): Int {
+        val digit = ch.digitToIntOrNull() ?: throw IllegalArgumentException(Constants.NON_DIGIT_EXCEPTION)
+        validateRangeOrException(digit)
+
+        return digit
+    }
+
+    fun validateRangeOrException(digit: Int) {
+        if (digit !in Constants.MIN_DIGIT..Constants.MAX_DIGIT) throw IllegalArgumentException(Constants.OUT_OF_RANGE_EXCEPTION)
+    }
+
+    fun validateDuplicatedOrException(inputs: Set<Int>) {
+        if (inputs.size != Constants.DIGIT_LENGTH) throw IllegalArgumentException(Constants.DUPLICATED_DIGIT_EXCEPTION)
+    }
+
+    fun validateRestartCode(code: String) {
+        validateLengthOrException(code, Constants.RESTART_LENGTH)
+        validateIsRestartCode(code)
+    }
+
+    fun validateIsRestartCode(code: String) {
+        if (code != Constants.YES_RESTART && code != Constants.NO_RESTART) throw IllegalArgumentException(Constants.WRONG_FORMAT_EXCEPTION)
+    }
+}
--- src/main/kotlin/utils/PlayerConsole.kt
+++ src/main/kotlin/utils/PlayerConsole.kt
@@ -3,18 +3,12 @@ package utils
 import camp.nextstep.edu.missionutils.Console
 import domain.Player
 import utils.Constants.DIGIT_LENGTH
-import utils.Constants.DUPLICATED_DIGIT_EXCEPTION
 import utils.Constants.ENTER_NUMBER_TEXT
-import utils.Constants.LENGTH_MISMATCH_EXCEPTION
-import utils.Constants.MAX_DIGIT
-import utils.Constants.MIN_DIGIT
-import utils.Constants.NON_DIGIT_EXCEPTION
-import utils.Constants.NO_RESTART
-import utils.Constants.OUT_OF_RANGE_EXCEPTION
 import utils.Constants.RESTART_ASKING_TEXT
-import utils.Constants.RESTART_LENGTH
-import utils.Constants.WRONG_FORMAT_EXCEPTION
-import utils.Constants.YES_RESTART
+import utils.ExceptionHandler.validateDigitOrException
+import utils.ExceptionHandler.validateDuplicatedOrException
+import utils.ExceptionHandler.validateLengthOrException
+import utils.ExceptionHandler.validateRestartCode
 
 object PlayerConsole {
     fun enterAnswer(): Player {
@@ -38,37 +32,8 @@ object PlayerConsole {
         println(RESTART_ASKING_TEXT)
 
         val restartCode = Console.readLine()
-        restartCode
         validateRestartCode(restartCode)
 
         return restartCode
-    }
-
-    private fun validateLengthOrException(answer: String, expectedLength: Int) {
-        if (answer.length != expectedLength) throw IllegalArgumentException(LENGTH_MISMATCH_EXCEPTION)
-    }
-
-    private fun validateDigitOrException(ch: Char): Int {
-        val digit = ch.digitToIntOrNull() ?: throw IllegalArgumentException(NON_DIGIT_EXCEPTION)
-        validateRangeOrException(digit)
-
-        return digit
-    }
-
-    private fun validateRangeOrException(digit: Int) {
-        if (digit !in MIN_DIGIT..MAX_DIGIT) throw IllegalArgumentException(OUT_OF_RANGE_EXCEPTION)
-    }
-
-    private fun validateDuplicatedOrException(inputs: Set<Int>) {
-        if (inputs.size != DIGIT_LENGTH) throw IllegalArgumentException(DUPLICATED_DIGIT_EXCEPTION)
-    }
-
-    private fun validateRestartCode(code: String) {
-        validateLengthOrException(code, RESTART_LENGTH)
-        validateIsRestartCode(code)
-    }
-
-    private fun validateIsRestartCode(code: String) {
-        if (code != YES_RESTART && code != NO_RESTART) throw IllegalArgumentException(WRONG_FORMAT_EXCEPTION)
     }
 }
