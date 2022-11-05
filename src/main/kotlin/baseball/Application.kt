a59e6e728fc3a95934c8fb0bd086b1f613f2f08d
LHS0731 <gustjq2536@gmail.com>
2022-11-06 02:09:24 +0900

    숫자 입력하는 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,23 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    var mynum = ""
+
+    while (true) {
+        mynum = NumInput()
+    }
+}
+
+fun NumInput(): String {
+    print("숫자를 입력해주세요 : ")
+    var fnum: String? = readLine()
+    if (fnum!!.length != 3)
+        throw IllegalArgumentException("Error(3자리가 아닌 수 입력!)")
+    if (fnum[0] == fnum[1] || fnum[1] == fnum[2] || fnum[2] == fnum[0])
+        throw IllegalArgumentException("Error(중복된 수 입력!)")
+    if (fnum.contains('0'))
+        throw IllegalArgumentException("Error(잘못된 수 입력!)")
+    return fnum
 }
