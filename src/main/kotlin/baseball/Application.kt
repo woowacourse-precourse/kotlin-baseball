941038f880a7ade66e2bc9e8e87c4d95266934a0
junhyungss <daba44@naver.com>
2022-11-05 18:39:49 +0900

    코틀린 코드 컨벤션에 맞게 변경

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -24,8 +24,7 @@ private fun restartOrFinish(finish: Boolean): Boolean {
 
     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    val choice = Console.readLine()
-    return when (choice) {
+    return when (val choice = Console.readLine()) {
         "1" -> false
         "2" -> true
         else -> throw IllegalArgumentException("게임 새로 시작, 종료 조건에 맞지 않음 : $choice")
