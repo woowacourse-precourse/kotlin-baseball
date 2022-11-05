8f53bdc79cc32d280c8c15868b6333f61427710b
Yerin <myheaven_b@naver.com>
2022-11-05 23:02:17 +0900

    test: add testcases for function getResultString

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -74,11 +74,21 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
-    fun `스트라이크 판정 확인 테스트`() {
+    fun `스트라이크 판정 테스트`() {
         val computer: List<Int> = listOf(1, 2, 3)
         val player: List<Int> = listOf(1, 2, 3)
 
         assertThat(checkStrike(computer, player)).isEqualTo(false)
+    }
+
+    @Test
+    fun `결과 출력 테스트`() {
+        assertThat(getResultString(0,0)).isEqualTo("낫싱")
+        assertThat(getResultString(1,0)).isEqualTo("1볼")
+        assertThat(getResultString(3,0)).isEqualTo("3볼")
+        assertThat(getResultString(0,1)).isEqualTo("1스트라이크")
+        assertThat(getResultString(0,3)).isEqualTo("3스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        assertThat(getResultString(1,2)).isEqualTo("1볼 2스트라이크")
     }
 
     override fun runMain() {
