9955e6572818a8b4ba2aacb9ada827acb2096312
dohun_ <silver2679@gmail.com>
2022-11-05 21:39:01 +0900

    Feat: fun createRandomNumber 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,12 +1,24 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun inputNumber(): String? {//숫자 입력
     print("숫자를 입력해주세요 : ")
     val inputNumber = readLine()//string
 
     return inputNumber
 }
-
+fun createRandomNumber(): String{ //랜덤 숫자 생성
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer.joinToString("")
+    println(computer.toString())
+}
 fun main() {
     println(inputNumber())
 }
