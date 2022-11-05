54856b5a4e89dc286d69bb36d2b17f4b95b04fb2
dpwns1234 <dpwns1234@naver.com>
2022-11-05 15:33:03 +0900

    feat: 2, 3 사용자 입력 값 받고, 올바른 입력인지 확인

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -9,15 +9,35 @@ import org.mockito.MockedStatic.Verification
 
 fun main() {
     val computer = getComputerRandomNumber()
-
+    println("숫자 야구 게임을 시작합니다.")
+    while (true) {
+        println("숫자를 입력해주세요 : ")
+        val player = Console.readLine()
+        if (!isCorrectNumber(player)) {
+            throw IllegalArgumentException()
+        }
+    }
 }
 
 fun getComputerRandomNumber(): List<Int> {
     val computer = mutableListOf<Int>()
     while (computer.size < 3) {
         val randomNumber = pickNumberInRange(1, 9)
-        if(!computer.contains(randomNumber))
+        if (!computer.contains(randomNumber))
             computer.add(randomNumber)
     }
     return computer
+}
+
+fun isCorrectNumber(player: String): Boolean {
+    // 1. 3자리 수가 아닐 경우 false
+    if (player.length != 3)
+        return false
+    // 2. 1-9의 범위를 벗어난 숫자가 아닐 경우 false
+    for (c in player) {
+        if (c < '1' || c > '9')
+            return false
+    }
+
+    return true
 }
