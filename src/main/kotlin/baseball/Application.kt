af9518e121f2b6aafa60f25e436a8413c9c92d4e
Yerin <myheaven_b@naver.com>
2022-11-05 21:15:54 +0900

    feat: add function getPlayerNumbers

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,24 +1,41 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
-
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     var flag = 1
     var computer: List<Int>
+    var person: List<Int>
 
+    computer = getComputerNumbers()
     while (flag == 1) {
-        computer = getComputerNumber()
+        person = getPlayerNumbers()
+        flag = 0
     }
 }
 
-fun getComputerNumber(): List<Int>{
+fun getComputerNumbers(): List<Int> {
     val computer: MutableList<Int> = mutableListOf()
+
     while (computer.size < 3) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
         if (!computer.contains(randomNumber)) {
             computer.add(randomNumber)
         }
     }
+
     return computer
 }
+
+fun getPlayerNumbers(): List<Int> {
+    val readNumber: String = Console.readLine()
+    var readList: MutableList<Int> = mutableListOf()
+
+    for (num in readNumber) {
+        readList.add(num - '0')
+    }
+
+    return readList
+}
+
