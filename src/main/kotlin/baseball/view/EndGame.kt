b471469c4a158484ad9c4794da6fa9121528070c
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-05 23:42:34 +0900

    feat: Add printAskPlayAgainMessage function Add a function that print the ask play again message

--- src/main/kotlin/baseball/view/EndGame.kt
+++ src/main/kotlin/baseball/view/EndGame.kt
@@ -5,4 +5,8 @@ class EndGame {
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
     }
 
+    fun printAskPlayAgainMessage(): Int{
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        return readLine()!!.toInt()
+    }
 }
