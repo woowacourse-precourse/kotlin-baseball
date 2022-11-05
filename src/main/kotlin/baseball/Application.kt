ef2762057241788ce2fe7cbded30d94e04ef2bf9
joh9911 <joh9911@naver.com>
2022-11-05 22:08:37 +0900

    feat: 게임을 시작하는 클래스 Application 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,29 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    startGame()
 }
+fun startGame(){
+    println("숫자 야구 게임을 시작합니다.")
+    while(true){
+        BaseballGame().processGame()
+        if (inputNumberForRestartGame() == 2){
+            println("게임 종료.")
+            break
+        }
+
+    }
+}
+fun inputNumberForRestartGame(): Int{
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    val input = readLine()
+    ThrowException().throwExceptionForRestartNumber(input)
+    return input?.toInt()!!
+}
+
+
+
+
+
