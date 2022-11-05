52ab556aef14af1f63bc7b35b30987e555438801
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-05 17:17:04 +0900

    feat : 입력값을 통한 점수 계산 로직 추가

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -3,6 +3,8 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 
 object Computer {
+    var strike = 0
+    var ball = 0
     val correctNumber = mutableListOf<Int>()
     fun getCorrectRandomNumber(){
         correctNumber.clear()
@@ -10,6 +12,18 @@ object Computer {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
             if (!correctNumber.contains(randomNumber)) {
                 correctNumber.add(randomNumber)
+            }
+        }
+    }
+    fun compareInputWithCorrectNumber(inputValue : String){
+        for(i in 0 until correctNumber.size){
+            if(inputValue.contains(correctNumber[i].toChar())){
+                if(inputValue[i]== correctNumber[i].toChar()){
+                    strike++
+                }
+                else{
+                    ball++
+                }
             }
         }
     }
