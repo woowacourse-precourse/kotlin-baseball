e97e475850645e2d72cac62be10dc1c3ee19b704
wonseok <dnjstjr245@gmail.com>
2022-11-06 08:57:56 +0900

    :sparkles: 1부터 9까지의 서로 다른 수로 이루어진 3자리의 랜덤 수 생성 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,20 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
+var randomGeneratedNumber: Int? = 0
 fun main() {
-    TODO("프로그램 구현")
+    generateRandomNumber()
+}
+
+fun generateRandomNumber() {
+    val randomNumbersSet = mutableSetOf<Int>()
+
+    while (randomNumbersSet.size != 3) {
+        randomNumbersSet.add(
+            Randoms.pickNumberInRange(1, 9)
+        )
+    }
+
+    randomGeneratedNumber = randomNumbersSet.joinToString("").toIntOrNull()
 }
