fc722a146285219e62217b0801b5cc1665d0b099
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-06 00:32:14 +0900

    feat: add printResult function Add a function that print result of user`s input

--- src/main/kotlin/baseball/view/PlayGame.kt
+++ src/main/kotlin/baseball/view/PlayGame.kt
@@ -5,4 +5,11 @@ class PlayGame {
         print("숫자를 입력해주세요 : ")
         return readLine()!!.toInt()
     }
+
+    fun printResult(resultArr: IntArray){
+        if(resultArr[0] == 0 && resultArr[1] == 0)
+            println("낫싱")
+        else
+            println("${resultArr[1]}볼 ${resultArr[0]}스트라이크")
+    }
 }
