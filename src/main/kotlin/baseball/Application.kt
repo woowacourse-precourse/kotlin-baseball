6f5ecaee564852c52d55b8efdd2f83524b7826c5
lh99j <grand2181@gmail.com>
2022-11-05 15:52:12 +0900

    refactor : print문 함수로 분리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,7 +3,7 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    println("숫자 야구 게임을 시작합니다.")
+    showStartMessage()
 
     baseballGameStart()
 }
@@ -85,8 +85,7 @@ fun isInValidNum(userNum: String): Boolean {
 }
 
 fun baseballGameEnd() {
-    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    showEndMessage()
 
     when (readLine()!!.toInt()) {
         1 -> {
@@ -99,4 +98,13 @@ fun baseballGameEnd() {
             throw IllegalArgumentException("Error")
         }
     }
+}
+
+fun showStartMessage(){
+    println("숫자 야구 게임을 시작합니다.")
+}
+
+fun showEndMessage(){
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 }
