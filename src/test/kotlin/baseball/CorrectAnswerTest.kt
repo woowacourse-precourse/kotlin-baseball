48709d2543fa02c9b99c753adc229f1c28305f8b
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-05 16:14:08 +0900

    [Refactor] CorrectAnswerTest 테스트 코드 중복 제거
    BaseballGame 객체 생성 중복 제거
    strike, ball 갯수 확인하는 코드 중복 제거

--- src/test/kotlin/baseball/CorrectAnswerTest.kt
+++ src/test/kotlin/baseball/CorrectAnswerTest.kt
@@ -6,26 +6,32 @@ import org.junit.jupiter.api.Test
 class CorrectAnswerTest {
     @Test
     fun `모두 스트라이크일 때 스트라이크 3개 반환`() {
-        val round = BaseballGame("123")
-        val checkStrike = round.strike("123")
-        assertThat(3).isEqualTo(checkStrike)
+        assertCheck("123", "123", 3, 0)
     }
 
     @Test
     fun `모두 볼일 때 볼 3개 반환`() {
-        val round = BaseballGame("123")
-        val checkBall = round.ball("321")
-        assertThat(3).isEqualTo(checkBall)
+        assertCheck("123", "312", 0, 3)
     }
 
     @Test
     fun `모두 오답일 때 낫싱을 반환`() {
-        val round = BaseballGame("123")
-        val checkStrike = round.strike("456")
-        val checkBall = round.ball("456")
-        assertThat(0).isEqualTo(checkStrike)
-        assertThat(0).isEqualTo(checkBall)
+        assertCheck("123", "456", 0, 0)
 
+    }
+
+
+    private fun assertCheck(
+        computerInput: String,
+        userInput: String,
+        expectedStrike: Int,
+        expectedBall: Int,
+    ) {
+        val round = BaseballGame(computerInput)
+        val checkStrike = round.strike(userInput)
+        val checkBall = round.ball(userInput)
+        assertThat(expectedStrike).isEqualTo(checkStrike)
+        assertThat(expectedBall).isEqualTo(checkBall)
     }
 
 }
