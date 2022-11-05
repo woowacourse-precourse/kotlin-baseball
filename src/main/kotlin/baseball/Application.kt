73f4c3f17384037bb6b9fc7d0fbedbf48563bc33
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-05 14:49:49 +0900

    [Feat] 모두 오답일 때, 3개의 숫자를 모두 맞힌 경우 출력하는 기능 추가
    3개의 숫자를 모두 맞힌 경우: 스트라이크가 3개일 때
    모두 오답인 경우: 볼이 0개, 스트라이크가 0개일 때

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,43 +1,61 @@
 package baseball
 
 
+
 fun main() {
 
-    println("숫자 야구 게임을 시작합니다.")
-
-    val computer = "123"
-    while (true) {
-        print("숫자를 입력해주세요 : ")
-        val userInput: String = readLine()!!
-        if (computer == userInput) {
-            println("${3}스트라이크")
-            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-            println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요")
-            val inputNumber: String = readLine()!!
-            if (inputNumber == "1") continue
-            if (inputNumber == "2") break
-
-        }
-
-
-    }
-
-
+    BaseballGame(computer = "123").game()
 
 }
 
 class BaseballGame(computer: String) {
     private val computerInput: String = computer
 
+
+    fun game() {
+
+        println("숫자 야구 게임을 시작합니다.")
+        while (true) {
+            print("숫자를 입력해주세요 : ")
+            val userInput: String = readLine()!!
+            if (strike(userInput) == 3) {
+                println("${strike(userInput)}스트라이크")
+                println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+                println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요")
+                val inputNumber: String = readLine()!!
+                if (inputNumber == "1") continue
+                if (inputNumber == "2") break
+            }
+            if (ball(userInput) == 3) {
+                println("${ball(userInput)}볼")
+            }
+
+            if (strike(userInput) == 0 && ball(userInput) == 0) {
+                println("낫싱")
+            }
+
+        }
+    }
+
+
     fun strike(user: String): Int {
         val userInput: String = user
         var strikeCount = 0
         for (index in userInput.indices) {
-            if (user[index] == computerInput[index]) strikeCount++
+            if (computerInput[index] == user[index]) strikeCount++
         }
 
         return strikeCount
 
+    }
+
+    fun ball(user: String): Int {
+        val userInput: String = user
+        var ballCount = 0
+        for (index in userInput.indices) {
+            if (computerInput[index] != user[index] && computerInput.contains(user[index])) ballCount++
+        }
+        return ballCount
     }
 
 
