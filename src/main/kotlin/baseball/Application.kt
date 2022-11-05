0be0415be927e9f26a0a074d684689c7d9daf791
nayoung511 <nayoungbae511@gmail.com>
2022-11-06 07:11:58 +0800

    feat($Application.kt): create fun baseballGame

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -41,6 +41,55 @@ fun receiveUserInput() : MutableList<Int> {
     return userInputList
 }
 
+// 유저의 입력값과 컴퓨터의 값을 비교
+fun baseballGame(userInputList: MutableList<Int>, computer: MutableList<Int>) : Boolean {
+    var correct = false // user의 입력값이 컴퓨터의 값과 일치하는지 판별
+    val checkNumber : MutableList<Boolean> = mutableListOf(false, false, false)
+
+    // 1. 스트라이크 판별
+    var strike = 0  // strike 갯수를 세는 변수
+    for ( i in 0 .. 2 ) {
+        if ( userInputList[i] == computer[i] ) {
+            // 해당 자릿수를 방문했다고 표시
+            checkNumber[i] = true
+            strike ++
+        }
+    }
+
+    // 스트라이크가 3개라면 정답으로, 루프에서 빠져나온다
+    if ( strike == 3 ) {
+        correct = true
+        println("3스트라이크")
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        return correct
+    }
+
+    // 2. 볼 판별
+    var ball = 0 // ball 갯수를 세는 변수
+    for ( i  in 0 .. 2 ){
+        // 해당 자릿수를 방문하지 않았을 때
+        if ( checkNumber[i] == false ){
+            if ( userInputList[i] in computer ) {
+                ball ++
+            }
+        }
+    }
+
+    // user의 입력이 0 strike와 0 ball이라면, 낫싱
+    if ( strike == 0 && ball == 0 ) {
+        println("낫싱")
+    }
+    else {
+        if ( ball != 0 ) {
+            print("$ball 볼 ")
+        }
+        if ( strike != 0 ){
+            println("$strike 스트라이크")
+        }
+    }
+    return correct
+}
+
 
 
 fun main() {
