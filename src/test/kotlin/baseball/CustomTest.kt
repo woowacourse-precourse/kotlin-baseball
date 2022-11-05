4211a237cb62e0518dd92d6900f2d7ef85c54021
ChunBaee <mljung10@naver.com>
2022-11-05 13:55:01 +0900

    test: 사용자 입력값에 문자열이 포함되어 있는 Test Case

--- src/test/kotlin/baseball/CustomTest.kt
+++ src/test/kotlin/baseball/CustomTest.kt
@@ -1,11 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.test.Assertions
 import camp.nextstep.edu.missionutils.test.NsTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class CustomTest : NsTest() {
     override fun runMain() {
         main()
     }
 
-
+    @Test
+    fun `문자열 포함시 에러 반환여부 체크`() {
+        Assertions.assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("6r4") }
+        }
+    }
 }
