239849281a9ce6fd1cca42679361ce80bce64cf5
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-05 21:25:36 +0900

    Feat: Add func 7 ~ 14 and Refactor 예외 처리 함수 in ApplicationTest.kt
    7. 생성한 숫자와 입력받은 숫자가 동일하며 위치도 같은지 확인
    8. 생성한 숫자와 입력받은 숫자가 동일한 숫자를 포함하고 있는지 확인
     - checkBothNumbers 함수의 매개변수로 listOfCreatedNumber와 listOfInputNumber를 받아 요소의 위치와 값이 같은지 확인 후 결과를 String 형태로 반환
    9. 확인 결과 출력
     - checkBothNumbers의 리턴 값을 println()으로 출력
    10. 확인 결과가 3스트라이크 아닐경우 3번으로 돌아감
    11. 3스트라이크면 게임종료 텍스트 출력 및 게임 재시작 혹은 종료 출력
    12. 예외처리 및 프로그램 종료
    13. 재시작을 누르면 2번으로 복귀
    14. 종료를 누르면 프로그램 종료
     - 10 ~ 14번은 flag 변수를 이용하여 동작하도록 코딩함
    추가적으로 validateLength를 잘못 해석한 것 같아 길이가 3이 아니면 무조건 예외처리하도록 되어 있던 것을 4이상이면 예외처리하도록 수정
    또한 3스트라이크 시 게임 종료 혹은 재시작을 입력했을 때 예외처리하는 함수 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -11,8 +11,8 @@ fun main() {
      * 4. 숫자 입력 받음
      * 5. 예외처리 및 프로그램 종료
      * 6. 입력 받은 숫자를 잘라서 리스트에 보관
-     * 7. 생성한 숫자와 입력받은 숫자가 동일한 숫자를 포함하고 있는지 확인
-     * 8. 생성한 숫자와 입력받은 숫자가 동일하며 위치도 같은지 확인
+     * 7. 생성한 숫자와 입력받은 숫자가 동일하며 위치도 같은지 확인
+     * 8. 생성한 숫자와 입력받은 숫자가 동일한 숫자를 포함하고 있는지 확인
      * 9. 확인 결과 출력
      * 10. 확인 결과가 3스트라이크 아닐경우 3번으로 돌아감
      * 11. 3스트라이크면 게임종료 텍스트 출력 및 게임 재시작 혹은 종료 출력
@@ -24,22 +24,23 @@ fun main() {
     /**
      * 예외처리 리스트
      * 1. 숫자로만 구성되어야 함
-     * 2. 길이는 3
+     * 2. 길이는 4 미만
      * 3. 모두 다른 숫자여야 함
      * */
 
     /** 게임 시작 문구 출력 */
     println("숫자 야구 게임을 시작합니다.")
 
-    var flag = 1
+    var gameStatusFlag = 3
+    var listOfCreatedNumber = createRandomNumber()
 
     while (true) {
-        if (flag == 1) {
+        if (gameStatusFlag == 0) {
             /** 랜덤한 숫자를 생성해 리스트에 저장 */
-            val listOfCreatedNumber = createRandomNumber()
-            flag += 1
+            listOfCreatedNumber = createRandomNumber()
+            gameStatusFlag = 3
         }
-        if (flag == 2) {
+        if (gameStatusFlag == 3) {
             /** 숫자 입력 문구 출력 */
             print("숫자를 입력해주세요 : ")
 
@@ -52,15 +53,42 @@ fun main() {
             validateRepeat(inputNumber)
 
             /** 입력 받은 숫자를 잘라서 리스트에 보관 */
-            val listOfInputNumber: MutableList<Int> = mutableListOf(3)
+            val listOfInputNumber: MutableList<Int> = mutableListOf()
             for (i in 0..2) {
                 listOfInputNumber.add(inputNumber[i].digitToInt())
             }
+            println()
 
-            /** 생성한 숫자와 입력받은 숫자가 동일한 숫자를 포함하고 있는지 확인 */
+            /**
+             * 생성한 숫자와 입력받은 숫자가 동일하며 위치도 같은지 확인
+             * 생성한 숫자와 입력받은 숫자가 동일한 숫자를 포함하고 있는지 확인
+             * */
+            val resultOfCheckBothNumbers = checkBothNumbers(listOfCreatedNumber, listOfInputNumber)
 
+            /** 확인 결과 출력 */
+            println(resultOfCheckBothNumbers)
+
+
+            gameStatusFlag = checkGameStatus(resultOfCheckBothNumbers)
+        }
+        if (gameStatusFlag == 2) {
+            break
+        }
+        if (gameStatusFlag == 1) {
+            gameStatusFlag = 0
         }
     }
+    println("게임 종료")
+}
+fun checkGameStatus(resultOfCheckBothNumbers: String): Int {
+    if (resultOfCheckBothNumbers == "3스트라이크") {
+        println("3개의 숫자를 모두 맞히셨습니다. 게임 종료")
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val inputStatusNumber = Console.readLine()
+        validateStatusNumber(inputStatusNumber)
+        return inputStatusNumber.toInt()
+    }
+    return 3
 }
 
 fun createRandomNumber(): List<Int> {
@@ -74,19 +102,24 @@ fun createRandomNumber(): List<Int> {
     return listOfCreatedNumber.toList()
 }
 
-fun validateNumber(inputNumber: String) {
-    val regexNumber = Regex("^[0-9]$")
+fun validateStatusNumber(inputStatusNumber: String) {
+    if (!inputStatusNumber.all { it.isDigit() }) {
+        throw IllegalArgumentException("입력한 문자열이 숫자가 아닙니다.")
+    }
+    if (inputStatusNumber.length > 1) {
+        throw IllegalArgumentException("입력한 문자열의 길이가 1보다 큽니다.")
+    }
+}
 
-    if (!regexNumber.matches(inputNumber)) {
+fun validateNumber(inputNumber: String) {
+    if (!inputNumber.all { it.isDigit() }) {
         throw IllegalArgumentException("입력한 문자열이 숫자가 아닙니다.")
     }
 }
 
 fun validateLength(inputNumber: String) {
-    val regexLength = Regex("^{3}$")
-
-    if (!regexLength.matches(inputNumber)) {
-        throw IllegalArgumentException("입력한 문자열의 길이가 3이 아닙니다.")
+    if (inputNumber.length >= 4) {
+        throw IllegalArgumentException("입력한 문자열의 길이가 3보다 큽니다.")
     }
 }
 
@@ -99,6 +132,34 @@ fun validateRepeat(inputNumber: String) {
     }
 }
 
-fun checkBothNumbers(listOfCreatedNumber: List<Int>, listOfInputNumber: List<Int>) {
+fun checkBothNumbers(listOfCreatedNumber: List<Int>, listOfInputNumber: List<Int>): String {
+    var result = ""
+    var ball = 0
+    var strike = 0
 
+    for (index in listOfInputNumber.indices) {
+        if (listOfCreatedNumber[index] == listOfInputNumber[index]) {
+            strike += 1
+        }
+        if ((listOfCreatedNumber.contains(listOfInputNumber[index])) and
+            (listOfCreatedNumber[index] != listOfInputNumber[index])
+        ) {
+            ball += 1
+        }
+    }
+
+    if ((strike == 0) and (ball == 0)) {
+        result += "낫싱"
+        return result
+    }
+    if ((strike == 0) and (ball != 0)) {
+        result += ball.toString() + "볼"
+        return result
+    }
+    if ((strike != 0) and (ball == 0)) {
+        result += strike.toString() + "스트라이크"
+        return  result
+    }
+    result += ball.toString() + "볼" + " " + strike.toString() + "스트라이크"
+    return result
 }
