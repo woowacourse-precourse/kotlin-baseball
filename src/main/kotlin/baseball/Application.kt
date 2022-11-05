02521c2b1b0cd9237ed88b11e9f1e7329953b357
ponopono0322 <ponopono0322@gmail.com>
2022-11-05 19:23:52 +0900

    docs(guide): 구현할 기능 목록 정의
    - docs/README.md 구현 기능 목록 작성
    - Application.kt 구현 기능 목록 함수 작성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,47 @@
 package baseball
 
+fun printStart() {
+    TODO("게임 시작 문구를 출력하는 함수 구현")
+}
+
+fun printNothing() {
+    TODO("v1이 없다면 낫싱 출력하는 함수 구현")
+}
+
+fun printEnd() {
+    TODO("v2 == 3 이라면 게임 종료 출력하는 함수 구현")
+}
+
+fun printContinue() {
+    TODO("새로 게임을 하려는지 문구 출력 및 입력 받는 함수 구현")
+}
+
+fun getInput(): String {
+    TODO("수를 문자열로 입력받는 함수 구현")
+}
+
+fun listConverter(inputString: String): ArrayList<String> {
+    TODO("각 문자열을 리스트로 변환하는 함수 구현")
+}
+
+fun listChecker(inputList: ArrayList<String>): Boolean {
+    TODO("입력받은 수가 올바른지 검사하는 함수 구현")
+}
+
+fun equalChecker(v2: Int): Boolean {
+    TODO("v2 == 3 확인하는 함수")
+}
+
+fun intersectCounter(): Boolean {
+    TODO("셋에서 공통 원소가 있는지 확인(볼/스트라이크)하는 함수 구현")
+}
+
+fun equalCounter(): Boolean {
+    TODO("맵에서 공통 원소의 value가 같은지 확인(스트라이크)하는 함수 구현")
+}
+
+
+
 fun main() {
     TODO("프로그램 구현")
 }
