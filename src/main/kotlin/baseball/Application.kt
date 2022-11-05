2bc6be01c524491031c354c60780e8dfbd5a1ad2
no1msh <no1msh1217@gmail.com>
2022-11-06 00:20:34 +0900

    feat: 기능 목록 2번 구현 및 숫자야구게임 메소드 임시구현
    1. createRandomAnswerNumber 메소드 구현
    - 랜덤한 3자리의 숫자를 생성하는 메소드
    2. playNumberBaseball 메소드 임시구현
    - 숫자야구의 진행이 구현될 메소드

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,23 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
+    playNumberBaseball()
+}
+
+fun playNumberBaseball() {
+    val answerNumber = createRandomAnswerNumber()
+}
+
+fun createRandomAnswerNumber(): Int {
+    val randomNumbers = mutableListOf<Int>()
+    while (randomNumbers.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!randomNumbers.contains(randomNumber)) {
+            randomNumbers.add(randomNumber)
+        }
+    }
+    return randomNumbers[0] * 100 + randomNumbers[1] * 10 + randomNumbers[2]
 }
