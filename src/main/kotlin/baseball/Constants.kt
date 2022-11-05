df370d6c487e3dadcfe267206be67831502bea28
dltkd1395 <dltkd13956@naver.com>
2022-11-05 14:46:08 +0900

    feat(Constants) : 상수 클래스 추가

---
+++ src/main/kotlin/baseball/Constants.kt
@@ -0,0 +1,11 @@
+package baseball
+
+class Constants {
+    companion object {
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 9
+        const val NUMBER_LENGTH = 3
+        const val RESTART = "1"
+        const val QUIT = "2"
+    }
+}
