4d3199ba36af57d72d89dfa0aaf432b3b971b307
junsu1023 <12jangdokg55@gmail.com>
2022-11-05 21:12:20 +0900

    [Add] playGame 함수 생성, 컴퓨터의 값, 사용자의 값 얻기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,33 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+
+    while(true) {
+        playGame()
+    }
+}
+
+fun playGame() {
+    val computerNum = randomComputerNum()
+
+    while (true) {
+        print("숫자를 입력해주세요 : ")
+        val userNum = Console.readLine().toCharArray()
+    }
+}
+
+fun randomComputerNum(): MutableList<Int> {
+    val computerNum = mutableListOf<Int>()
+    while (computerNum.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computerNum.contains(randomNumber)) {
+            computerNum.add(randomNumber)
+        }
+    }
+
+    return computerNum
 }
