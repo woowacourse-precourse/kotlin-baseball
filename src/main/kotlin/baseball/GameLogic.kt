765f6ce07bacadb5781114f0c17f9fa8d7c46880
Hyeongseon <hsun5163@gmail.com>
2022-11-06 03:22:29 +0900

    feat : GameLogic 클래스 구현

--- src/main/kotlin/baseball/GameLogic.kt
+++ src/main/kotlin/baseball/GameLogic.kt
@@ -1,4 +1,30 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class GameLogic {
+    private val comNumber = mutableListOf<Int>()
+    private val userNumber = mutableListOf<Int>()
+    fun gameStart(){
+        while (comNumber.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!comNumber.contains(randomNumber)) {
+                comNumber.add(randomNumber)
+            }
+        }
+    }
+    fun comparison(){
+
+    }
+    fun userNumToList(num : Int) : List<Int>{
+        val userList = mutableListOf<Int>()
+        var quotient = num
+        var inputNum = 0
+        while(quotient != 0){
+            inputNum = quotient/10
+            quotient %= 10
+            userList.add(0,inputNum)
+        }
+        return userList
+    }
 }
