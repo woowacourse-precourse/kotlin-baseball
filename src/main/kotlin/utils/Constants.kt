10fffdd3668d51af517070bda2829429902162c2
tmdgh1592 <tmdgh1592@naver.com>
2022-11-05 12:40:51 +0900

    refactor: modify LENGTH_MISMATCH_EXCEPTION phrase
    - Modify the phrase to a generalized phrase.

--- src/main/kotlin/utils/Constants.kt
+++ src/main/kotlin/utils/Constants.kt
@@ -8,7 +8,7 @@ object Constants {
     const val CORRECT_AND_FINISH = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
 
     // Exceptions
-    const val LENGTH_MISMATCH_EXCEPTION = "입력값의 길이가 3이 아닙니다."
+    const val LENGTH_MISMATCH_EXCEPTION = "입력값의 길이가 올바르지 않습니다."
     const val NON_DIGIT_EXCEPTION = "입력값이 숫자가 아닙니다."
     const val OUT_OF_RANGE_EXCEPTION = "입력값이 올바른 범위가 아닙니다."
     const val DUPLICATED_DIGIT_EXCEPTION = "중복된 숫자가 존재합니다."
