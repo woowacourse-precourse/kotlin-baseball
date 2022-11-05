4c261d91e60d10bb14730e327b79af790aa92e5e
Ojongseok <whdtjr6889@naver.com>
2022-11-05 14:19:44 +0900

    플레이어의 서로 다른 세 자리 숫자 입력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,15 +4,25 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     val computerNumber = createComputerNumber()
-
+    startGame(computerNumber)
 }
 
-fun createComputerNumber() {
+fun startGame(computerNumber: ArrayList<Int>) {
+    var SUCCESS = false
+    while (!SUCCESS) {
+        print("숫자를 입력해주세요 : ")
+        var userInput = readLine()?.toMutableList()
+    }
+}
+
+fun createComputerNumber(): ArrayList<Int> {
     val computerNumber = ArrayList<Int>()
     while (computerNumber.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1,9)
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
         if (!computerNumber.contains(randomNumber)) {
             computerNumber.add(randomNumber)
         }
     }
+    println("숫자 야구 게임을 시작합니다.")
+    return computerNumber
 }
