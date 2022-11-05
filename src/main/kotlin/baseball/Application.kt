81df0e35f9ffa9b433be910c1c6c949447b2ce9b
[alsswl] <[alswl4223@naver.com]>
2022-11-05 14:51:22 +0900

    feat: 사용자에게 수를 입력받고 각 자릿수별로 분리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,27 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import java.util.*
+
+
 fun main() {
-    TODO("프로그램 구현")
+
+    var input = readLine()
+    var InputInt = input?.toInt()
+
+    var InputFirst = InputInt!!/100;
+    var InputSecond = (InputInt!!-100*InputFirst)/10
+    var InputThird = InputInt/10
+
+
+
+
+
+
+
+
+
+
+
+
 }
