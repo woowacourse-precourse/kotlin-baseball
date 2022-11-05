664f31a5a3d954718d03d9bc9ae383f7bc605f89
sonac.kim <104493771+kimsonac@users.noreply.github.com>
2022-11-05 17:29:06 +0900

    Add Function 2: Set computer's random number

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,14 +1,26 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
 
+    val computer : MutableSet<Int> = mutableSetOf() // LinkedHashSet 생성: 순서O 중복X
+    val player = mutableListOf<Int>()
+
     printStartMessage()
+    setRandomNumber(computer)
 }
 
 fun printStartMessage() {
     println("숫자 야구 게임을 시작합니다.")
 }
 
-
+fun setRandomNumber(com : MutableSet<Int>){
+    while(com.size < 3){
+        val random = Randoms.pickNumberInRange(1, 9)
+        com.add(random)
+    }
+}
 
 
