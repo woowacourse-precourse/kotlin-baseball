159aad6dcae2b556007aece6667cfc8ea30972d1
Ojongseok <whdtjr6889@naver.com>
2022-11-05 12:24:08 +0900

    컴퓨터의 서로 다른 세 자리 수 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,18 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    val computerNumber = createComputerNumber()
+
+}
+
+fun createComputerNumber() {
+    val computerNumber = ArrayList<Int>()
+    while (computerNumber.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1,9)
+        if (!computerNumber.contains(randomNumber)) {
+            computerNumber.add(randomNumber)
+        }
+    }
 }
