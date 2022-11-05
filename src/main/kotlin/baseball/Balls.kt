e0387d057fbeb854852736cd9989aa96a838b899
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-05 17:07:02 +0900

    refactor:기능 3번 리팩토링

--- src/main/kotlin/baseball/Balls.kt
+++ src/main/kotlin/baseball/Balls.kt
@@ -1,19 +1,22 @@
 package baseball
 
 class Balls {
+    companion object {
+        const val LIMIT_BALL_LIST_SIZE = 3
+    }
 
     val balls : List<Ball>
 
     constructor(ballList: String) {
 
-        if(ballList.length > 3) {
+        if(ballList.length > LIMIT_BALL_LIST_SIZE) {
             throw IllegalArgumentException("입력한 숫자야구 길이가 3보다 깁니다.")
         }
 
-        val balls = ballList.mapIndexed { index, c -> Ball(c.toInt() - '0'.toInt(), index + 1) }
-        println(balls)
-        this.balls = balls
+        this.balls = mapBallList(ballList)
     }
+
+    fun mapBallList(ballList: String) : List<Ball>  = ballList.mapIndexed { index, c ->  Ball(c.toInt() - '0'.toInt(), index + 1)}
 
     fun playGame(ballList : String) : GameResult {
         val userInput = Balls(ballList)
