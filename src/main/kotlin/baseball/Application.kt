4d3628f5e3d32f5c8ea94e2c12d4f5606de87df0
SEO-J17 <qw4643987@naver.com>
2022-11-06 00:45:22 +0900

    #1 feat: 랜덤으로 숫자 생성 기능 구현함

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,18 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    val targetNumber: String = createNumber()
+}
+
+fun createNumber(): String {
+    val numberList = mutableListOf<Int>()
+    while (numberList.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!numberList.contains(randomNumber)) {
+            numberList.add(randomNumber)
+        }
+    }
+    return numberList.joinToString("")
 }
