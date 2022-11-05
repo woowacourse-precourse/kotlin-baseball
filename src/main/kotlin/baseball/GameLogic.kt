dc32c9a7a9e737a0d73f5a771138be56663a7a6c
Hyeongseon <hsun5163@gmail.com>
2022-11-06 03:49:19 +0900

    feat : comparison 함수구현

--- src/main/kotlin/baseball/GameLogic.kt
+++ src/main/kotlin/baseball/GameLogic.kt
@@ -5,6 +5,9 @@ import camp.nextstep.edu.missionutils.Randoms
 class GameLogic {
     private val comNumber = mutableListOf<Int>()
     private val userNumber = mutableListOf<Int>()
+
+    private var ballCount = 0
+    private var strikeCount = 0
     fun gameStart(){
         while (comNumber.size < 3) {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
@@ -13,8 +16,15 @@ class GameLogic {
             }
         }
     }
-    fun comparison(){
-
+    fun comparison() : Unit{
+        val resultList = mutableListOf<Int>()
+        for (i in 0 until userNumber.size) {
+            if(userNumber[i]==comNumber[i]){
+                strikeCount += 1
+            } else if(userNumber[i] in comNumber){
+                ballCount += 1
+            }
+        }
     }
     fun userNumToList(num : Int) : List<Int>{
         val userList = mutableListOf<Int>()
