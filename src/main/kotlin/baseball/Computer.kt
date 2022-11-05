a81e3450d4f1f2160c98b4d3f0c85467f1649026
joh9911 <joh9911@naver.com>
2022-11-05 21:04:41 +0900

    refractor(Computer.kt): 컴퓨터 랜덤값 생성 함수에 반환값 추가

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -5,14 +5,16 @@ import camp.nextstep.edu.missionutils.Randoms
 class Computer {
 
     /**
-     * 컴퓨터의 랜덤한 숫자를 정해주고 리스트에 넣어주는 함수
+     * 컴퓨터의 랜덤한 숫자를 정해주고 리스트에 넣어 반환해주는 함수
      */
-    fun createComputerNumberList(computerNumber: MutableList<Int>){
+    fun createComputerNumberList(): MutableList<Int> {
+        val computerNumber = mutableListOf<Int>()
         while (computerNumber.size < 3) {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber)
             }
         }
+        return computerNumber
     }
 }
