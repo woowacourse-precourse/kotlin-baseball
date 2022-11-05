95caeed5f26ce868e17b4862cf4d889e53dc4b98
sujin-kk <sjoo0002@gmail.com>
2022-11-06 03:54:56 +0900

    test(processor): 컴퓨터와 플레이어의 숫자를 비교해서 올바른 볼스트라이크 결과를 처리하는지 테스트한다.

--- src/test/kotlin/baseball/computer/ComputerTest.kt
+++ src/test/kotlin/baseball/computer/ComputerTest.kt
@@ -7,12 +7,12 @@ class ComputerTest {
     private val computer = Computer()
 
     @Test
-    fun `컴퓨터가 생성한 숫자가 3자리 숫자인지 확인한다`() {
+    fun `컴퓨터가 생성한 숫자가 3자리 숫자인지 테스트`() {
        assertThat(computer.numberOfComputer.length).isEqualTo(3)
     }
 
     @Test
-    fun `컴퓨터가 생성한 숫자에 중복이 없는지 확인한다`() {
+    fun `컴퓨터가 생성한 숫자에 중복이 없는지 테스트`() {
         assertThat(computer.numberOfComputer.toCharArray().toHashSet().size).isEqualTo(3)
     }
 }
