a0069a47fb1dfa5b16e08bbd7bcb9eccefde0ca5
EmilyCh0 <seoyeon3089@naver.com>
2022-11-05 10:32:28 +0900

    feat(Game): 게임 시작 문구 출력

--- src/main/kotlin/baseball/Game.kt
+++ src/main/kotlin/baseball/Game.kt
@@ -3,7 +3,9 @@ package baseball
 class Game {
     private val computer = Computer()
     private val user = User()
-
+    init {
+        println("숫자 야구 게임을 시작합니다.")
+    }
     fun start() {
         computer.generateAnswer()
         do {
