5f26e668186f1bb0c401ceabc2d6a44350951d26
Ojongseok <whdtjr6889@naver.com>
2022-11-05 17:22:59 +0900

    일치, 포함, 미포함 여부 확인 및 게임 종료

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -12,7 +12,42 @@ fun startGame(computerNumber: ArrayList<Int>) {
     while (!SUCCESS) {
         print("숫자를 입력해주세요 : ")
         var userInput = readLine()?.toMutableList()
+        val strikeAndBall = checkNumber(computerNumber,userInput)
+
+        if (strikeAndBall[0] == 3) {
+            println("${strikeAndBall[0]}스트라이크")
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            SUCCESS = true
+        }
+        else if (strikeAndBall[0] != 0 && strikeAndBall[1] != 0) {
+            println("${strikeAndBall[1]}볼 " + "${strikeAndBall[0]}스트라이크")
+        }
+        else if (strikeAndBall[0]!=0) {
+            println("${strikeAndBall[0]}스트라이크")
+        }
+        else if (strikeAndBall[1]!=0) {
+            println("${strikeAndBall[1]}볼")
+        }
+        else {
+            println("낫싱")
+        }
     }
+}
+
+fun checkNumber(computerNumber: ArrayList<Int>, userInput: MutableList<Char>?): Array<Int> {
+    // strikeAndBall[0] -> 스트라이크, strikeAndBall[1] -> 볼
+    var strikeAndBall = Array<Int>(2) {0}
+
+    for (i in 0 until 3) {
+        if (computerNumber[i] == userInput!![i].digitToInt()) {
+            strikeAndBall[0]++
+        }
+        else if (computerNumber.contains(userInput!![i].digitToInt())) {
+            strikeAndBall[1]++
+        }
+    }
+
+    return strikeAndBall
 }
 
 fun createComputerNumber(): ArrayList<Int> {
@@ -23,6 +58,6 @@ fun createComputerNumber(): ArrayList<Int> {
             computerNumber.add(randomNumber)
         }
     }
-    println("숫자 야구 게임을 시작합니다.")
+    println("숫자 야구 게임을 시작합니다.$computerNumber")
     return computerNumber
 }
