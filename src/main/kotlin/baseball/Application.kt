16bd8f3c66e16c500d7b2f6b50f7e0be1669dbc9
joh9911 <joh9911@naver.com>
2022-11-06 16:17:41 +0900

    refactor(Application.kt): inputNumberForRestartGame 함수 이름 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -9,14 +9,18 @@ fun startGame(){
     println("숫자 야구 게임을 시작합니다.")
     while(true){
         BaseballGame().processGame()
-        if (inputNumberForRestartGame() == 2){
+        if (getNumberForRestartGame() == 2){
             println("게임 종료.")
             break
         }
 
     }
 }
-fun inputNumberForRestartGame(): Int{
+
+/**
+ * 숫자를 입력받고 예외 체크 후 반환하는 함수
+ */
+fun getNumberForRestartGame(): Int{
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
     val input = readLine()
     ThrowException().throwExceptionForRestartNumber(input)
