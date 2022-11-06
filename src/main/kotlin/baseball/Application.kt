e030b149703599af1534d476dba33e965a5e9689
ChanwooAn <55792553+ChanwooAn@users.noreply.github.com>
2022-11-06 16:30:11 +0900

    feat(사전 준비) : 게임 시작 사전 준비 과정
    1-1 컴퓨터가 플레이어가 맞춰야할 랜덤 번호를 생성한다.
    1-2 "숫자 야구 게임을 시작합니다" 멘트를 출력한다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,32 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import kotlin.properties.Delegates
+
+lateinit var answer:String
+
+fun setRandomNumber() {
+    answer = ""
+    while (answer.length < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!answer.contains(randomNumber.digitToChar())) {
+            answer += randomNumber
+        }
+    }
+}
+
+fun prepareNewGame(){
+    setRandomNumber()
+    println("숫자 야구 게임을 시작합니다.")
+}
+
 fun main() {
-    TODO("프로그램 구현")
+    var playNewGameOrStop by Delegates.notNull<Int>()
+
+    do{
+        prepareNewGame()
+
+    }while(playNewGameOrStop==1)
+
+
 }
