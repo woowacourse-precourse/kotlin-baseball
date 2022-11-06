66c5fd76fce839ac13da0a10230012f479e5e831
82104 <tir2986@naver.com>
2022-11-06 13:32:29 +0900

    feat: 함수 시그니처 작성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,21 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     TODO("프로그램 구현")
 }
+
+fun generateRandomThreeDigits(): List<Int> {
+    TODO("임의의 3자리 자연수 생성")
+}
+
+fun validateUserInput(input: String): Boolean {
+    TODO("유저 입력 검사")
+}
+
+fun splitUserInput(input: String): List<Int> {
+    TODO("유저 입력 각 자릿수 분리")
+}
+
+
