7840f82c5c0260c3ff8296472c6c48bf09ff67ff
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-06 22:55:08 +0900

    refactor: chooseComputerNumber 리턴 값 String으로 변경

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,18 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     TODO("프로그램 구현")
 }
 
-fun chooseComputerNumber(): MutableList<Int> {
+fun chooseComputerNumber(): String {
     val computerNumber = mutableListOf<Int>()
     while (computerNumber.size < 3) {
         var randomNumber: Int = Randoms.pickNumberInRange(1, 9)
         addComputerNumber(randomNumber, computerNumber)
     }
-    return computerNumber
+    return computerNumber.joinToString("")
 }
 
 /* 뽑은 랜덤 숫자의 중복 여부를 확인하고 아닐 시 리스트에 추가하는 메소드  */
@@ -25,3 +26,5 @@ fun addComputerNumber(randomNumber: Int, computerNumber: MutableList<Int>) {
 fun printFirstStartMessage() {
     println("숫자 야구 게임을 시작합니다.")
 }
+
+
