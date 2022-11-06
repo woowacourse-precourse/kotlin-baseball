8787d750324877c6da9aca6bcf2f2c0e725e1581
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-06 23:07:35 +0900

    feat: 3번 기능- getUserNumber 메소드 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -27,4 +27,25 @@ fun printFirstStartMessage() {
     println("숫자 야구 게임을 시작합니다.")
 }
 
+fun getUserNumber(): String {
+    val userNumber = mutableListOf<Char>()
+    val userInput = Console.readLine().toString()
 
+    // 사용자의 입력이 세 자리 수가 아닌 경우
+    if (userInput.length != 3) {
+        throw IllegalArgumentException("You can enter only a three-digit number")
+    }
+
+    for (index in userInput.indices) {
+        // 사용자의 입력이 숫자가 아닌 경우
+        if (userInput[index] < '0' || userInput[index] > '9') {
+            throw IllegalArgumentException("You can enter only a number")
+        }
+        // 사용자의 입력이 같은 숫자를 포함하는 경우
+        if (userNumber.contains(userInput[index])) {
+            throw IllegalArgumentException("Each digit number must be different")
+        }
+        userNumber.add(userInput[index])
+    }
+    return userNumber.toString()
+}
