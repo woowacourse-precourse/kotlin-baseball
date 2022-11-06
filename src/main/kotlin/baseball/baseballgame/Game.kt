cfc04785adf5075ad4020fcedcf77f920b2ac50c
jiingom <vvankatam0@gmail.com>
2022-11-06 23:49:41 +0900

    feat: 게임의 결과를 판단해 힌트를 출력해주는 기능 구현

--- src/main/kotlin/baseball/baseballgame/Game.kt
+++ src/main/kotlin/baseball/baseballgame/Game.kt
@@ -51,11 +51,32 @@ class Game {
             }
         }
 
+        println(makeResultString(strike, ball))
+
         if (strike == 3) {
             println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
             return true
         }
 
         return false
+    }
+
+    private fun makeResultString(strike: Int, ball: Int): String {
+        var resultString = ""
+        if (strike == 0 && ball == 0) {
+            resultString += "낫싱"
+        }
+
+        if (ball > 0) {
+            resultString += ball
+            resultString += "볼 "
+        }
+
+        if (strike > 0) {
+            resultString += strike
+            resultString += "스트라이크"
+        }
+
+        return resultString
     }
 }
