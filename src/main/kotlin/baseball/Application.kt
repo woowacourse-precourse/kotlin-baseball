a3ac1ac3c3fc3c103f2fb0cbb585081a647947ff
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-07 00:10:22 +0900

    feat (Application.kt) : 컴퓨터가 서로 다른 세개의 수 만드는 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,21 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.*
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
     do {
+        val computerNumber = make3RandomNumber()
+
+
+fun make3RandomNumber(): MutableList<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
 }
