ebfee52cb5d608831ecd3b365058362c62865126
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-11-06 17:57:58 +0900

    feat<main> : 볼, 스트라이크 개수 구하기
    볼스트라이크 개수 구하는 함수추가
    3자리수를 한개씩 List에 넣는 함수 추가
    
    기능목록 네번째 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,13 +4,17 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
 
-    generateComputer()
+    var computer = generateComputer()
     var inputNum = getInputFormUser()
+    var user = mutableListOf<Int>()
     verificationNumver(inputNum)
+    user = intToList(inputNum) as MutableList<Int>
+    countingStrikeBall(computer, user)
+
 //6. 2~5과정을 반복 만약 스트라이크 3개가 나오면 종료
 }
 
-fun generateComputer(): List<Int>{
+fun generateComputer(): List<Int> {
     val computer = mutableListOf<Int>()
     while (computer.size < 3) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
@@ -19,20 +23,63 @@ fun generateComputer(): List<Int>{
         }
     }
 
-return computer
+    return computer
 }
 
-fun getInputFormUser():Int{
+fun getInputFormUser(): Int {
     print("숫자를 입력해주세요 : ")
     var inputNumber = readLine()!!.toInt()
     return inputNumber
 }
-//3. 입력받은 데이터 값 검증
-fun verificationNumver(inputNum : Int):Unit{
-    if(inputNum !in 100..999){
+
+fun verificationNumver(inputNum: Int): Unit {
+    if (inputNum !in 100..999) {
         throw IllegalArgumentException("User entered an incorrect value")
     }
 }
 
+//4. 값에서 볼, 스트라이크 개수 구하기
+fun countingStrikeBall(computer: List<Int>, inputNum: List<Int>): List<Int> {
+    var result = mutableListOf<Int>(0, 0) //첫번째 인덱스는 스트라이크 두번째 인덱스는 볼
 
+    for (i in 0..2) {
+        if (computer[i].equals(inputNum[0])) {
+            if (i == 0) {                                   //리팩토링 필요 들여쓰기 초과
+                result[0]++
+            }
+            if (i != 0) {
+                result[1]++
+            }
+        }
+        if (computer[i].equals(inputNum[1])) {
+            if (i == 1) {
+                result[0]++
+            }
+            if (i != 1) {
+                result[1]++
+            }
+        }
+        if (computer[i].equals(inputNum[2])) {
+            if (i == 2) {
+                result[0]++
+            }
+            if (i != 2) {
+                result[1]++
+            }
+        }
+    }
 
+    return result
+}
+
+fun intToList(num: Int): List<Int> {
+    var num: Int = num
+    var result = mutableListOf<Int>(0, 0, 0)
+    var index: Int = 2
+    while (index >= 0) {
+        result[index] = num % 10
+        num /= 10
+        index--
+    }
+    return result
+}
