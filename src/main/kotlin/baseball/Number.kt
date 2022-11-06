fbcf29d2ba9b33ee84679f6a23ecb124555f3184
cyjadela <adelacho@naver.com>
2022-11-06 22:01:02 +0900

    refactor: checkNum -> Number class로 이동

--- src/main/kotlin/baseball/Number.kt
+++ src/main/kotlin/baseball/Number.kt
@@ -28,4 +28,18 @@ class Number {
         checkNum(playerNum)
         return playerNum
     }
+
+    private fun checkNum(playerNum: String) {
+        if (playerNum.length != 3) {
+            throw IllegalArgumentException("세자리 수를 입력해주세요.")
+        }
+        if (playerNum.contains("0")) {
+            throw IllegalArgumentException("1~9사이 숫자를 입력해주세요.")
+        }
+        for (i in 0..1) {
+            if (playerNum[i] == playerNum[i + 1]) {
+                throw IllegalArgumentException("서로 다른 숫자를 입력해주세요.")
+            }
+        }
+    }
 }
