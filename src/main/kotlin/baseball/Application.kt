31cce40ab47af0bd763b6d24e926ec055438d3a9
cyjadela <adelacho@naver.com>
2022-11-06 19:54:20 +0900

    fix: 결과 출력 반복문 안으로 이동

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -28,40 +28,41 @@ fun main() {
             if (playerNum.length != 3) {
                 throw IllegalArgumentException("세자리 수를 입력해주세요.")
             }
-        }
 
-        var ball = 0
-        var strike = 0
 
-        for (j in 0..2) {
-            for (i in 0..2) {
-                if (computerNum[j] == playerNum[i]) {
-                    if (i == j)
-                        strike++
-                    else
-                        ball++
+            var ball = 0
+            var strike = 0
+
+            for (j in 0..2) {
+                for (i in 0..2) {
+                    if (computerNum[j] == playerNum[i]) {
+                        if (i == j)
+                            strike++
+                        else
+                            ball++
+                    }
                 }
             }
-        }
 
-        if (ball == 0) {
-            if (strike == 0)
-                println("낫싱")
-            else
-                println("${strike}스트라이크")
-        }
-
-        if (strike == 0) {
-            if (ball != 0) {
-                println("${ball}볼")
+            if (ball == 0) {
+                if (strike == 0)
+                    println("낫싱")
+                else
+                    println("${strike}스트라이크")
             }
+
+            if (strike == 0) {
+                if (ball != 0) {
+                    println("${ball}볼")
+                }
+            }
+
+            if (ball != 0 && strike != 0)
+                println("${ball}볼 ${strike}스트라이크")
         }
 
-        if (ball != 0 && strike != 0)
-            println("${ball}볼 ${strike}스트라이크")
+
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        chooseGameNum = Console.readLine().toInt()
     }
-
-    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    chooseGameNum = Console.readLine().toInt()
-
 }
