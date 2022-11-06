7d39cdfb91ae174381eb68f08bac13fd834af3fe
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-11-06 18:26:01 +0900

    move(BoogiWoogiTest) : 작성한 test code를 ApplicationTest로 이동

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -27,6 +27,76 @@ class ApplicationTest : NsTest() {
         }
     }
 
+    @Test
+    fun `사용자의 입력이 숫자가 아닌 경우 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            runException("우테코 합격기원")
+        }
+    }
+
+    @Test
+    fun `사용자의 입력에 0이 포함된 숫자가 있는 경우 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            runException("103")
+        }
+    }
+
+    @Test
+    fun `사용자의 입력에 중복된 숫자가 있는 경우 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            runException("113")
+        }
+    }
+
+    @Test
+    fun `사용자의 입력이 세자리가 아닌 경우 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            runException("7777")
+        }
+    }
+
+    @Test
+    fun `컴퓨터가 발생시킨 임의의 숫자 리스트 크기가 3인지 확인`() {
+        Computer.generateNewRandomNumbers()
+        assertThat(Computer.randomNumbers.size).isEqualTo(3)
+    }
+
+    @Test
+    fun `스트라이크된 숫자의 개수가 하나인 경우`() {
+        Computer.randomNumbers = arrayListOf(3, 5, 4)
+        assertThat(BaseballGameReferee.calcStrikeCount(arrayListOf(4, 5, 1))).isEqualTo(1)
+    }
+
+    @Test
+    fun `스트라이크된 숫자의 개수가 두개인 경우`() {
+        Computer.randomNumbers = arrayListOf(1, 5, 4)
+        assertThat(BaseballGameReferee.calcStrikeCount(arrayListOf(1, 2, 4))).isEqualTo(2)
+    }
+
+    @Test
+    fun `스트라이크된 숫자의 개수가 세개인 경우`() {
+        Computer.randomNumbers = arrayListOf(1, 5, 4)
+        assertThat(BaseballGameReferee.calcStrikeCount(arrayListOf(1, 5, 4))).isEqualTo(3)
+    }
+
+    @Test
+    fun `볼이된 숫자의 개수가 하나인 경우`() {
+        Computer.randomNumbers = arrayListOf(3, 5, 4)
+        assertThat(BaseballGameReferee.calcBallCount(arrayListOf(4, 5, 1))).isEqualTo(1)
+    }
+
+    @Test
+    fun `볼이된 숫자의 개수가 두개인 경우`() {
+        Computer.randomNumbers = arrayListOf(1, 5, 4)
+        assertThat(BaseballGameReferee.calcBallCount(arrayListOf(4, 2, 1))).isEqualTo(2)
+    }
+
+    @Test
+    fun `볼이된 숫자의 개수가 세개인 경우`() {
+        Computer.randomNumbers = arrayListOf(4, 5, 1)
+        assertThat(BaseballGameReferee.calcBallCount(arrayListOf(5, 1, 4))).isEqualTo(3)
+    }
+
     override fun runMain() {
         main()
     }
