2ed8a1770b07ca92209d2f6ddf31799f09941e22
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-07 01:18:12 +0900

    move(Application) : baseballgame을 진행을 담당하는 함수를 BaseballGameProcessor로 이동

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,28 +1,4 @@
 package baseball
 
-fun processBaseballGame() {
-    Computer.generateNewRandomNumbers()
-    ScreenManipulator.printGameStart()
-
-    while (true) {
-        val userInput = User.guessNumbers()
-
-        if(!BaseballGameReferee.checkIsValid(userInput)){
-            throw java.lang.IllegalArgumentException("입력 오류입니다.")
-        }
-        val guessedNumbers = User.toBaseballNumbers(userInput)
-        val (strikeCount, ballCount) = arrayOf(BaseballGameReferee.calcStrikeCount(guessedNumbers),
-            BaseballGameReferee.calcBallCount(guessedNumbers))
-
-        when (BaseballGameReferee.decideEachTurn(strikeCount, ballCount)) {
-            GameStatus.TERMINATE -> break
-            GameStatus.NEW_GAME -> Computer.generateNewRandomNumbers()
-            GameStatus.ERROR -> throw IllegalArgumentException("입력 오류입니다.")
-            else -> continue
-        }
-    }
-}
-
 fun main() {
-    processBaseballGame()
 }
---
+++ src/main/kotlin/baseball/BaseballGameProcessor.kt
@@ -0,0 +1,36 @@
+package baseball
+
+class BaseballGameProcessor(private val user: User, private val computer: Computer) {
+    fun baseballGameProcess() {
+        ScreenManipulator.printGameStart()
+
+        while (true) {
+            val userInput = user.guessNumbers()
+
+            if (!Validator.checkIsValid(userInput)) {
+                throw java.lang.IllegalArgumentException("입력 오류입니다.")
+            }
+
+            val guessedNumbers = userInput.toBaseballNumbers()
+            val (strikeCount, ballCount) = arrayOf(BSCounter.calcStrikeCount(guessedNumbers),
+                BSCounter.calcBallCount(guessedNumbers))
+
+            when (BaseballGameReferee.decideEachTurn(strikeCount, ballCount)) {
+                GameStatus.TERMINATE -> break
+                GameStatus.NEW_GAME -> Computer.generateNewRandomNumbers()
+                GameStatus.ERROR -> throw IllegalArgumentException("입력 오류입니다.")
+                else -> continue
+            }
+        }
+    }
+
+    private fun String.toBaseballNumbers(): ArrayList<Int> {
+        val guessedNumbers = ArrayList<Int>()
+
+        this.forEach { eachNum ->
+            guessedNumbers.add(Character.getNumericValue(eachNum))
+        }
+        return guessedNumbers
+    }
+
+}
