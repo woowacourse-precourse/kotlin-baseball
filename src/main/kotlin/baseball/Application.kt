0928982f1aa670156b2860b186a27e5259431de0
healthykim <bsbs8645@snu.ac.kr>
2022-11-06 19:25:19 +0900

    feat(inputDevice): Add function for input validation
    Add isValidInput function in InputDevice class for input validation.
    Use memoization from line 18 to 22, because of indent limitation.
    Use Exception as global val variable for easy modification.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,28 @@
 package baseball
 
+import java.lang.IllegalArgumentException
+
+val EXCEPTION = IllegalArgumentException()
+
+class InputDevice {
+    private fun isValidInput(input: String) {
+        // input should be length 3
+        if(input.length != 3) throw EXCEPTION
+        // input should be composed by numbers
+        for(c in input) {
+            if(!c.isDigit()) throw EXCEPTION
+        }
+        // input digits must be placed between 1 to 9
+        if(input.contains('0')) throw EXCEPTION
+        // input digits must be different from each other
+        val appeared = MutableList(10) {false}
+        for(c in input) {
+            if(appeared[c.digitToInt()]) throw EXCEPTION
+            appeared[c.digitToInt()] = true
+        }
+    }
+}
+
 fun main() {
     TODO("프로그램 구현")
 }
