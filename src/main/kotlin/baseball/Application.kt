8f598275ef5cd954b134036cbe008f6bbb73a5ba
wonseok <dnjstjr245@gmail.com>
2022-11-06 10:05:58 +0900

    :sparkles: 플레이어가 입력한 서로 다른 3개의 숫자에 대한 결과 판단 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,9 +2,11 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
 
-var randomGeneratedNumber: Int? = 0
+var randomGeneratedNumber: String = ""
 fun main() {
     generateRandomNumber()
+    println("답 : $randomGeneratedNumber")
+    printGuidelinePhraseAndWaitForInput()
 }
 
 fun generateRandomNumber() {
@@ -15,6 +17,52 @@ fun generateRandomNumber() {
             Randoms.pickNumberInRange(1, 9)
         )
     }
+    randomGeneratedNumber = randomNumbersSet.joinToString("")
+}
 
-    randomGeneratedNumber = randomNumbersSet.joinToString("").toIntOrNull()
+fun printGuidelinePhraseAndWaitForInput() {
+    print("숫자를 입력해주세요 : ")
+    val userInput = readln()
+}
+
+fun String.gameResult(): GameResultType {
+    // 유저
+    return if (this == randomGeneratedNumber) GameResultType.CorrectResult
+    else if (
+        !this.contains(randomGeneratedNumber[0]) &&
+        !this.contains(randomGeneratedNumber[1]) &&
+        !this.contains(randomGeneratedNumber[2])
+    ) GameResultType.NothingResult
+    else GameResultType.NormalResult(
+        countBallAndStrike(this, randomGeneratedNumber).first,
+        countBallAndStrike(this, randomGeneratedNumber).second
+    )
+}
+
+fun countBallAndStrike(userInput: String, answer: String): Pair<Int, Int> {
+    var ballCount = 0
+    var strikeCount = 0
+
+    userInput.forEachIndexed { index, userNum ->
+        if (userNum != answer[index] &&
+            answer.contains(userNum)
+        ) ballCount++
+    }
+
+    for (idx in 0 until 3) {
+        if (userInput[idx] == answer[idx]) strikeCount++
+    }
+
+    return Pair(ballCount, strikeCount)
+}
+
+sealed class GameResultType {
+    data class NormalResult(
+        val ball: Int,
+        val strike: Int
+    ) : GameResultType()
+
+    object NothingResult : GameResultType()
+
+    object CorrectResult : GameResultType()
 }
