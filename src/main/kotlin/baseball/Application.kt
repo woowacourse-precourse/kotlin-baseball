088e8cfd6c5884eff595dc6fd3224d59dd43004d
82104 <tir2986@naver.com>
2022-11-06 17:19:21 +0900

    refactor: 함수 분리 부분 롤백
    테스트를 수월하게 하기 위해서 입력 부분과 로직 부분 분리 되어야함

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -9,32 +9,29 @@ fun main() {
     while (true) {
         val computer = generateRandomThreeDigits()
 
-        while (!readAndCompare(computer).allMatched())
+        var matched = false
+        while (!matched) {
+            print("숫자를 입력해주세요 : ")
+            val user = Console.readLine()
+            validatePlayInput(user)
 
-        if (finish()) {
+            val compResult = CompResult(computer, splitUserInput(user))
+            println(compResult.toString())
+
+            matched = compResult.allMatched()
+        }
+
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val input = Console.readLine()
+        validateEndInput(input)
+        if (finish(input)) {
             break
         }
     }
 }
 
-fun readAndCompare(computer: List<Int>): CompResult {
-    print("숫자를 입력해주세요 : ")
-    val user = Console.readLine()
-    validatePlayInput(user)
-
-    val compResult = CompResult(computer, splitUserInput(user))
-    println(compResult.toString())
-
-    return compResult
-}
-
-fun finish(): Boolean {
-    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    val input = Console.readLine()
-    validateEndInput(input)
-    return input == "2"
-}
+fun finish(input: String) = input == "2"
 
 fun generateRandomThreeDigits(): List<Int> {
     val digits = mutableListOf<Int>()
