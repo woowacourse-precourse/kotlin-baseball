fb4854d960c066f2d7e082c6bea55336afa4b6c8
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-07 01:21:36 +0900

    refactor(User) : user를 object class 에서 class로 변경

--- src/main/kotlin/baseball/User.kt
+++ src/main/kotlin/baseball/User.kt
@@ -3,15 +3,6 @@ package baseball
 import camp.nextstep.edu.missionutils.Console
 import javax.swing.text.html.HTMLDocument.HTMLReader.CharacterAction
 
-object User {
+class User {
     fun guessNumbers(): String = Console.readLine()
-
-    fun toBaseballNumbers(userInput : String): ArrayList<Int> {
-        val guessedNumbers = ArrayList<Int>()
-
-        userInput.forEach { eachNum ->
-            guessedNumbers.add(Character.getNumericValue(eachNum))
-        }
-        return guessedNumbers
-    }
 }
