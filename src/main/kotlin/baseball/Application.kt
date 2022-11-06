59f7636a17248851cc915230d646e54492c3b983
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-07 00:10:53 +0900

    feat (Application.kt) : 사용자가 서로 다른 세개의 수 입력하는 코드 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -6,6 +6,9 @@ fun main() {
     println("숫자 야구 게임을 시작합니다.")
     do {
         val computerNumber = make3RandomNumber()
+        do {
+            print("숫자를 입력해주세요 : ")
+            val playerInput = Console.readLine()
 
 
 fun make3RandomNumber(): MutableList<Int> {
