566b75c176380b138fb841c192dbed875fae2f5d
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-07 00:48:51 +0900

    test (MyTest.kt) : 볼, 스트라이크 개수 확인하는 countBallStrike 함수 테스트 코드 추가

--- src/test/kotlin/baseball/MyTest.kt
+++ src/test/kotlin/baseball/MyTest.kt
@@ -32,6 +32,41 @@ class MyTest : NsTest() {
         }
     }
 
+
+    @Nested
+    inner class `볼, 스트라이크 개수 확인` {
+        @Test
+        fun case1() {
+            val result = countBallStrike(listOf(1, 2, 3), listOf(4, 5, 6))
+            assertThat(result).isEqualTo(listOf(0, 0))
+        }
+
+        @Test
+        fun case2() {
+            val result = countBallStrike(listOf(1, 2, 3), listOf(3, 4, 5))
+            assertThat(result).isEqualTo(listOf(1, 0))
+        }
+
+        @Test
+        fun case3() {
+            val result = countBallStrike(listOf(1, 2, 3), listOf(1, 3, 2))
+            assertThat(result).isEqualTo(listOf(2, 1))
+        }
+
+
+        @Test
+        fun case4() {
+            val result = countBallStrike(listOf(1, 2, 3), listOf(1, 2, 5))
+            assertThat(result).isEqualTo(listOf(0, 2))
+        }
+
+        @Test
+        fun case5() {
+            val result = countBallStrike(listOf(1, 2, 3), listOf(1, 2, 3))
+            assertThat(result).isEqualTo(listOf(0, 3))
+        }
+    }
+
     override fun runMain() {
         main()
     }
