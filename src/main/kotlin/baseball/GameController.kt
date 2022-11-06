60f455ddee5c72b5ff8553acbd16b16677533896
Joonhoo <mnc5@naver.com>
2022-11-06 23:23:36 +0900

    feat: 스트라이크, 볼 개수 구하는 함수 추가

--- src/main/kotlin/baseball/GameController.kt
+++ src/main/kotlin/baseball/GameController.kt
@@ -13,4 +13,26 @@ object GameController {
             throw IllegalArgumentException()
         }
     }
+
+    // 스트라이크 -> 위치와 숫자가 모두 같음
+    fun getStrikeCount(computer: List<Int>, user: List<Int>): Int {
+        return user.filterIndexed { index, number ->
+            computer[index] == number
+        }.count()
+    }
+
+    // 볼 -> 위치는 다르지만 숫자가 포함
+    fun getBallCount(computer: List<Int>, user: List<Int>): Int {
+        var ball = 0
+        // 위치와 숫자가 같은 부분 필터링
+        val filterList = computer.filterIndexed { index, computerNumber ->
+            user[index] != computerNumber
+        }
+        user.forEach { number ->
+            if (filterList.contains(number)) {
+                ball++
+            }
+        }
+        return ball
+    }
 }
