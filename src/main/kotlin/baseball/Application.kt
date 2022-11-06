1947a331afe6c13ce802b17d60eb9edf3c0b2ae7
idra79haza <idra79haza@github.com>
2022-11-07 01:18:29 +0900

    fix(checkStrikeBallNothing): change nothing error
    change nothing error. "nothing" happens everytime if one element is
    added. check nothing at the first time to avoid duplicate happening of
    "nothing"

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,14 +4,16 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    val input = inputNum()
+    println("숫자 야구 게임을 시작합니다.")
+    print("숫자를 입력해주세요 : ")
+    var input = inputNum()
     rightInputCheck(input)
     val computerNum = pickRandomNums()
-    val result = checkStrikeBallNothing(input, computerNum)
+    var result = checkStrikeBallNothing(input, computerNum)
     while(result)
     {
-        val input = inputNum()
-        val result = checkStrikeBallNothing(input, computerNum)
+        input = inputNum()
+        result = checkStrikeBallNothing(input, computerNum)
     }
     println("result: $result")
 }
@@ -49,42 +51,54 @@ fun checkStrikeBallNothing(inputNum: String, computerNum: MutableList<String>): 
     }
     if(pickRandomNumsResult == inputNum) {
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         gameRestartCheck(inputNum())
-        return false
     }
 
     // 게임 종료가 아닐 경우 낫싱 -> 스트라이크 -> 볼 순으로 구분
+    var numForNothing: Int = 0
     for (index in inputNum.indices) {
-        if (computerNum.contains(inputNum[index].toString())) {
-            var strikeNum = 0
-            var ballNum = 0
-            for (i in inputNum.indices) { // 스트라이크부터 먼저 체크하고 제거하기
-                if (inputNum[i].toString() == computerNum[i]) {
-                    strikeNum += 1
-                    computerNum.removeAt(i)
-                    inputNum.replace("${inputNum[i]}", "")
+        if (!computerNum.contains(inputNum[index].toString())) {
+            numForNothing += 1
+        }
+    }
+    if (numForNothing == 3) {
+        println("낫싱")
+    } else { // 스트라이크 -> 볼
+        for (index in inputNum.indices) {
+            if (computerNum.contains(inputNum[index].toString())) {
+                var strikeNum = 0
+                var ballNum = 0
+                val indexSize = inputNum.length
+                for (i in 0 until indexSize) { // 스트라이크부터 먼저 체크하고 제거하기
+                    println("inputNum: ${inputNum[index]}")
+                    if (inputNum[i].toString() == computerNum[i]) {
+                        strikeNum += 1
+                        computerNum.removeAt(i)
+                        inputNum.replace("${inputNum[i]}", "")
+                    }
                 }
-            }
-            for (j in inputNum.indices) { // 남은 것에서 볼 체크하기
-                if (computerNum.contains(inputNum[j].toString())) {
-                    ballNum += 1
+                for (j in 0 until indexSize) { // 남은 것에서 볼 체크하기
+                    if (computerNum.contains(inputNum[j].toString())) {
+                        ballNum += 1
+                    }
                 }
-            }
-            if (strikeNum != 0 && ballNum != 0) {
-                println("${ballNum}볼 ${strikeNum}스트라이크")
+                if (strikeNum != 0 && ballNum != 0) {
+                    println("${ballNum}볼 ${strikeNum}스트라이크")
 //                    return "${ballNum}볼 ${strikeNum}스트라이크"
-            } else if (strikeNum == 0 && ballNum != 0) {
-                println("${ballNum}볼")
+                } else if (strikeNum == 0 && ballNum != 0) {
+                    println("${ballNum}볼")
 //                    return "${ballNum}볼"
-            } else {
-                println("${strikeNum}스트라이크")
+                } else {
+                    println("${strikeNum}스트라이크")
 //                    return "${strikeNum}스트라이크"
+                }
             }
         }
-        println("낫싱")
-//            return "낫싱"
     }
-    println("숫자를 입력해주세요")
+
+    // return "낫싱"
+    print("숫자를 입력해주세요 : ")
     // return "숫자를 입력해주세요"
     return true
 }
