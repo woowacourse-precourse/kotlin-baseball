2b17a05d3c1fb0e1f90881a777dfecee1be50430
cyjadela <adelacho@naver.com>
2022-11-06 20:52:24 +0900

    refactor: 게임 진행 여부 함수로 추출

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,9 +5,10 @@ import camp.nextstep.edu.missionutils.Randoms
 
 var ball = 0
 var strike = 0
+var chooseGameNum = 1
 
 fun main() {
-    var chooseGameNum = 1
+    chooseGameNum = 1
     println("숫자 야구 게임을 시작합니다.")
 
     while (chooseGameNum == 1) {
@@ -18,9 +19,7 @@ fun main() {
             getScore(computerNum, playerNum)
             printResult(ball, strike)
         }
-
-        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        chooseGameNum = Console.readLine().toInt()
+        gameAgainOrExit()
     }
 }
 
@@ -84,6 +83,11 @@ fun printResult(ball: Int, strike: Int) {
 
     if (ball != 0 && strike != 0)
         println("${ball}볼 ${strike}스트라이크")
+}
+
+fun gameAgainOrExit() {
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    chooseGameNum = Console.readLine().toInt()
 }
 
 fun checkNum(playerNum: String) {
