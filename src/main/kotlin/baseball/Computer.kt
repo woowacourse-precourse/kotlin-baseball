057ccdc22c849572424b0a5c060aa8b6d6d56b19
EmilyCh0 <seoyeon3089@naver.com>
2022-11-06 21:38:44 +0900

    refactor(Computer): 하드 코딩 값 상수로 분리

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -2,13 +2,21 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
 
+private const val NOTHING_MESSAGE = "낫싱"
+private const val BALL_MESSAGE = "볼"
+private const val STRIKE_MESSAGE = "스트라이크"
+private const val CORRECT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+const val ANSWER_COUNT = 3
+const val MIN_NUMBER = 1
+const val MAX_NUMBER = 9
+
 class Computer {
     private var answer = arrayListOf<Int>()
 
     fun generateAnswer() {
         answer.clear()
-        while (answer.size < 3) {
-            val randomNumber = Randoms.pickNumberInRange(1, 9)
+        while (answer.size < ANSWER_COUNT) {
+            val randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER)
             if (!answer.contains(randomNumber)) {
                 answer.add(randomNumber)
             }
@@ -21,21 +29,21 @@ class Computer {
         val result = StringBuilder()
 
         if (ballCount == 0 && strikeCount == 0) {
-            result.append("낫싱")
+            result.append(NOTHING_MESSAGE)
             println(result.toString())
             return true
         }
         if (ballCount > 0) {
-            result.append(ballCount).append("볼")
+            result.append(ballCount).append(BALL_MESSAGE)
             if (strikeCount > 0) {
                 result.append(' ')
             }
         }
         if (strikeCount > 0) {
-            result.append(strikeCount).append("스트라이크")
+            result.append(strikeCount).append(STRIKE_MESSAGE)
         }
-        if (strikeCount==3){
-            result.append('\n').append("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        if (strikeCount == ANSWER_COUNT) {
+            result.append('\n').append(CORRECT_MESSAGE)
             println(result.toString())
             return false
         }
@@ -45,7 +53,7 @@ class Computer {
 
     fun countStrike(userGuess: List<Int>): Int {
         var strikeCount = 0
-        for (index in 0..2) {
+        for (index in 0 until ANSWER_COUNT) {
             if (answer[index] == userGuess[index]) {
                 strikeCount++
             }
@@ -55,7 +63,7 @@ class Computer {
 
     fun countBall(userGuess: List<Int>): Int {
         var ballCount = 0
-        for (index in 0..2) {
+        for (index in 0 until ANSWER_COUNT) {
             if (answer[index] == userGuess[index]) continue
             if (answer.contains(userGuess[index])) {
                 ballCount++
