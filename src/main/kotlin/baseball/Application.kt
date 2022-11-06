2f48da96b68f461dbf5aecd06693e15d21b1e2cc
inseonyun <yuninseon@naver.com>
2022-11-06 23:44:26 +0900

    feat: 상대방 숫자 생성 함수 구현
    Randoms 라이브러리 사용해 상대방 3자리 숫자 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
+fun createOpponentData(): String {
+    var opponentData = ""
+
+    while(opponentData.length < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if(!opponentData.contains(randomNumber.toString()))
+            opponentData += randomNumber.toString()
+    }
+
+    return opponentData
+}
+
 fun main() {
     TODO("프로그램 구현")
 }
