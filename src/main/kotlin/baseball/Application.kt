557313a5e2edb0e4af96f0b2636e9d84d484020a
cyjadela <adelacho@naver.com>
2022-11-06 21:59:27 +0900

    refactor: Calculate calss로 분리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,13 +1,12 @@
 package baseball
 
-import camp.nextstep.edu.missionutils.Console
-
 var ball = 0
 var strike = 0
 var chooseGameNum = 1
 
 fun main() {
     var number = Number()
+    var calculate = Calculate()
     chooseGameNum = 1
     println("숫자 야구 게임을 시작합니다.")
 
@@ -16,54 +15,11 @@ fun main() {
         var playerNum = ""
         while (playerNum != computerNum) {
             playerNum = number.getPlayerNum()
-            getScore(computerNum, playerNum)
-            printResult(ball, strike)
+            calculate.getScore(computerNum, playerNum)
+            calculate.printResult(ball, strike)
         }
-        gameAgainOrExit()
+        calculate.gameAgainOrExit()
     }
-}
-
-
-fun getScore(computerNum: String, playerNum: String) {
-    ball = 0
-    strike = 0
-    for (j in 0..2) {
-        for (i in 0..2) {
-            compareNum(computerNum, playerNum, i, j)
-        }
-    }
-}
-
-fun compareNum(computerNum: String, playerNum: String, i: Int, j: Int) {
-    if (computerNum[j] == playerNum[i]) {
-        if (i == j)
-            strike++
-        else
-            ball++
-    }
-}
-
-fun printResult(ball: Int, strike: Int) {
-    if (ball == 0) {
-        if (strike == 0)
-            println("낫싱")
-        else
-            println("${strike}스트라이크")
-    }
-
-    if (strike == 0) {
-        if (ball != 0) {
-            println("${ball}볼")
-        }
-    }
-
-    if (ball != 0 && strike != 0)
-        println("${ball}볼 ${strike}스트라이크")
-}
-
-fun gameAgainOrExit() {
-    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    chooseGameNum = Console.readLine().toInt()
 }
 
 fun checkNum(playerNum: String) {
