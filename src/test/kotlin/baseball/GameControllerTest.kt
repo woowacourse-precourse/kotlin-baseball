db5ca516dbd5cd703ad45ad14fa587ea5d0b3e84
Joonhoo <mnc5@naver.com>
2022-11-06 23:25:40 +0900

    test: 스트라이크, 볼 각 1,2,3개 테스트 완료

--- src/test/kotlin/baseball/GameControllerTest.kt
+++ src/test/kotlin/baseball/GameControllerTest.kt
@@ -1,5 +1,6 @@
 package baseball
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -26,5 +27,53 @@ class GameControllerTest {
         assertThrows<IllegalArgumentException> {
             GameController.exception(userList)
         }
+    }
+
+    @Test
+    fun `스트라이크 카운트 1 확인`() {
+        val computer = listOf(1, 2, 3)
+        val user = listOf(1, 3, 2)
+        val strike = GameController.getStrikeCount(computer, user)
+        assertThat(strike).isEqualTo(1)
+    }
+
+    @Test
+    fun `스트라이크 카운트 2 확인`() {
+        val computer = listOf(1, 2, 3)
+        val user = listOf(1, 2, 4)
+        val strike = GameController.getStrikeCount(computer, user)
+        assertThat(strike).isEqualTo(2)
+    }
+
+    @Test
+    fun `스트라이크 카운트 3 확인`() {
+        val computer = listOf(1, 2, 3)
+        val user = listOf(1, 2, 3)
+        val strike = GameController.getStrikeCount(computer, user)
+        assertThat(strike).isEqualTo(3)
+    }
+
+    @Test
+    fun `볼 카운트 1 확인`() {
+        val computer = listOf(1, 2, 3)
+        val user = listOf(1, 3, 4)
+        val ball = GameController.getBallCount(computer, user)
+        assertThat(ball).isEqualTo(1)
+    }
+
+    @Test
+    fun `볼 카운트 2 확인`() {
+        val computer = listOf(1, 2, 3)
+        val user = listOf(3, 1, 4)
+        val ball = GameController.getBallCount(computer, user)
+        assertThat(ball).isEqualTo(2)
+    }
+
+    @Test
+    fun `볼 카운트 3 확인`() {
+        val computer = listOf(1, 2, 3)
+        val user = listOf(3, 1, 2)
+        val ball = GameController.getBallCount(computer, user)
+        assertThat(ball).isEqualTo(3)
     }
 }
