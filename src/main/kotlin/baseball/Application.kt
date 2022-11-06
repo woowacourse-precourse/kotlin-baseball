caf1598b4e1e208bc738c6bcc31e439e2925d849
sojk401 <sojk401@gmail.com>
2022-11-06 23:29:48 +0900

    feat: 중복되지 않는 랜덤 숫자 생성
    pickNumberInRange 메소드를 사용하여 랜덤한 숫자를 뽑고, 중복되지 않으면 리스트에 추가
    리스트의 길이가 매개변수 length 와 같으면 리스트를 반환

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,20 @@
 package baseball
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    TODO("프로그램 구현")
+    var computerNumber = makeRandomNumber(3)
+    print("랜덤숫자 : ${computerNumber}")
+}
+
+fun makeRandomNumber(length: Int) : MutableList<Int> {
+    var noOverlapRandomNumber = mutableListOf<Int>()
+
+    while (noOverlapRandomNumber.size < 3){
+        var randNum = Randoms.pickNumberInRange(1, 9)
+        if (randNum !in noOverlapRandomNumber){
+            noOverlapRandomNumber.add(randNum)
+        }
+    }
+    return noOverlapRandomNumber
 }
