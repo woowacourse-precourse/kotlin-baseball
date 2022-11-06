7fa5dc84d3217964ef886f8aed093932ca2c429a
sojk401 <sojk401@gmail.com>
2022-11-07 00:39:40 +0900

    feat: 입력받은 플레이어 숫자에 대한 예외처리
    플레이어의 입력이 숫자로 이루어지지 않은 경우, 길이가 3이 아닌 경우, 중복된 숫자를 입력한 경우에 대해 throw를 통해 IllegalAccessException 를 발생

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,8 +3,17 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
+    println("숫자 야구 게임을 시작합니다.")
+
     var computerNumber = makeRandomNumber(3)
-    print("랜덤숫자 : ${computerNumber}")
+    println("랜덤숫자 : ${computerNumber}")
+
+    while (true){
+        print("숫자를 입력해주세요 : ")
+        var userInput = Console.readLine()
+        println(userInput)
+        checkNumberException(userInput)
+    }
 }
 
 fun makeRandomNumber(length: Int) : MutableList<Int> {
@@ -17,4 +26,23 @@ fun makeRandomNumber(length: Int) : MutableList<Int> {
         }
     }
     return noOverlapRandomNumber
+}
+
+fun checkNumberException(userInput: String){
+    if (userInput.toIntOrNull() == null){
+        throw IllegalArgumentException("IllegalAccessException. 프로그램을 종료합니다.")
+    }
+    else if (userInput.length != 3){
+        throw IllegalArgumentException("IllegalAccessException. 프로그램을 종료합니다.")
+    }
+
+    var userNumber = mutableListOf<String>()
+    for (i in userNumber){
+        if (i !in userNumber){
+            userNumber.add(i)
+        }
+        else{
+            throw IllegalArgumentException("IllegalAccessException. 프로그램을 종료합니다.")
+        }
+    }
 }
