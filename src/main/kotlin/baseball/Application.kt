d38b3961c42ca85891c9ef99a9dd56d56f872dea
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-11-06 18:51:32 +0900

    feat: 추가 서로 다른 세자리 난수 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
+    val computerNumber = randomNumber()
     TODO("프로그램 구현")
+}
+
+fun randomNumber(): MutableList<Int> {
+    val computerNumber = mutableListOf<Int>()
+    while (computerNumber.size < 3) {
+        val number = Randoms.pickNumberInRange(1, 9)
+        if (!computerNumber.contains(number)) {
+            computerNumber.add(number)
+        }
+    }
+    return computerNumber
 }
