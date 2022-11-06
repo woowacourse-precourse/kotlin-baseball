9eba928cc46fea0ecfe1aec89f29d6549c13ad10
WoeunJung <leena09123@naver.com>
2022-11-06 23:06:48 +0900

    feat: add printResult function that prints hint message

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,9 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     val randomValue = createRandomValue()
     val inputValue  = createInputValue()
-
-    println(randomValue)
-    println(inputValue)
+    val compared = compareNumber(randomValue, inputValue)
+    printResult(compared)
 }
 
 fun createRandomValue(): List<Int> {
@@ -41,4 +40,19 @@ fun compareNumber(
             ball += 1
     }
     return Pair(ball, strike)
+}
+
+fun printResult(result: Pair<Int, Int>){
+    val ball   = result.first
+    val strike = result.second
+    var hintMessage = ""
+
+    if (ball != 0)
+        hintMessage += "${ball}볼 "
+    if (strike != 0)
+        hintMessage += "${strike}스트라이크"
+
+    if (hintMessage.isBlank()) hintMessage = "낫싱"
+
+    println(hintMessage)
 }
