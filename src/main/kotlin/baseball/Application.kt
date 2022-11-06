33c9481ff2e1ecbf0c3b5da7cd772e254fc6c49d
윤동현 <donghyunyoon81@gmail.com>
2022-11-06 14:47:30 +0900

    refactor:Change getComputerNumber to getRandomNumbers  컴퓨터의 무작위 값을 구하기만 하는 함수는 재사용성이 떨어진다고 생각하여 getRandomNubers로변경하고 return 값도 Randoms 클래스를 분석하여 짧고 알아보기 쉽도록 리팩토링하였습니다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,18 +1,14 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
 
 fun main() {
-    print(getComputerNumbers())
+    print(getRandomNumbers(1,9,3))
+
 }
 
-fun getComputerNumbers(): List<Int> {
-    val computer = mutableListOf<Int>()
-    while (computer.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber)
-        }
-    }
-    return computer
+fun getRandomNumbers(randomStartNumber:Int,randomEndNumber:Int,randomsLength:Int): List<Int> {
+    return Randoms.pickUniqueNumbersInRange(randomStartNumber,randomEndNumber,randomsLength)
 }
