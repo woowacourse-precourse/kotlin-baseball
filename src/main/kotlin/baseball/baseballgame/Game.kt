7382f3b8d2cf2c8ca75158e00cbc27169679a107
jiingom <vvankatam0@gmail.com>
2022-11-06 23:44:14 +0900

    feat: 볼과 스트라이크의 개수를 구하는 기능 구현 및 숫자 야구 게임이 실행되는 로직 구현
    3개의 숫자를 모두 맞혀 승리한 경우 게임이 종료, 재시작 할지 말지 결정할 수 있음

--- src/main/kotlin/baseball/baseballgame/Game.kt
+++ src/main/kotlin/baseball/baseballgame/Game.kt
@@ -1,8 +1,29 @@
 package baseball.baseballgame
 
+import baseball.util.createRandomNumber
+import baseball.util.userInput
 import camp.nextstep.edu.missionutils.Console
 
 class Game {
+    fun startGame() {
+        var flag = true
+        println("숫자 야구 게임을 시작합니다.")
+        while (flag) {
+            gameController()
+            flag = newGameStart()
+        }
+    }
+
+    private fun gameController() {
+        val computerNum = createRandomNumber()
+        var flag = false
+        val list = computerNum.toList()
+        while (!flag) {
+            print("숫자를 입력해주세요 : ")
+            flag = checkBallOrStrike(list)
+        }
+    }
+
     fun newGameStart(): Boolean {
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 
@@ -15,5 +36,26 @@ class Game {
             return false
         }
         return throw IllegalArgumentException()
+    }
+
+    private fun checkBallOrStrike(comNumList: List<Char>): Boolean {
+        val userInput = userInput()
+        var ball = 0
+        var strike = 0
+
+        for (i in userInput.indices) {
+            if (comNumList[i] == userInput[i]) {
+                strike += 1
+            } else if (comNumList.contains(userInput[i])) {
+                ball += 1
+            }
+        }
+
+        if (strike == 3) {
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            return true
+        }
+
+        return false
     }
 }
