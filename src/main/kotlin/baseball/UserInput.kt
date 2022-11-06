c899bf1c73f1c1daa173c3b060bf51c352cbaf68
jwson <sonjungwoo9@gmail.com>
2022-11-06 15:59:25 +0900

    [feat] 1 - 9 사이의 수가 아닌 수를 입력했을 때, IllegalArgumentException Error를 발생시킨다.

--- src/main/kotlin/baseball/UserInput.kt
+++ src/main/kotlin/baseball/UserInput.kt
@@ -18,15 +18,17 @@ fun inputUserNumber(): MutableList<Int> {
     return numbers
 }
 
-fun validate_3digit(numbers : MutableList<Int>){
+fun validate_3digit(numbers: MutableList<Int>) {
     if (numbers.count() != 3) {
         throw IllegalArgumentException("3자리 수만 입력 가능합니다.")
     }
 }
 
-fun validate_Number9to1(numbers : MutableList<Int>){
-    if (numbers.count() != 3) {
-        throw IllegalArgumentException("3자리 수만 입력 가능합니다.")
+fun validate_Number9to1(numbers: MutableList<Int>) {
+    for (number in numbers) {
+        if (number > 9 || number < 1) {
+            throw IllegalArgumentException("1에서 9까지의 수만 입력 가능합니다.")
+        }
     }
 }
 
