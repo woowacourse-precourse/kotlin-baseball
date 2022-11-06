5a7c8942f17ed4224ac0b82b37a3d4994fff3e8a
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-06 15:37:23 +0900

    fix: fix numberFormatting function Fix numberFormatting function err in number[1] assignment

--- src/main/kotlin/baseball/logic/User.kt
+++ src/main/kotlin/baseball/logic/User.kt
@@ -6,7 +6,7 @@ class User {
     fun numberFormatting(intNum: Int){
         this.number = IntArray(3)
         number[0] = intNum / 100
-        number[1] = (intNum - number[0]) / 10
+        number[1] = (intNum - number[0]*100) / 10
         number[2] = intNum % 10
     }
 
