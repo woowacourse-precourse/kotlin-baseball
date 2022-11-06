8c06418e88814c68b2fe7830a104642b336884f5
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-06 20:18:53 +0900

    refactor : 게임 오버 로직을 Computer -> Game으로 이동
    Computer는 정답값 발급 및 힌트제공 로직을, Game는 게임이 진행되는 과정을 관리한다.

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 object Computer {
-    const val INPUT_MAX_SIZE = 3
     private const val INPUT_MAX_VALUE = 9
     private const val INPUT_MIN_VALUE = 1
-    private const val INPUT_GAME_RESTART = "1"
-    private const val INPUT_GAME_STOP = "2"
+    const val INPUT_MAX_SIZE = 3
+    const val INPUT_GAME_RESTART = "1"
+    const val INPUT_GAME_STOP = "2"
     private var strike = 0
     private var ball = 0
     private val correctNumber = mutableListOf<Int>()
@@ -46,7 +46,7 @@ object Computer {
         } else if (strike > 0 && ball == 0) {
             println("${strike}스트라이크")
             if (strike == 3) {
-                gameOver()
+                Game.gameOver()
             }
         } else if (strike > 0 && ball > 0) {
             println("${ball}볼 ${strike}스트라이크")
@@ -56,23 +56,5 @@ object Computer {
     fun resetScore() {
         strike = 0
         ball = 0
-    }
-
-    private fun gameOver() {
-        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-        checkGameRestartOrStop()
-    }
-
-    private fun checkGameRestartOrStop() {
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        var input = Console.readLine()
-        if (input == INPUT_GAME_RESTART) {
-            getCorrectRandomNumber()
-        } else if (input == INPUT_GAME_STOP) {
-            println("게임종료")
-            Game.gameStop = true
-        } else if (input != "1" && input != "2") {
-            require(false)
-        }
     }
 }
--- src/main/kotlin/baseball/Game.kt
+++ src/main/kotlin/baseball/Game.kt
@@ -73,4 +73,22 @@ object Game {
         }
         return checkFlag
     }
+
+    fun gameOver() {
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        checkGameRestartOrStop()
+    }
+
+    private fun checkGameRestartOrStop() {
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        var input = Console.readLine()
+        if (input == Computer.INPUT_GAME_RESTART) {
+            Computer.getCorrectRandomNumber()
+        } else if (input == Computer.INPUT_GAME_STOP) {
+            println("게임종료")
+            gameStop = true
+        } else if (input != "1" && input != "2") {
+            require(false)
+        }
+    }
 }
