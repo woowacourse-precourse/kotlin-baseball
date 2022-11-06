1d6064c7673e78319516f5d56414adb285f320b8
WoeunJung <leena09123@naver.com>
2022-11-06 23:56:02 +0900

    feat: Exception for UserInput

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,9 +4,18 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     val randomValue = createRandomValue()
-    val inputValue  = createInputValue()
-    val compared = compareNumber(randomValue, inputValue)
-    printResult(compared)
+    while (true) {
+        val inputValue = createInputValue()
+        if (!isValid(inputValue))
+            throw IllegalArgumentException("입력 오류")
+
+        val compared = compareNumber(randomValue, inputValue)
+        printResult(compared)
+
+        // 3strike일때 게임종료하고 새로운 숫자 입력받기
+        // 새로운 숫자가 1 또는 2인지 확인
+
+    }
 }
 
 fun createRandomValue(): List<Int> {
@@ -21,8 +30,13 @@ fun createRandomValue(): List<Int> {
 }
 
 fun createInputValue(): List<Int>{
+    val input = readLine()
+    val isDigitOnly = input!!.all {
+        it.isDigit() && it.digitToInt() > 0
+    }
+    if (!isDigitOnly) return emptyList()
 
-    return readLine()!!
+    return input
         .map { it.digitToIntOrNull() }
         .filterNotNull()
 }
@@ -55,4 +69,12 @@ fun printResult(result: Pair<Int, Int>){
     if (hintMessage.isBlank()) hintMessage = "낫싱"
 
     println(hintMessage)
+}
+
+fun isValid(inputValue: List<Int>): Boolean{
+    val isEmpty = inputValue.isEmpty()
+    if (isEmpty) return false
+
+    val listSize = inputValue.distinct().size
+    return (listSize == 3)
 }
