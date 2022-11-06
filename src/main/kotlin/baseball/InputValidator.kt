eda36a532a0078f718632fc2006792289caca707
jjiiin <wjdwlin4051@gmail.com>
2022-11-07 00:11:16 +0900

    feat(InputValidatior): 사용자 입력값의 유효성 검사

---
+++ src/main/kotlin/baseball/InputValidator.kt
@@ -0,0 +1,37 @@
+package baseball
+
+
+class InputValidator(val inputNumber: String) {
+    companion object {
+        const val NUMBER_LENGTH = 3
+    }
+
+    fun inputNumberExceptionCheck(): Boolean {
+        inputNumber.map { it.digitToInt() }.toList()
+        if (!(lengthCheck() && overlapCheck() && rangeCheck())) {
+            throw IllegalArgumentException()
+        }
+        return true
+    }
+
+    fun lengthCheck(): Boolean {
+        return inputNumber.length == NUMBER_LENGTH
+    }
+
+    fun overlapCheck(): Boolean {
+        val myset: Set<Char> = HashSet(inputNumber.toList())
+        return myset.size == NUMBER_LENGTH
+    }
+
+    fun rangeCheck(): Boolean {
+        return !inputNumber.contains("0")
+    }
+
+    fun restartNumberExceptionCheck(): Boolean {
+        val validNumber: List<Int> = listOf(1, 2)
+        if (!validNumber.contains(inputNumber.toInt())) {
+            throw IllegalArgumentException()
+        }
+        return true
+    }
+}
