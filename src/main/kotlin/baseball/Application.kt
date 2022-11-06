4e09bedf4459d74ba07e5b6d939add73fefa93a2
Lee-GS <leegs4831@naver.com>
2022-11-07 02:52:51 +0900

    숫자 입력 함수 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,14 @@
 package baseball
-
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+
 }
+fun InputNumber(): String{ //숫자 입력
+    print("숫자를 입력해주세요: ")
+    var number = readLine().toString()
+
+    return number
+}
+
+
