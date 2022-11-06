73c2f803b17552fad9ffec8c700985a5ee15d615
qhfhdls <79788971+qhfhdls@users.noreply.github.com>
2022-11-06 17:21:42 +0900

    Update startGame()
    경기 진행 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,7 +4,12 @@ import camp.nextstep.edu.missionutils.Randoms
 
 // 경기 진행 함수
 fun startGame() {
-
+    val computerNum = randomNumber()
+    while(true){
+        var playerNum = randomNumberInput()
+        var result = checkResult()
+        if (result == 1) break
+    }
 }
 
 // 서로 다른 임의의 수 3개 선택 기능
@@ -20,8 +25,8 @@ fun randomNumber(): MutableList<Int> {
 }
 
 // 서로 다른 3개의 숫자 입력받는 기능
-fun randomNumberInput() {
-
+fun randomNumberInput(): MutableList<Int> {
+    exceptionHandling()
 }
 
 // 잘못된 값을 입력할 경우 예외 처리 기능
@@ -29,8 +34,11 @@ fun exceptionHandling() {
 
 }
 
-// 정답 확인 기능
-fun checkCorrect() {
+// 결과 확인 기능
+fun checkResult(): Int {
+    checkStrike()
+    checkBall()
+    resultOutput()
 
 }
 // 스트라이크 확인 기능
@@ -44,18 +52,22 @@ fun checkBall() {
 }
 
 // 비교 결과 출력 기능
-fun gameResult() {
+fun resultOutput() {
 
 }
 
 // 게임 종료 기능
-fun endGame() {
+fun endGame(): Int {
 
 }
 
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    startGame()
+    var result = 1
+    while(result == 1){
+        startGame()
+        result = endGame()
+    }
 
 }
