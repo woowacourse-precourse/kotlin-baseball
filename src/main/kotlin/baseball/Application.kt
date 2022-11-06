53009bb04d7f1376b93e8b3ccfaa6de4c6475189
ParkSeungMin <qazwsxedc1226@gmail.com>
2022-11-06 22:26:45 +0900

    Feat: [0]게임시작 문구 구현, [6]재시작/종료를 위한 반복문 생성
    최초 게임 시작 시 게임 시작 문구와 게임이 종료되었을 때, 재시작과 종료를 위한
    반복문을 만들었습니다.[완]
    반복문안에는 전체 게임을 진행할 함수를 호출하였고,해당 함수 구현은 아직 진행하지 않았습니다.[미완]
    
    참고 기능 구현 목록: 0번, 6번

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,18 @@
 package baseball
 
+private const val GAMESTART=1
+private const val GAMEEND=2
+
 fun main() {
-    TODO("프로그램 구현")
+    var userChoice=1
+    println("숫자 야구 게임을 시작합니다.")
+
+    while(userChoice==GAMESTART){
+        userChoice= entireGame()
+    }
+}
+
+fun entireGame():Int {
+
+    return 0
 }
