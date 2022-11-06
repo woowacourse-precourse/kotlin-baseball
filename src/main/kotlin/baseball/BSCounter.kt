018a8ca0d2711c072ee06aebd1e61de2ce124adf
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-07 01:23:36 +0900

    refactor(BSCounter) : computer를 class로 변경함에 따라서 인자 추가

--- src/main/kotlin/baseball/BSCounter.kt
+++ src/main/kotlin/baseball/BSCounter.kt
@@ -1,22 +1,22 @@
 package baseball
 /** ball과 strike의 개수를 세어주는 class **/
 object BSCounter {
-    fun calcStrikeCount(userInput: ArrayList<Int>): Int {
+    fun calcStrikeCount(computerNumbers:List<Int> ,userNumbers: List<Int>): Int {
         var strikeCount = 0
 
-        userInput.forEachIndexed { index, eachNum ->
-            if (Computer.randomNumbers[index] == eachNum) {
+        userNumbers.forEachIndexed { index, eachNum ->
+            if (computerNumbers[index] == eachNum) {
                 strikeCount++
             }
         }
         return strikeCount
     }
 
-    fun calcBallCount(userInput: ArrayList<Int>): Int {
+    fun calcBallCount(computerNumbers:List<Int> ,userNumbers: List<Int>): Int {
         var ballCount = 0
 
-        userInput.forEachIndexed { userNumberIndex, userNum ->
-            ballCount += Computer.randomNumbers.filterIndexed { computerNumberIndex, _ -> computerNumberIndex != userNumberIndex }
+        userNumbers.forEachIndexed { userNumberIndex, userNum ->
+            ballCount += computerNumbers.filterIndexed { computerNumberIndex, _ -> computerNumberIndex != userNumberIndex }
                 .filter { computerNumber -> computerNumber == userNum }.size
         }
         return ballCount
