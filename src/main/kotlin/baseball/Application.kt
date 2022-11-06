9e66ce23d15346da0d96da4a3b376314a7acb7a8
WoeunJung <leena09123@naver.com>
2022-11-06 21:57:35 +0900

    feat: add createRandomValue and createInputValue function

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,29 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    val randomValue = createRandomValue()
+    val inputValue  = createInputValue()
+
+    println(randomValue)
+    println(inputValue)
+}
+
+fun createRandomValue(): List<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
+
+fun createInputValue(): List<Int>{
+
+    return readLine()!!
+        .map { it.digitToIntOrNull() }
+        .filterNotNull()
 }
