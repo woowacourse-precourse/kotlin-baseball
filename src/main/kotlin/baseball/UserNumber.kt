08e35e52ea4b90eb7d7f665603c4e3b4c1159078
jjiiin <wjdwlin4051@gmail.com>
2022-11-07 00:12:13 +0900

    feat: 사용자에게서 입력값 받음

---
+++ src/main/kotlin/baseball/UserNumber.kt
@@ -0,0 +1,12 @@
+package baseball
+
+import camp.nextstep.edu.missionutils.Console
+
+class UserNumber {
+
+    fun inputNum(): String {
+        val inputNumber = Console.readLine()
+        return inputNumber
+    }
+
+}
