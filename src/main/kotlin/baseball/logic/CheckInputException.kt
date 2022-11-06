88b6fc473349a5725ac8a3d55da3817090725958
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-06 16:47:12 +0900

    feat: add inputDuplicatedChecker function Add a function that check not duplicated in user`s input

--- src/main/kotlin/baseball/logic/CheckInputException.kt
+++ src/main/kotlin/baseball/logic/CheckInputException.kt
@@ -6,5 +6,20 @@ class CheckInputException {
             throw IllegalArgumentException()
         if(input.contains("0"))
             throw IllegalArgumentException()
+        if(inputDuplicatedChecker(input.toInt()))
+            throw IllegalArgumentException()
+    }
+
+    private fun inputDuplicatedChecker(input: Int): Boolean{
+        //100의 자리와 10의 자리 비교
+        if(input/100 == (input/10) - 10)
+            return true
+        //100의 자리와 1의 자리 비교
+        if(input/100 == input%10)
+            return true
+        //10의 자리와 1의 자리 비교
+        if((input/10) - 10 == input%10)
+            return true
+        return false
     }
 }
