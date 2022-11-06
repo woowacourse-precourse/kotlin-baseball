4919520a784b1d53aa2507a9bf868f506f671799
윤동현 <donghyunyoon81@gmail.com>
2022-11-06 17:43:30 +0900

    feat: Create validateInputString  한 자릿수를 지정한 갯수만큼 입력하지 않거나 한 자릿수외에 다른 값이 입력되었을시 IllegalArgumentException을 호출하고 애플리케이션을 종료하는 기능을 구현하였습니다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,16 +5,23 @@ import camp.nextstep.edu.missionutils.Console
 import java.lang.IllegalArgumentException
 
 fun main() {
-    println(getRandomNumbers(1,9,3))
-    println(readInputString())
+    println(getRandomNumbers(1, 9, 3))
+    validateInputString(readInputString(), 3)
 
 }
 
-fun getRandomNumbers(randomStartNumber:Int,randomEndNumber:Int,randomsLength:Int): List<Int> {
-    return Randoms.pickUniqueNumbersInRange(randomStartNumber,randomEndNumber,randomsLength)
+fun getRandomNumbers(randomStartNumber: Int, randomEndNumber: Int, randomsLength: Int): List<Int> {
+    return Randoms.pickUniqueNumbersInRange(randomStartNumber, randomEndNumber, randomsLength)
 }
-fun readInputString():String {
+
+fun readInputString(): String {
     return Console.readLine()
+
+}
+
+fun validateInputString(inputNumbers: String, stringLengthToSpecify: Int) {
+    require(inputNumbers.length == stringLengthToSpecify) { "한 자릿수를 지정한 갯수만큼 입력해주세요." }
+    inputNumbers.map { number -> requireNotNull(number.digitToIntOrNull()) { "잘못된 값이 입력되어 애플리케이션이 종료됩니다." } }
 }
 
 
