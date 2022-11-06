9b0cc767cef7ae2e9f145611ba0ac7ef02fc28f9
ChanwooAn <55792553+ChanwooAn@users.noreply.github.com>
2022-11-06 16:36:34 +0900

    feat(게임 플레이) : 게임플레이 기능 추가
      2-1 플레이어가 중복없는 세자리 수를 입력한다. 이때 3자리 수가 아닐 경우 illegal argument exception을 발생시킨다.
      2-2 플레이어가 제출한 숫자에 대해서 strike count와 ball count를 센다.
      2-3 숫자를 못 맞출 경우 strike count와 ball count를 플레이어에게 출력한다.
      2-4 숫자를 맞출 경우 엔딩 멘트와 함께 게임을 종료한다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,7 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import java.lang.IllegalArgumentException
 import kotlin.properties.Delegates
 
 lateinit var answer:String
@@ -20,11 +21,65 @@ fun prepareNewGame(){
     println("숫자 야구 게임을 시작합니다.")
 }
 
+fun countBallAndStrke(playersAnswer:String):Array<Int>{
+    var strikeCount=0
+    var ballCount=0
+    val strikeAndBall= arrayOf(0,0)
+
+    for(i in 0 until 3){
+        val containment=answer.contains(playersAnswer[i])
+        if(containment && answer[i]==playersAnswer[i]){
+            strikeCount++
+        }
+        else if(containment){
+            ballCount++
+        }
+
+    }
+    strikeAndBall[0]=strikeCount
+    strikeAndBall[1]=ballCount
+
+    return strikeAndBall
+}
+fun playGame(){
+    lateinit var playerAnswer:String
+    lateinit var ballCountAndStrikeCount :Array<Int>
+    while(true){
+        playerAnswer= readln()
+        if(playerAnswer.length!=3){
+            throw IllegalArgumentException("number's digit should be 3.")
+        }//잘못된 입력일 경우 illegalargumentexception 발생
+
+        ballCountAndStrikeCount= countBallAndStrke(playerAnswer)
+
+        if(ballCountAndStrikeCount[0]==3){
+            println("3스트라이크\n" +
+                    "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"+
+                    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+            )
+            return
+        }
+
+        if(ballCountAndStrikeCount[0]==0 && ballCountAndStrikeCount[1]==0){
+            println("낫싱")
+        }else if(ballCountAndStrikeCount[0]==0){
+            println("${ballCountAndStrikeCount[1]}볼")
+        }else if(ballCountAndStrikeCount[1]==0){
+            println("${ballCountAndStrikeCount[0]}스트라이크")
+        }else{
+            println("${ballCountAndStrikeCount[1]}볼 ${ballCountAndStrikeCount[0]}스트라이크")
+        }
+    }
+
+}
+
+
 fun main() {
     var playNewGameOrStop by Delegates.notNull<Int>()
 
     do{
         prepareNewGame()
+        playGame()
 
     }while(playNewGameOrStop==1)
 
