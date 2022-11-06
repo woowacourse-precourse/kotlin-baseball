fdc5477f0cb5e013c9525983902ce4d5e0bafb8a
lsakee <lsls4868@gmail.com>
2022-11-06 18:38:14 +0900

    feat : inputUserNumber()기능 추가 -사용자가 수를 입력하는 기능 -스트링으로 반환

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -10,6 +10,7 @@ fun main() {
     println("숫자 야구 게임을 시작합니다.")
     while (true) {
 
+
     }
 }
 
@@ -26,3 +27,10 @@ fun createComputerRandomNumber(): String {
     return computerNumStringList
             .joinToString("")
 }
+
+fun inputUserNumber(): String {
+    print("숫자를 입력해주세요 : ")
+    val userInputNumber = Console.readLine()
+    return userInputNumber.toString()
+}
+
