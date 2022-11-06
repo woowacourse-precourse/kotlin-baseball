f2d0ffebef9c6f86761ea403192340c6be598d71
lsakee <lsls4868@gmail.com>
2022-11-06 18:21:46 +0900

    feat : createComputeRandoNumber() 기능 추가 -컴퓨터가 무작위 3자리수를 생성하는 기능 -중복이 아닐경우 리스트에 추가한다. -리스트를 문자열로 반환한다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,28 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 fun main() {
-    TODO("프로그램 구현")
+    var computerRandomNumber = createComputerRandomNumber()
+    println("숫자 야구 게임을 시작합니다.")
+    while (true) {
+
+    }
+}
+
+fun createComputerRandomNumber(): String {
+
+    val computerNumStringList = mutableListOf<String>()
+
+    while (computerNumStringList.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computerNumStringList.contains(randomNumber.toString())) {
+            computerNumStringList.add(randomNumber.toString())
+        }
+    }
+    return computerNumStringList
+            .joinToString("")
 }
