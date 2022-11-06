f67ace283a69d94b650d973deae6936ae74626a9
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-11-06 18:31:06 +0900

    move(BaseballGameReferee) : 게임의 진행을 담당하는 함수를 main으로 이동

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,12 +1,23 @@
 package baseball
 
-import camp.nextstep.edu.missionutils.Console
-import kotlin.IllegalArgumentException as IllegalArgumentException
+fun processBaseballGame() {
+    Computer.generateNewRandomNumbers()
+    ScreenManipulator.printGameStart()
 
+    while (true) {
+        val guessedNumbers = User.guessNumber() ?: throw IllegalArgumentException("입력 오류입니다.")
+        val (strikeCount, ballCount) = arrayOf(BaseballGameReferee.calcStrikeCount(guessedNumbers),
+            BaseballGameReferee.calcBallCount(guessedNumbers))
 
-
-
+        when (BaseballGameReferee.decideEachTurn(strikeCount, ballCount)) {
+            GameStatus.TERMINATE -> break
+            GameStatus.NEW_GAME -> Computer.generateNewRandomNumbers()
+            GameStatus.ERROR -> throw IllegalArgumentException("입력 오류입니다.")
+            else -> continue
+        }
+    }
+}
 
 fun main() {
-
+    processBaseballGame()
 }
