a2cc243dcb95134aa15c6d9528f0852945e4b21a
dltkd1395 <dltkd13956@naver.com>
2022-11-06 19:22:16 +0900

    refactor(BaseBall) : 리스트를 결합하는 메서드 추가

--- src/main/kotlin/baseball/model/BaseBall.kt
+++ src/main/kotlin/baseball/model/BaseBall.kt
@@ -4,16 +4,17 @@ class BaseBall(
     val computerNumbers: List<Int>,
     val playerNumbers: List<Int>
 ) {
-    fun getStrikeCount(): Int = computerNumbers
-        .zip(playerNumbers)
+    fun getStrikeCount(): Int = combineNumberList()
         .filter { num -> num.first == num.second }
         .count()
 
-    fun getBallCount(): Int = computerNumbers
-        .zip(playerNumbers)
+    fun getBallCount(): Int = combineNumberList()
         .filter { num -> num.first != num.second && hasNumberInComputer(num.second) }
         .count()
 
     private fun hasNumberInComputer(playerNumber: Int): Boolean =
         computerNumbers.contains(playerNumber)
+
+    private fun combineNumberList() =
+        computerNumbers.zip(playerNumbers)
 }
