0bdeadc3b204de8cae60bc926f27fbf35ed937a7
phs <gustjd0808@pusan.ac.kr>
2022-11-06 20:07:47 +0900

    기능 목록 1번 구현 수정

--- src/main/kotlin/baseball/ComputerNumbers.kt
+++ src/main/kotlin/baseball/ComputerNumbers.kt
@@ -1,17 +1,6 @@
 package baseball
 
-class ComputerNumbers {
-    private val computerNumberList: List<Int>
-    init {
-        computerNumberList = generate()
-    }
+class ComputerNumbers(val computerNumberList: Map<Int, Int>) {
 
-    private fun generate():List<Int>{
-        val computerNumbers = mutableListOf<Int>()
-        for(i in 0 until BaseballSetting.DIGIT_NUMBER.number){
-            computerNumbers.add(RandomNumberGenerator.generateNotDuplicatedNumber(computerNumbers))
-        }
-        return computerNumbers
-    }
 
 }
--- src/main/kotlin/baseball/RandomNumberGenerator.kt
+++ src/main/kotlin/baseball/RandomNumberGenerator.kt
@@ -3,9 +3,34 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 
 object RandomNumberGenerator {
-    fun generateNotDuplicatedNumber(computerNumbers: List<Int>): Int {
+
+    fun nDigitList(digitNumber: Int): List<Int> {
+        val nDigitList = mutableListOf<Int>()
+        for (i in 0 until digitNumber) {
+            nDigitList.add(generateNotDuplicatedNumber(nDigitList))
+        }
+        return nDigitList
+    }
+
+    fun nDigitMap(digitNumber: Int): Map<Int, Int> {
+        val nDigitMap = hashMapOf<Int, Int>()
+        for (i in 0 until digitNumber) {
+            nDigitMap[generateNotDuplicatedNumber(nDigitMap)] = i
+        }
+        return nDigitMap
+    }
+
+    private fun generateNotDuplicatedNumber(computerNumbers: List<Int>): Int {
         var randomNumber = Randoms.pickNumberInRange(1, 9)
         while (computerNumbers.contains(randomNumber)) {
+            randomNumber = Randoms.pickNumberInRange(1, 9)
+        }
+        return randomNumber
+    }
+
+    private fun generateNotDuplicatedNumber(computerNumbers: Map<Int, Int>): Int {
+        var randomNumber = Randoms.pickNumberInRange(1, 9)
+        while (computerNumbers.containsKey(randomNumber)) {
             randomNumber = Randoms.pickNumberInRange(1, 9)
         }
         return randomNumber
