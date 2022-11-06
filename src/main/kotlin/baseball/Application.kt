7357034d1aa84f9e68544c98598d0bd06f94c4db
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-06 21:03:33 +0900

    refactor: 중복 랜덤 숫자에 대한 예외처리 addComputerNumber 메소드로 분

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -6,13 +6,18 @@ fun main() {
     TODO("프로그램 구현")
 }
 
-fun createComputerNumber(): MutableList<Int> {
+fun chooseComputerNumber(): MutableList<Int> {
     val computerNumber = mutableListOf<Int>()
     while (computerNumber.size < 3) {
         var randomNumber: Int = Randoms.pickNumberInRange(1, 9)
-        if (!computerNumber.contains(randomNumber)) {
-            computerNumber.add(randomNumber)
-        }
+        addComputerNumber(randomNumber, computerNumber)
     }
     return computerNumber
+}
+
+/* 뽑은 랜덤 숫자의 중복 여부를 확인하고 아닐 시 리스트에 추가하는 메소드  */
+fun addComputerNumber(randomNumber: Int, computerNumber: MutableList<Int>) {
+    if (!computerNumber.contains(randomNumber)) {
+        computerNumber.add(randomNumber)
+    }
 }
