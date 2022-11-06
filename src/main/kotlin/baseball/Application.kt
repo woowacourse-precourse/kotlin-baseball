a174f0b64d76e83d9e0953422ed8be31df59ed09
idra79haza <idra79haza@github.com>
2022-11-06 22:06:10 +0900

    feat(rightInputCheck): checks length of input number
    checks length of input number of user, if it is not size of 3, it throws
    error message

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,6 +2,7 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
 
 fun main() {
 
@@ -20,4 +21,11 @@ fun pickRandomNums(){
 fun inputNum(): String {
     val inputNum: String = Console.readLine()
     return inputNum
+}
+
+fun rightInputCheck(num: String) {
+    val inputNum = num
+    if (num.length != 3){
+        throw IllegalArgumentException("세자리 숫자를 말씀해주세요.")
+    }
 }
