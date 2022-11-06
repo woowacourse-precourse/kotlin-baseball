acc46d6439aba0574f4702a26b719b9f966aaa91
nayoung511 <nayoungbae511@gmail.com>
2022-11-07 06:38:29 +0800

    feat($Application.kt): add exception handling function
     - add checkInputValidity(userInputList)
     - 유저가 서로 다른 세자리의 올바른 수를 입력했는지 확인

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -41,6 +41,34 @@ fun receiveUserInput() : MutableList<Int> {
     return userInputList
 }
 
+/*
+* 예외 처리
+* */
+
+// 중복값이 있는지 확인하는 함수
+fun <Int> hasDuplicates(arr: MutableList<Int>): Boolean {
+    return arr.size != arr.distinct().count();
+}
+
+// 예외처리: 유저가 올바른 값을 입력했는지 확인
+fun checkInputValidity(userInputList: MutableList<Int>) {
+    // 중복 값이 있다면
+    if ( hasDuplicates(userInputList) ) {
+        throw IllegalArgumentException("서로 다른 세자리 수를 입력해 주세요.")
+    }
+
+    // 백의 자리가 0으로 시작하지 않는지
+    if ( userInputList[0] == 0 ) {
+        throw IllegalArgumentException("백의 자리는 0이 될 수 없습니다.")
+    }
+
+    // 3자리 초과의 수를 입력했다면
+    if ( userInputList[0] > 10 ) {
+        throw  IllegalArgumentException("100 ~ 999사이의 값을 입력해주세요. (자릿수 초과)")
+    }
+
+}
+
 // 유저의 입력값과 컴퓨터의 값을 비교
 fun baseballGame(userInputList: MutableList<Int>, computer: MutableList<Int>) : Boolean {
     var correct = false // user의 입력값이 컴퓨터의 값과 일치하는지 판별
@@ -94,17 +122,19 @@ fun baseballGame(userInputList: MutableList<Int>, computer: MutableList<Int>) : 
 
 
 fun main() {
-    //TODO("프로그램 구현")
     println("숫자 야구 게임을 시작합니다.")
 
-    // 여기서 while값 하나 넣고 flag에 따라 게임 계속 할건지 말건지 판단
+    // outer loop: 게임을 새로 진행할건지 판단
     do {
         val computer = generateRandomNumber()
-        var correct = false
-        var keepPlaying = 1
+        var correct = false // 정답을 맞추었는지 확인하는 변수
+        var keepPlaying = 1 // 게임을 새로 진행하는 변수 (1 = 새로운 게임, 2 = 종료)
 
+        // inner loop: 현재 게임에서 정답을 맞출 떄까지
         do {
             var userInputList = receiveUserInput()
+            // 유저 입력값이 올바른지 확인
+            checkInputValidity((userInputList))
             correct = baseballGame(userInputList, computer)
         } while (correct != true)
 
