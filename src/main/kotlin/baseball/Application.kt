bd60dc1718a3b74ee393bf28951ce9662ee96938
bluecat0823 <star4_you@naver.com>
2022-11-07 00:09:02 +0900

    <feat> 1부터 9까지 숫자 중 서로 다른 세 수로 이루어진 숫자를 랜덤하게 생성하여 문제를 만든다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,18 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
+fun Numbergenerator(): MutableList<Int> {
+    val computerNumber: MutableList<Int> = mutableListOf()
+    while (computerNumber.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computerNumber.contains(randomNumber)) {
+            computerNumber.add(randomNumber)
+        }
+    }
+    return computerNumber
+}
+
 fun main() {
-    TODO("프로그램 구현")
+
 }
