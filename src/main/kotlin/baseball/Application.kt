92075b84f2ce4ada1ef2ce3b3f3c9f3f862dbd00
vrexpert <vrexpert2302@gmail.com>
2022-11-06 21:52:44 +0900

    Feat : Player 세자리 수 입력받기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,26 @@
 package baseball
-
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 fun main() {
-    TODO("프로그램 구현")
+    val computer = randomThreeNumber()
+    val user = playerThreeNumber()
 }
+
+fun randomThreeNumber():MutableList<Int>{
+    val computer = mutableListOf<Int>()
+    while(computer.size<3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
+
+fun playerThreeNumber():MutableList<Int>{
+    val playerInput = Console.readLine()
+    val playerInputMutableList = playerInput.map {it.toString().toInt()}.toMutableList()
+    //inputErrorCheck(playerInputMutableList)
+    return playerInputMutableList
+}
+
