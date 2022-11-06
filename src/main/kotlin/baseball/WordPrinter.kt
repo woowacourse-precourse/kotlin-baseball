ca3416ffdd5ff97e8118ae3cf12600c831a9afd3
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-11-06 17:19:41 +0900

    fix(WordPrinter) : 불필요한 개행 문자가 들어가 두칸이 띄어져 버리는 버그 수정

--- src/main/kotlin/baseball/WordPrinter.kt
+++ src/main/kotlin/baseball/WordPrinter.kt
@@ -36,7 +36,7 @@ object WordPrinter {
 
     fun printGameEnd() {
         screen.write("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n")
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         screen.flush()
     }
 
