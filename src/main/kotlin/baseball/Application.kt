116b42c634a190b5c892f752cceaadf01f727d3e
qhfhdls <79788971+qhfhdls@users.noreply.github.com>
2022-11-06 16:13:17 +0900

    Update functionlist & code reset
    기능 목록 생성
    randomNumber 함수 초기화

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,54 +2,52 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 
+// 경기 진행 함수
+fun startGame() {
 
-// 3자리 랜덤 수 추출 함수
+}
+
+// 서로 다른 임의의 수 3개 선택 기능
 fun randomNumber(): Array<Int> {
-    var array: Array<Int> = arrayOf(0, 0, 0)
-    for(i in 0..2)
-        array[i] = pickNumberInRange(1, 9)
-    return array
-}
-
-// --------- 한 라운드 진행 함수
-fun roundStart() {
 
 }
 
-// 비교 결과 리턴 함수
+// 서로 다른 3개의 숫자 입력받는 기능
+fun randomNumberInput() {
+
+}
+
+// 잘못된 값을 입력할 경우 예외 처리 기능
+fun exceptionHandling() {
+
+}
+
+// 정답 확인 기능
+fun checkCorrect() {
+
+}
+// 스트라이크 확인 기능
+fun checkStrike() {
+
+}
+
+// 볼 확인 기능
+fun checkBall() {
+
+}
+
+// 비교 결과 출력 기능
 fun gameResult() {
 
 }
 
-// 정답 확인 함수
-fun checkCorrect() {
+// 게임 종료 기능
+fun endGame() {
 
 }
-// 스트라이크 확인 함수
-fun checkStrike() {
 
-}
-// 볼 확인 함수
-fun checkBall() {
-
-}
-// ---------
-
-// 서로 다른 3자리 수 생성
-// 게임 시작 알림문 -> 입력 -> 입력된 숫자와 비교해 정답 비교
-// 정답x -> 스트라이크 및 볼 판정 -> 입력 반복
-// 정답o -> 게임 종료 -> 진행 선택
 
 fun main() {
 
-    // 랜덤 수 발생
-
-    // 게임 시작 알림문 출력
-    // 숫자 입력 받기
-
-    // 입력된 숫자를 통해 라운드 진행
-
-    // 라운드 결과 확인
-    // 결과에 따라 라운드 재진행 및 종료 후 진행선택
 
 }
