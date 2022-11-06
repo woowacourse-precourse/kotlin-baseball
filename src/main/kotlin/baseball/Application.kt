053b887f94eb66d83fb2916106b2cd3a3a51797d
82104 <tir2986@naver.com>
2022-11-06 16:41:58 +0900

    refactor($main): 사용자 입력, 숫자 비교 부분 함수로 분리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -9,17 +9,7 @@ fun main() {
     while (true) {
         val computer = generateRandomThreeDigits()
 
-        var matched = false
-        while (!matched) {
-            print("숫자를 입력해주세요 : ")
-            val user = Console.readLine()
-            validatePlayInput(user)
-
-            val compResult = CompResult(computer, splitUserInput(user))
-            println(compResult.toString())
-
-            matched = compResult.allMatched()
-        }
+        while (!readAndCompare(computer).allMatched())
 
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
                 "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
@@ -30,6 +20,17 @@ fun main() {
             break
         }
     }
+}
+
+fun readAndCompare(computer: List<Int>): CompResult {
+    print("숫자를 입력해주세요 : ")
+    val user = Console.readLine()
+    validatePlayInput(user)
+
+    val compResult = CompResult(computer, splitUserInput(user))
+    println(compResult.toString())
+
+    return compResult
 }
 
 fun generateRandomThreeDigits(): List<Int> {
