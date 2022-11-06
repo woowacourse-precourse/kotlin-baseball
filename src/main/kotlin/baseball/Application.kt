e621d7c2b1e6167924ec7e3896a6a3e94e983028
윤동현 <donghyunyoon81@gmail.com>
2022-11-06 15:32:17 +0900

    feat: Create readInputString 사용자의 입력값을 문자열로 저장하는 기능입니다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,10 +5,16 @@ import camp.nextstep.edu.missionutils.Console
 import java.lang.IllegalArgumentException
 
 fun main() {
-    print(getRandomNumbers(1,9,3))
+    println(getRandomNumbers(1,9,3))
+    println(readInputString())
 
 }
 
 fun getRandomNumbers(randomStartNumber:Int,randomEndNumber:Int,randomsLength:Int): List<Int> {
     return Randoms.pickUniqueNumbersInRange(randomStartNumber,randomEndNumber,randomsLength)
 }
+fun readInputString():String {
+    return Console.readLine()
+}
+
+
