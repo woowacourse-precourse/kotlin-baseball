53f593225bb5159ec2d5cdc9815d72a92e19115a
kyoung_mo <kumong97@knu.ac.kr>
2022-11-06 23:25:31 +0900

    숫자입력, 숫자생성 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,23 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+// import camp.nextstep.edu.missionutils.Console
+
+val basic_list = mutableListOf<Int>()
+fun make_number(): String {
+    while (basic_list.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!basic_list.contains(randomNumber)) {
+            basic_list.add(randomNumber)
+        }
+    }
+    return basic_list.toString()
+}
+
 fun main() {
-    TODO("프로그램 구현")
+    print("숫자를 입력해주세요 : ")
+    val input_number = readLine()
+    if (input_number!!.length > 3)
+        throw IllegalArgumentException() // throw??
+    make_number()
 }
