5b1ee1a3d0048367db768864f445c9e18fab7b5a
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-11-06 19:30:25 +0900

    feat: 추가 예외처리에 대한 테스트코드
    잘못된 입력값 4개를 예시로 들어 예외가 발생할 경우를 테스트함.

--- src/test/kotlin/baseball/MyTest.kt
+++ src/test/kotlin/baseball/MyTest.kt
@@ -2,6 +2,7 @@ package baseball
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class MyTest {
     @Test
@@ -14,5 +15,37 @@ class MyTest {
     fun `같은 숫자가 있는지 확인`() {
         val input = "334"
         Assertions.assertThat(checkSameNumber(input)).isEqualTo(true)
+    }
+
+    @Test
+    fun `같은 숫자를 입력했을 때 예외처리`() {
+        val assertThrows = assertThrows<IllegalArgumentException> {
+            exceptionHandling("223")
+        }
+        org.junit.jupiter.api.Assertions.assertEquals(assertThrows.message, null)
+    }
+
+    @Test
+    fun `세자리의 수를 입력하지 않았을 때 예외처리`() {
+        val assertThrows = assertThrows<IllegalArgumentException> {
+            exceptionHandling("2")
+        }
+        org.junit.jupiter.api.Assertions.assertEquals(assertThrows.message, null)
+    }
+
+    @Test
+    fun `한글을 입력했을 때 예외처리`() {
+        val assertThrows = assertThrows<IllegalArgumentException> {
+            exceptionHandling("가나다")
+        }
+        org.junit.jupiter.api.Assertions.assertEquals(assertThrows.message, null)
+    }
+
+    @Test
+    fun `영어를 입력했을 때 예외처리`() {
+        val assertThrows = assertThrows<IllegalArgumentException> {
+            exceptionHandling("abc")
+        }
+        org.junit.jupiter.api.Assertions.assertEquals(assertThrows.message, null)
     }
 }
