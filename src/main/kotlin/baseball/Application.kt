33ed2e0f568779a63952165767f9e66502834619
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-07 20:49:12 +0900

    refactor: [게임진행] 컴퓨터 숫자 생성 기능 리팩토링
    addNotDuplicateNum() 추가
    getComputerNum() 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -10,12 +10,19 @@ fun printStartMessage() = "숫자 야구 게임을 시작합니다."
 
 fun getNum() = Randoms.pickNumberInRange(0, 9)
 
+fun addNotDuplicateNum(
+        numbers: MutableList<Int>,
+        number: Int
+) {
+    if (!numbers.contains(number)) numbers.add(number)
+}
+
 fun getComputerNum(): MutableList<Int> {
     val numbers = mutableListOf<Int>()
 
-    while (numbers.size != 3){
+    while (numbers.size != 3) {
         val number = getNum()
-        if (!numbers.contains(number)) numbers.add(number)
+        addNotDuplicateNum(numbers, number)
     }
 
     return numbers
