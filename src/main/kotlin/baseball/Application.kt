53a37cb85446759ce26860faf3cd20cd424b7cdb
권용민 <briandr@naver.com>
2022-11-08 02:57:07 +0900

    feat(Application.kt): Referee의 기능 목록 구현
    심판 판정 (유저가 삼진으로 모두 맞췄는지 검사)
      - 스트라이크 카운트 계산 : countStrike
      - 볼 카운트 계산 : countBall
      - 스트라이크 카운트가 3인지 검사 : strikeCount == 3
      - 판정 반환을 위한 데이터 클래스 생성 : RefereeDecision

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -67,6 +67,38 @@ class User() {
     }
 }
 
+class Referee() {
+    fun decideGameEnd(computerNumbers: List<Int>, userNumbers: List<Int>): RefereeDecision {
+        val strikeIndices: List<Int> = countStrike(computerNumbers, userNumbers)
+        val ballCount: Int = countBall(computerNumbers, userNumbers, strikeIndices)
+        val strikeCount = strikeIndices.size
+
+        return RefereeDecision(ballCount, strikeCount, strikeCount == 3)
+    }
+
+    private fun countStrike(computerNumbers: List<Int>, userNumbers: List<Int>): List<Int> {
+        val strikeIndex = listOf<Int>(0, 1, 2)
+        return strikeIndex.filterIndexed { index, _ ->
+            computerNumbers[index] == userNumbers[index]
+        }
+    }
+
+    private fun countBall(computerNumbers: List<Int>, userNumbers: List<Int>, strikeIndices: List<Int>): Int {
+        var ballCount = 0
+        computerNumbers.forEachIndexed { index, computerNum ->
+            if (strikeIndices.contains(index)) return@forEachIndexed
+            if (userNumbers.contains(computerNum)) ballCount++
+        }
+        return ballCount
+    }
+}
+
+data class RefereeDecision(
+    val ballCount: Int,
+    val strikeCount: Int,
+    val isStrikeOut: Boolean
+)
+
 class Printer() {
     fun showStartMessage() {
         println("숫자 야구 게임을 시작합니다.")
