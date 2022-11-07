19fcfc9e9afe50b4dcf4e4bbe5627ddc4bfd82a2
phs <gustjd0808@pusan.ac.kr>
2022-11-07 23:56:38 +0900

    RandomNumberGenerator 메서드 단위 테스트 구현

--- src/main/kotlin/baseball/RandomNumberGenerator.kt
+++ src/main/kotlin/baseball/RandomNumberGenerator.kt
@@ -3,15 +3,6 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 
 object RandomNumberGenerator {
-
-    fun nDigitList(digitNumber: Int): List<Int> {
-        val nDigitList = mutableListOf<Int>()
-        for (i in 0 until digitNumber) {
-            nDigitList.add(generateNotDuplicatedNumber(nDigitList))
-        }
-        return nDigitList
-    }
-
     fun nDigitMap(digitNumber: Int): Map<Int, Int> {
         val nDigitMap = hashMapOf<Int, Int>()
         for (i in 0 until digitNumber) {
@@ -20,15 +11,7 @@ object RandomNumberGenerator {
         return nDigitMap
     }
 
-    private fun generateNotDuplicatedNumber(computerNumbers: List<Int>): Int {
-        var randomNumber = Randoms.pickNumberInRange(1, 9)
-        while (computerNumbers.contains(randomNumber)) {
-            randomNumber = Randoms.pickNumberInRange(1, 9)
-        }
-        return randomNumber
-    }
-
-    private fun generateNotDuplicatedNumber(computerNumbers: Map<Int, Int>): Int {
+    fun generateNotDuplicatedNumber(computerNumbers: Map<Int, Int>): Int {
         var randomNumber = Randoms.pickNumberInRange(1, 9)
         while (computerNumbers.containsKey(randomNumber)) {
             randomNumber = Randoms.pickNumberInRange(1, 9)
