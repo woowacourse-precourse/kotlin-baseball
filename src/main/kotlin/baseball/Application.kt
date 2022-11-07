33be47e8acfc9f439cfde04df4677a23d27fd2b3
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-07 23:40:01 +0900

    feat(Application.kt): 프로그램 시작 main() 함수 구현
    BaseballGame.Builder 클래스를 통해 인스턴스 생성 후 게임 시작

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,14 @@
 package baseball
 
+import baseball.game.BaseballGame
+import baseball.game.Provider.pickRandomNumber
+import baseball.game.Provider.readLine
+
 fun main() {
-    TODO("프로그램 구현")
+    BaseballGame.Builder()
+        .onStart(::pickRandomNumber)
+        .onProgress(::readLine)
+        .onFinish(::readLine)
+        .build()
+        .play()
 }
