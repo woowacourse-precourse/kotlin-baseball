35252d7c8807161895bd8381d2cee6a18bfef6ec
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-07 23:36:33 +0900

    feat(Provider.kt): 클래스 생성 및 함수 정의
    함수 정의
    - pickRandomNumber()
    - readLine()

---
+++ src/main/kotlin/baseball/game/Provider.kt
@@ -0,0 +1,15 @@
+package baseball.game
+
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+object Provider {
+
+    fun pickRandomNumber(): String {
+        TODO()
+    }
+
+    fun readLine(): String {
+        TODO()
+    }
+}
