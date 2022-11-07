3f1d84501a6f921158fe0b8a4d3d76025eb0d7fa
kyounggudev <homin222@naver.com>
2022-11-07 17:53:23 +0900

    test: 정상작동 테스트와 입력 길이초과/부족, 숫자 외 값, 0포함, 널/공백 테스트 추가

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -21,9 +21,79 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
-    fun `예외 테스트`() {
+    fun `게임종료 후 종료`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("246", "245", "641", "346", "2")
+                assertThat(output())
+                    .contains("2스트라이크", "1스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료")
+            },
+            3, 4, 6
+        )
+    }
+
+    @Test
+    fun `게임 재시작 1,2 외 정수 입력 오류`() {
+        assertRandomNumberInRangeTest(
+            {
+                assertThrows<IllegalArgumentException> {run("246", "245", "641", "346", "5")}
+            },
+            3, 4, 6
+        )
+    }
+
+    @Test
+    fun `게임 재시작 알파벳 입력 오류`() {
+        assertRandomNumberInRangeTest(
+            {
+                assertThrows<IllegalArgumentException> {run("246", "245", "641", "346", "a")}
+            },
+            3, 4, 6
+        )
+    }
+
+    @Test
+    fun `게임 재시작 널값 입력 오류`() {
+        assertRandomNumberInRangeTest(
+            {
+                assertThrows<IllegalArgumentException> {run("246", "245", "641", "346", null)}
+            },
+            3, 4, 6
+        )
+    }
+
+    @Test
+    fun `길이 초과 예외 테스트`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("1234") }
+        }
+    }
+
+    @Test
+    fun `길이 부족 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("1") }
+        }
+    }
+
+    @Test
+    fun `정수외 값 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("a34") }
+        }
+    }
+
+    @Test
+    fun `널 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException() }
+        }
+    }
+
+    @Test
+    fun `0 포함 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("012") }
         }
     }
 
