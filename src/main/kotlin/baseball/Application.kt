43400e5a4b86c43c55020384b305895c7128d5a7
jjw0071 <jjw0071@naver.com>
2022-11-07 14:39:27 +0900

    main function

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -16,8 +16,17 @@ var strike : Int = 0
 var ball : Int = 0
 
 fun main() {
-    TODO("프로그램 구현")
-
+    println("숫자야구게임을 시작합니다.")
+    do{
+        randomMaker()
+        do{
+            print("숫자를 입력해 주세요 : ")
+            inputValidationCheck(Console.readLine())
+            checkStrikeBall()
+        }while(strike != 3)
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력해주세요.")
+    }while(restartCheck(Console.readLine()))
 
 }
 
