f0dd602ad928fb8c860e396259ab2e63d90c5a19
mendel <mendel.urssu@gmail.com>
2022-11-07 23:46:16 +0900

    feat: 기능 8번 BaseballInputValidCheckAndConverter 클래스 정의
    InputValidCheckAndConverter 추상클래스를 상속한 숫자야구게임을 위한 클래스 생성

--- src/main/kotlin/baseball/InputValidCheck.kt
+++ src/main/kotlin/baseball/InputValidCheck.kt
@@ -9,3 +9,43 @@ abstract class InputValidCheckAndConverter(private val inputConverter: InputConv
     protected abstract fun inputValidCheck(questionType: Int, input: String): Boolean
     protected abstract fun isNotValidThrowException()
 }
+
+class BaseballInputValidCheckAndConverter(private val inputConverter: InputConverter) :
+    InputValidCheckAndConverter(inputConverter) {
+
+    override fun inputValidCheck(questionType: Int, input: String): Boolean =
+        when (questionType) {
+            BaseballGameInputConverter.INPUT_CUR_ROUND -> curRoundInputValidCheck(input)
+            BaseballGameInputConverter.INPUT_RESTART_GAME -> restartGameInputValidCheck(input)
+            else -> false
+        }
+
+    override fun isNotValidThrowException() {
+        throw IllegalArgumentException()
+    }
+
+    private fun curRoundInputValidCheck(input: String): Boolean {
+        if (input.isNullOrEmpty() ||
+            input.length != 3
+        ) {
+            return false
+        }
+
+        input.forEach { if (!isNumberValidRange(it.code - 48)) return false }
+        return true
+    }
+
+    private fun isNumberValidRange(num: Int): Boolean = num in 1..9
+
+    private fun restartGameInputValidCheck(input: String): Boolean {
+        if (input.isNullOrEmpty() ||
+            input.length != 1
+        ) {
+            return false
+        }
+
+        return restartAnswerInputValidRange(input)
+    }
+
+    private fun restartAnswerInputValidRange(input: String): Boolean = input[0].code == 49 || input[0].code == 50
+}
