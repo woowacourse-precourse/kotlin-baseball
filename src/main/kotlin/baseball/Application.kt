d1288cd74b7c681615654f034988333614138ccd
Hyemin Song <hyemdooly@gmail.com>
2022-11-07 15:01:07 +0900

    docs and feat(docs and application): edit docs and judge the result of numbers

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -6,7 +6,7 @@ import java.util.IllegalFormatCodePointException
 
 fun pickNumbers(): List<Char> {
     val result = mutableListOf<Char>()
-    for(i in 1..3) {
+    for (i in 1..3) {
         result.add(Randoms.pickNumberInRange(1, 9).toChar())
     }
     return result
@@ -21,20 +21,52 @@ fun inputNumbers(): List<Char> {
     return input.toList()
 }
 
-fun main() {
-    val userNumbers: List<Char>
+fun judgeNumbers(userNumbers: List<Char>, randNumbers: List<Char>): List<Int> {
+    var boll = 0
+    var strike = 0
 
+    for (i in userNumbers.indices) {
+        if (userNumbers[i] == randNumbers[i]) {
+            strike += 1
+        } else if (randNumbers.contains(userNumbers[i])) {
+            boll += 1
+        }
+    }
+
+    return listOf(boll, strike)
+}
+
+fun printResult(result: List<Int>) {
+    val boll = result[0]
+    val strike = result[1]
+    if (boll != 0) {
+        print("${boll}볼 ")
+    }
+    if (strike != 0) {
+        print("${strike}스트라이크")
+    }
+    if(boll == 0 && strike == 0) {
+        print("낫싱")
+    }
+    println()
+}
+
+
+fun main() {
     println("숫자 야구 게임을 시작합니다.")
     do {
         val randNumbers = pickNumbers()
-
+//        val randNumbers = "425".toList()
         print("숫자를 입력해주세요 : ")
-        val userNumbers:List<Char> = try {
+        val userNumbers: List<Char> = try {
             inputNumbers()
         } catch (e: IllegalArgumentException) {
             println(e.message)
+            println("게임을 종료합니다.")
             return
         }
 
-    } while(true)
+        val judgeResult = judgeNumbers(userNumbers, randNumbers)
+        printResult(judgeResult)
+    } while (true)
 }
