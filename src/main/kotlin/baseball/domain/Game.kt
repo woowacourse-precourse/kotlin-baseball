fc09489cb2f25b1f02fa3cbb641ead6adccdf0c9
Grandelwald <artjr97@naver.com>
2022-11-07 20:56:06 +0900

    Game.kt 숫자 비교하는 기능 추가

--- src/main/kotlin/baseball/domain/Game.kt
+++ src/main/kotlin/baseball/domain/Game.kt
@@ -2,7 +2,15 @@ package baseball.domain
 
 class Game(var strike: Int = 0, var ball: Int = 0) {
 
+    private val computerNumber = mutableListOf<Int>()
 
+    fun compare(userNumber: List<Int>) {
+        userNumber.onEachIndexed { index, number ->
+            if(number == computerNumber[index]) {
+                strike += 1
+            } else if(userNumber.contains(number)){
+                ball += 1
+            }
+        }
+    }
 }
-
-
