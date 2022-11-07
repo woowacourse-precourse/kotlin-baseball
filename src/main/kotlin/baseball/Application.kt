f71af68d57e2a571f4eef130b2700f87943f8994
2taezeat <xorudfl772@naver.com>
2022-11-07 21:46:17 +0900

    feat: 상대방(컴퓨터)가 1~9까지 서로 다른 임의의 수 3개를 선택하고 리스트에 담아 리턴하는 함수 작성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,7 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     startBaseBallGame()
 
@@ -8,4 +10,18 @@ fun main() {
 
 private fun startBaseBallGame() {
     println("숫자 야구 게임을 시작합니다.")
+    val computerRandomNumber = computerSelectThreeRandomNumber()
+    println(computerRandomNumber)
+}
+
+
+private fun computerSelectThreeRandomNumber(): List<Int> {
+    val computerList = mutableListOf<Int>()
+    while (computerList.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computerList.contains(randomNumber)){
+            computerList.add(randomNumber)
+        }
+    }
+    return computerList
 }
