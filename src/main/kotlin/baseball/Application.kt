59cb10afd49edb9032c99b9b457b250359638de1
sojk401 <sojk401@gmail.com>
2022-11-08 03:44:27 +0900

    refactor: 입력받은 플레이어의 숫자와 컴퓨터 숫자를 전역 변수로 변경, 중복된 숫자를 입력하는 경우 예외처리 수정
    플레이어의 숫자와 컴퓨터의 숫자는 대부분의 함수에서 사용되므로 전역변수로 바꾸어 인자로 받지 않도록 함.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,24 +3,27 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
+var computerNumber = ""
+var userNumber = ""
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
 
-    var computerNumber = makeRandomNumber(3)
+    computerNumber = makeRandomNumber(3)
     println("랜덤숫자 : ${computerNumber}")
 
     while (true) {
         print("숫자를 입력해주세요 : ")
-        var userInput = Console.readLine()
-        println(userInput)
-        checkNumberException(userInput)
+        userNumber = Console.readLine()
+        println(userNumber)
+        checkNumberException()
 
-        var (isNothing, userindexIncomputerNum) = searchNothingCase(userInput, computerNumber)
+        var (isNothing, userindexIncomputerNum) = searchNothingCase()
 
         if (isNothing) {
             println("낫싱")
         } else {
-            println("${userInput}의 ${userindexIncomputerNum} 인덱스는 computerNumber에 존재")
+            println("${userNumber}의 ${userindexIncomputerNum} 인덱스는 computerNumber에 존재")
             TODO("스트라이크/볼 판단")
         }
     }
@@ -38,28 +41,28 @@ fun makeRandomNumber(length: Int): String {
     return noOverlapRandomNumber.joinToString("")
 }
 
-fun checkNumberException(userInput: String) {
-    if (userInput.toIntOrNull() == null) {
-        throw IllegalArgumentException("IllegalAccessException. 프로그램을 종료합니다.")
-    } else if (userInput.length != 3) {
-        throw IllegalArgumentException("IllegalAccessException. 프로그램을 종료합니다.")
+fun checkNumberException() {
+    if (userNumber.toIntOrNull() == null) {
+        throw IllegalArgumentException("IllegalAccessException. 숫자로 이루어지지 않았습니다.")
+    } else if (userNumber.length != 3) {
+        throw IllegalArgumentException("IllegalAccessException. 3개의 숫자가 아닙니다.")
     }
 
-    var userNumber = mutableListOf<String>()
+    var checkNumber = mutableListOf<Char>()
     for (i in userNumber) {
-        if (i !in userNumber) {
-            userNumber.add(i)
+        if (i !in checkNumber) {
+            checkNumber.add(i)
         } else {
-            throw IllegalArgumentException("IllegalAccessException. 프로그램을 종료합니다.")
+            throw IllegalArgumentException("IllegalAccessException. 중복된 숫자가 존재합니다.")
         }
     }
 }
 
-fun searchNothingCase(userNum: String, computerNum: String): Pair<Boolean, MutableList<Int>> {
+fun searchNothingCase(): Pair<Boolean, MutableList<Int>> {
     var isNothing = true
     var userindexIncomputerNum = mutableListOf<Int>()
-    for (i in userNum.indices) {
-        if (userNum[i] in computerNum) {
+    for (i in userNumber.indices) {
+        if (userNumber[i] in computerNumber) {
             isNothing = false
             userindexIncomputerNum.add(i)
         }
