279bd8268988e42151d02cab5d8ac6005a733a42
2taezeat <xorudfl772@naver.com>
2022-11-07 22:51:22 +0900

    feat: 사용자가 게임을 새로 시작할지에 대한 input을 받는 함수 작성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -95,5 +95,6 @@ private fun printGameResult(strikeBallCountPair: Pair<Int, Int>): Int {
 private fun userDecideGameContinue() {
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
     val userInput = readLine()
-
 }
+
+
