b66c082f5f6e1e83323283ae5a100fb52446ef35
mendel <mendel.urssu@gmail.com>
2022-11-08 00:05:26 +0900

    feat: 기능 5번 BaseballStringMaker 클래스 정의
    StringMaker 인터페이스를 구현한 숫자야구게임을 위한 스트링 메이커 클래스 정의

--- src/main/kotlin/baseball/StringMakeModel.kt
+++ src/main/kotlin/baseball/StringMakeModel.kt
@@ -1,5 +1,7 @@
 package baseball
 
+import java.lang.StringBuilder
+
 interface StringMaker {
     fun initGameStartMessage(): String
     fun inputForEachRoundMessage(): String
@@ -7,4 +9,64 @@ interface StringMaker {
     fun gameClearMessage(): String
     fun gameFailedMessage(): String
     fun restartGameQuestionMessage(): String
+}
+
+class BaseballStringMaker : StringMaker {
+    override fun initGameStartMessage(): String {
+        return INIT_GAME_START_MESSAGE
+    }
+
+    override fun inputForEachRoundMessage(): String {
+        return PLEASE_INPUT_NUMBER
+    }
+
+    override fun resultCurRound(user: Any, computer: Any): String {
+        user as List<Int>
+        computer as List<Int>
+        val ballCount = ballCount(user, computer)
+        val strikeCount = strikeCount(user, computer)
+        val result = StringBuilder()
+        if (ballCount != 0) result.append("$ballCount$BALL")
+        if (strikeCount != 0) result.append("$strikeCount$STRIKE")
+        if (ballCount == 0 && strikeCount == 0) result.append(NOTHING)
+        return result.toString()
+    }
+
+    override fun gameClearMessage(): String {
+        return GAME_CLEAR_MESSAGE
+    }
+
+    override fun restartGameQuestionMessage(): String {
+        return RESTART_GAME_MESSAGE
+    }
+
+    private fun ballCount(user: List<Int>, computer: List<Int>): Int {
+        var containsCount = 0
+        val samePositionCount = strikeCount(user, computer)
+        user.forEach { if (it in computer) containsCount++ }
+        return containsCount - samePositionCount
+    }
+
+    private fun strikeCount(user: List<Int>, computer: List<Int>): Int {
+        var samePositionCount = 0
+        for (i in 0 until 3) {
+            if (user[i] == computer[i]) samePositionCount++
+        }
+        return samePositionCount
+    }
+
+    override fun gameFailedMessage(): String {
+        return ""
+    }
+
+    companion object {
+        const val BALL = "볼 "
+        const val STRIKE = "스트라이크 "
+        const val NOTHING = "낫싱"
+        const val INIT_GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다."
+        const val PLEASE_INPUT_NUMBER = "숫자를 입력해주세요 : "
+        const val END_CONDITION_MESSAGE = "3스트라이크 "
+        const val GAME_CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+        const val RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    }
 }
