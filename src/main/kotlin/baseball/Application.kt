1859a659393ae96d17adf629b320c1ca399dd1fc
Lee-GS <leegs4831@naver.com>
2022-11-07 17:52:57 +0900

    숫자 스트라이크 및 볼 개수 계산 함수 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -32,6 +32,39 @@ fun create_computer_number(): String{
     var computer_number = computer.joinToString("")
     return computer_number
 }
+fun test_number(computer_number: String,number: String){ // 스트라이크 볼 갯수 검사
+    var strike = 0
+    var ball = 0
+    for (i : Int in 0..2){
+        if(computer_number[i]==number[i]){
+            strike+=1
+        }
+        else if (computer_number[i] != number[i] && number[i] in computer_number){
+            ball+=1
+        }
+    }
+    if (strike == 3){
+        println("3스트라이크")
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+
+    }
+    else if(strike == 0 && ball == 0){
+        println("낫싱")
+
+    }
+    else if (strike == 0 && ball!=0){
+        println("$ball 볼")
+
+    }
+    else if (strike != 0 && ball == 0){
+        println("$strike 스트라이크")
+
+    }
+    else {
+        println("${ball}볼 ${strike}스트라이크")
+    }
+}
+
 
 
 
