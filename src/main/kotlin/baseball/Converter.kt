3c556fc2a2915114343a772d93ac6d87344b863d
mendel <mendel.urssu@gmail.com>
2022-11-07 23:26:27 +0900

    feat: 기능 10번 BaseballGameInputConverter 클래스 정의
    InputConverter 인터페이스를 구현하고 숫자야구게임에서 입력받은 문자열을 변환해주는 클래스 정의

--- src/main/kotlin/baseball/Converter.kt
+++ src/main/kotlin/baseball/Converter.kt
@@ -3,3 +3,19 @@ package baseball
 interface InputConverter {
     fun convertInput(questionType: Int, input: String): Any?
 }
+
+class BaseballGameInputConverter : InputConverter {
+    private fun convertCurRoundInput(input: String): List<Int> = input.map { it.code - 48 }
+    private fun convertRestartGameInput(input: String): Int = input.toInt()
+    override fun convertInput(questionType: Int, input: String): Any? =
+        when (questionType) {
+            INPUT_CUR_ROUND -> convertCurRoundInput(input)
+            INPUT_RESTART_GAME -> convertRestartGameInput(input)
+            else -> null
+        }
+
+    companion object {
+        const val INPUT_CUR_ROUND = 0
+        const val INPUT_RESTART_GAME = 1
+    }
+}
