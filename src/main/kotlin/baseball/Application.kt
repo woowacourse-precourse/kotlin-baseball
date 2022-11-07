cfb286dde929287ac6898c8b6d120d4d0c52c08e
person00333 <person00333@gmail.com>
2022-11-07 21:18:52 +0900

    feat : 게임 실행 알고리즘

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,24 @@
 package baseball
 
+
 fun main() {
-    TODO("프로그램 구현")
+
+    println("숫자 야구 게임을 시작합니다.")
+
+    var gameStart = 1
+    var count = 1
+    while (gameStart == 1){
+        try {
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+            gameStart = readLine()!!.toInt()
+            ++count
+            if ((gameStart != 1) and (gameStart != 2)){
+                throw IllegalArgumentException("입력 오류")
+            }
+        }catch (e: IllegalArgumentException){
+            throw IllegalArgumentException("입력 오류")
+        }
+    }
+    println("게임 종료")
+
 }
