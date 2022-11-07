062695bc1839abad691937db4c82da79c7fcfef6
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-07 18:39:24 +0900

    feat(BaseBallGame): printGameOverMessage - 게임 종료시 문구 출력 메소드 정의

--- src/main/kotlin/baseball/BaseBallGame.kt
+++ src/main/kotlin/baseball/BaseBallGame.kt
@@ -57,6 +57,10 @@ class BaseBallGame {
             }.let { println(it) }
         }
     }
+
+    private fun printGameOverMessage() {
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    }
     companion object {
         private const val RUNNING = 1
         private const val RESTART = 2
