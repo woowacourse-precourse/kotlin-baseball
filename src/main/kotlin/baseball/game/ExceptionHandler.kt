32a8f50ab4ec2d65362c5852fcbd01d6f5c9fd72
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-07 23:35:52 +0900

    feat(ExceptionHandler.kt): 클래스 생성 및 함수 정의
    함수 정의
    - checkNumberFormat()
    - throwFinishCode()

---
+++ src/main/kotlin/baseball/game/ExceptionHandler.kt
@@ -0,0 +1,12 @@
+package baseball.game
+
+object ExceptionHandler {
+
+    fun checkNumberFormat(number: String) {
+        TODO()
+    }
+
+    fun throwFinishCode(code: String) {
+        TODO()
+    }
+}
