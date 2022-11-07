1d8dcd8d98ba147acd8076209b3e8e366f10eab8
yelm <21yrshin@naver.com>
2022-11-08 00:52:16 +0900

    menucheck and restart, StrikeCheck and notThreeStrike

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -11,7 +11,7 @@ fun compareList(userInputToInt: List<Int>, computerList: List<Int>): Boolean {
 
 }
 
-fun main() {
+fun startgame(){
     println("숫자 야구 게임을 시작합니다.")
     println("숫자를 입력해주세요 : ")
 
@@ -19,11 +19,53 @@ fun main() {
     var computerList = createComputerList()
 
     while (!compareList(userInputList, computerList)){
-
+        notThreeStrike(userInputList, computerList)
     }
 
     println("3스트라이크\n" +
             "3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+}
+
+fun notThreeStrike(userInputList: List<Int>, computerList: List<Int>) {
+    var strike = 0
+    for (i in 0..1) {
+        strike  += StrikeCheck(i, userInputList, computerList)
+    }
+
+}
+
+fun StrikeCheck(i: Int, userInputList: List<Int>, computerList: List<Int>) : Int{
+    if(userInputList[i] == computerList[i]){
+        return 1
+    }else{
+        return 0
+    }
+}
+
+fun main() {
+    startgame()
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    var menu : Int? = readLine()?.toInt()
+    while(restart(menu)) {
+        startgame()
+    }
+
+}
+
+fun restart(menu: Int?): Boolean {
+
+    if (!menuCheck(menu)){
+        println("유효하지 않은 입력입니다.")
+    }
+
+    return if(menu == 1){
+        true
+    }else if(menu == 2){
+        false
+    }else{
+        throw IllegalArgumentException("1 또는 2를 입력하세요. ")
+        return false
+    }
 
 }
 
@@ -38,6 +80,10 @@ fun userInputToInt() : List<Int> {
     var userInput : Int? = readLine()?.toInt()
     var inputs : List<Int> = emptyList()
 
+   if(!userInputCheck(userInput)){
+       println("유효하지 않은 입력입니다.")
+   }
+
     while (userInput!! > 0){
         var tmp = userInput % 10
         userInput /= 10
@@ -45,4 +91,22 @@ fun userInputToInt() : List<Int> {
     }
 
     return inputs
+}
+
+fun userInputCheck(userInput: Int?): Boolean {
+    if (userInput !in 0..100){
+        throw IllegalArgumentException("숫자는 세자리 이하의 정수여야 합니다.")
+        return false
+    }
+
+    return userInput is Int
+}
+
+fun menuCheck(menu: Int?): Boolean {
+    return if(menu is Int){
+        true
+    }else {
+        throw IllegalArgumentException("숫자를 입력하세요.")
+        false
+    }
 }
