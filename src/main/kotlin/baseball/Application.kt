382210e1ae7bf3a365aa856cc125e9bd9e9f31be
DormunDoorlock <skrudgh2001@naver.com>
2022-11-07 17:33:58 +0900

    feat: 1번 기능 구현. 정답 숫자 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,25 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    playNumberBaseballGame()
+}
+
+fun playNumberBaseballGame(){
+    val answerNumberArray= makeAnswerNumber()
+    //print(answerNumberArray)
+}
+
+fun makeAnswerNumber():List<Int>{
+    val answerNumberArray = mutableListOf<Int>()
+    while (answerNumberArray.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!answerNumberArray.contains(randomNumber)) {
+            answerNumberArray.add(randomNumber)
+        }
+    }
+    return answerNumberArray
 }
