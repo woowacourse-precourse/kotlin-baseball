9619df1d67e1c25b35f32bfb31660ddb7d65c7c8
Grandelwald <artjr97@naver.com>
2022-11-08 07:31:14 +0900

    Game.kt 컴퓨터 숫자 초기화 함수 생성, 스트라이크 볼 점수 초기화 함수 생성

--- src/main/kotlin/baseball/domain/Game.kt
+++ src/main/kotlin/baseball/domain/Game.kt
@@ -1,16 +1,36 @@
 package baseball.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Game(var strike: Int = 0, var ball: Int = 0) {
 
-    private val computerNumber = mutableListOf<Int>()
+    val computerNumber = mutableListOf<Int>()
+
+    init {
+        generateComputerNumber()
+    }
+
+    private fun generateComputerNumber() {
+        while (computerNumber.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber)
+            }
+        }
+    }
 
     fun compare(userNumber: List<Int>) {
         userNumber.onEachIndexed { index, number ->
-            if(number == computerNumber[index]) {
+            if (number == computerNumber[index]) {
                 strike += 1
-            } else if(userNumber.contains(number)){
+            } else if (computerNumber.contains(number)) {
                 ball += 1
             }
         }
+    }
+
+    fun resetScore() {
+        strike = 0
+        ball = 0
     }
 }
