cdc4cda2b674bb53a8e1a74e8ee31ffc71ba58fa
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-07 17:52:20 +0900

    feat(BaseBallGame): printGuide - 숫자를 입력받기 위해 안내 문구를 출력하는 메소드 정의

--- src/main/kotlin/baseball/BaseBallGame.kt
+++ src/main/kotlin/baseball/BaseBallGame.kt
@@ -19,6 +19,10 @@ class BaseBallGame {
         return answerList.joinToString("")
     }
 
+    private fun printGuide() {
+        println("숫자 야구 게임을 시작합니다.")
+        print("숫자를 입력해주세요 : ")
+    }
     companion object {
         private const val RUNNING = 1
         private const val RESTART = 2
