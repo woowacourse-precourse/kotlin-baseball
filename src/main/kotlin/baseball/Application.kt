326dcf4235c544f34b65fa4d2947c9c1fb91eb4c
Hyemin Song <hyemdooly@gmail.com>
2022-11-07 12:32:48 +0900

    docs and feat(docs and application): add docs and 3 rand number pick

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,27 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+import java.util.IllegalFormatCodePointException
+
+fun pickNumbers(): List<Char> {
+    val result = mutableListOf<Char>()
+    for(i in 1..3) {
+        result.add(Randoms.pickNumberInRange(1, 9).toChar())
+    }
+    return result
+}
+
 fun main() {
-    TODO("프로그램 구현")
+    val userNumbers: List<Char>
+
+    println("숫자 야구 게임을 시작합니다.")
+    do {
+        val randNumbers = pickNumbers()
+
+
+
+
+
+    } while(true)
 }
