baac170aacdad1ea19496ccedf6bb0e936183b99
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-07 20:30:10 +0900

    feat: [게임진행] 컴퓨터 숫자 생성 기능 추가
    getNum()
    getComputerNum() 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,7 +1,22 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     printStartMessage()
 }
 
 fun printStartMessage() = "숫자 야구 게임을 시작합니다."
+
+fun getNum() = Randoms.pickNumberInRange(0, 9)
+
+fun getComputerNum(): MutableList<Int> {
+    val numbers = mutableListOf<Int>()
+
+    while (numbers.size != 3){
+        val number = getNum()
+        if (!numbers.contains(number)) numbers.add(number)
+    }
+
+    return numbers
+}
