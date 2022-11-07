7d166403fe053c5d87fc8678b28924fd36fb8860
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-08 01:27:47 +0900

    [Test] assertCheck 입력 값 상수화
    COMPUTER_INPUT_ONE_TWO_THREE: "123"
    EXPECTED_NONE_STRIKE: 스트라이크 없음 예상됨
    EXPECTED_ONE_BALL: 볼 1개 예상됨

--- src/test/kotlin/baseball/BaseballGameTest.kt
+++ src/test/kotlin/baseball/BaseballGameTest.kt
@@ -7,17 +7,17 @@ import org.junit.jupiter.api.assertThrows
 class BaseballGameTest {
     @Test
     fun `모두 스트라이크일 때 스트라이크 3개 반환`() {
-        assertCheck("123", "123", 3, 0)
+        assertCheck(COMPUTER_INPUT_ONE_TWO_THREE, "123", EXPECTED_THREE_STRIKE, EXPECTED_NONE_BALL)
     }
 
     @Test
     fun `모두 볼일 때 볼 3개 반환`() {
-        assertCheck("123", "312", 0, 3)
+        assertCheck(COMPUTER_INPUT_ONE_TWO_THREE, "312", EXPECTED_NONE_STRIKE, EXPECTED_THREE_BALL)
     }
 
     @Test
     fun `모두 오답일 때 낫싱을 반환`() {
-        assertCheck("123", "456", 0, 0)
+        assertCheck(COMPUTER_INPUT_ONE_TWO_THREE, "456", EXPECTED_NONE_STRIKE, EXPECTED_NONE_BALL)
 
     }
 
@@ -25,38 +25,38 @@ class BaseballGameTest {
     fun `사용자가 잘못된 값을 입력했을 때 IllegalArgumentException 반환`() {
         assertThrows<IllegalArgumentException> {
             throw IllegalArgumentException("잘못된 입력입니다.")
-            assertCheck("123", "1234", 0, 1)
+            assertCheck(COMPUTER_INPUT_ONE_TWO_THREE, "1234", EXPECTED_NONE_STRIKE, EXPECTED_ONE_BALL)
         }
     }
 
     @Test
     fun `오답 2개, 볼 1개일 때 볼 1개 반환`() {
-        assertCheck("123", "782", 0, 1)
+        assertCheck(COMPUTER_INPUT_ONE_TWO_THREE, "782", EXPECTED_NONE_STRIKE, EXPECTED_ONE_BALL)
     }
 
     @Test
     fun `오답 2개, 스트라이크 1개일 때 스트라이크 1개 반환`() {
-        assertCheck("123", "783", 1, 0)
+        assertCheck(COMPUTER_INPUT_ONE_TWO_THREE, "783", EXPECTED_ONE_STRIKE, EXPECTED_NONE_BALL)
     }
 
     @Test
     fun `오답 1개, 볼 2개일 때 볼 2개 반환`() {
-        assertCheck("123", "412", 0, 2)
+        assertCheck(COMPUTER_INPUT_ONE_TWO_THREE, "412", EXPECTED_NONE_STRIKE, EXPECTED_TWO_BALL)
     }
 
     @Test
     fun `오답 1개, 스트라이크 2개일 때 스트라이크 2개 반환`() {
-        assertCheck("123", "423", 2, 0)
+        assertCheck(COMPUTER_INPUT_ONE_TWO_THREE, "423", EXPECTED_TWO_STRIKE, EXPECTED_NONE_BALL)
     }
 
     @Test
     fun `볼이 2개, 스트라이크 1개일 때 볼 2개,스트라이크 1개 반환`() {
-        assertCheck("123", "132", 1, 2)
+        assertCheck(COMPUTER_INPUT_ONE_TWO_THREE, "132", EXPECTED_ONE_STRIKE, EXPECTED_TWO_BALL)
     }
 
     @Test
     fun `볼이 1개, 스트라이크 1개, 오답 1개일 때 볼 1개,스트라이크 1개 반환`() {
-        assertCheck("123", "243", 1, 1)
+        assertCheck(COMPUTER_INPUT_ONE_TWO_THREE, "243", EXPECTED_ONE_STRIKE, EXPECTED_ONE_BALL)
     }
 
 
@@ -73,3 +73,15 @@ class BaseballGameTest {
         assertThat(expectedBall).isEqualTo(checkBall)
     }
 }
+
+const val EXPECTED_NONE_BALL = 0
+const val EXPECTED_ONE_BALL = 1
+const val EXPECTED_TWO_BALL = 2
+const val EXPECTED_THREE_BALL = 3
+
+const val EXPECTED_NONE_STRIKE = 0
+const val EXPECTED_ONE_STRIKE = 1
+const val EXPECTED_TWO_STRIKE = 2
+const val EXPECTED_THREE_STRIKE = 3
+
+const val COMPUTER_INPUT_ONE_TWO_THREE = "123"
