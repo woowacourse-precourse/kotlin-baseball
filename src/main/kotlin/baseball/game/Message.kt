7fc087d5d3021099ab42b5df0aebfc3339d3e5bb
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-07 23:34:56 +0900

    feat(Message.kt): 클래스 생성 및 함수 정의
    함수 정의
    - showStartGame()
    - requestUserNumber()
    - showHint()
    - requestRestartGame()
    - showFinishGame()

---
+++ src/main/kotlin/baseball/game/Message.kt
@@ -0,0 +1,24 @@
+package baseball.game
+
+object Message {
+
+    fun showStartGame() {
+        TODO()
+    }
+
+    fun requestUserNumber(read: () -> String): String {
+        TODO()
+    }
+
+    fun showHint(ball: Int, strike: Int) {
+        TODO()
+    }
+
+    fun requestRestartGame(read: () -> String): Boolean {
+        TODO()
+    }
+
+    fun showFinishGame() {
+        TODO()
+    }
+}
