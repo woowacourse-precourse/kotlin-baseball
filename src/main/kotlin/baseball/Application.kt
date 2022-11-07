6cfccf7a07dd0b378f78879c61ef3e301b86d123
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-07 23:35:56 +0900

    feat: [게임진행] 사용자 숫자 입력 및 생성 기능 추가
    isNum()
    isThreeLength()
    isNotDuplicate()
    isSatisfyConditions()
    enterNum()
    numToList()
    getUserNum() 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,13 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
+import java.lang.IllegalArgumentException
 
 fun main() {
     printStartMessage()
+    val computerNum = getComputerNum()
+    val userNum = getUserNum()
 }
 
 fun printStartMessage() = "숫자 야구 게임을 시작합니다."
@@ -26,4 +30,54 @@ fun getComputerNum(): MutableList<Int> {
     }
 
     return numbers
+}
+
+fun isNum(string: String): Boolean {
+    return try {
+        string.toInt()
+        true
+    } catch (e: NumberFormatException) {
+        false
+    }
+}
+
+fun isThreeLength(string: String) = string.length == 3
+
+fun isNotDuplicate(string: String): Boolean {
+    var result = true
+
+    for (i in 1 until string.length)
+        if (string[0] == string[i]) result = false
+
+    return result
+}
+
+fun isSatisfyConditions(numbers: String): Boolean {
+    return isNum(numbers) &&
+            isThreeLength(numbers) &&
+            isNotDuplicate(numbers)
+}
+
+fun enterNum(): String {
+    print("\n숫자를 입력해주세요 : ")
+    return Console.readLine()
+}
+
+fun numToList(numbers: String): MutableList<Int> {
+    val result = mutableListOf<Int>()
+
+    for (i in numbers.indices)
+        result.add(numbers[i]
+                .toString()
+                .toInt()
+        )
+
+    return result
+}
+
+fun getUserNum(): MutableList<Int> {
+    val numbers = enterNum()
+
+    if (isSatisfyConditions(numbers)) return numToList(numbers)
+    else throw IllegalArgumentException("수를 잘못 입력하였습니다.")
 }
