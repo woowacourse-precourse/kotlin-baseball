8a44dbe162652199d047383bf64b02f306308d48
changhwan <chang5hustle@gmail.com>
2022-11-08 01:53:27 +0900

    feat : 랜덤 숫자문자열 생성 함수 작성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -9,3 +9,15 @@ fun main() {
 private fun printStartGameString() {
     println(START_PHRASE)
 }
+
+private fun getRandomNumberList(listLength: Int): MutableList<Int> {
+    val candidateNumberList = mutableListOf<Int>().apply { addAll(1..9) }
+    val randomNumberList = mutableListOf<Int>()
+    for (i in 1..listLength) {
+        val stageNumber = camp.nextstep.edu.missionutils.Randoms.pickNumberInList(candidateNumberList)
+        candidateNumberList.remove(stageNumber)
+        randomNumberList.add(stageNumber)
+    }
+
+    return randomNumberList
+}
