fa659f3dac15ed89252e45d0026df852841c838c
Yujin <jini1514@soongsil.ac.kr>
2022-11-07 16:24:50 +0900

    Feat : getComputerNumber 함수 분리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,16 +5,10 @@ import camp.nextstep.edu.missionutils.Randoms
 fun playGame(){
     //컴퓨터 숫자 만들기
     //리드미 파일을 참조함
-    val computer = mutableListOf<Int>()
-    while (computer.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber)
-        }
-    }
+    getComputerNumber()
     getUserNumber()
 
-    message(computer)
+    //message(computer)
     print("3개의 숫자를 모두 맞히셨습니다!")
     println("게임 종료")
 
@@ -24,7 +18,7 @@ fun playGame(){
 fun nextGame(){
     var select = 0
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    select= readLine().toInt()
+    select= readLine()!!.toInt()
     if(select==1)
         playGame()
     else if(select==2)
@@ -33,6 +27,16 @@ fun nextGame(){
 
 fun message():String{
     //판단 및 메시지 출력
+}
+
+fun getComputerNumber(){
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
 }
 
 fun getUserNumber(): String? {
