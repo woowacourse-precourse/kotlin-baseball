1da9ad52f0c2ac369d50c5fea4a234fe5ba8f841
[alsswl] <[alswl4223@naver.com]>
2022-11-08 00:39:21 +0900

    fix : 예외처리 수정
    input를 배열에 char로 저장, 예외처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,36 +4,42 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 import java.util.*
 
+fun checkTrue(input : CharArray){
+
+    if(input[0]== input[1])  throw IllegalArgumentException("서로 다른 수를 입력해야 합니다")
+    if(input[1] == input[2])  throw IllegalArgumentException("서로 다른 수를 입력해야 합니다")
+    if(input[2] == input[0])  throw IllegalArgumentException("서로 다른 수를 입력해야 합니다")
+
+    if(input.size != 3) throw IllegalArgumentException("세개의 숫자를 입력하여야 합니다.")
+
+    var IsNum = 0
+
+    for(i in 0 until 3){
+        IsNum = 0
+        for(j in '1' .. '9'){
+            if(input[i] == j){
+                IsNum = 1
+                break
+            }
+        }
+        if(IsNum == 0) throw IllegalArgumentException("숫자가 아닙니다.")
+        if(input[i] == '0') throw IllegalArgumentException("0이 포함되어있습니다.")
+    }
+}
+
 
 fun main() {
 
-    var input = readLine()
-    var InputInt = input?.toInt()
+    var Continue = 1
 
-    if (InputInt != null) {
-        if (InputInt < 100 || InputInt >= 1000) {
-            throw IllegalArgumentException("잘못입력하셨습니다.");
-        }
+    while(Continue == 1) {
+
+        val input = readLine()?.toCharArray()
+        if (input != null) {
+            checkTrue(input)
+        } else throw IllegalArgumentException("입력 값이 없습니다.");
+
     }
-
-
-
-    var InputFirst = InputInt!!/100;
-    var InputSecond = (InputInt!!-100*InputFirst)/10
-    var InputThird = InputInt/10
-
-    if(InputFirst == InputSecond)  throw IllegalArgumentException("서로 다른 수를 입력해야 합니다");
-    if(InputThird == InputSecond)  throw IllegalArgumentException("서로 다른 수를 입력해야 합니다");
-    if(InputFirst == InputThird)  throw IllegalArgumentException("서로 다른 수를 입력해야 합니다");
-
-
-
-
-
-
-
-
-
 
 
 
