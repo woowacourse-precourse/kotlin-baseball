97a26dece0f3b5d5179efaa0bebf81680221ce4f
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-07 23:51:02 +0900

    feat(ExceptionHandler.kt): 숫자 입력 예외 처리 함수 checkNumberFormat() 구현

--- src/main/kotlin/baseball/game/ExceptionHandler.kt
+++ src/main/kotlin/baseball/game/ExceptionHandler.kt
@@ -2,11 +2,18 @@ package baseball.game
 
 object ExceptionHandler {
 
-    fun checkNumberFormat(number: String) {
-        TODO()
+    fun checkNumberFormat(number: String): String {
+        if (number.length != 3)
+            throw IllegalArgumentException("number=$number, 3자리 숫자여야 합니다.")
+        if (number.toSet().size != 3)
+            throw IllegalArgumentException("number=$number, 중복된 숫자가 있으면 안 됩니다.")
+        if (number.any { it !in '1'..'9' })
+            throw IllegalArgumentException("number=$number, 각 자리의 숫자는 (1 ~ 9) 사이의 숫자여야 합니다.")
+
+        return number
     }
 
-    fun throwFinishCode(code: String) {
+    fun throwFinishCode(code: String): Nothing {
         TODO()
     }
 }
