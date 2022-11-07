32260b1ac7cfc08e29ec767bd98ff39d7f807d30
sojk401 <sojk401@gmail.com>
2022-11-08 03:05:21 +0900

    refactor: 입력받은 플레이어의 숫자가 컴퓨터 숫자에 하나도 없는 경우 낫싱 출력
    플레이어의 숫자를 하나씩 읽어 컴퓨터 숫자에 없는 경우 false를 반환하면 나머지 숫자가 컴퓨터 숫자에 포함되는 경우를 판별할 수 없음. bool 변수인 isNothing을 생성하여 모든 숫자를 판별할 수 있도록 하고, 컴퓨터 숫자에 포함된다면 isNothing을 false로 변경하여 마지막에 isNothing을 반환하도록 함

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,4 +1,5 @@
 package baseball
+
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
@@ -8,58 +9,56 @@ fun main() {
     var computerNumber = makeRandomNumber(3)
     println("랜덤숫자 : ${computerNumber}")
 
-    while (true){
+    while (true) {
         print("숫자를 입력해주세요 : ")
         var userInput = Console.readLine()
         println(userInput)
         checkNumberException(userInput)
 
-        if (!searchNothingCase(userInput, computerNumber)){
+        if (searchNothingCase(userInput, computerNumber)) {
             println("낫싱")
-        }
-        else{
+        } else {
+            println("컴퓨터 숫자에 포함된 숫자가 존재")
             TODO("낫싱이 아닌 경우")
         }
     }
 }
 
-fun makeRandomNumber(length: Int) : String {
+fun makeRandomNumber(length: Int): String {
     var noOverlapRandomNumber = mutableListOf<Int>()
 
-    while (noOverlapRandomNumber.size < 3){
+    while (noOverlapRandomNumber.size < 3) {
         var randNum = Randoms.pickNumberInRange(1, 9)
-        if (randNum !in noOverlapRandomNumber){
+        if (randNum !in noOverlapRandomNumber) {
             noOverlapRandomNumber.add(randNum)
         }
     }
     return noOverlapRandomNumber.joinToString("")
 }
 
-fun checkNumberException(userInput: String){
-    if (userInput.toIntOrNull() == null){
+fun checkNumberException(userInput: String) {
+    if (userInput.toIntOrNull() == null) {
         throw IllegalArgumentException("IllegalAccessException. 프로그램을 종료합니다.")
-    }
-    else if (userInput.length != 3){
+    } else if (userInput.length != 3) {
         throw IllegalArgumentException("IllegalAccessException. 프로그램을 종료합니다.")
     }
 
     var userNumber = mutableListOf<String>()
-    for (i in userNumber){
-        if (i !in userNumber){
+    for (i in userNumber) {
+        if (i !in userNumber) {
             userNumber.add(i)
-        }
-        else{
+        } else {
             throw IllegalArgumentException("IllegalAccessException. 프로그램을 종료합니다.")
         }
     }
 }
 
-fun searchNothingCase(userNum: String, computerNum: String) : Boolean {
-    for (i in userNum){
-        if (i !in computerNum){
-            return false
+fun searchNothingCase(userNum: String, computerNum: String): Boolean {
+    var isNothing = true
+    for (i in userNum) {
+        if (i in computerNum) {
+            isNothing = false
         }
     }
-    return true
+    return isNothing
 }
-
