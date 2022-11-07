9862f495d8d8150c6adc62eecebaf1ca41aaf69c
jklom0326 <jklom0326@gmail.com>
2022-11-08 00:35:16 +0900

    refactor(BaseBallGame.kt): 입력에 대한 예외 분리
    Exceptions.kt에 예외 처리 함수를 분리함

---
+++ src/main/kotlin/baseball/Exceptions.kt
@@ -0,0 +1,9 @@
+package baseball
+
+fun inputNumberException(input: String) {
+    when {
+        input.toSet().size < 3 -> throw IllegalArgumentException("중복된 수입니다.")
+        input.length > 3 -> throw IllegalArgumentException("3자리 수만 입력해주세요")
+        "[1-9]{3}".toRegex().matches(input) -> throw IllegalArgumentException("0을 제외한 숫자만 입력해 주세요")
+    }
+}
