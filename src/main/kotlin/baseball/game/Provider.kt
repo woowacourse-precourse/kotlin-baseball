fddc002c660f08b4dd1d3c3dc7db5598ede4a1f4
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-07 23:53:25 +0900

    feat(Provider.kt): 랜덤 숫자 뽑기 함수 pickRandomNumber() 구현

--- src/main/kotlin/baseball/game/Provider.kt
+++ src/main/kotlin/baseball/game/Provider.kt
@@ -1,12 +1,21 @@
 package baseball.game
 
-import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 object Provider {
 
     fun pickRandomNumber(): String {
-        TODO()
+        val computer = mutableListOf<Int>()
+
+        while (computer.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber)
+            }
+        }
+
+        return computer.joinToString("")
     }
 
     fun readLine(): String {
