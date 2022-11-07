3985872af0331ea537e242da335930ed435b49d9
2taezeat <xorudfl772@naver.com>
2022-11-07 21:50:14 +0900

    feat: 사용자가 잘못된 입력을 하면 IllegalArgumentException 예외를 throw 하는 함수 작성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,7 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import java.lang.IllegalArgumentException
 
 fun main() {
     startBaseBallGame()
@@ -12,6 +13,8 @@ private fun startBaseBallGame() {
     println("숫자 야구 게임을 시작합니다.")
     val computerRandomNumber = computerSelectThreeRandomNumber()
     println(computerRandomNumber)
+    val userNumber = userThreeNumberInput()
+    println(userNumber)
 }
 
 
@@ -22,4 +25,31 @@ private fun computerSelectThreeRandomNumber(): List<Int> {
         if (!computerList.contains(randomNumber)) computerList.add(randomNumber)
     }
     return computerList
+}
+
+
+private fun userThreeNumberInput(): List<Int> {
+    print("숫자를 입력해주세요 : ")
+    val userInput = readLine()
+    if (userInputExceptionCheck(userInput)) {
+        throw IllegalArgumentException()
+    }
+
+    return userInput!!.toList().map { it.digitToInt() }
+}
+
+
+
+private fun userInputExceptionCheck(inputString: String?): Boolean {
+    if (inputString == null || inputString == "") {
+        return true
+    }
+
+    for (code in inputString.toList().map { it.code }) {
+        if (code < 48 || code > 57) {
+            return true
+        }
+    }
+
+    return false
 }
