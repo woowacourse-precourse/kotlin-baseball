3da01e2c85414a5d2f15ba9a6baeb3708f53e0f0
dlgocks1 <cksgodl1@naver.com>
2022-11-07 11:17:27 +0900

    feat($Application) : 솔루션 코드 작성 및 로직관련 코드를 BaseBallModel로 넘김

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,30 +1,20 @@
 package baseball
 
-import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-//    TODO("프로그램 구현")
+    val baseballModel: BaseballModel = BaseballModel()
+    with(baseballModel) {
+        do {
+            print("숫자를 입력해주세요 : ")
+            val userInput = Console.readLine()
+            checkInputIsCorrect(userInput)
+            checkBalls(userInput)
+            setResultString()
+            // 결과 값을 출력한다.
+            isEndedCheck()
+        } while (/* 끝났는지 체크 */)
+    }
 }
 
-fun checkInputIsCorrect(userNum: String) {
-    val regex = Regex("[0-9]{3}")
-    require(userNum.matches(regex)) {
-        "$userNum 3자리가 아니거나, 숫자가 아닌 값"
-    }
-    require(userNum.toCharArray().distinct().size == 3) {
-        "$userNum 각각 다른 3자리 숫자가 아님"
-    }
-}
 
-
-/** 3개의 각각 다른 랜덤 값 생성한다. */
-fun makeRandomNum(): MutableList<Int> {
-    val computer = mutableListOf<Int>()
-    while (computer.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber)
-        }
-    }
-    return computer
-}
