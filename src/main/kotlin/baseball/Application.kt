7e0349ce04910b3cfe9c39a9621fecb64c249171
jinsilWoo <wryt9224@gmail.com>
2022-11-07 20:28:33 +0900

    [FEAT] 랜덤 수 구하기 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,17 @@
 package baseball
 
-fun main() {
-    TODO("프로그램 구현")
+import camp.nextstep.edu.missionutils.Randoms
+
+
+
+
+fun setRandomAnswer(): ArrayList<Int>{
+    var answer = arrayListOf<Int>()
+
+    while (answer.size < 3) {
+        val randomNum = Randoms.pickNumberInRange(1, 9)
+        if (!answer.contains(randomNum))
+            answer += randomNum
+    }
+    return answer
 }
