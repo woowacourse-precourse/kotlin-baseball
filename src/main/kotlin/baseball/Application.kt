bbeb083917443446e94c42d97a005f2c52701860
no1msh <no1msh1217@gmail.com>
2022-11-07 15:48:35 +0900

    feat: 기능 목록 7번 구현
    validationUserInputNumber 메소드
    - 사용자가 숫자를 올바르게 입력했는지 확인하고 틀리다면 IllegalArgumentException 발생
    validationUserInputRestart 메소드
    - 사용자가 재시작 여부를 입력할 떄 1또는 2의 값이 아니라면 IllegalArgumentException 발생

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -12,7 +12,8 @@ fun playNumberBaseball() {
     val answerNumber = createRandomAnswerNumber().toString()
     do {
         print("숫자를 입력해주세요 : ")
-        val userInputNumber = Console.readLine()
+        val userInputNumber = Console.readLine().trim()
+        validationUserInputNumber(userInputNumber)
         if (userInputNumber == answerNumber) {
             break
         }
@@ -75,13 +76,37 @@ fun printBallCount(ballCount: Pair<Int, Int>) {
 }
 
 fun isRestart(): Boolean {
-    val userInput = Console.readLine()
-
+    val userInput = Console.readLine().trim()
+    validationUserInputRestart(userInput)
     if (userInput == "1") {
         return true
     }
-    if (userInput == "2") {
-        return false
-    }
     return false
+}
+
+fun validationUserInputNumber(userInputNumber: String) {
+    if (userInputNumber.isEmpty()) {
+        throw IllegalArgumentException("입력값이 비었습니다.")
+    }
+    if (userInputNumber.contains(" ")){
+        throw IllegalArgumentException("공백을 포함하지 않고 입력해주세요.")
+    }
+    if (!userInputNumber.matches(Regex("^\\d*\$"))) {
+        throw IllegalArgumentException("숫자로 이루어진 값만 입력될 수 있습니다.")
+    }
+    if (userInputNumber.length != 3) {
+        throw IllegalArgumentException("3자리의 숫자를 입력해주세요.")
+    }
+    if (userInputNumber[0] == userInputNumber[1]
+        || userInputNumber[0] == userInputNumber[2]
+        || userInputNumber[1] == userInputNumber[2]
+    ) {
+        throw IllegalArgumentException("서로 다른 수로 이루어져야합니다.")
+    }
+}
+
+fun validationUserInputRestart(userInput: String) {
+    if (userInput != "1" && userInput != "2") {
+        throw IllegalArgumentException("1 또는 2의 값을 입력하세요.")
+    }
 }
