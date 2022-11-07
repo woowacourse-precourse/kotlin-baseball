0ee7c254c1b7419292a64f76abdfe21f91e59906
mendel <mendel.urssu@gmail.com>
2022-11-07 23:18:14 +0900

    feat: 기능 2번 GameProgress 추상 클래스 정의
    GameProgressImpl을 구현하는 GameProgress 추상 클래스를 정의

--- src/main/kotlin/baseball/GameProgressRule.kt
+++ src/main/kotlin/baseball/GameProgressRule.kt
@@ -3,3 +3,30 @@ package baseball
 interface GameProgressImpl {
     fun run()
 }
+
+abstract class GameProgress(
+    private val participant1: Participant,
+    private val participant2: Participant,
+    private val messageMaker: StringMaker,
+) : GameProgressImpl {
+    override fun run() {
+        println(messageMaker.initGameStartMessage())
+        do {
+            if (gameStart(participant1, participant2, messageMaker)) { //게임 성공하면 종료하고 나오면 됨.
+                println(messageMaker.gameClearMessage())
+            } else {
+                println(messageMaker.gameFailedMessage())
+            }
+            println(messageMaker.restartGameQuestionMessage())
+            val isRestart = isRestartGame(participant1) //게임 다시할지 말지...
+        } while (isRestart)
+    }
+
+    protected abstract fun isRestartGame(participant: Participant): Boolean
+
+    protected abstract fun gameStart(
+        participant1: Participant,
+        participant2: Participant,
+        messageMaker: StringMaker,
+    ): Boolean
+}
