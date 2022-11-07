67c4507daac7ef2dd051b88428f40ea9bf208a9e
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-07 17:57:07 +0900

    test(BaseBallGameTest): checkValidGuess - 입력결과 검사 함수 테스트 케이스 추가

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -50,6 +50,32 @@ class ApplicationTest : NsTest() {
         val getElement = method.invoke(baseBallGame, *parameters)
         assertThat(output()).isEqualTo("숫자 야구 게임을 시작합니다.\n숫자를 입력해주세요 :")
     }
+
+    @Test
+    fun `checkValidGuess 메서드출력 결과 확인`() {
+        val baseBallGame = BaseBallGame()
+
+        val method = baseBallGame.javaClass.getDeclaredMethod("checkValidGuess", String::class.java)
+        method.isAccessible = true
+
+        val parameters = arrayOfNulls<Any>(1)
+
+        parameters[0] = "111"
+        var getElement = method.invoke(baseBallGame, *parameters)
+        assertThat(getElement).isEqualTo(false)
+
+        parameters[0] = "123"
+        getElement = method.invoke(baseBallGame, *parameters)
+        assertThat(getElement).isEqualTo(true)
+
+        parameters[0] = "121"
+        getElement = method.invoke(baseBallGame, *parameters)
+        assertThat(getElement).isEqualTo(false)
+
+        parameters[0] = "1211"
+        getElement = method.invoke(baseBallGame, *parameters)
+        assertThat(getElement).isEqualTo(false)
+    }
     override fun runMain() {
         main()
     }
