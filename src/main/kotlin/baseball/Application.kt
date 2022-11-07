b8fa9a651c1790f3692bc98d4cf767f7176e1c93
person00333 <person00333@gmail.com>
2022-11-07 21:20:24 +0900

    feat : 야구 게임 알고리즘

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -11,6 +11,45 @@ fun randomNum(): List<Char> {
     return tmp.toString().toList().filter { it.isDigit() }
 }
 
+fun playGame(count:Int){
+
+    while (true) {
+        var comNum = randomNum()
+
+
+        var playerNum: List<Char>? = null
+        var ball = 0
+        var strike = 0
+        print("숫자를 입력해주세요 :")
+        playerNum = readLine()?.toList()?.distinct()
+        println()
+        if (playerNum!!.size != 3) {
+            throw IllegalArgumentException("입력 오류")
+            return
+        }
+
+        comNum.forEachIndexed { index, c ->
+            val p = playerNum!!.indexOf(c)
+            when {
+                p == index -> ++strike
+                p != -1 -> ++ball
+            }
+        }
+
+        if ((ball == 0) and (strike == 0)) {
+            println("낫싱")
+        } else if ((ball != 0) and (strike == 0)) {
+            println("${ball}볼")
+        } else if ((ball == 0) and (strike != 0)) {
+            println("${strike}스트라이크")
+            if (strike == 3) {
+                return
+            }
+        } else {
+            println("${ball}볼 ${strike}스트라이크")
+        }
+    }
+}
 
 
 fun main() {
@@ -21,6 +60,7 @@ fun main() {
     var count = 1
     while (gameStart == 1){
         try {
+            playGame(count)
             println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
             gameStart = readLine()!!.toInt()
             ++count
