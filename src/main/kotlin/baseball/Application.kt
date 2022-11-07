b3d57b5b1a13730fed0e32b23c3ee43c668e6858
s9hn <tnghqn@naver.com>
2022-11-07 16:37:17 +0900

    [FEAT] 컴퓨터 랜덤리스트 생성함수 추가 & 해당 기능목록 체크

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,21 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+
+    print(createRandomList())
+
+}
+
+fun createRandomList(): MutableList<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
+    return computer
 }
