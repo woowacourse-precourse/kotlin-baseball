958707a1eed8e870c68719a1da2158d7616a809c
junhyungss <daba44@naver.com>
2022-11-08 01:21:21 +0900

    feat: 컴퓨터에게 랜덤 숫자 3개 받기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,28 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    // 시작
+    while (true) {
+        // 랜덤 숫자 3개 받아오기
+        val computer = randomThreeNumber()
+
+
+    }
+}
+
+
+// 랜덤 3글자 추출
+private fun randomThreeNumber(): String {
+    println("숫자 야구 게임을 시작합니다.")
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (randomNumber !in computer) computer.add(randomNumber)
+    }
+
+    println(computer)
+
+    return computer.joinToString("")
 }
