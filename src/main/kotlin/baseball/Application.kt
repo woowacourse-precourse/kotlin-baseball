00de531dbf6a653bbb5df30bf5af600df0db7476
SeungHyeon Shim <tladhehd@naver.com>
2022-11-07 15:31:59 +0900

    <fix>: 새로 시작하는 것을 물어봤을 때 1,2외 IllegalArgumentException 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -111,6 +111,10 @@ fun askRegame(): Int { // 게임을 새로 시작할것인지 묻는 메서드
     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
     var restart = readLine().toInt()
+    if (restart != 1 && restart != 2){
+        throw IllegalArgumentException("1,2 외 입력하였음")
+    }
+
     return restart
 }
 
