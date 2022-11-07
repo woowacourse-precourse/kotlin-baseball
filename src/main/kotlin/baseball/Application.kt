5a4ff685d3ee7030d8ae01e42042bbdb7f7e8109
dlgocks1 <cksgodl1@naver.com>
2022-11-07 09:22:26 +0900

    feat() 3개의 랜덤 숫자 생성기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,20 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+//    TODO("프로그램 구현")
+}
+
+
+/** 3개의 각각 다른 랜덤 값 생성한다. */
+fun makeRandomNum(): MutableList<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
