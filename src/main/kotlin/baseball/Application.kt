503027d1371cc58eb78bf6d2802e4bf44cc9b60e
Jaeheon Jeong <drdd1120@naver.com>
2022-11-07 15:03:31 +0900

    1. 컴퓨터가 서로 다른 3자리의 난수를 만든다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,35 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    gameStart()
+    return
+}
+
+private fun gameStart() {
+    while (true) {
+        playNumBaseBall()
+    }
+}
+
+private fun playNumBaseBall() {
+    var com: String = makeRandomNumber()
+    println(com)
+}
+
+private fun makeRandomNumber(): String {
+    var com: String
+
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
+    com = "${computer[0]}${computer[1]}${computer[2]}"
+
+    return com
 }
