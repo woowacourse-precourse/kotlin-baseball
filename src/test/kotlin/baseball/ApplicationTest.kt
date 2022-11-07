e5e242082a489e19c356d88a15e972be3c737321
DormunDoorlock <skrudgh2001@naver.com>
2022-11-07 20:14:02 +0900

    test: 3번 사용자 입력값 과 정답 숫자와 비교 기능 테스트

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -54,6 +54,77 @@ class ApplicationTest : NsTest() {
         assertThat(result).isEqualTo(par)
     }
 
+    @Test
+    fun `strike,ball 세는 함수 테스트1`(){
+        val par1 = listOf(1,2,3)
+        val result = countStrikeBall(par1,par1)
+        assertThat(result).isEqualTo(Pair(3,0))
+    }
+
+    @Test
+    fun `strike,ball 세는 함수 테스트2`(){
+        val par1 = listOf(1,2,3)
+        val par2 = listOf(1,2,7)
+        val result = countStrikeBall(par1,par2)
+        assertThat(result).isEqualTo(Pair(2,0))
+    }
+
+    @Test
+    fun `strike,ball 세는 함수 테스트3`(){
+        val par1 = listOf(1,2,3)
+        val par2 = listOf(1,9,7)
+        val result = countStrikeBall(par1,par2)
+        assertThat(result).isEqualTo(Pair(1,0))
+    }
+
+    @Test
+    fun `strike,ball 세는 함수 테스트4`(){
+        val par1 = listOf(1,2,3)
+        val par2 = listOf(9,8,7)
+        val result = countStrikeBall(par1,par2)
+        assertThat(result).isEqualTo(Pair(0,0))
+    }
+
+    @Test
+    fun `strike,ball 세는 함수 테스트5`(){
+        val par1 = listOf(1,2,3)
+        val par2 = listOf(1,7,2)
+        val result = countStrikeBall(par1,par2)
+        assertThat(result).isEqualTo(Pair(1,1))
+    }
+
+    @Test
+    fun `strike,ball 세는 함수 테스트6`(){
+        val par1 = listOf(1,2,3)
+        val par2 = listOf(1,3,2)
+        val result = countStrikeBall(par1,par2)
+        assertThat(result).isEqualTo(Pair(1,2))
+    }
+
+    @Test
+    fun `strike,ball 세는 함수 테스트7`(){
+        val par1 = listOf(1,2,3)
+        val par2 = listOf(2,3,7)
+        val result = countStrikeBall(par1,par2)
+        assertThat(result).isEqualTo(Pair(0,2))
+    }
+
+    @Test
+    fun `strike,ball 세는 함수 테스트8`(){
+        val par1 = listOf(1,2,3)
+        val par2 = listOf(7,1,8)
+        val result = countStrikeBall(par1,par2)
+        assertThat(result).isEqualTo(Pair(0,1))
+    }
+
+    @Test
+    fun `strike,ball 세는 함수 테스트9`(){
+        val par1 = listOf(1,2,3)
+        val par2 = listOf(3,1,2)
+        val result = countStrikeBall(par1,par2)
+        assertThat(result).isEqualTo(Pair(0,3))
+    }
+
     override fun runMain() {
         main()
     }
