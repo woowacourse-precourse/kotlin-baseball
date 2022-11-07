391281b69f636885b9ccfb9d9a1eb7e172215bf3
82104 <tir2986@naver.com>
2022-11-07 22:43:44 +0900

    fix: 문자열 멀티라인 적용해제
    - multiline string 을 """ """ 로 적용 시 탭과 함께 출력 되어서 롤백

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -22,10 +22,7 @@ fun main() {
         }
 
         println(
-            """
-            3개의 숫자를 모두 맞히셨습니다! 게임 종료
-            게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.
-            """
+            "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
         )
         val input = Console.readLine()
         validateEndInput(input)
