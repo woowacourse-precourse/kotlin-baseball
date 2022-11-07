8ef16af538998d75c179409772914a272451a381
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-08 03:45:54 +0900

    fix(ScreenManipulator) : 불필요한 개행 문자가 들어가는 것을 수정

--- src/main/kotlin/baseball/ScreenManipulator.kt
+++ src/main/kotlin/baseball/ScreenManipulator.kt
@@ -8,7 +8,7 @@ object ScreenManipulator {
     private const val MSG_STRIKE = "스트라이크 "
     private const val MSG_BALL = "볼 "
     private const val THREE_STRIKE_GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
     private const val NOTHING = "낫싱"
     const val START_BASEBALLGAME = "숫자 야구 게임을 시작합니다.\n"
     const val REQUEST_USER_INPUT = "숫자를 입력해주세요 : "
@@ -21,12 +21,12 @@ object ScreenManipulator {
             strikeCount + ballCount == 0 -> showMessageToScreen(NOTHING)
             strikeCount == 3 -> {
                 showStrikeCount(strikeCount)
-                screen.newLine()
+                showNewLine()
                 showMessageToScreen(THREE_STRIKE_GAME_END)
             }
             strikeCount in 1..2 -> showStrikeCount(strikeCount)
         }
-        screen.newLine()
+        showNewLine()
     }
 
     fun showMessageToScreen(msg:String){
@@ -44,6 +44,11 @@ object ScreenManipulator {
 
     private fun showBallCount(ballCount: Int) {
         screen.write("${ballCount}${MSG_BALL}")
+        screen.flush()
+    }
+
+    private fun showNewLine(){
+        screen.write("\n")
         screen.flush()
     }
 
