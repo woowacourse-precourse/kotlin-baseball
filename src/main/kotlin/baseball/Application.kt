f3b593fc8d70e5d4c049e32d2dbc705b3430e6ff
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-07 22:15:55 +0900

    refacotr: chooseReplay함수 리팩토링

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,7 +2,7 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
 
-
+//class문 써보기
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
     playBaseball()
@@ -106,7 +106,13 @@ fun againInput(computerNumber: List<Int>) {
 fun chooseReplay() {
     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    when (readLine().toString()) {
-        "1" -> playBaseball()
+    val replayNumber = readLine().toString().toInt()
+    when{
+        replayNumber==1  -> {
+            playBaseball()
+        }
+        replayNumber != 2 -> {
+            throw IllegalArgumentException()
+        }
     }
 }
