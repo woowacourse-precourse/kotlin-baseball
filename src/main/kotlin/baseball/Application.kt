f2b6285e80aecff63913a53085654b922ad4a478
inseonyun <yuninseon@naver.com>
2022-11-07 17:44:34 +0900

    fix: 게임 결과 출력문 함수 문자열 수정
    게임 결과 출력시 3스트라이크일 때 3스트라이크도 출력 해주도록 함

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -73,10 +73,10 @@ fun gameResultString(inputResult: List<Int>): String {
     }
 
     if(strikeCount > 0) {
-        printString += "${strikeCount}스트라이크"
+        printString = "${strikeCount}스트라이크"
 
         if(strikeCount == 3)
-            printString = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+            printString += "\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
                     "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
     }
 
