64b0d2ca7f4142e5102bcd428ab3a4a27aa7366c
dlwgnud <wngud980411@naver.com>
2022-11-07 17:14:36 +0900

    feat(checkInput): 잘못된 값을 입력할 경우 예외처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     val computerNumber = getRandomNumber()
     printGameStart()
-    inputUserNumber()
-
+    val userNumber = inputUserNumber()
+    if(!checkInput(userNumber)) throw IllegalArgumentException()
 }
 
 fun getRandomNumber(): MutableList<Int> {
@@ -29,3 +29,11 @@ fun inputUserNumber(): String {
     return readLine().toString()
 }
 
+fun checkInput(number: String): Boolean {
+    if(number.length != 3) return false
+    number.forEach {
+        if(it !in '1'..'9') return false
+    }
+    if(number.toSet().size != 3) return false
+    return true
+}
