87ad5b2fcea5e05c2a8567a244c7e88e9d5527f1
SEO-J17 <qw4643987@naver.com>
2022-11-08 03:31:15 +0900

    #1 refactor: 불필요한 함수,코드 삭제, 게임 시작하기 위한 코드 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,30 +1,5 @@
 package baseball
 
-import camp.nextstep.edu.missionutils.Console
-import camp.nextstep.edu.missionutils.Randoms
-
 fun main() {
-    val targetNumber = createNumber()
-    val inputNumber = enterNumber()
-}
-
-fun createNumber(): String {
-    println("숫자 야구 게임을 시작합니다.")
-    val numberList = mutableListOf<Int>()
-    while (numberList.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!numberList.contains(randomNumber)) {
-            numberList.add(randomNumber)
-        }
-    }
-    return numberList.joinToString("")
-}
-
-fun enterNumber(): String {
-    print("숫자를 입력해주세요: ")
-    return Console.readLine().trim().apply {
-        if (length != 3 || !all { it.isDigit() } || isBlank()) {
-            throw IllegalArgumentException("올바르지 않은 입력값입니다.")
-        }
-    }
+    StartGame().start()
 }
