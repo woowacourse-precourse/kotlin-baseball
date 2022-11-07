b51284285055bcbd917adc7daf2a2bc94d3a0669
choiwoosuk <wooseokchoi0508@gmail.com>
2022-11-07 13:42:49 +0900

    refactor(Game): 게임 클래스 코드 리팩토링
    스트라이크와 볼의 개수를 출력할 때 있던 불필요한 조건문 삭제
    THREE_STRIKE_MESSAGE 출력문구 수정

--- src/main/kotlin/baseball/Game.kt
+++ src/main/kotlin/baseball/Game.kt
@@ -6,6 +6,8 @@ class Game {
     private val computer = Computer()
     private val gameManager = GameManager()
     private val exception = Exception()
+    private var ball=0
+    private var strike=0
 
     fun start() {
         computer.number = computer.generateComputerNumber()
@@ -14,8 +16,8 @@ class Game {
             user.input = user.getInputFromKeyboard()
             exception.checkAllException(user.input)
             user.number = user.getUserNumber(user.input)
-            var ball = gameManager.countBall(user.number, computer.number)
-            val strike = gameManager.countStrike(user.number, computer.number)
+            ball = gameManager.countBall(user.number, computer.number)
+            strike = gameManager.countStrike(user.number, computer.number)
             ball = gameManager.getFinalBallCount(ball, strike)
             printBallAndStrike(ball, strike)
         } while (!gameManager.isThreeStrike(strike))
@@ -51,12 +53,10 @@ class Game {
     }
 
     private fun printBallAndStrike(ball: Int, strike: Int) {
-        if (!gameManager.isThreeStrike(strike)) {
             printBall(ball)
             printStrike(strike)
             printNothing(ball, strike)
             println("")
-        }
     }
 
     private fun printThreeStrike() {
@@ -71,7 +71,7 @@ class Game {
         const val RESTART = "1"
         const val START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다."
         const val ENTER_NUMBER_MESSAGE = "숫자를 입력해주세요 : "
-        const val THREE_STRIKE_MESSAGE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+        const val THREE_STRIKE_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
         const val RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
         const val NOTHING_MESSAGE = "낫싱"
         const val STRIKE_MESSAGE = "스트라이크"
