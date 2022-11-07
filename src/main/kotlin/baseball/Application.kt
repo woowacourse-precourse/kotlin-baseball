98039890ed649f09c2cb644c732c982b3672a51d
JinJoa <mitejoa@gmail.com>
2022-11-07 10:13:11 +0900

    feat : 플레이어에게 값 입력받기
    fun playerNumber() 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,7 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
 }
@@ -15,3 +16,14 @@ fun computerNumber() : List<Int>{
     }
     return computer
 }
+
+fun playerNumber() : List<Int>{
+    print("숫자를 입력해주세요 :")
+    val playerInput = Console.readLine()!!
+
+    val player = mutableListOf<Int>()
+    playerInput.forEach { player.add(it-'0') }
+
+    return player
+}
+
