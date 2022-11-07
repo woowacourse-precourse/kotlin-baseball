3d784a22435447243cf2ac7e18804b206a256f2b
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-07 18:32:07 +0900

    refactor(test): 테스트 케이스 파일별로 분리

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -27,55 +27,6 @@ class ApplicationTest : NsTest() {
             assertThrows<IllegalArgumentException> { runException("1234") }
         }
     }
-    @Test
-    fun `generateAnswer 메소드 반환 결과 확인`() {
-        val baseBallGame = BaseBallGame()
-        val method = baseBallGame.javaClass.getDeclaredMethod("generateAnswer")
-        method.isAccessible = true
-
-        val parameters = arrayOfNulls<Any>(0)
-        val getElement: String = method.invoke(baseBallGame, *parameters) as String
-        val guessRegex = "[1-9]{3}".toRegex()
-
-        assertTrue(guessRegex.matches(getElement) && getElement.toSet().size == 3)
-    }
-
-    @Test
-    fun `printGuide 메서드출력 결과 확인`() {
-        val baseBallGame = BaseBallGame()
-        val method = baseBallGame.javaClass.getDeclaredMethod("printGuide")
-        method.isAccessible = true
-
-        val parameters = arrayOfNulls<Any>(0)
-        val getElement = method.invoke(baseBallGame, *parameters)
-        assertThat(output()).isEqualTo("숫자 야구 게임을 시작합니다.\n숫자를 입력해주세요 :")
-    }
-
-    @Test
-    fun `checkValidGuess 메서드출력 결과 확인`() {
-        val baseBallGame = BaseBallGame()
-
-        val method = baseBallGame.javaClass.getDeclaredMethod("checkValidGuess", String::class.java)
-        method.isAccessible = true
-
-        val parameters = arrayOfNulls<Any>(1)
-
-        parameters[0] = "111"
-        var getElement = method.invoke(baseBallGame, *parameters)
-        assertThat(getElement).isEqualTo(false)
-
-        parameters[0] = "123"
-        getElement = method.invoke(baseBallGame, *parameters)
-        assertThat(getElement).isEqualTo(true)
-
-        parameters[0] = "121"
-        getElement = method.invoke(baseBallGame, *parameters)
-        assertThat(getElement).isEqualTo(false)
-
-        parameters[0] = "1211"
-        getElement = method.invoke(baseBallGame, *parameters)
-        assertThat(getElement).isEqualTo(false)
-    }
     override fun runMain() {
         main()
     }
