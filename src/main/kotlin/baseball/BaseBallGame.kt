a6af68d09606e86f6be243bfd515500b26d98e1b
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-07 17:50:29 +0900

    feat(BaseBallGame): 실행 상황을 나타내는 클래스 상수 생성

--- src/main/kotlin/baseball/BaseBallGame.kt
+++ src/main/kotlin/baseball/BaseBallGame.kt
@@ -3,6 +3,7 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 
 class BaseBallGame {
+    private var progressStatus = RUNNING
     private fun generateAnswer(): String {
         val answerList = mutableListOf<Int>()
         while (answerList.size < 3) {
@@ -12,5 +13,11 @@ class BaseBallGame {
             }
         }
         return answerList.joinToString("")
+    }
+
+    companion object {
+        private const val RUNNING = 1
+        private const val RESTART = 2
+        private const val END = -1
     }
 }
