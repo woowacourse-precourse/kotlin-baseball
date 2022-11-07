0e440bdd88a83888bf299d4f8d7c56518ebe0b51
yelm <21yrshin@naver.com>
2022-11-08 00:47:56 +0900

    userInputToInt

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,30 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+
 fun main() {
-    TODO("프로그램 구현")
+    var userInputToInt = userInputToInt()
+
+
+    var computer : List<Any> = mutableListOf()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.plus(randomNumber)
+        }
+    }
+}
+fun userInputToInt() : IntArray{
+    var userInput : Int? = readLine()?.toInt()
+    var inputs = intArrayOf()
+
+    while (userInput!! > 0){
+        var tmp = userInput % 10
+        userInput /= 10
+        inputs.plus(tmp)
+    }
+
+    return inputs
 }
