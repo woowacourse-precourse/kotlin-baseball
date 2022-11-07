93758cfea18bd6e47edb670fbde8504d8a7528c7
jklom0326 <jklom0326@gmail.com>
2022-11-08 00:49:45 +0900

    refactor(Exception.kt): 문자열을 상수 처리함

--- src/main/kotlin/baseball/Exceptions.kt
+++ src/main/kotlin/baseball/Exceptions.kt
@@ -1,9 +1,13 @@
 package baseball
 
+const val DUPLICATED_NUMBER = "중복된 수입니다."
+const val NOT_THREE_NUMBER = "3자리 수만 입력해주세요"
+const val NOT_NUMBER = "0을 제외한 숫자만 입력해 주세요"
+
 fun inputNumberException(input: String) {
     when {
-        input.toSet().size < 3 -> throw IllegalArgumentException("중복된 수입니다.")
-        input.length > 3 -> throw IllegalArgumentException("3자리 수만 입력해주세요")
-        !("[1-9]{3}".toRegex().matches(input)) -> throw IllegalArgumentException("0을 제외한 숫자만 입력해 주세요")
+        input.toSet().size < 3 -> throw IllegalArgumentException(DUPLICATED_NUMBER)
+        input.length > 3 -> throw IllegalArgumentException(NOT_THREE_NUMBER)
+        !("[1-9]{3}".toRegex().matches(input)) -> throw IllegalArgumentException(NOT_NUMBER)
     }
 }
