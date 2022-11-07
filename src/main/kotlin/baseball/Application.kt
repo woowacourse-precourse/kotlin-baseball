0da2802975ad349ab0ef6958e509a2450ea2174c
junhyungss <daba44@naver.com>
2022-11-08 01:24:57 +0900

    feat: 사용자에게 숫자 받기(예외처리 포함)

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,6 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
@@ -18,10 +19,38 @@ fun main() {
 // 게임 시작
 private fun gameStart(computer: String): Boolean {
     while (true) {
+        // 사용자 입력 숫자
+        val userNumber = guessNumber()
 
 
 
     }
+}
+
+// 사용자에게 숫자 받기
+private fun guessNumber(): String {
+    print("숫자를 입력해주세요 : ")
+    val userNumber = Console.readLine()
+
+    var check = true
+
+
+    // 중복 숫자 체크
+    val number = userNumber.toSet().joinToString("")
+    // 3개가 아닐 경우 에러
+    if (number.length != 3) check = false
+    // 문자 체크
+    for (i in number) {
+        // 1~9까지만
+        if (i !in '1'..'9') {
+            check = false
+            break
+        }
+    }
+
+    // 예외처리
+    if (check) return number
+    throw IllegalArgumentException("숫자 입력 조건에 맞지 않음 : $number")
 }
 
 // 랜덤 3글자 추출
