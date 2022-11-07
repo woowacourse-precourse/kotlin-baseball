53b813251f8e840e2bc0899380ebea0385656448
joh9911 <joh9911@naver.com>
2022-11-07 22:20:40 +0900

    refactor: getUserNumberForRestart 함수 삭제

--- src/main/kotlin/baseball/BaseballGame.kt
+++ src/main/kotlin/baseball/BaseballGame.kt
@@ -10,7 +10,9 @@ class BaseballGame {
         println("숫자 야구 게임을 시작합니다.")
         while(true){
             processGame()
-            if (getNumberForRestartGame() == 2){
+            val input = readLine()
+            ThrowException().throwExceptionForRestartNumber(input)
+            if (input?.toInt() == 2){
                 println("게임 종료.")
                 break
             }
@@ -32,16 +34,6 @@ class BaseballGame {
                 break
             }
         }
-    }
-
-    /**
-     * 숫자를 입력받고 예외 체크 후 반환하는 함수
-     */
-    fun getNumberForRestartGame(): Int{
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        val input = readLine()
-        ThrowException().throwExceptionForRestartNumber(input)
-        return input?.toInt()!!
     }
 
     /**
