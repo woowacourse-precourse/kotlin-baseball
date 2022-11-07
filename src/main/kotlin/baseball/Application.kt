3928b21ddaea1493c814a508f7075ee33a9e27aa
hyungmin <utopia96@hanmail.net>
2022-11-07 22:04:21 +0900

    feat:  야구게임 로직 구현
    - 게임 시작 메시지 출력
    - 사용자의 입력값과 컴퓨터의 숫자를 index별로 비교하여 ball,Strike 판별
    - 사용자의 입력값중 illegalArgumentException 발생시 게임종료되도록 예외처리
    - 사용자의 입력값에 대해 ball,Strike판별이 끝난뒤, ballCnt, strikeCnt에 따라 결과 메시지 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,15 +1,93 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    val user = getUserInput()
-    println(user)
+    val com = getComInput()
+    println("숫자 야구 게임시작합니다")
+    var exitFlag = false
+    while (!exitFlag) {
+        exitFlag = startBaseballGame(com)
+    }
+    println("게임 종료")
+}
+
+fun startBaseballGame(com: List<Int>): Boolean {
+
+    val user = getUserInput().map { it - '0' }.toList()
+    if(user.isEmpty()){
+        return true
+    }
+    var strikeCnt = 0
+    var ballCnt = 0
+
+    for (i in user.indices) {
+        when (checkBallOrStrike(i, user[i], com)) {
+            "Strike" -> strikeCnt++
+            "Ball" -> ballCnt++
+            else -> {}
+        }
+    }
+    when(val message = displayResult(strikeCnt, ballCnt)){
+        "Nothing"->{
+            println("낫싱")
+        }
+        "Finish"->{
+            println("3 스트라이크")
+            // 종료 재시작 로직 추가 필요
+            return true
+        }
+        else -> println(message)
+    }
+
+    return false
+}
+
+fun displayResult(strikeCnt:Int, ballCnt:Int):String{
+    return if(strikeCnt==0 && ballCnt==0) {
+        "Nothing"
+    }
+    else if(strikeCnt==3) {
+        "Finish"
+    }
+    else {
+        "${ballCnt}볼 ${strikeCnt}스트라이크"
+    }
+}
+
+fun checkBallOrStrike(i: Int, num: Int, com: List<Int>): String {
+    for (j in com.indices) {
+        return if (com[j] == num && j == i) {
+            "Strike"
+        } else if (com[j] == num) {
+            "Ball"
+        } else continue
+    }
+    return "Nothing"
+}
+
+fun getComInput(): List<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
 
 fun getUserInput(): String {
+    print("숫자를 입력해주세요: ")
     val input = Console.readLine()
-    return if (userInputValidation(input)) input else ""
+    try{
+        userInputValidation(input)
+    }catch (e: java.lang.IllegalArgumentException){
+        return ""
+    }
+    return input
+
 }
 
 fun userInputValidation(input: String): Boolean {
