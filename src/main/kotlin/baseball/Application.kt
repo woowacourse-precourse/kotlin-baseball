fb7c6dfa13b1efa919f1f54aad26e21590faeb55
Isaac0424 <eodud3858@gmail.com>
2022-11-07 18:05:38 +0900

    create askeContinue() and Exeption about Illeagal Arguments in askContinue()

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,24 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+fun play(){
+
+}
+fun askContinue(): Boolean{
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    var answer: String = Console.readLine()
+    if(answer !in "12"){
+        throw IllegalArgumentException("입력 값이 1 또는 2가 아닙니다.")
+    }
+    return answer=="1"
+}
+
 fun main() {
-    TODO("프로그램 구현")
+    var flag: Boolean = true
+    println("숫자 야구 게임을 시작합니다.")
+    while(flag){
+        play()
+        flag = askContinue()
+    }
 }
