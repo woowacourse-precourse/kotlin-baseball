16750e5800877a7627166495c85501be537c3b02
leehandsub <gustjq7027@naver.com>
2022-11-07 18:35:13 +0900

    feat(baseball): 자리와 숫자 모두 같으면 스트라이크로 저장

--- src/main/kotlin/baseball/Baseball.kt
+++ src/main/kotlin/baseball/Baseball.kt
@@ -5,14 +5,35 @@ class Baseball {
     private val computer = Computer()
     private val user = User()
 
-    init{
+    init {
         playGame()
     }
 
     private fun playGame() {
-        while(true){
+        while (true) {
             val computerList = computer.makeComputerList()
             val userList = user.inputUser()
+
+            checkBaseBall(computerList, userList)
         }
+    }
+
+    private fun checkBaseBall(computerList: MutableList<Int>, userList: MutableList<Int>) {
+        val strike = checkStrike(computerList, userList)
+        val ball = checkBall(computerList, userList) - strike
+
+    }
+
+    private fun checkStrike(computerList: MutableList<Int>, userList: MutableList<Int>): Int {
+        var strike = 0
+        for (i in 0..userList.size) {
+            if (userList[i] == computerList[i])
+                strike++
+        }
+        return strike
+    }
+
+    private fun checkBall(computerList: MutableList<Int>, userList: MutableList<Int>): Int {
+
     }
 }
