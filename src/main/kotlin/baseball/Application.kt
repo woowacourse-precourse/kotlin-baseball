ee71544c48043a415749a8b7736b598ae2e46395
Jaehun-Jo <chojh922@gmail.com>
2022-11-07 17:55:14 +0900

    [Chores] 상수 선언

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,6 +3,9 @@ package baseball
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
+const val RESTART = 1
+const val EXIT = 2
+
 fun main() {
     TODO("프로그램 구현")
 }
