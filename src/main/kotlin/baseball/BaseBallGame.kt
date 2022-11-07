619ee3c31582246bfd21b7500d90220306ce08d5
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-07 17:48:49 +0900

    feat(BaseBallGame): generateAnswer - 숫자 야구의 정답 문자열 랜덤 생성 함수 정의

--- src/main/kotlin/baseball/BaseBallGame.kt
+++ src/main/kotlin/baseball/BaseBallGame.kt
@@ -1,5 +1,16 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class BaseBallGame {
-    // Do Nothing
+    private fun generateAnswer(): String {
+        val answerList = mutableListOf<Int>()
+        while (answerList.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (answerList.contains(randomNumber).not()) {
+                answerList.add(randomNumber)
+            }
+        }
+        return answerList.joinToString("")
+    }
 }
