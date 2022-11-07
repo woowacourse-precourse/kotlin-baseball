3243b1b47fb540dfffa51aeddd8afff46ff6d344
Jaeheon Jeong <drdd1120@naver.com>
2022-11-07 15:09:47 +0900

    4. 1와 2이 서로 같다면(성공했다면) 게임을 종료한다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -33,6 +33,10 @@ private fun playNumBaseBall() {
         }
         // 판정
         checkStrikeOrBall(user, com)
+        if (user == com) {
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            break
+        }
     }
 }
 
