ae222162afdde648119a05b7781028e6ad4e22c1
wonseok <dnjstjr245@gmail.com>
2022-11-08 14:56:54 +0900

    :recycle: 랜덤 숫자를 생성하는 Computer 클래스 분리

--- src/main/kotlin/baseball/model/Computer.kt
+++ src/main/kotlin/baseball/model/Computer.kt
@@ -1,18 +1,22 @@
-package baseball
+package baseball.model
 
+import baseball.utils.Constant.CNT_NUMBER
+import baseball.utils.Constant.DEFAULT_STRING
+import baseball.utils.Constant.MAX_NUMBER
+import baseball.utils.Constant.MIN_NUMBER
 import camp.nextstep.edu.missionutils.Randoms
 
 object Computer {
 
-    var randomNumber: String = ""
+    var randomNumber: String = DEFAULT_STRING
         private set
 
     fun generateRandomNumber() {
         val randomNumbersSet = mutableSetOf<Int>()
 
-        while (randomNumbersSet.size != 3) {
+        while (randomNumbersSet.size != CNT_NUMBER) {
             randomNumbersSet.add(
-                Randoms.pickNumberInRange(1, 9)
+                Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER)
             )
         }
         randomNumber = randomNumbersSet.joinToString("")
