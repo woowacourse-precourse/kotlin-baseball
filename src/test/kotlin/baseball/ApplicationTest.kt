63ca2b0e086c803bec8d716f4f98ca2e05f39b2e
lkeonwoo94 <lkeonw0o@naver.com>
2022-11-08 20:05:52 +0900

    test($ApplicationTest) : create testsuite for application
    Added new testsuite to $Application:
    -  ⌨️SEQ1. UserInput
        - [ ] `입력 값 @datatype` `IllegalArgumentException`
        - [ ] 입력 값 범위 : `1~9`
        - [ ] 입력 값 중복 : `서로 다른 수`
        - [ ] 입력 자리 수 : `3자리`
    - 💻 SEQ3.종료조건
        - [ ] **if 1 입력**  : goto  ⌨️SEQ1.
        - [ ] **if 2 입력**  : program termination
        - [ ] **else** : `TODO` Exception occurred

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -9,7 +9,42 @@ import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest : NsTest() {
     @Test
-    fun `게임종료 후 재시작`() {
+    fun `입력값데이터타입1`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException(null) }
+        }
+    }
+
+    @Test
+    fun `입력값데이터타입2`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("12a") }
+        }
+    }
+
+    @Test
+    fun `입력값범위`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("120") }
+        }
+    }
+
+    @Test
+    fun `입력중복`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("122") }
+        }
+    }
+
+    @Test
+    fun `!입력3자리`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("1234") }
+        }
+    }
+
+    @Test
+    fun `게임종료 후 재시작 if 1`() {
         assertRandomNumberInRangeTest(
             {
                 run("246", "135", "1", "597", "589", "2")
@@ -21,10 +56,25 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
-    fun `예외 테스트`() {
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("1234") }
-        }
+    fun `게임종료 if 2`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("135", "2")
+                assertThat(output())
+                    .contains("3스트라이크", "게임 종료")
+            },
+            1, 3, 5
+        )
+    }
+
+    @Test
+    fun `게임종료 else`() {
+        assertRandomNumberInRangeTest(
+            {
+                assertThrows<IllegalArgumentException> { runException("135", "3") }
+            },
+            1, 3, 5
+        )
     }
 
     override fun runMain() {
