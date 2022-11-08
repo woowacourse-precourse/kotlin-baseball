b10e2cde8415e35a32a8a532fc297fcd1536b22e
username <ieejo716@naver.com>
2022-11-08 23:28:17 +0900

    [FEAT] : 테스트 케이스 작성

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -28,20 +28,20 @@ class ApplicationTest : NsTest() {
 
     @Test
     fun `게임 시작 메시지 출력 테스트`() {
-        printGameStartText()
+        printPlayBallText()
         val result = "숫자 야구 게임을 시작합니다."
         assertThat(output()).isEqualTo(result)
     }
 
+
     @Nested
     inner class ComputerTest {
-        private val computerNumberList = mutableListOf<Int>()
 
         @Test
         fun `컴퓨터 난수로 생성된 숫자 개수 및 중복 테스트`() {
-            inputComputerNumbers(computerNumberList)
-            assertThat(computerNumberList.size).isEqualTo(3)
-            assertThat(computerNumberList.size).isEqualTo(computerNumberList.toSet().size)
+            val numbers = setComputerNumber()
+            assertThat(numbers.size).isEqualTo(3)
+            assertThat(numbers.size).isEqualTo(numbers.toSet().size)
         }
     }
 
@@ -49,51 +49,45 @@ class ApplicationTest : NsTest() {
     inner class UserTest {
         @Test
         fun `유저가 입력한 숫자의 길이 테스트`() {
-            val result = inputUserNumberExceptionCheck(mutableListOf(3, 2, 1))
+            val result = userNumberSizeCheck(mutableListOf(3, 2, 1))
             assertThat(result).isEqualTo(false)
         }
 
         @Test
         fun `유저가 입력한 숫자 중복 테스트`() {
-            val result = inputUserNumberExceptionCheck(mutableListOf(3, 2, 2))
+            val result = userNumberOverlapCheck(mutableListOf(3, 2, 2))
             assertThat(result).isEqualTo(true)
+        }
+
+        @Test
+        fun `유저가 입력한 숫자 범위 테스트`() {
+            val result = userNumberRangeException(mutableListOf(3, 2, 1))
+            assertThat(result).isEqualTo(false)
         }
     }
 
     @Nested
     inner class GameTest {
         private val computerNumbers = mutableListOf<Int>(1, 2, 3)
-        val test1 = mutableListOf(4, 5, 6)
-        val test2 = mutableListOf(3, 7, 8)
-        val test3 = mutableListOf(7, 8, 3)
-        val test4 = mutableListOf(1, 5, 3)
-        val test5 = mutableListOf(1, 2, 3)
 
         @Test
-        fun `게임 종료 검사`() {
-            assertThat(checkUserNumbers(test1, computerNumbers)).isEqualTo(false)
-            assertThat(checkUserNumbers(test2, computerNumbers)).isEqualTo(false)
-            assertThat(checkUserNumbers(test3, computerNumbers)).isEqualTo(false)
-            assertThat(checkUserNumbers(test4, computerNumbers)).isEqualTo(false)
-            assertThat(checkUserNumbers(test5, computerNumbers)).isEqualTo(true)
+        fun `게임 종료 조건 검사`() {
+            assertThat(compareGameValues(computerNumbers, "243")).isEqualTo(false)
+            assertThat(compareGameValues(computerNumbers, "567")).isEqualTo(false)
+            assertThat(compareGameValues(computerNumbers, "234")).isEqualTo(false)
+            assertThat(compareGameValues(computerNumbers, "678")).isEqualTo(false)
+            assertThat(compareGameValues(computerNumbers, "123")).isEqualTo(true)
         }
 
         @Test
         fun `게임 결과 검사`() {
-            printUserCount(3, 0)
-            assertThat(output()).isEqualTo("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            printCompareResult(3, 0)
+            assertThat(output()).isEqualTo("3스트라이크")
+        }
 
-//            printUserCount(1, 0)
-//            assertThat(output()).isEqualTo("1스트라이크")
-//
-//            printUserCount(0, 1)
-//            assertThat(output()).isEqualTo("1볼")
-//
-//            printUserCount(0, 0)
-//            assertThat(output()).isEqualTo("낫싱")
-//
-//            printUserCount(1, 1)
-//            assertThat(output()).isEqualTo("1볼 1스트라이크")
+        @Test
+        fun `게임 종료 값 검사`() {
+            assertThat(restartCheck(2))
         }
     }
 
