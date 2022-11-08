565852ed4a7d6714c300b428b452e7f77c591a6c
[alsswl] <[alswl4223@naver.com]>
2022-11-08 14:56:15 +0900

    fix : input를 char형태에서 int형태로 바꿈

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,30 +2,29 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
+import org.mockito.internal.matchers.Null
 import java.util.*
 
-fun checkTrue(input : CharArray){
+fun checkTrue(input : String) : Array<Int>{
+    var inputNum = input.toIntOrNull()
+    if(inputNum == null) throw IllegalArgumentException("숫자가 아닙니다.")
+    if(inputNum<100||inputNum>=1000) throw IllegalArgumentException("세개의 숫자를 입력하여야 합니다.")
 
+    var first = inputNum/100
+    var second = (inputNum-first*100)/10
+    var third = inputNum%10
+
+    var input : Array<Int> = arrayOf(first,second,third)
+
+    if(input[0] == 0||input[1] == 0||input[2] == 0) throw IllegalArgumentException("0이 포함되어있습니다.")
     if(input[0]== input[1])  throw IllegalArgumentException("서로 다른 수를 입력해야 합니다")
     if(input[1] == input[2])  throw IllegalArgumentException("서로 다른 수를 입력해야 합니다")
     if(input[2] == input[0])  throw IllegalArgumentException("서로 다른 수를 입력해야 합니다")
+    return input
 
-    if(input.size != 3) throw IllegalArgumentException("세개의 숫자를 입력하여야 합니다.")
-
-    var isNum = 0
-
-    for(i in 0 until 3){
-        isNum = 0
-        for(j in '1' .. '9'){
-            if(input[i] == j){
-                isNum = 1
-                break
-            }
-        }
-        if(isNum == 0) throw IllegalArgumentException("숫자가 아닙니다.")
-        if(input[i] == '0') throw IllegalArgumentException("0이 포함되어있습니다.")
     }
-}
+
+
 
 fun random():MutableList<Int>{
     val computer = mutableListOf<Int>()
@@ -37,14 +36,14 @@ fun random():MutableList<Int>{
     }
     return computer
 }
-fun checkCorrect(now : Char, input:CharArray):Int{
+fun checkCorrect(now : Int, input:Array<Int>):Int{
     for(i in 0 until 3){
         if(now == input[i]) return i
     }
     return -1
 }
 
-fun countCorrect(computer:MutableList<Char>,input:CharArray):MutableList<Int>{
+fun countCorrect(computer:MutableList<Int>,input:Array<Int>):MutableList<Int>{
     var strike_ball = mutableListOf<Int>()
     strike_ball.add(0)
     strike_ball.add(0)
@@ -58,13 +57,6 @@ fun countCorrect(computer:MutableList<Char>,input:CharArray):MutableList<Int>{
     return strike_ball
 }
 
-fun changeToChar(get : MutableList<Int>):MutableList<Char>{
-    var getToChar = mutableListOf<Char>()
-    for(i in 0 until 3){
-        getToChar[i] = (get[i]+48).toChar()
-    }
-    return getToChar
-}
 
 fun print(strike_ball: MutableList<Int>) : Int{
     if (strike_ball[0] == 0 && strike_ball[1] == 0) {
@@ -82,31 +74,31 @@ fun print(strike_ball: MutableList<Int>) : Int{
     }
 }
 
-fun input(Computer: MutableList<Char>):Int{
+fun input(Computer: MutableList<Int>){
     while(true) {
-        val input = readLine()?.toCharArray()
+        val input = readLine()!!.toString()
+        var input_array : Array<Int> = arrayOf(1,2,3)
         if (input != null) {
-            checkTrue(input)
+             input_array = checkTrue(input)
         } else throw IllegalArgumentException("입력 값이 없습니다.");
-        var strike_ball = countCorrect(Computer, input)
 
-        if (print(strike_ball) == 1)
-            return 0
+        val strike_ball = countCorrect(Computer, input_array)
+
+        if (print(strike_ball) == 1) return
 
     }
 
 }
 
-
+//메인
 fun main() {
 
     var Continue = 1
 
     while (Continue == 1) {
         val ComputerNum = random()
-        val ComputerChar = changeToChar(ComputerNum)
 
-        var input = input(ComputerChar)
+       input(ComputerNum)
 
         println("모든 수를 맞히셨습니다. 계속하려면 1, 종료하려면 2를 눌러주세요")
         var input_again = readLine()
