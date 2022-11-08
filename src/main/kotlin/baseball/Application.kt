d32316148762df06c4869304a4c3202e71a62da3
최재훈 <be_clever@naver.com>
2022-11-08 13:27:34 +0900

    feat(start): 난수 생성 및 저장 추가
    난수를 통해 정답을 생성하여 반환하는 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,23 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
 
 }
 
 fun printStartNotice() {
     print("숫자 야구 게임을 시작합니다.")
+}
+
+fun createAnswer(): MutableList<Int> {
+    val answer = mutableListOf<Int>()
+
+    while (answer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!answer.contains(randomNumber)) {
+            answer.add(randomNumber)
+        }
+    }
+    return answer
 }
