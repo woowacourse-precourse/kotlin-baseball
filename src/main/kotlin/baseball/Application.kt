3b1a7d177f8a0fd6d97b4a9038a744da83acb840
bmlsj <gaga2353@gmail.com>
2022-11-08 16:18:45 +0900

    feat : 컴퓨터(상대방)의 랜덤한 3자리 수 생성 함수

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,21 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+
+}
+fun computerNumber(): MutableList<Int> {
+    // 1부터 9까지의 랜덤한 3가지 수 생성
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
+    // println("computer Input 값 : $computer")
+    return computer
 }
