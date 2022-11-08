284a672ccbfcdf90b12e754f9c78a9fed4fb5f4b
s9hn <tnghqn@naver.com>
2022-11-08 13:37:21 +0900

    [FEAT] 스트라이크 카운트 함수 추가 & 해당 기능목록 체크

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -9,6 +9,7 @@ const val gameScoreNothing = 3
 const val gameEnd = 4
 const val gameReplay = 5
 var ballCount = 0
+var strikeCount = 0
 
 
 fun main() {
@@ -21,6 +22,7 @@ fun main() {
     println(createRandomNum())
     println(createUserNum())
     println(ballCount(userNum, comNum))
+    println(strikeCount(userNum, comNum))
 }
 
 fun createRandomNum(): MutableList<Int> {
@@ -55,3 +57,13 @@ fun ballCount(userNum: List<Int>, comNum: List<Int>) {
     }
 }
 
+fun strikeCount(userNum: List<Int>, comNum: List<Int>) {
+    strikeCount = 0
+
+    for (index in userNum.indices) {
+        if (userNum[index] == comNum[index]) {
+            strikeCount++
+            ballCount--
+        }
+    }
+}
