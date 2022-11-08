bae224798b7576addbb5c014cfc0fb9f4f9f580e
Hyuna Kim <akxmcse@gmail.com>
2022-11-08 18:08:04 +0900

    refactor: 게임 결과를 출력하는 함수 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,12 +1,14 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
+import kotlin.IllegalArgumentException
 
 fun main() {
     TODO("프로그램 구현")
 }
 
-private fun setRandomNumber():String {
+private fun setRandomNumber(): String {
     val computer: MutableList<Int> = mutableListOf()
     while (computer.size < 3) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
@@ -21,8 +23,8 @@ private fun getNumberOfStrike(computer: String, user: String): Int {
     val computerNumber: String = computer
     val userNumber: String = user
     var numberOfStrike = 0
-    for(i in 0..2){
-        if(computerNumber[i] == userNumber[i]){
+    for (i in 0..2) {
+        if (computerNumber[i] == userNumber[i]) {
             numberOfStrike++
         }
     }
@@ -33,23 +35,26 @@ private fun getNumberOfBall(computer: String, user: String): Int {
     val computerNumber: String = computer
     val userNumber: String = user
     var numberOfBall = 0
-    for(i in 0..2){
-        if(computerNumber.contains(userNumber[i]) && computerNumber[i] != userNumber[i]){
+    for (i in 0..2) {
+        if (computerNumber.contains(userNumber[i]) && computerNumber[i] != userNumber[i]) {
             numberOfBall++
         }
     }
     return numberOfBall
 }
 
-private fun printGameResult(computer: String, user: String){
+private fun printGameResult(computer: String, user: String) {
     val numberOfBall = getNumberOfBall(computer, user)
     val numberOfStrike = getNumberOfStrike(computer, user)
     when {
+        numberOfBall != 0 && numberOfStrike !=0 -> {
+            print("${numberOfBall}볼 ${numberOfStrike}스트라이크")
+        }
         numberOfBall != 0 -> {
-            print(numberOfBall.toString()+"볼")
+            print("${numberOfBall}볼")
         }
         numberOfStrike != 0 -> {
-            print(numberOfStrike.toString()+"스트라이크")
+            print("${numberOfStrike}스트라이크")
         }
         else -> {
             print("낫싱")
