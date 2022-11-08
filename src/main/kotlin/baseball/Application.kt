8fe80c0ce3a9eabb9cf58b3814fc2bec6a02544c
sososochar <dlwps12345@naver.com>
2022-11-08 14:52:16 +0900

    오류 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,51 +5,66 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    val computer = mutableListOf<Int>()
-    while (computer.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber)
-        }
+    var computer = mutableListOf<Int>()
+    var num = 0
+    var end = true
+    computer = Random_Num()
+    while (end){
+        if (num == 1) computer = Random_Num()
+        num = k_Baseball(computer)
+        end = Retry(num)
     }
-    var num : Int = readLine()!!.toInt()
-    k_Baseball(num, computer)
-
 }
-fun k_Baseball(num : Int, comp : MutableList<Int>){
+
+fun Retry(num : Int) : Boolean{
+    if (num == 2) return false
+    else return true
+}
+
+fun k_Baseball(comp : MutableList<Int>) : Int{
+    print("숫자를 입력해주세요 : ")
+    var num : Int = readLine()!!.toInt()
     val list = mutableListOf<Int>()
     var str = 0
     var ball = 0
     var result = 1
+    list.add((num / 100).toInt())
+    num = num % 100
+    list.add((num / 10).toInt())
+    num = num % 10
+    list.add(num)
+
     for (i in 0..2){
         if (comp[i] == list[i]) str++
         else if (list[i] in comp) ball++
         else continue
     }
 
-    while (result < 2) {
-        if (str == 0 && ball == 0) str0_ball0()
-        else if (str == 0) only_Ball(ball)
-        else if (str == 3) Clear()
-        else if (ball == 0) only_Strike(str)
-        else str_ball(str, ball)
-    }
+        if (str == 0 && ball == 0) return str0_ball0()
+        else if (str == 0) return only_Ball(ball)
+        else if (str == 3) return Clear()
+        else if (ball == 0) return only_Strike(str)
+        else return str_ball(str, ball)
 
 }
-fun str0_ball0(){
-    return println("낫싱")
+fun str0_ball0() : Int{
+    println("낫싱")
+    return 0
 }
 
-fun only_Ball(ball : Int){
-    return println("${ball}볼")
+fun only_Ball(ball : Int) : Int{
+    println("${ball}볼")
+    return 0
 }
 
-fun only_Strike(str : Int){
-    return println("${str}스트라이크")
+fun only_Strike(str : Int) : Int{
+    println("${str}스트라이크")
+    return 0
 }
 
-fun str_ball(str : Int, ball : Int){
+fun str_ball(str : Int, ball : Int) : Int{
     println("${ball}볼 ${str}스트라이크")
+    return 0
 }
 
 fun Clear() : Int{
@@ -58,4 +73,15 @@ fun Clear() : Int{
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
     var n : Int = readLine()!!.toInt()
     return n
+}
+
+fun Random_Num() : MutableList<Int>{
+    var computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
