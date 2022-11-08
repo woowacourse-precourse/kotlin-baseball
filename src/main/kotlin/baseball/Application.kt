78b3667326791c89d40fcc564d871aab4d3dcfed
changhwan <chang5hustle@gmail.com>
2022-11-08 10:01:44 +0900

    feat : ONGOING_INPUT 상태 일시 입력받은후 예외처리 로직 작성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,6 +3,7 @@ package baseball
 import baseball.GameState.ONGOING_STATE
 import baseball.InputState.ONGOING_INPUT
 import baseball.InputState.RESTART_INPUT
+import baseball.NumberBaseBallGamePhrases.ONGOING_INPUT_PHRASE
 import baseball.NumberBaseBallGamePhrases.START_PHRASE
 import java.util.regex.Pattern
 
@@ -13,8 +14,15 @@ fun main() {
     val inputState = ONGOING_INPUT
 
     while (gameState == ONGOING_STATE) {
+        lateinit var stepUserInput: String
+
         when (inputState) {
             ONGOING_INPUT -> {
+                print(ONGOING_INPUT_PHRASE)
+                stepUserInput = camp.nextstep.edu.missionutils.Console.readLine()
+                if (!checkInputLength(stepUserInput, 3)) throw IllegalArgumentException("입력된 숫자의 길이가 적절치 않습니다.")
+                if (!isInputTypeNumber(stepUserInput)) throw IllegalArgumentException("입력이 숫자타입이 아닙니다")
+                if (!checkDuplicatePartInInput(stepUserInput)) throw IllegalArgumentException("중복된 숫자가 입력되었습니다.")
             }
             RESTART_INPUT -> {
             }
