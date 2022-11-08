e970fe4c4bad87801b1a50fb4468a1e010259c79
lee989898 <wnsrud1352@gmail.com>
2022-11-08 20:18:40 +0900

    [feat](ComputerRandomNumber) 3자리 난수 생성에 사용되는 난수 최소값, 최대값 상수 값 추가

--- src/main/kotlin/baseball/ComputerRandomNumber.kt
+++ src/main/kotlin/baseball/ComputerRandomNumber.kt
@@ -1,3 +1,9 @@
 package baseball
 
-class ComputerRandomNumber
+class ComputerRandomNumber {
+
+    companion object {
+        const val RANDOM_NUMBER_MIN_RANGE = 1
+        const val RANDOM_NUMBER_MAX_RANGE = 9
+    }
+}
