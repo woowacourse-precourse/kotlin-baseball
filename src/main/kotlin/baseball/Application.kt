4f594962e431eeb29bcf9dedc8521b7a14c2befd
Hyeongseon <hsun5163@gmail.com>
2022-11-08 12:29:52 +0900

    refactor : main 함수에서 게임 동작이 아닌 GameLogic 클래스에서 게임 진행

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,23 +1,9 @@
 package baseball
 fun main() {
     val gp = GamePrint()
-    val gl = GameLogic()
-
-    var count = 0
-    while(true){
-        gp.gameStart()
-        gl.gameStart()
-        while (true){
-            gp.inputNum()
-            gl.userInput()
-            gl.userNumToList(gl.number)
-            gl.comparison()
-            gp.result()
-            count += 1
-            if(count == 10){
-                break
-            }
-        }
-        break
-    }
+    println("숫자 야구 게임을 시작합니다.")
+    gp.gameStart()
 }
+
+
+
