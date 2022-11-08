00937eb808d7fa2c175de3fe7a4f127c427182f0
lanpia <95679243+lanpia@users.noreply.github.com>
2022-11-08 22:32:50 +0900

    헤더 & 변수 선언 1.strike : 값과 위치가 맞음 2.ball : 값만 맞음 3.inputnum : User의 입력 값 4.comAnser : 랜덤으로 생성된 해답

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,10 @@
 package baseball
+import kotlin.text.toInt
+import kotlin.io.println
+import kotlin.collections.mutableListOf
+import camp.nextstep.edu.missionutils.Randoms
 
-fun main() {
-    TODO("프로그램 구현")
-}
+var strike :Int = 0
+var ball : Int = 0
+val inputnum : Array<Int?> = arrayOfNulls<Int>(3)
+val comAnswer : Array<Int?> = arrayOfNulls<Int>(3)
