a150abe821fcf64d0ee6a5cdaa6e635519921b17
yeongjin <dudwls901@naver.com>
2022-11-08 14:27:38 +0900

    [Refactor] 사용자 숫자 입력 기회 수정 3번 -> 맞을 때까지

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -100,8 +100,8 @@ fun matchNumber(input: Int, resultNum: Int): Boolean {
 
 fun play(resultNum: Int) {
     println("숫자야구 게임을 시작합니다")
-    val chance = 3
-    for(i in 0 until chance) {
+
+    while(true) {
         print("숫자를 입력해주세요 : ")
         val input = Console.readLine().mappingInputNumber()
         if (matchNumber(input, resultNum)) {
