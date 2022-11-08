119fbb7991131a20f31cab6217b388c224bf314f
navillera11 <lunatickblue@naver.com>
2022-11-08 23:40:39 +0900

    feat: 게임 시작 여부를 물어보는 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,12 @@
 package baseball
-fun main(){
-    println("숫자 야구 게임을 시작합니다.")
-    playGame()
+
+fun askRestart(){
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    val select= Console.readLine()
+    if(select=="1"){
+        playGame()
+    }
+    if(select=="2"){
+        endMessage()
+    }
 }
