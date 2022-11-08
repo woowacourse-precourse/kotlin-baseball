aa89323ca8f29863ce668b297624e8f6745f123c
wonseok <dnjstjr245@gmail.com>
2022-11-08 14:56:23 +0900

    :recycle: Referee 클래스 분리 (controller, 게임 진행 역할)

--- src/main/kotlin/baseball/controller/Referee.kt
+++ src/main/kotlin/baseball/controller/Referee.kt
@@ -1,24 +1,74 @@
-package baseball
+package baseball.controller
 
-import baseball.Computer.generateRandomNumber
-import baseball.Printer.printGameStartPhrase
-import baseball.Printer.printGuidelinePhrase
+import baseball.utils.Constant.ACTIVE_STATE_CODE
+import baseball.utils.Constant.QUIT_STATE_CODE
+import baseball.utils.ResultType
+import baseball.model.Computer
+import baseball.model.Computer.generateRandomNumber
+import baseball.utils.Calculator
+import baseball.view.Player
+import baseball.view.Printer.printCorrectResult
+import baseball.view.Printer.printExitGamePhrase
+import baseball.view.Printer.printGameStartPhrase
+import baseball.view.Printer.printNormalResult
+import baseball.view.Printer.printNothingPhrase
+import baseball.view.Printer.printWinGamePhrase
 
-object Referee {
+class Referee(
+    private val player: Player
+) {
+
+    lateinit var curGameStateCode: String
+        private set
 
     init {
         printGameStartPhrase()
     }
 
-    fun gameStart() {
+    fun startGame() {
+        curGameStateCode = ACTIVE_STATE_CODE
         generateRandomNumber()
         processBaseBallGame()
     }
 
     private fun processBaseBallGame() {
-        while (true) {
-            printGuidelinePhrase()
-            Validator(userInput = readln().trim(), exceptionType = ExceptionType.GameException).validateUserInput()
+        while (curGameStateCode == ACTIVE_STATE_CODE) {
+            printGameResult(player.inputAnswerNumber().toGameResult())
         }
     }
+
+    private fun printGameResult(gameResultType: ResultType) {
+        when (gameResultType) {
+            is ResultType.NormalResult -> printNormalResult(gameResultType.ball, gameResultType.strike)
+            is ResultType.NothingResult -> printNothingPhrase()
+            is ResultType.CorrectResult -> {
+                printCorrectResult()
+                printWinGamePhrase()
+                when (player.inputExitCode()) {
+                    "1" -> this.startGame()
+                    "2" ->  {
+                        printExitGamePhrase()
+                        curGameStateCode = QUIT_STATE_CODE
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+private fun String.toGameResult(): ResultType {
+    val calculator = Calculator(userInput = this)
+    calculator.countBallAndStrike()
+
+    return if (this == Computer.randomNumber) ResultType.CorrectResult
+    else if (
+        !this.contains(Computer.randomNumber[0]) &&
+        !this.contains(Computer.randomNumber[1]) &&
+        !this.contains(Computer.randomNumber[2])
+    ) ResultType.NothingResult
+    else ResultType.NormalResult(
+        calculator.ballAndStrike.first,
+        calculator.ballAndStrike.second
+    )
 }
