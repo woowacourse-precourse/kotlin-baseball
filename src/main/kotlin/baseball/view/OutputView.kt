6500c22b06a36662cd363c56420a6b263763b9cf
Grandelwald <artjr97@naver.com>
2022-11-08 22:58:46 +0900

    OutputView.kt 출력형식 수정

--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -14,8 +14,15 @@ object OutputView {
             println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         } else if (game.strike == NOTHING && game.ball == NOTHING) {
             println("낫싱")
-        } else {
-            println("${game.strike} 스트라이크 ${game.ball} 볼")
+        }
+        else if(game.strike == NOTHING && game.ball != NOTHING){
+            println("${game.ball}볼")
+        }
+        else if (game.strike != NOTHING && game.ball == NOTHING){
+            println("${game.strike}스트라이크")
+        }
+        else {
+            println("${game.ball}볼 ${game.strike}스트라이크")
         }
     }
 }
