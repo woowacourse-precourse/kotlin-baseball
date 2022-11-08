e358782d09859b373b29a87f9325be44044828d4
Grandelwald <artjr97@naver.com>
2022-11-08 21:59:37 +0900

    InputView.kt 게임 재시작을 구분하는 숫자 입력 함수 수장

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -6,8 +6,8 @@ object InputView {
         return readln().map { Character.getNumericValue(it) }
     }
 
-    fun inputRestartingDecideNumber() : String  {
+    fun inputGameStatus() : Int  {
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        return readln()
+        return readln().toInt()
     }
 }
--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -5,22 +5,18 @@ import baseball.domain.Game
 object OutputView {
     fun printStaringMent(): Unit = print("숫자 야구 게임을 시작합니다.")
 
-    fun printResult(game: Game): Int {
-        return if (game.strike == 3) {
+    fun printResult(game: Game){
+        return if (game.strike == THREE_STRIKE) {
             println("3스트라이크")
             println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-            THREE_STRIKE
-        } else if (game.strike == 0 && game.ball == 0) {
+        } else if (game.strike == NOTHING && game.ball == NOTHING) {
             println("낫싱")
-            NOTHING
         } else {
             println("${game.strike} 스트라이크 ${game.ball} 볼")
-            STRIKE_AND_BALL
         }
     }
 
 
-    const val THREE_STRIKE = 0
-    const val NOTHING = 1
-    const val STRIKE_AND_BALL = 2
+    const val THREE_STRIKE = 3
+    private const val NOTHING = 0
 }
