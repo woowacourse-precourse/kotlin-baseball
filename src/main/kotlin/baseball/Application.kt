67f4cbda6b61ded5ed6af1206efea100e0c7044d
choHarmony <jws0183@naver.com>
2022-11-08 18:57:50 +0900

    feat: 컴퓨터가 랜덤한 세 자리 수 선택

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,20 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+lateinit var computerNum: ArrayList<Int>
+
 fun main() {
-    TODO("프로그램 구현")
+
+}
+
+fun makeComputerChoose(): ArrayList<Int> {
+    computerNum = ArrayList()
+    while (computerNum.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computerNum.contains(randomNumber)) computerNum.add(randomNumber)
+    }
+
+    return computerNum
 }
