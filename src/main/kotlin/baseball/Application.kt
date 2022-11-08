d2ba2aad916f4bf789713e1e7bc5161789869487
username <ieejo716@naver.com>
2022-11-08 22:57:17 +0900

    [FEAT] : 3자리의 중복 없는 난수 발생

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,4 +8,19 @@ fun main() {
     playBall()
 }
 
+fun playBall() {
+    val computerNumber = setComputerNumber()
+}
+
 fun printPlayBallText() = println("숫자 야구 게임을 시작합니다.")
+
+fun setComputerNumber(): List<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
