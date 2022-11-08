4b6c264c93186505098c8a9ce3db1cd1a5522d59
noion0511 <noion0511@gmail.com>
2022-11-08 16:22:14 +0900

    Feat : 컴퓨터 숫자와 사용자의 숫자를 비교하는 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,33 +5,56 @@ import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    println(setRandomNumber())
-    println(getUserNumber())
+    val randomNumber = setRandomNumber()
+    val userNumber = getUserNumber()
+    countBaseballScore(randomNumber, userNumber)
+}
+
+
+fun countBaseballScore(randomNumber: List<Int>, userNumber: String) {
+    var ball = 0
+    var strike = 0
+
+    userNumber.forEach {
+        if (randomNumber.contains(it.code)) ball += 1
+    }
+
+    for (i in 0 until 3) {
+        if (randomNumber[i] == userNumber[i].code) strike += 1
+    }
+
+    ball -= strike
 }
 
 
 fun getUserNumber(): String {
     val userNumber = readLine()
+
     if(isInvalidNumber(userNumber)) {
         throw IllegalArgumentException()
     }
+
     return readLine()
 }
 
 
 fun setRandomNumber(): List<Int> {
     val baseballNumber = mutableListOf<Int>()
+
     while (baseballNumber.size < 3) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
+
         if (!baseballNumber.contains(randomNumber)) {
             baseballNumber.add(randomNumber)
         }
     }
+
     return baseballNumber
 }
 
 
 fun isInvalidNumber(userNumber : String) : Boolean{
     val regex = Regex("^[0-9]{3}\$")
+
     return !regex.matches(userNumber)
 }
