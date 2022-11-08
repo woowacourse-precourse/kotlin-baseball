978878142273439da2a24d48a7bb755833d76333
DormunDoorlock <skrudgh2001@naver.com>
2022-11-08 17:21:58 +0900

    feat: 기능 5번 6번 구현값이 3 strike 인 경우, 멘트 및 게임 시작, 종료 결정 질문 출력, 아닐 경우 2번 과정으로 이동 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,14 +5,24 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    playNumberBaseballGame()
+    do {
+        playNumberBaseballGame()
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val userInput = Console.readLine()
+        if(userInput=="1")
+            continue
+        else if(userInput == "2")
+            break
+        else
+            throw IllegalArgumentException()
+    }while (true)
+    print("게임 종료")
 }
 
 fun playNumberBaseballGame(){
     val answerNumberArray= makeAnswerNumber()
-
     do {
-        print("숫자를 입력해주세요 : ")
+        println("숫자를 입력해주세요 : ")
         val userInput = Console.readLine()
         val userInputArray = makeNumToList(userInput.toInt())
         if(!checkValidNumber(userInputArray))
