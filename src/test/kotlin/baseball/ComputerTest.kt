293fa0bff6b208834bbbd7a61c88fbfb84202d06
EmilyCh0 <seoyeon3089@naver.com>
2022-11-08 11:33:30 +0900

    test(ComputerTest): 정답과 유저입력 비교 결과 테스트

--- src/test/kotlin/baseball/ComputerTest.kt
+++ src/test/kotlin/baseball/ComputerTest.kt
@@ -3,6 +3,13 @@ package baseball
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+private const val NOTHING_MESSAGE = "낫싱"
+private const val BALL_MESSAGE = "볼"
+private const val STRIKE_MESSAGE = "스트라이크"
+private val output = ByteArrayOutputStream()
 
 class ComputerTest {
     @Test
@@ -15,6 +22,34 @@ class ComputerTest {
         assertThat(answer.distinct().size).isEqualTo(3)
     }
 
+    @Test
+    fun `스트라이크 확인`() {
+        computer.testSetAnswer(arrayListOf(1, 2, 3))
+        computer.compareGuessAndAnswer(listOf(1, 5, 6))
+        assertThat(output.toString().trim()).isEqualTo("1$STRIKE_MESSAGE")
+    }
+
+    @Test
+    fun `볼 확인`() {
+        computer.testSetAnswer(arrayListOf(1, 2, 3))
+        computer.compareGuessAndAnswer(listOf(2, 1, 5))
+        assertThat(output.toString().trim()).isEqualTo("2$BALL_MESSAGE")
+    }
+
+    @Test
+    fun `볼 스트라이크 확인`() {
+        computer.testSetAnswer(arrayListOf(1, 2, 3))
+        computer.compareGuessAndAnswer(listOf(1, 3, 2))
+        assertThat(output.toString().trim()).isEqualTo("2$BALL_MESSAGE 1$STRIKE_MESSAGE")
+    }
+
+    @Test
+    fun `낫싱 확인`() {
+        computer.testSetAnswer(arrayListOf(1, 2, 3))
+        computer.compareGuessAndAnswer(listOf(6, 5, 4))
+        assertThat(output.toString().trim()).isEqualTo(NOTHING_MESSAGE)
+    }
+
     companion object {
         lateinit var computer: Computer
         lateinit var answer: ArrayList<Int>
@@ -25,6 +60,12 @@ class ComputerTest {
             computer = Computer()
             computer.generateAnswer()
             answer = computer.testGetAnswer()
+        }
+
+        @BeforeAll
+        @JvmStatic
+        internal fun setUpOutputStream() {
+            System.setOut(PrintStream(output))
         }
     }
 }
