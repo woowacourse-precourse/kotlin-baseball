fcb32d88fa12de2cc4cedf2d80e9de89003c6bf1
lkeonwoo94 <lkeonw0o@naver.com>
2022-11-08 21:03:39 +0900

    feat($Application): Create GameStartMessage object
    Added print message to $Application
    - '숫자 야구게임을 시작합니다.'

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,9 +5,12 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     while(true){
         // TODO()
+        println("숫자 야구 게임을 시작합니다.")
+
         if(getEndloopCondition()) break
     }
 }
+
 
 fun getEndloopCondition(): Boolean {
     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
