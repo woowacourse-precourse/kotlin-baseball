adb525dcdb65c58555469bbdfb33187a7151dbb5
changhwan <chang5hustle@gmail.com>
2022-11-08 11:16:45 +0900

    feat : RESTART_INPUT 상태 일시 입력받은후 예외처리 로직 작성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,7 +4,9 @@ import baseball.GameState.ONGOING_STATE
 import baseball.InputState.ONGOING_INPUT
 import baseball.InputState.RESTART_INPUT
 import baseball.NumberBaseBallGamePhrases.ONGOING_INPUT_PHRASE
+import baseball.NumberBaseBallGamePhrases.RESTART_INPUT_PHRASE
 import baseball.NumberBaseBallGamePhrases.START_PHRASE
+import camp.nextstep.edu.missionutils.Console.readLine
 import java.util.regex.Pattern
 
 fun main() {
@@ -19,10 +21,13 @@ fun main() {
         when (inputState) {
             ONGOING_INPUT -> {
                 print(ONGOING_INPUT_PHRASE)
-                stepUserInput = camp.nextstep.edu.missionutils.Console.readLine()
+                stepUserInput = readLine()
                 checkAppropriateOngoingInput(stepUserInput)
             }
             RESTART_INPUT -> {
+                println(RESTART_INPUT_PHRASE)
+                val gameStateInput = readLine()
+                checkAppropriateRestartInput(gameStateInput)
             }
         }
     }
@@ -56,4 +61,10 @@ private fun checkAppropriateOngoingInput(userInput: String) {
     if (!checkInputLength(userInput, 3)) throw IllegalArgumentException("입력된 숫자의 길이가 적절치 않습니다.")
     if (!isInputTypeNumber(userInput)) throw IllegalArgumentException("입력이 숫자타입이 아닙니다")
     if (!checkDuplicatePartInInput(userInput)) throw IllegalArgumentException("중복된 숫자가 입력되었습니다.")
+}
+
+private fun checkAppropriateRestartInput(userInput: String) {
+    if (!checkInputLength(userInput, 1)) throw IllegalArgumentException("입력된 숫자의 길이가 적절치 않습니다.")
+    if (!isInputTypeNumber(userInput)) throw IllegalArgumentException("입력이 숫자타입이 아닙니다")
+    if (!checkValidateRestartInput(userInput)) throw IllegalArgumentException("적절하지 못한 숫자가 입력되었습니다")
 }
