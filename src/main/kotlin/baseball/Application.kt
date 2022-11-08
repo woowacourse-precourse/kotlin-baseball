44776c881bfeafa4e5df1f6b572901e7289e4966
noion0511 <noion0511@gmail.com>
2022-11-08 14:47:37 +0900

    Feat : 서로 다른 3자리의 수를 선택하는 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
+    setRandomNumber()
+}
 
+fun setRandomNumber(): List<Int> {
+    val baseballNumber = mutableListOf<Int>()
+    while (baseballNumber.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!baseballNumber.contains(randomNumber)) {
+            baseballNumber.add(randomNumber)
+        }
+    }
+    return baseballNumber
 }
