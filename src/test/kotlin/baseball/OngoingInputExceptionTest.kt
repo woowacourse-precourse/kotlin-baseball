00393e90c2e4ae8c2291c879652a24827f5fb9f7
changhwan <chang5hustle@gmail.com>
2022-11-08 10:47:36 +0900

    test : ONGOING_INPUT 상태 예외 체크 테스트

---
+++ src/test/kotlin/baseball/OngoingInputExceptionTest.kt
@@ -0,0 +1,47 @@
+package baseball
+
+import camp.nextstep.edu.missionutils.test.Assertions
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class OngoingInputExceptionTest: NsTest() {
+    @Test
+    fun `커스텀 예외 테스트 ONGOING_INPUT 일떄 숫자가 아닌입력`() {
+        Assertions.assertSimpleTest {
+            assertThrows<IllegalArgumentException>("입력이 숫자타입이 아닙니다") { runException("!34") }
+        }
+    }
+
+    @Test
+    fun `커스텀 예외 테스트 ONGOING_INPUT 일떄 숫자가 아닌입력2`() {
+        Assertions.assertSimpleTest {
+            assertThrows<IllegalArgumentException>("입력이 숫자타입이 아닙니다") { runException("a23") }
+        }
+    }
+
+    @Test
+    fun `커스텀 예외 테스트 ONGOING_INPUT 일떄 적은 숫자입력`() {
+        Assertions.assertSimpleTest {
+            assertThrows<IllegalArgumentException>("입력된 숫자의 길이가 적절치 않습니다.") { runException("12") }
+        }
+    }
+
+    @Test
+    fun `커스텀 예외 테스트 ONGOING_INPUT 일떄 큰 숫자 입력`() {
+        Assertions.assertSimpleTest {
+            assertThrows<IllegalArgumentException>("입력된 숫자의 길이가 적절치 않습니다.") { runException("1234") }
+        }
+    }
+
+    @Test
+    fun `커스텀 예외 테스트 ONGOING_INPUT 일떄 중복된 숫자 입력`() {
+        Assertions.assertSimpleTest {
+            assertThrows<IllegalArgumentException>("중복된 숫자가 입력되었습니다.") { runException("223") }
+        }
+    }
+
+    override fun runMain() {
+        main()
+    }
+}
