884a6f6342c85c45777f25f0f3f0655fdb68937f
Yerin <myheaven_b@naver.com>
2022-11-08 19:56:49 +0900

    style: change valuable names and fix spacing

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,20 +4,19 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    var start: Boolean = true
-    var play: Boolean = true
+    var isStart: Boolean = true
+    var isPlay: Boolean = true
     var computer: List<Int> = listOf()
     var player: List<Int>
 
     println("숫자 야구 게임을 시작합니다.")
-    while (start or play) {
-        if (start) {
+    while (isStart or isPlay) {
+        if (isStart) {
             computer = getComputerNumbers()
-            start = false
         }
         player = getPlayerNumbers()
-        play = checkStrike(computer, player)
-        start = checkPlayAgain(play)
+        isPlay = checkStrike(computer, player)
+        isStart = checkPlayAgain(isPlay)
     }
 }
 
@@ -36,47 +35,50 @@ fun getComputerNumbers(): List<Int> {
 
 fun getPlayerNumbers(): List<Int> {
     print("숫자를 입력해주세요 : ")
-    val readNumber: String = Console.readLine()
-    var readList: MutableList<Int> = mutableListOf()
+    val playerInput: String = Console.readLine()
+    val playerNumbers: MutableList<Int> = mutableListOf()
 
-    checkPlayerInput(readNumber)
-    for (num in readNumber) {
-        readList.add(num - '0')
+    checkPlayerInput(playerInput)
+    for (num in playerInput) {
+        playerNumbers.add(num - '0')
     }
 
-    return readList
+    return playerNumbers
 }
 
 fun checkPlayerInput(str: String) {
-    if (str.length != 3){
+    if (str.length != 3) {
         throw IllegalArgumentException("길이가 3이 아닙니다.")
     }
     for (x in str) {
         if (x !in '0'..'9') {
             throw IllegalArgumentException("범위를 벗어난 숫자입니다.")
         }
-        if (str.count{it == x} != 1){
+        if (str.count { it == x } != 1) {
             throw IllegalArgumentException("중복된 숫자가 존재합니다.")
         }
     }
 }
 
-fun checkStrike(computer: List<Int>, player: List<Int>): Boolean {
+fun checkStrike(computerNumbers: List<Int>, playerNumbers: List<Int>): Boolean {
     var ball: Int = 0
     var strike: Int = 0
 
-    for (i in player.indices) {
-        if (player[i] !in computer)
+    for (i in playerNumbers.indices) {
+        if (playerNumbers[i] !in computerNumbers) {
             continue
-        if (player[i] == computer[i])
+        }
+        if (playerNumbers[i] == computerNumbers[i]) {
             strike++
-        else
+        } else {
             ball++
+        }
     }
     println(getResultString(ball, strike))
 
-    if (strike == 3)
+    if (strike == 3) {
         return false
+    }
     return true
 }
 
@@ -99,13 +101,13 @@ fun getResultString(ball: Int, strike: Int): String {
     return str
 }
 
-fun checkPlayAgain(x: Boolean): Boolean {
-    var input: String
+fun checkPlayAgain(isOnGame: Boolean): Boolean {
+    val input: String
 
-    if (!x) {
+    if (!isOnGame) {
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         input = Console.readLine()
-        return when(input) {
+        return when (input) {
             "1" -> true
             "2" -> false
             else -> throw IllegalArgumentException("1과 2 중 선택해야합니다. !")
