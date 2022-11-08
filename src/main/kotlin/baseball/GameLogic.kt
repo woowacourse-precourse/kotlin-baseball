e5bb62a71dde6d69753c1e7b7bed3ee0a8a8ab8d
Hyeongseon <hsun5163@gmail.com>
2022-11-08 12:19:41 +0900

    feat : gameProgress 함수 구현

--- src/main/kotlin/baseball/GameLogic.kt
+++ src/main/kotlin/baseball/GameLogic.kt
@@ -1,18 +1,19 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 import org.mockito.internal.matchers.Null
 
-var ballCnt = 0
-var strikeCnt = 0
 class GameLogic {
+
     private val comNumber = mutableListOf<Int>()
     private val userNumber = mutableListOf<Int>()
 
     private var inputNumber = ""
-    var number = 0
+    private var number = 0
 
     fun gameStart(){
+        comNumber.clear()
         while (comNumber.size < 3) {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
             if (!comNumber.contains(randomNumber)) {
@@ -20,10 +21,11 @@ class GameLogic {
             }
         }
     }
-    fun comparison() : Unit{
-        strikeCnt = 0
-        ballCnt = 0
-        println("userNumber : $userNumber comNumber : $comNumber")
+    fun comparison() : Map<String , Int>{
+        val mapCnt = mutableMapOf<String , Int>()
+        var ballCnt = 0
+        var strikeCnt = 0
+
         for (i in 0 until userNumber.size) {
             if(userNumber[i]==comNumber[i]){
                 strikeCnt += 1
@@ -31,20 +33,50 @@ class GameLogic {
                 ballCnt += 1
             }
         }
+        mapCnt["strike"] = strikeCnt
+        mapCnt["ball"] = ballCnt
+        return mapCnt
     }
-    fun userNumToList(num : Int) : List<Int>{
+    private fun userNumToList() : Unit{
         userNumber.clear()
-        var quotient = num
+        var quotient = number
         var inputNum = 0
         while(quotient != 0){
             inputNum = quotient%10
             quotient /= 10
-            userNumber.add(0,inputNum)
+            userNumber.add(0, inputNum)
         }
-        return userNumber
+        if(userNumber.distinct().size < 3){
+            throw IllegalArgumentException("중복된 숫자를 입력했습니다.")
+        }
     }
     fun userInput() : Unit{
-        inputNumber = readln()
-        number = inputNumber.toInt()
+        inputNumber = Console.readLine()
+        if("0" in inputNumber){
+            throw IllegalArgumentException("잘못된 숫자를 입력했습니다.")
+        }
+        if(inputNumber.length > 3 || inputNumber.length < 3){
+            throw IllegalArgumentException("범위를 초과했습니다.")
+        }
+        try {
+            number = inputNumber.toInt()
+        } catch (e:Exception) {
+            throw IllegalArgumentException("문자를 입력했습니다.")
+        }
+
+        userNumToList()
     }
+    fun gameProgress() : Boolean{
+        var ctrl = Console.readLine()
+        if(ctrl == "1"){
+            return true
+        }
+        else if(ctrl == "2"){
+            return false
+        }
+        else{
+            throw IllegalArgumentException("잘못된 키를 입력했습니다.")
+        }
+    }
+
 }
