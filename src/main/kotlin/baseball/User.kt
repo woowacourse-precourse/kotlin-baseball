825045b29540cd1c672d54468e51c3a81d14feb5
leehandsub <gustjq7027@naver.com>
2022-11-08 13:24:09 +0900

    feat(user): 입력 예외 중 숫자가 아닐 경우 구현

--- src/main/kotlin/baseball/User.kt
+++ src/main/kotlin/baseball/User.kt
@@ -7,8 +7,22 @@ class User {
         val userList = mutableListOf<Int>()
         print(INPUT_USER_MESSAGE)
         val input = Console.readLine().toCharArray()
-        for(i in input)
+
+        if (!checkNumber(input))
+            throw IllegalArgumentException()
+
+        for (i in input)
             userList.add(i.digitToInt())
         return userList
     }
+
+    private fun checkNumber(input: CharArray): Boolean {
+        for (i in input) {
+            if (!i.isDigit())
+                return false
+        }
+        return true
+    }
+
+
 }
