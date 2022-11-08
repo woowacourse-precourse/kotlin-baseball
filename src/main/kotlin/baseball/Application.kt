5f7706459c20381241b414b990f0d47de1eb0e6e
Yerin <myheaven_b@naver.com>
2022-11-08 21:12:12 +0900

    refactor: change string to constant val

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,13 +3,26 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
+const val GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다."
+const val INPUT_MESSAGE = "숫자를 입력해주세요 : "
+const val WRONG_LENGTH_MESSAGE = "길이가 3이 아닙니다. 게임 종료"
+const val OUT_OF_RANGE_MESSAGE = "범위를 벗어난 숫자입니다. 게임 종료"
+const val EXIST_DUPLICATE_MESSAGE = "중복된 숫자가 존재합니다. 게임 종료"
+const val GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+const val RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+const val WARNING_RESTART_MESSAGE = "1과 2 중 선택해야합니다! 게임 종료"
+
+const val BALL = "볼"
+const val STRIKE = "스트라이크"
+const val NOTHING = "낫싱"
+
 fun main() {
     var isStart: Boolean = true
     var isPlay: Boolean = true
     var computer: List<Int> = listOf()
     var player: List<Int>
 
-    println("숫자 야구 게임을 시작합니다.")
+    println(GAME_START_MESSAGE)
     while (isStart or isPlay) {
         if (isStart) {
             computer = getComputerNumbers()
@@ -34,7 +47,7 @@ fun getComputerNumbers(): List<Int> {
 }
 
 fun getPlayerNumbers(): List<Int> {
-    print("숫자를 입력해주세요 : ")
+    print(INPUT_MESSAGE)
     val playerInput: String = Console.readLine()
     val playerNumbers: MutableList<Int> = mutableListOf()
 
@@ -48,14 +61,14 @@ fun getPlayerNumbers(): List<Int> {
 
 fun checkPlayerInput(str: String) {
     if (str.length != 3) {
-        throw IllegalArgumentException("길이가 3이 아닙니다.")
+        throw IllegalArgumentException(WRONG_LENGTH_MESSAGE)
     }
     for (x in str) {
         if (x !in '0'..'9') {
-            throw IllegalArgumentException("범위를 벗어난 숫자입니다.")
+            throw IllegalArgumentException(OUT_OF_RANGE_MESSAGE)
         }
         if (str.count { it == x } != 1) {
-            throw IllegalArgumentException("중복된 숫자가 존재합니다.")
+            throw IllegalArgumentException(EXIST_DUPLICATE_MESSAGE)
         }
     }
 }
@@ -87,15 +100,15 @@ fun getResultString(ball: Int, strike: Int): String {
 
     if (ball > 0) {
         str = when (strike) {
-            0 -> "${ball}볼"
-            else -> "${ball}볼 ${strike}스트라이크"
+            0 -> "$ball$BALL"
+            else -> "$ball$BALL $strike$STRIKE"
         }
     }
     if (ball == 0) {
         str = when (strike) {
-            0 -> "낫싱"
-            3 -> "3스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
-            else -> "${strike}스트라이크"
+            0 -> NOTHING
+            3 -> "3$STRIKE\n$GAME_OVER_MESSAGE"
+            else -> "$strike$STRIKE"
         }
     }
     return str
@@ -105,12 +118,12 @@ fun checkPlayAgain(isOnGame: Boolean): Boolean {
     val input: String
 
     if (!isOnGame) {
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        println(RESTART_GAME_MESSAGE)
         input = Console.readLine()
         return when (input) {
             "1" -> true
             "2" -> false
-            else -> throw IllegalArgumentException("1과 2 중 선택해야합니다. !")
+            else -> throw IllegalArgumentException(WARNING_RESTART_MESSAGE)
         }
     }
 
