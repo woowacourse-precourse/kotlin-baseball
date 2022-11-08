41fd616722c1349fb0919133492d02fa55c2a3f8
Grandelwald <artjr97@naver.com>
2022-11-08 23:27:46 +0900

    OutputView.kt 멘트 상수로 감싸기

--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -5,13 +5,15 @@ import baseball.domain.Game
 object OutputView {
     const val THREE_STRIKE = 3
     private const val NOTHING = 0
+    private const val OPENING_MENT = "숫자 야구 게임을 시작합니다."
+    private const val ENDING_MENT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
 
-    fun printStaringMent(): Unit = print("숫자 야구 게임을 시작합니다.")
+    fun printStaringMent(): Unit = print(OPENING_MENT)
 
     fun printResult(game: Game){
         return if (game.strike == THREE_STRIKE) {
             println("3스트라이크")
-            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            println(ENDING_MENT)
         } else if (game.strike == NOTHING && game.ball == NOTHING) {
             println("낫싱")
         }
