58590db859229443a921665cef95040b01df091e
lanpia <95679243+lanpia@users.noreply.github.com>
2022-11-08 21:50:04 +0900

    testcaes00~03 만들기 각 case별 포함해야할 mesage

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest : NsTest() {
     @Test
-    fun `게임종료 후 재시작`() {
+    fun `test00`(){
         assertRandomNumberInRangeTest(
             {
                 run("246", "135", "1", "597", "589", "2")
@@ -19,14 +19,40 @@ class ApplicationTest : NsTest() {
             1, 3, 5, 5, 8, 9
         )
     }
-
     @Test
-    fun `예외 테스트`() {
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("1234") }
-        }
+    fun `test01`(){
+        assertRandomNumberInRangeTest(
+            {
+                run("123", "1", "789", "165", "256", "356", "456", "2")
+                assetThat(output())
+                    .contains("3스트라이크","낫싱","2볼","2스트라이크","2스트라이크","3스트라이크","게임 종료")
+            },
+            1, 2, 3, 4, 5, 6
+        )
     }
-
+    @Test
+    fun `test02`(){
+        assertRandomNumberInRangeTest(
+            {
+                run("123","456","789","1","123","124","135","2")
+                assetThat(output())
+                    .contains("낫싱","낫싱","3스트라이크","1볼1스트라이크","1스트라이크","3스트라이크","게임 종료")
+                },
+            7, 8, 9, 1, 3, 5
+        )
+    }
+    @Test
+    fun `test03`(){
+        assertRandomNumberInRangeTest(
+            {
+                run("642", "426", "246", "1","123","234","345","367","368","379","2")
+                assetThat(output())
+                    .contains("3볼", "2볼1스트라이크", "3스트라이크","1볼","1볼","1볼","2볼","1볼","3스트라이크","게임 종료")
+                
+            },
+            2, 4, 6, 3, 7, 9
+        )
+    }
     override fun runMain() {
         main()
     }
