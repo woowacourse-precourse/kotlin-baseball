924ea4dd3df487e3f021b9e70c793967788e9d2b
lanpia <95679243+lanpia@users.noreply.github.com>
2022-11-08 22:35:47 +0900

    make a comAnswer 숫자 야구 게임의 해답 : 랜덤으로 생성된 3자리수

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,3 +8,24 @@ var strike :Int = 0
 var ball : Int = 0
 val inputnum : Array<Int?> = arrayOfNulls<Int>(3)
 val comAnswer : Array<Int?> = arrayOfNulls<Int>(3)
+
+fun make_answer(comAnswer: Array<Int>) 
+{
+    val comAnswer : Array<Int?> = arrayOfNulls<Int>(3)
+    var switch = BooleanArray(10)
+    for (i in switch.indices)
+    {
+        switch[i] = false 
+    }
+    var w =0 
+    while(w<3)
+    {   
+        var r = (Math.random() * 10).toInt()
+        if(switch[r]==false)
+        {
+            switch[r] =true 
+            comAnswer[w] = r + 1 
+            w++
+        }
+    }
+}
