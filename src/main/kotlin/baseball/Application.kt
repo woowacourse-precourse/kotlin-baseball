b1ce609019b496e687f374d906384cf5890b549d
White-Patrasche <5338095@gmail.com>
2022-11-08 09:48:31 +0900

    feat: main 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,17 @@
 package baseball
 
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    while(true) {
+        val computerNum = pickComputerNumber()
+        playGame(computerNum)
+        println("숫자 3개를 모두 맞히셨습니다! 게임 종료")
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        if(isFinishGame()) {
+            break
+        }
+    }
+
 }
+
