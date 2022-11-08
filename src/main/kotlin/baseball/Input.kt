2c919ba6e41a388a4a52a0b13b199b02db324638
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-08 22:56:31 +0900

    style: validateAnswer 함수의 변수 c를 char로 변경
    변수 c를 봤을 때 문자라는 것을 알기 힘들기 때문에
    c를 char로 변경

--- src/main/kotlin/baseball/Input.kt
+++ src/main/kotlin/baseball/Input.kt
@@ -13,7 +13,7 @@ private fun validateAnswer(answer: String) {
     when {
         answer.length != 3 -> throw IllegalArgumentException(INVALID_ANSWER.message)
         !answer.all { it.isDigit() && it != '0' } -> throw IllegalArgumentException(INVALID_ANSWER.message)
-        !answer.all { answer.count { c -> c == it } == 1 } -> throw IllegalArgumentException(INVALID_ANSWER.message)
+        !answer.all { answer.count { char -> char == it } == 1 } -> throw IllegalArgumentException(INVALID_ANSWER.message)
     }
 }
 
