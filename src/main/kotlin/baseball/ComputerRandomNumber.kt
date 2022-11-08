4c517de212226528bb05e326b06f0461bb005cd7
lee989898 <wnsrud1352@gmail.com>
2022-11-08 20:19:26 +0900

    [feat](ComputerRandomNumber) 랜덤한 수를 담을 randomNumber 변수 추가

--- src/main/kotlin/baseball/ComputerRandomNumber.kt
+++ src/main/kotlin/baseball/ComputerRandomNumber.kt
@@ -2,6 +2,10 @@ package baseball
 
 class ComputerRandomNumber {
 
+    private var _randomNumber: String = ""
+    val randomNumber: String
+        get() = _randomNumber
+
     companion object {
         const val RANDOM_NUMBER_MIN_RANGE = 1
         const val RANDOM_NUMBER_MAX_RANGE = 9
