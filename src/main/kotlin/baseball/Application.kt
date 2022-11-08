2d79247a99f386a29ca585f25852e818035a5fda
lanpia <95679243+lanpia@users.noreply.github.com>
2022-11-08 22:37:27 +0900

    User에게 3자리 수를 입력 받음 1. User에게 메세지 송출 2. 3자리 숫자 입력받음 3. 입력받은 3자리 수를 배열로 처리함

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -9,6 +9,21 @@ var ball : Int = 0
 val inputnum : Array<Int?> = arrayOfNulls<Int>(3)
 val comAnswer : Array<Int?> = arrayOfNulls<Int>(3)
 
+fun playinggame() : Int
+{
+    val comAnswer : Array<Int?> = arrayOfNulls<Int>(3)
+    println("숫자 야구 게임을 시작합니다.")
+    var i : Int = 0
+    println("숫자를 입력해주세요 : ")
+    val inputnum : Array<Int?> = arrayOfNulls<Int>(3)
+    inputnum[2] = i % 10
+    inputnum[1] = i % 10
+    inputnum[0] = i % 10
+    inputIllegal(inputnum)
+    val (strike, ball) = checknum();
+    if (print_message() == 0) return (0);
+    return -1;
+}
 fun make_answer(comAnswer: Array<Int>) 
 {
     val comAnswer : Array<Int?> = arrayOfNulls<Int>(3)
