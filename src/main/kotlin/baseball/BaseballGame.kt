b4b7c18222a7fb9908c8aecfc83e513d1b245d82
lee989898 <wnsrud1352@gmail.com>
2022-11-08 20:56:34 +0900

    [feat](BaseballGame) 숫자야구게임이 시작하는 기능 구현

--- src/main/kotlin/baseball/BaseballGame.kt
+++ src/main/kotlin/baseball/BaseballGame.kt
@@ -1,6 +1,21 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
+
 class BaseballGame(private val humanPlayer: Player, private var computerPlayer: Player) {
+
+    fun startBaseballGame() {
+        var restartGame = true
+        println("숫자 야구 게임을 시작합니다.")
+        while (restartGame) {
+            humanSelectedNumber()
+            if (judgeHumanSelectedNumber() == CORRECT_ANSWER) {
+                restartGame = selectRestartOrTerminate()
+            }
+        }
+    }
+
     companion object {
         const val CORRECT_ANSWER = "3스트라이크"
         const val WRONG_ANSWER = ""
