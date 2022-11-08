823871fbf159c139465dc3d9269343d4c12caf09
lee989898 <wnsrud1352@gmail.com>
2022-11-08 20:50:51 +0900

    [feat](HumanPlayer) IllegalArgumentException이 발생하면 "유효하지 않은 숫자형식입니다."을 출력하는 함수 오버라이드

--- src/main/kotlin/baseball/HumanPlayer.kt
+++ src/main/kotlin/baseball/HumanPlayer.kt
@@ -7,4 +7,9 @@ class HumanPlayer : Player {
     override fun getSelectedNumber(): String {
         return humanSelectedNumber
     }
+
+    override fun setSelectedNumber(selectedNumber: String) {
+        require(Player.validateSelectedNumber(selectedNumber)) { "유효하지 않은 숫자형식입니다." }
+        humanSelectedNumber = selectedNumber
+    }
 }
