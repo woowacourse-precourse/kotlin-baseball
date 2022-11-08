3ff8dbdecfde7e87d5026dba9249a1d86bf6ca9a
DormunDoorlock <skrudgh2001@naver.com>
2022-11-08 23:08:13 +0900

    refactor: 게임을 더 진행할지 결정하는 메소드 구분 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -7,16 +7,18 @@ fun main() {
     println("숫자 야구 게임을 시작합니다.")
     do {
         playNumberBaseballGame()
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        val userInput = Console.readLine()
-        if(userInput=="1")
-            continue
-        else if(userInput == "2")
-            break
-        else
-            throw IllegalArgumentException()
-    }while (true)
+    }while (checkGamePlayMore())
     print("게임 종료")
+}
+fun checkGamePlayMore():Boolean{
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    val userInput = Console.readLine()
+    return if (userInput=="1")
+        true
+    else if(userInput == "2")
+        false
+    else
+        throw IllegalArgumentException()
 }
 
 fun playNumberBaseballGame(){
