975c094e0a0d930490cb7ba670133a8bb7153576
parkji1on <103587652+parkji1on@users.noreply.github.com>
2022-11-08 17:30:34 +0900

    랜덤한 baseball 값 설정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,27 @@
 package baseball
+import java.util.Random
+
+fun make_random() : MutableList<Int> {
+    val random = Random()
+    var list = mutableListOf<Int>()
+
+    while (list.size < 3){
+            val anyNumber = random.nextInt(10)+1
+            if (list.contains(anyNumber)){
+                continue
+            }
+            list.add(anyNumber)
+    }
+
+    return list
+}
 
 fun main() {
-    TODO("프로그램 구현")
+
+    var input:String
+
+    while(true){
+        var number = make_random()
+    }
+
 }
