5d37db8692581709dd704c90d0c2d3d69a87ff69
hayeon <hayeon2401@naver.com>
2022-11-08 17:11:07 +0900

    feat: 컴퓨터로부터 랜덤한 수 3개 받기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,20 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     TODO("프로그램 구현")
+}
+
+fun cptChooseRandomNum():List<Int>{ // 컴퓨터가 서로 다른 랜덤한 수 3개를 뽑게 하는 함수
+    val computer = mutableListOf<Int>()
+
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
+    return computer
 }
