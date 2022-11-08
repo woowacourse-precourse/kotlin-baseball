73531a6a5a24e82c9da18bad53467f7c960a57fc
wonseok <dnjstjr245@gmail.com>
2022-11-08 14:55:32 +0900

    :recycle: Constant object class 분리 (자주쓰는 상수들을 따로 관리)

---
+++ src/main/kotlin/baseball/utils/Constant.kt
@@ -0,0 +1,21 @@
+package baseball.utils
+
+object Constant {
+    const val DEFAULT_NUMBER = 0
+    const val DEFAULT_STRING = ""
+    const val MIN_NUMBER = 1
+    const val MAX_NUMBER = 9
+    const val CNT_NUMBER = 3
+    const val ACTIVE_STATE_CODE = "1"
+    const val QUIT_STATE_CODE = "2"
+
+    const val INPUT_MESSAGE = "숫자를 입력해주세요 : "
+    const val START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다."
+    const val RETRY_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    const val QUIT_GAME_MESSAGE = "게임을 종료합니다."
+    const val RESULT_NOTHING_MESSAGE = "낫싱"
+    const val RESULT_BALL_MESSAGE = "%d볼"
+    const val RESULT_STRIKE_MESSAGE = "%d스트라이크"
+    const val RESULT_BALL_AND_STRIKE_MESSAGE = "%d볼 %d스트라이크"
+    const val WIN_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+}
