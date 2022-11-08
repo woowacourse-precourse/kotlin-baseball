ae5f2a48c07ac96a1c60b38d730988be3af2dec0
Jaehun-Jo <chojh922@gmail.com>
2022-11-08 14:15:30 +0900

    [Feat] 잘못된 값을 입력한 경우 오류 처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 
 const val RESTART = 1
 const val EXIT = 2
+const val INPUT_LENGTH = 3
 
 fun main() {
     printStartMessage()
@@ -38,9 +39,33 @@ private fun userInput(): List<Int> {
     println("숫자를 입력해주세요 : ")
     val input = Console.readLine()
     val intArray = mutableListOf<Int>()
+    checkThrowException(input)
 
     for (element in input) {
         intArray.add(element.toString().toInt())
     }
     return intArray
+}
+
+private fun checkThrowException(input: String) {
+    throwIsNumberException(input)
+    throwNumberLengthException(input)
+    throwNumberDuplicationException(input)
+}
+
+fun throwIsNumberException(input: String) {
+    val numbers = input.toCharArray().filter { char ->
+        char in '1'..'9'
+    }
+    if (numbers.size != input.length) {
+        throw java.lang.IllegalArgumentException()
+    }
+}
+
+fun throwNumberLengthException(input: String) {
+    if (input.length != INPUT_LENGTH) throw IllegalArgumentException()
+}
+
+fun throwNumberDuplicationException(input: String) {
+    if (input[0] == input[1] || input[0] == input[2] || input[1] == input[2]) throw IllegalArgumentException()
 }
