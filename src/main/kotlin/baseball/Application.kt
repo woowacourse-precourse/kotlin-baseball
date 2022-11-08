be4c0d1a8308f57f87b6220cacc45305fc1c8afa
hyunji1203 <hj27720@gmail.com>
2022-11-08 12:44:47 +0900

    feat: 컴퓨터가 임의의 수 3개 선택
    컴퓨터(상대방)이 임의의 수를 Randoms를 이용해 랜덤으로 3개 뽑기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,28 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    while(true){
+        //게임 시작 문구
+        println("숫자 야구 게임을 시작합니다.")
+
+        //컴퓨터 랜덤 3자리 수 뽑기
+        val computer = computerRandom()
+
+    }
 }
+
+//컴퓨터 랜덤 3자리 수 뽑기
+fun computerRandom() : MutableList<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
+
