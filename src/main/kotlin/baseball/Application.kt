0dd8b7fc8fe534e4b8aa55c2709105a677b4b2c4
navillera11 <lunatickblue@naver.com>
2022-11-08 23:42:22 +0900

    feat: 컴퓨터의 랜덤 숫자를 얻는 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,39 +1,12 @@
 package baseball
-
-fun getGameResult(computerNumber: MutableList<Int>){
-    var strike = 0
-    var ball = 0
-    var index = 0
-    val userNumber = getUserNumber()
-
-    for(i in userNumber){
-        if(computerNumber.contains(i - '0'))
-            ball++
-        if(computerNumber[index] == i - '0'){
-            ball--
-            strike++
+import camp.nextstep.edu.missionutils.Randoms
+fun getComputerNumber(): MutableList<Int> {
+    val computerNumber = mutableListOf<Int>()
+    while(computerNumber.size < 3){
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if(!computerNumber.contains(randomNumber)){
+            computerNumber.add(randomNumber)
         }
-        index++
     }
-
-    if(strike == 3){
-        println("${strike}스트라이크")
-        print("3개의 숫자를 모두 맞히셨습니다!")
-        println(" 게임종료")
-        askRestart()
-        return
-    }
-
-    else if(strike==0&&ball==0)
-        println("낫싱")
-    else if(strike==0)
-        println("${ball}볼")
-    else if(ball==0)
-        println("${strike}스트라이크")
-    else
-        println("${ball}볼 ${strike}스트라이크")
-
-    getGameResult(computerNumber)
+    return computerNumber
 }
-
-
