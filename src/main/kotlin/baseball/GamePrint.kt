a21823f0d4e0884cc33675daabb85c45f58b7c3e
Hyeongseon <hsun5163@gmail.com>
2022-11-08 12:23:21 +0900

    refactor : GamePrint 클래스 리팩토링

--- src/main/kotlin/baseball/GamePrint.kt
+++ src/main/kotlin/baseball/GamePrint.kt
@@ -1,28 +1,48 @@
 package baseball
 
+
 class GamePrint {
+
+    private val gl = GameLogic()
+
     fun gameStart(){
-        println("숫자 야구 게임을 시작합니다.")
+
+        gl.gameStart()
+        result()
     }
     fun inputNum(){
         print("숫자를 입력해주세요 : ")
+        gl.userInput()
     }
     fun result(){
-        if(ballCnt > 0){
+        inputNum()
+        val ballCnt = gl.comparison()["ball"]
+        val strikeCnt = gl.comparison()["strike"]
+        if(ballCnt!! > 0){
             print("${ballCnt}볼 ")
         }
-        if(strikeCnt > 0){
+        if(strikeCnt!! > 0){
             print("${strikeCnt}스트라이크 ")
         }
         if(strikeCnt + ballCnt == 0){
             print("낫싱")
         }
         println()
+        if(strikeCnt == 3){
+            answer()
+            if(gl.gameProgress()){
+                gameStart()
+            } else{
+                return
+            }
+        }
+        else{
+            result()
+        }
+
     }
-    fun answer(){
+    private fun answer(){
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-    }
-    fun gameProgress(){
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
     }
 }
