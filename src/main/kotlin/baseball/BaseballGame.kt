23c3fd959fcee4a26d33c173bd3e665e9bb13b42
lee989898 <wnsrud1352@gmail.com>
2022-11-08 20:55:12 +0900

    [feat](BaseballGame) 사람 플레이어가 3자리 수를 모두 맞추었을때 반환되는 String 상수,추측이 틀렸을때 반환되는 String 상수 추가

--- src/main/kotlin/baseball/BaseballGame.kt
+++ src/main/kotlin/baseball/BaseballGame.kt
@@ -1,3 +1,8 @@
 package baseball
 
-class BaseballGame(private val humanPlayer: Player, private var computerPlayer: Player)
+class BaseballGame(private val humanPlayer: Player, private var computerPlayer: Player) {
+    companion object {
+        const val CORRECT_ANSWER = "3스트라이크"
+        const val WRONG_ANSWER = ""
+    }
+}
