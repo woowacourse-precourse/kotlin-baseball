2a1f5847fd26f6dfbeab74978012ee3a37ee8d61
navillera11 <lunatickblue@naver.com>
2022-11-08 23:41:27 +0900

    feat: 게임 결과 값을 받아 메세지를 출력하는 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,12 +1,39 @@
 package baseball
 
-fun askRestart(){
-    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    val select= Console.readLine()
-    if(select=="1"){
-        playGame()
+fun getGameResult(computerNumber: MutableList<Int>){
+    var strike = 0
+    var ball = 0
+    var index = 0
+    val userNumber = getUserNumber()
+
+    for(i in userNumber){
+        if(computerNumber.contains(i - '0'))
+            ball++
+        if(computerNumber[index] == i - '0'){
+            ball--
+            strike++
+        }
+        index++
     }
-    if(select=="2"){
-        endMessage()
+
+    if(strike == 3){
+        println("${strike}스트라이크")
+        print("3개의 숫자를 모두 맞히셨습니다!")
+        println(" 게임종료")
+        askRestart()
+        return
     }
+
+    else if(strike==0&&ball==0)
+        println("낫싱")
+    else if(strike==0)
+        println("${ball}볼")
+    else if(ball==0)
+        println("${strike}스트라이크")
+    else
+        println("${ball}볼 ${strike}스트라이크")
+
+    getGameResult(computerNumber)
 }
+
+
