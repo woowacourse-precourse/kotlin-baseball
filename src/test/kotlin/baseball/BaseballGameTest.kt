015cb6d9b2a243a2dd636e35e1870249c8fbf432
lh99j <grand2181@gmail.com>
2022-11-08 13:07:20 +0900

    test : 컴퓨터 리스트와 유저 리스트가 정확히 비교되는지 테스트

--- src/test/kotlin/baseball/BaseballGameTest.kt
+++ src/test/kotlin/baseball/BaseballGameTest.kt
@@ -37,7 +37,7 @@ class BaseballGameTest : NsTest() {
             }
         }
     }
-    
+
     @Test
     fun `게임 끝나고 숫자 입력시 1 또는 2가 아니면 Exception 테스트`() {
         camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest {
@@ -48,6 +48,38 @@ class BaseballGameTest : NsTest() {
                 Exception().isValidEndNum("12")
             }
         }
+    }
+
+    @Test
+    fun `3스트라이크가 아니면 false를 리턴하는지 테스트`() {
+        val userList1 = listOf(1, 2, 3)
+        val computerList1 = listOf(3, 4, 5)
+        val userList2 = listOf(5, 1, 4)
+        val computerList2 = listOf(5, 1, 3)
+        val userList3 = listOf(6, 7, 3)
+        val computerList3 = listOf(6, 3, 7)
+        val userList4 = listOf(8, 9, 1)
+        val computerList4 = listOf(1, 8, 9)
+        Assertions.assertThat(GamePlay().compareUserNumAndComputerNum(computerList1, userList1)).isEqualTo(false)
+        Assertions.assertThat(GamePlay().compareUserNumAndComputerNum(computerList2, userList2)).isEqualTo(false)
+        Assertions.assertThat(GamePlay().compareUserNumAndComputerNum(computerList3, userList3)).isEqualTo(false)
+        Assertions.assertThat(GamePlay().compareUserNumAndComputerNum(computerList4, userList4)).isEqualTo(false)
+    }
+
+    @Test
+    fun `3스트라이크면 true를 리턴하는지 테스트`() {
+        val userList1 = listOf(1, 2, 3)
+        val computerList1 = listOf(1, 2, 3)
+        val userList2 = listOf(5, 1, 4)
+        val computerList2 = listOf(5, 1, 4)
+        val userList3 = listOf(6, 7, 3)
+        val computerList3 = listOf(6, 7, 3)
+        val userList4 = listOf(8, 9, 1)
+        val computerList4 = listOf(8, 9, 1)
+        Assertions.assertThat(GamePlay().compareUserNumAndComputerNum(computerList1, userList1)).isEqualTo(true)
+        Assertions.assertThat(GamePlay().compareUserNumAndComputerNum(computerList2, userList2)).isEqualTo(true)
+        Assertions.assertThat(GamePlay().compareUserNumAndComputerNum(computerList3, userList3)).isEqualTo(true)
+        Assertions.assertThat(GamePlay().compareUserNumAndComputerNum(computerList4, userList4)).isEqualTo(true)
     }
 
     override fun runMain() {
