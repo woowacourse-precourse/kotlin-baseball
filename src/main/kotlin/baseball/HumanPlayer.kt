272354c8fd7cb42521a41da3195a12d7787de9e2
lee989898 <wnsrud1352@gmail.com>
2022-11-08 20:50:15 +0900

    [feat](HumanPlayer) 사람이 정한 3자리 자연수 가져오는 함수 오바라이드

--- src/main/kotlin/baseball/HumanPlayer.kt
+++ src/main/kotlin/baseball/HumanPlayer.kt
@@ -3,4 +3,8 @@ package baseball
 class HumanPlayer : Player {
 
     private var humanSelectedNumber: String = ""
+
+    override fun getSelectedNumber(): String {
+        return humanSelectedNumber
+    }
 }
