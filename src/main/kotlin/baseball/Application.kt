9307cc486f01beff2748ef6480ac2c3cd9f6ea4f
bluecat0823 <star4_you@naver.com>
2022-11-08 12:17:13 +0900

    <feat> 종료 입력 전까지 게임을 반복한다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,8 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
-import camp.nextstep.edu.missionutils.Console
 
-fun Numbergenerator(): MutableList<Int> {
+fun numbergenerator(): MutableList<Int> {
     val computerNumber: MutableList<Int> = mutableListOf()
     while (computerNumber.size < 3) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
@@ -22,9 +21,9 @@ fun playerInput():String {
     return playerAnswer
 }
 fun playerInputToList(playerInputString:String) : MutableList<Int>{
-    val playerInputSplit = playerInputString.split("")
+    val playerInputSplit = playerInputString.chunked(1).map { it.toInt() }.toIntArray()
     val playerInputList : MutableList<Int> = mutableListOf()
-    for(count in 0..2) playerInputList[count]=playerInputSplit[count].toInt()
+    for(count in 0..2) playerInputList.add(count,playerInputSplit[count])
     return playerInputList
 }
 fun playerInputValidCheck(CheckList : MutableList<Int>){
@@ -38,7 +37,7 @@ fun playerInputValidCheck(CheckList : MutableList<Int>){
         throw IllegalArgumentException()
     }
 }
-fun StrikeBallCheck(Answer : MutableList<Int>,Input : MutableList<Int>) : Int{
+fun strikeBallCheck(Answer : MutableList<Int>,Input : MutableList<Int>) : Int{
     var Strike = 0
     var Ball = 0
     for (count in 0..2){
@@ -51,17 +50,53 @@ fun StrikeBallCheck(Answer : MutableList<Int>,Input : MutableList<Int>) : Int{
     if (Strike==3){
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
     } else if(Strike!=0 && Ball==0){
-        println("$Strike+스트라이크")
+        print("$Strike")
+        println("스트라이크")
     } else if(Strike==0 && Ball!=0){
-        println("$Ball+볼")
+        print("$Ball")
+        println("볼")
     } else if(Strike==0 && Ball==0){
         println("낫싱")
     } else{
-        println("$Ball+볼 $Strike+스트라이크")
+        print("$Ball")
+        print("볼 ")
+        print("$Strike")
+        println("스트라이크")
     }
     return Strike
 }
+fun gameMenu(menu:Int):Boolean{
+    when (menu) {
+        1 -> {
+            return true
+        }
+        2 -> {
+            return false
+        }
+        else -> {
+            throw IllegalArgumentException()
+        }
+    }
+}
+fun menuInput(): Int {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    return readLine()!!.toInt()
+}
 fun main() {
-    /*var computerNumber = Numbergenerator()
-    println(computerNumber)*/
+    println("숫자 야구 게임을 시작합니다.")
+    while(true){
+        var computer = numbergenerator()
+        while (true){
+            var player = playerInput()
+            var playerInputList=playerInputToList(player)
+            playerInputValidCheck(playerInputList)
+            if (strikeBallCheck(computer,playerInputList)==3){
+                break
+            }
+        }
+        val menu = menuInput()
+        if (!gameMenu(menu)){
+            break
+        }
+    }
 }
