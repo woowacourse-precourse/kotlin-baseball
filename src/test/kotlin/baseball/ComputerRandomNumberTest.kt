c77efc8f948f80b8524db19832cb49ddaf20f242
lee989898 <wnsrud1352@gmail.com>
2022-11-08 20:38:59 +0900

    [test](ComputerRandomNumberTest) randomNumber에 3자리 난수가 생성됐는지 테스트하는 기능 구현

--- src/test/kotlin/baseball/ComputerRandomNumberTest.kt
+++ src/test/kotlin/baseball/ComputerRandomNumberTest.kt
@@ -1,3 +1,17 @@
 package baseball
 
-class ComputerRandomNumberTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("ComputerRandomNumber 클래스")
+class ComputerRandomNumberTest {
+
+    @DisplayName("randomNumber에 3자리 난수가 생성됐는지 확인한다")
+    @Test
+    fun getThreeDigitsNumber() {
+        val computerRandomNumber = ComputerRandomNumber()
+        Assertions.assertFalse(computerRandomNumber.randomNumber.isEmpty())
+        Assertions.assertEquals(computerRandomNumber.randomNumber.length, 3)
+    }
+}
