9cf045d5ee91b8c1bfc3451eaad0567f5c0bbe11
dltkd1395 <dltkd13956@naver.com>
2022-11-08 15:22:07 +0900

    test(NumberTest) : 스트라이크/볼/낫싱 출력 결과 테스트 추가

--- src/test/kotlin/baseball/NumberTest.kt
+++ src/test/kotlin/baseball/NumberTest.kt
@@ -1,6 +1,7 @@
 package baseball
 
 import baseball.model.BaseBall
+import baseball.view.OutputView
 import org.junit.jupiter.api.Test
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
@@ -48,6 +49,30 @@ class NumberTest : NsTest() {
         val computerNumbers = listOf(1, 3, 5)
         val playerNumbers = listOf(2, 5, 3)
         assertThat(BaseBall(computerNumbers, playerNumbers).getBallCount()).isEqualTo(2)
+    }
+
+    @Test
+    fun `스트라이크 볼 출력 결과 테스트`() {
+        OutputView().printResult(1, 2)
+        assertThat(output()).isEqualTo("1볼 2스트라이크")
+    }
+
+    @Test
+    fun `스트라이크 출력 결과 테스트`() {
+        OutputView().printResult(0, 2)
+        assertThat(output()).isEqualTo("2스트라이크")
+    }
+
+    @Test
+    fun `볼 출력 결과 테스트`() {
+        OutputView().printResult(1, 0)
+        assertThat(output()).isEqualTo("1볼")
+    }
+
+    @Test
+    fun `낫싱 출력 결과 테스트`() {
+        OutputView().printResult(0, 0)
+        assertThat(output()).isEqualTo("낫싱")
     }
 
     override fun runMain() {
