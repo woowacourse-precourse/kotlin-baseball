77f83e3e2b8607795e67ee276f877af0f68df69c
lee989898 <wnsrud1352@gmail.com>
2022-11-08 20:55:52 +0900

    [feat](BaseballGame) 게임 재시작함을 의미하는 사람플레이어의 입력하는 String 상수,게임 끝을 의미하는 사람플레이어의 입력하는 String 상수 추가

--- src/main/kotlin/baseball/BaseballGame.kt
+++ src/main/kotlin/baseball/BaseballGame.kt
@@ -4,5 +4,7 @@ class BaseballGame(private val humanPlayer: Player, private var computerPlayer: 
     companion object {
         const val CORRECT_ANSWER = "3스트라이크"
         const val WRONG_ANSWER = ""
+        const val RESTART_GAME = "1"
+        const val TERMINATE_GAME = "2"
     }
 }
