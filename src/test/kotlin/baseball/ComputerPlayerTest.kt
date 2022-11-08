70d6a33127045672ac152c6d2037d6821a8bd4e0
lee989898 <wnsrud1352@gmail.com>
2022-11-08 20:46:58 +0900

    [test](ComputerPlayerTest) ComputerRandomNumber 인스턴스를 인자로 넘긴 ComputerPlayer의 인스턴스가 3자리 난수를 생성하는지 테스트하는 기능 구현

--- src/test/kotlin/baseball/ComputerPlayerTest.kt
+++ src/test/kotlin/baseball/ComputerPlayerTest.kt
@@ -1,3 +1,20 @@
 package baseball
 
-class ComputerPlayerTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class ComputerPlayerTest {
+    @Nested
+    @DisplayName("생성자에 ComputerRandomNumber 인스턴스를 인자로 넘긴 ComputerPlayer의 인스턴스는")
+    inner class ComputerPlayerConstructorWithParameter {
+        @DisplayName("3자리 난수를 생성한다")
+        @Test
+        fun getThreeDigitsNumber() {
+            val computerPlayer: Player = ComputerPlayer(ComputerRandomNumber())
+            Assertions.assertFalse(computerPlayer.getSelectedNumber().isEmpty())
+            Assertions.assertEquals(computerPlayer.getSelectedNumber().length, 3)
+        }
+    }
+}
