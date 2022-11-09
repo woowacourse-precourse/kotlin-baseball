73b9089512041363afe0a219e663f68bf1d849fc
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-11-09 12:00:59 +0900

    test(Match) : companion obejct + JvmStatic 을 이용하여, 초기화시 한 번만 메서드 사용

--- src/test/kotlin/baseball/MatchTest.kt
+++ src/test/kotlin/baseball/MatchTest.kt
@@ -3,26 +3,10 @@ package baseball
 import baseball.domain.Match
 import baseball.domain.Player
 import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 class MatchTest {
-    private lateinit var player: Player
-    private lateinit var random: RandomTest
-
-    @BeforeEach
-    fun `컴퓨터와 플레이어 숫자 가져오기`() {
-        player = Player()
-        player.saveIfValid("123")
-        val result = "123"
-        Assertions.assertThat(player.number).isEqualTo(result)
-
-        random = RandomTest()
-        random.createRandomValue("456")
-        val received = random.generate()
-        val expected = "456"
-        Assertions.assertThat(received).isEqualTo(expected)
-    }
 
     @Test
     fun `정상적을 hint 값이 나오는가?`() {
@@ -33,5 +17,25 @@ class MatchTest {
         val received = method.invoke(match) as String
         val expected = "낫싱"
         Assertions.assertThat(received).isEqualTo(expected)
+    }
+
+    companion object {
+        private lateinit var player: Player
+        private lateinit var random: RandomTest
+
+        @JvmStatic
+        @BeforeAll
+        fun `컴퓨터와 플레이어 숫자 가져오기`() {
+            player = Player()
+            player.saveIfValid("123")
+            val result = "123"
+            Assertions.assertThat(player.number).isEqualTo(result)
+
+            random = RandomTest()
+            random.createRandomValue("456")
+            val received = random.generate()
+            val expected = "456"
+            Assertions.assertThat(received).isEqualTo(expected)
+        }
     }
 }
