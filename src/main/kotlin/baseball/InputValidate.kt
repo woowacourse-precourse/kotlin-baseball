2a2d39e503c1597ff20bc3ed3c41bf30a03898a5
윤동현 <donghyunyoon81@gmail.com>
2022-12-02 16:59:09 +0900

    feat<InputValidate> : duplicateBaseballNumbers 기능 구현
    야구 숫자의 중복값이 있을 경우 예외를 발생시키는 기능 구현

--- src/main/kotlin/baseball/InputValidate.kt
+++ src/main/kotlin/baseball/InputValidate.kt
@@ -4,6 +4,11 @@ class InputValidate {
     fun baseballNumbersForm(input:String) {
         require(BASEBALL_NUMBERS_FORM.toRegex().matches(input)) { println("1부터 9까지의 숫자를 3개 입력해주세요")}
     }
+
+    fun duplicateBaseballNumbers(input:String) {
+        val input = input.toCharArray()
+        require(input.size == input.distinct().size ) { println("숫자의 중복이 없도록 입력해주세요.")}
+    }
     companion object {
         const val BASEBALL_NUMBERS_FORM = "^[1-9]{3}\$"
     }
