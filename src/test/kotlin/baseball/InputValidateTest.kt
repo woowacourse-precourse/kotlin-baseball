fe91efd0601d039fd14c41010f2375aa3538002b
윤동현 <donghyunyoon81@gmail.com>
2022-12-02 14:50:48 +0900

    feat<InputValidateTest> : 테스트 할때 사용할 클레스를 인스턴스화 하는 setup 기능 구현
    tdd 방식으로 구현 하기 위해 test를 먼저 작성하여 <RED> 단계를 구성한다

--- src/test/kotlin/baseball/InputValidateTest.kt
+++ src/test/kotlin/baseball/InputValidateTest.kt
@@ -1,4 +1,17 @@
 package baseball
 
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import kotlin.IllegalArgumentException
+
 class InputValidateTest {
+    private lateinit var inputValidate: InputValidate
+
+    @BeforeEach
+    fun setup() {
+        val inputValidate: InputValidate()
+    }
 }
