f4311a18158af123c435c79d74cf0aa34c8b605c
윤동현 <donghyunyoon81@gmail.com>
2022-12-02 16:28:40 +0900

    feat<InputValidate> : baseballNumbersForm 기능 구현
    야구 숫자 형식에 맞지 않는 값을 입력할 경우 예외를 발생시키는 기능

--- src/main/kotlin/baseball/InputValidate.kt
+++ src/main/kotlin/baseball/InputValidate.kt
@@ -1,5 +1,11 @@
 package baseball
 
 class InputValidate {
+    fun baseballNumbersForm(input:String) {
+        require(BASEBALL_NUMBERS_FORM.toRegex().matches(input)) { println("1부터 9까지의 숫자를 3개 입력해주세요")}
+    }
+    companion object {
+        const val BASEBALL_NUMBERS_FORM = "^[1-9]{3}\$"
+    }
 
 }
