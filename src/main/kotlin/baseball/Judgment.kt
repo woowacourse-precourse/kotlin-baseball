5ae73eaec0b3bb1c1b2dcc52e0e86ca71e63fe4c
윤동현 <donghyunyoon81@gmail.com>
2022-12-03 23:46:25 +0900

    feat<Judgment> : countStrike 기능 구현
    입력 받은 숫자들과 생성한 숫자들을 비교합니다
    자리와 숫자가 같으면 스트라이크 개수를 +1 합니다.
    총 개수와 스트라이크 문자열을 반환합니다.

--- src/main/kotlin/baseball/Judgment.kt
+++ src/main/kotlin/baseball/Judgment.kt
@@ -1,4 +1,13 @@
 package baseball
 
 class Judgment {
+
+    fun countStrike(inputNumbers: List<Int>, createNumbers: List<Int>): String {
+        var strikeCount = 0
+        inputNumbers.forEachIndexed { index, number ->
+            if (createNumbers[index] == number) strikeCount++
+        }
+        return "${strikeCount}스트라이크"
+    }
+
 }
