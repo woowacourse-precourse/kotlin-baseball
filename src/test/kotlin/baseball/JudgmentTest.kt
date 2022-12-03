ae40667f6caa6ab9bf0bafd1d0b4bc197dff3785
윤동현 <donghyunyoon81@gmail.com>
2022-12-04 00:52:36 +0900

    test<JudgmentTest> : 스트라이크 테스트에 CsvSource의 3번째 데이터에 ''추가
    빈 문자열을 반환하고 싶을대 ''을 추가해야 합니다.
    추가하지 않을 경우 null 데이터로 반환됩니다.

--- src/test/kotlin/baseball/JudgmentTest.kt
+++ src/test/kotlin/baseball/JudgmentTest.kt
@@ -19,7 +19,7 @@ class JudgmentTest {
     @CsvSource(
         "1,2,3,3스트라이크",
         "1,2,5,2스트라이크",
-        "6,9,5,")
+        "6,9,5,''")
     fun `스트라이크 테스트`(number1:Int,number2:Int,number3:Int, expectedResult:String) {
         val countStrikeResult = judgment.countStrike(inputNumbers, listOf<Int>(number1, number2, number3))
         Assertions.assertThat(countStrikeResult).isEqualTo(expectedResult)
