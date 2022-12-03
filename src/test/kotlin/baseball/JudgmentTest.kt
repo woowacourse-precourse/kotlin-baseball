2f75f2447b8fc044bed0ea066e36cdcb392a3720
윤동현 <donghyunyoon81@gmail.com>
2022-12-03 23:33:45 +0900

    test<JudgmentTest> : 스트라이크 판정 테스트 구현
    스트라이크를 판정하는 테스트 기능을 구성만 한다

--- src/test/kotlin/baseball/JudgmentTest.kt
+++ src/test/kotlin/baseball/JudgmentTest.kt
@@ -1,4 +1,21 @@
 package baseball
 
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
 class JudgmentTest {
+    private lateinit var judgment: Judgment
+
+    @BeforeEach
+    fun setup() {
+        judgment = Judgment()
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1,7,8])
+    fun `스트라이크 판정 테스트`(number:Int) {
+
+    }
+
 }
