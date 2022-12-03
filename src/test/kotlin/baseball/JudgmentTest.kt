8eb47d98c4d65952ec4a3af500c953883269a3a6
윤동현 <donghyunyoon81@gmail.com>
2022-12-04 00:25:11 +0900

    test<JudgmentTest> : 카운트 스트라이크 테스트 구현
    스트라이크의 개수와 스트라이크 문자를 반환 하는 기능 테스트
    스트라이크가 없을 경우 공백을 출력하는 기능을 구현하지 않아 테스트에 실패 합니다.
    공백을 출력하는 기능을 구현합니다.

--- src/test/kotlin/baseball/JudgmentTest.kt
+++ src/test/kotlin/baseball/JudgmentTest.kt
@@ -1,10 +1,13 @@
 package baseball
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.CsvSource
 
 class JudgmentTest {
+    private val inputNumbers = listOf<Int>(1, 2, 3)
     private lateinit var judgment: Judgment
 
     @BeforeEach
@@ -13,9 +16,13 @@ class JudgmentTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [1,7,8])
-    fun `스트라이크 판정 테스트`(number:Int) {
-
+    @CsvSource(
+        "1,2,3,3스트라이크",
+        "1,2,5,2스트라이크",
+        "6,9,5,")
+    fun `스트라이크 테스트`(number1:Int,number2:Int,number3:Int, expectedResult:String) {
+        val countStrikeResult = judgment.countStrike(inputNumbers, listOf<Int>(number1, number2, number3))
+        Assertions.assertThat(countStrikeResult).isEqualTo(expectedResult)
     }
 
 }
