4d98edbab63e64467beb2d889a3ae28cdbe04e89
윤동현 <donghyunyoon81@gmail.com>
2022-12-03 16:39:56 +0900

    test<OutputViewTest> : OutputView를 테스트하는 클래스 생성 및 setup()
    OutputView 타입의 변수를 선언하고 변수에 OutputView() 클래스를 인스턴스화 한다.

---
+++ src/test/kotlin/baseball/OutputViewTest.kt
@@ -0,0 +1,12 @@
+package baseball
+
+import org.junit.jupiter.api.BeforeEach
+
+class OutputViewTest {
+    private lateinit var outputView :OutputView
+
+    @BeforeEach
+    fun setup() {
+        outputView = OutputView()
+    }
+}
