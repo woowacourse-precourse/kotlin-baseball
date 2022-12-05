d5c0e7bcae29c47ba503f4eb959fbc62f431a810
윤동현 <donghyunyoon81@gmail.com>
2022-12-05 23:06:16 +0900

    feat<Judgment> : countBall 기능 구현
    입력 받은 숫자들과 생성한 숫자들을 비교합니다.
    자리와 숫자가 다르지만 생성한 숫자들에 포함되어 있으면 볼 개수를 +1 합니다.
    총 개수와 볼 문자열을 반환합니다.

--- src/main/kotlin/baseball/Judgment.kt
+++ src/main/kotlin/baseball/Judgment.kt
@@ -11,9 +11,19 @@ class Judgment {
         return strikeCount.toString() + STRIKE
     }
 
+    fun countBall(inputNumbers: List<Int>, createNumbers: List<Int>): String {
+        var ballCount = INITIALIZE_COUNT
+        inputNumbers.forEachIndexed { index, number ->
+            if (createNumbers[index] != number && createNumbers.contains(number)) ballCount++
+        }
+        if (ballCount == INITIALIZE_COUNT) return ""
+        return ballCount.toString() + BALL
+    }
+
     companion object {
         const val INITIALIZE_COUNT = 0
         const val STRIKE = "스트라이크"
+        const val BALL = "볼"
     }
 
 }
