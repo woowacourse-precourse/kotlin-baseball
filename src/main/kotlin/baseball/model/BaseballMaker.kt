b108fbaef8bdc1f43e4fc4885c7d43f415d502bb
vrexpert <vrexpert2302@gmail.com>
2022-12-05 23:02:42 +0900

    feat(BaseballMaker): 랜덤 세 자리 숫자 생성

--- src/main/kotlin/baseball/model/BaseballMaker.kt
+++ src/main/kotlin/baseball/model/BaseballMaker.kt
@@ -2,6 +2,11 @@ package baseball.model
 
 class BaseballMaker (private val randomNumberGenerator: RandomNumberGenerator){
     fun makeBaseball():List<Int>{
-        return listOf()
+        var baseBall = mutableListOf<Int>()
+        for (index in 0..2){
+            val number = randomNumberGenerator.generate()
+            baseBall.add(number)
+        }
+        return baseBall
     }
 }
