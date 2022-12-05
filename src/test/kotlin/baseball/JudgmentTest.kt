4b3f98d1b4ee913f23ec3f427a61a3e60c0c1af6
윤동현 <donghyunyoon81@gmail.com>
2022-12-05 23:22:41 +0900

    test<JudgmentTest> : 카운트 볼 테스트 구현
    볼의 개수와 볼 문자를 반환 하는 기능 테스트
    볼의 개수가 없을 경우 공백을 호출 합니다.

--- src/test/kotlin/baseball/JudgmentTest.kt
+++ src/test/kotlin/baseball/JudgmentTest.kt
@@ -19,10 +19,22 @@ class JudgmentTest {
     @CsvSource(
         "1,2,3,3스트라이크",
         "1,2,5,2스트라이크",
-        "6,9,5,''")
-    fun `스트라이크 테스트`(number1:Int,number2:Int,number3:Int, expectedResult:String) {
+        "6,9,5,''"
+    )
+    fun `스트라이크 테스트`(number1: Int, number2: Int, number3: Int, expectedResult: String) {
         val countStrikeResult = judgment.countStrike(inputNumbers, listOf<Int>(number1, number2, number3))
         Assertions.assertThat(countStrikeResult).isEqualTo(expectedResult)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "3,1,2,3볼",
+        "1,3,2,2볼",
+        "1,2,3,''"
+    )
+    fun `볼 테스트`(number1: Int, number2: Int, number3: Int, expectedResult: String) {
+        val countBallResult = judgment.countBall(inputNumbers, listOf<Int>(number1, number2, number3))
+        Assertions.assertThat(countBallResult).isEqualTo(expectedResult)
     }
 
 }
