bcb4b27f73f28b9bb2608a2ebfae29883fac1106
vrexpert <vrexpert2302@gmail.com>
2022-12-06 00:30:08 +0900

    feat(OutputView): OutputView 작성
    printBall(): 볼 출력
    printStrike(): 스트라이크 출력
    printBallStrike(): 볼, 스트라이크 출력
    printMessage(): 메세지 출력

--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -1,20 +1,20 @@
 package baseball.view
 
 class OutputView {
-    fun printBall() {
-
+    fun printBall(ball: Int) {
+        print("${ball}볼")
     }
 
-    fun printStrike() {
-
+    fun printStrike(strike: Int) {
+        println("${strike}스트라이크")
     }
 
-    fun printMessage() {
-
+    fun printBallStrike(ball: Int, strike: Int) {
+        println("${ball}볼 ${strike}스트라이크")
     }
 
-    fun printBallStrike() {
-
+    fun printMessage(message: String) {
+        println(message)
     }
 
 }
