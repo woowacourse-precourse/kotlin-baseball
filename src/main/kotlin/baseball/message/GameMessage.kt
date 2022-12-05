e0c1dc978e8a047d328563e782f3a7b630cd1402
vrexpert <vrexpert2302@gmail.com>
2022-12-06 00:31:55 +0900

    feat(GameMessage): 게임 메세지 출력 오브젝트 작성

---
+++ src/main/kotlin/baseball/message/GameMessage.kt
@@ -0,0 +1,7 @@
+package baseball.message
+
+object GameMessage {
+    const val GAME_START = "숫자 야구 게임을 시작합이다."
+    const val CORRECT_ANSWER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    const val RETRY_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+}
