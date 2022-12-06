4c95377689b38a0d57171a3c629057407dfb8546
vrexpert <vrexpert2302@gmail.com>
2022-12-06 15:52:54 +0900

    feat(InputView): 사용자 입력 예외 처리
    문자 입력
    숫자 범위 밖 입력
    숫자 개수 제한

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -1,15 +1,37 @@
 package baseball.view
 
+import baseball.utils.checkNumberRange
+import baseball.utils.checkNumbersLength
+import baseball.utils.inputTypeException
 import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     fun readNumbers(): List<Int> {
         val input = Console.readLine()
-        return input.map { it.toString().toInt() }.toMutableList()
+        with(input) {
+            inputTypeException()
+            checkNumbersLength(NUMBERS_LENGTH)
+            checkNumberRange(LOWER_NUMBER,UPPER_NUMBER)
+            return map { it.toString().toInt() }.toList()
+        }
     }
 
-    fun readRetry():Int{
-        val input = Console.readLine().toInt()
-        return input
+    fun readRetry(): Int {
+        val input = Console.readLine()
+        with(input) {
+            inputTypeException()
+            checkNumbersLength(RETRY_END_LENGTH)
+            checkNumberRange(RETRY, END)
+        }
+        return input.toInt()
+    }
+
+    companion object {
+        const val NUMBERS_LENGTH = 3
+        const val RETRY_END_LENGTH = 1
+        const val UPPER_NUMBER = 9
+        const val LOWER_NUMBER = 1
+        const val RETRY = 1
+        const val END = 2
     }
 }
