6e7b9962fef6f5229c9ad0b0df9bad1a3dfe6168
vrexpert <vrexpert2302@gmail.com>
2022-12-06 12:39:20 +0900

    feat(GameMessage): 메세지 추가

--- src/main/kotlin/baseball/message/GameMessage.kt
+++ src/main/kotlin/baseball/message/GameMessage.kt
@@ -1,7 +1,11 @@
 package baseball.message
 
 object GameMessage {
-    const val GAME_START = "숫자 야구 게임을 시작합이다."
+    const val GAME_START = "숫자 야구 게임을 시작합니다."
     const val CORRECT_ANSWER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    const val PLEASE_INPUT = "숫자를 입력해주세요 : "
     const val RETRY_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+
+    const val RETRY_GAME = 1
+    const val END_GAME = 2
 }
