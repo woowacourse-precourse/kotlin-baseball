35584123069c878cc46cbef5b6a56efeb5b0dc5d
윤동현 <donghyunyoon81@gmail.com>
2022-12-07 12:06:54 +0900

    feat<OutputView> : 판정 결과를 출력하는 judgementResult 기능 구현
    when 조건을 사용하려 하였으나 when 범위 안에 옳은 조건 하나만 실행 되기 때문에
    if를 사용하였습니다 더 좋은 방법이 있는지 못찾아서 완성뒤에 찾아볼 예정입니다.

--- src/main/kotlin/baseball/OutputView.kt
+++ src/main/kotlin/baseball/OutputView.kt
@@ -1,5 +1,15 @@
 package baseball
 
 class OutputView {
+    private val judgment:Judgment = Judgment()
 
+    fun judgementResult(inputNumbers: List<Int>, createNumbers: List<Int>) {
+        val ball = judgment.countBall(inputNumbers, createNumbers)
+        val strike = judgment.countStrike(inputNumbers, createNumbers)
+        var compareResult = ""
+        if(strike+ball == 0) compareResult = "낫씽"
+        if(ball != 0) compareResult += "${ball}볼 "
+        if(strike != 0)  compareResult += "${strike}스트라이크"
+        println(compareResult)
+    }
 }
