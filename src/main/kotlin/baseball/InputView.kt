5f3f06ba73beed5a18e71395600eb90091da9e54
윤동현 <donghyunyoon81@gmail.com>
2022-12-07 20:16:39 +0900

    feat<InputView> : inputRestartNumber 기능 구현
    게임을 다시할지 종료할지 선택하는 숫자를 입력 받는 기능

--- src/main/kotlin/baseball/InputView.kt
+++ src/main/kotlin/baseball/InputView.kt
@@ -12,4 +12,12 @@ class InputView {
         inputValidate.duplicateBaseballNumbers(inputBaseballNumbers)
         return inputBaseballNumbers.toList().map { number -> number.digitToInt() }
     }
+
+    fun inputRestartNumber(): Int {
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val inputRestartNumber = Console.readLine()
+        inputValidate.restartNumber(inputRestartNumber)
+        return inputRestartNumber.first().digitToInt()
+    }
+
 }
