94f0001d863f5ccca5a0b260bb6d905210fc6367
윤동현 <donghyunyoon81@gmail.com>
2022-12-07 12:40:01 +0900

    feat<InputValidate> : restartNumber 기능 구현
    게임 종료시 새로 시작하거나 종료할시 입력되는 문자열의 유효성 검사 기능 입니다.

--- src/main/kotlin/baseball/InputValidate.kt
+++ src/main/kotlin/baseball/InputValidate.kt
@@ -10,8 +10,13 @@ class InputValidate {
         require(input.size == input.distinct().size ) { println("숫자의 중복이 없도록 입력해주세요.")}
     }
 
+    fun restartNumber(input: String){
+        require(RESTART_NUMBER_FORM.toRegex().matches(input)) { println("1 또는 2의 숫자를 하나만 입력해주세요")}
+    }
+
     companion object {
         const val BASEBALL_NUMBERS_FORM = "^[1-9]{3}\$"
+        const val RESTART_NUMBER_FORM = "^[1-2]{1}\$"
     }
 
 }
