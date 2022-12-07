0b484b6ecb50511f19f3651d7c0fca125b744bee
윤동현 <donghyunyoon81@gmail.com>
2022-12-08 00:05:30 +0900

    feat<OutputView> : baseballGame 기능 구현
    야구 게임을 진행하는 기능 구현

--- src/main/kotlin/baseball/OutputView.kt
+++ src/main/kotlin/baseball/OutputView.kt
@@ -2,11 +2,24 @@ package baseball
 
 class OutputView {
     private val judgment:Judgment = Judgment()
+    private val numberGenerator : NumberGenerator = NumberGenerator()
+    private val inputView: InputView = InputView()
 
     init {
         println("숫자 야구 게임을 시작합니다")
     }
 
+    private fun baseballGame() {
+        val createNumbers = numberGenerator.createBaseballNumbers()
+        while (true) {
+            val inputNumbers = inputView.inputBaseballNumbers()
+            judgementResult(inputNumbers, createNumbers)
+            if (isThreeStrike(inputNumbers, createNumbers)) {
+                println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+                break
+            }
+        }
+    }
     fun isThreeStrike(inputNumbers: List<Int>, createNumbers: List<Int>) =
         judgment.countStrike(inputNumbers, createNumbers) == 3
 
