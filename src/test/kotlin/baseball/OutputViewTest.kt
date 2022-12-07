135934c6809e571cfc4e25c1460627a0188c1658
윤동현 <donghyunyoon81@gmail.com>
2022-12-07 12:04:20 +0900

    test<OutputViewTest> : 숫자 야구 게임 판정 결과 테스트 구현
    판정 결과가 올바르게 나오는지 테스트하는 기능

--- src/test/kotlin/baseball/OutputViewTest.kt
+++ src/test/kotlin/baseball/OutputViewTest.kt
@@ -1,12 +1,35 @@
 package baseball
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
+import java.io.PrintStream
 
 class OutputViewTest {
-    private lateinit var outputView :OutputView
+    private lateinit var outputView: OutputView
+    private val inputNumbers = listOf<Int>(1, 2, 3)
+    private val outputStream: OutputStream = ByteArrayOutputStream()
 
     @BeforeEach
     fun setup() {
         outputView = OutputView()
+        System.setOut(PrintStream(outputStream))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1,2,3,3스트라이크",
+        "1,6,5,1스트라이크",
+        "3,1,2,3볼",
+        "3,7,9,1볼",
+        "1,3,4,1볼 1스트라이크",
+        "6,9,5,낫씽"
+    )
+    fun `숫자 야구 게임 판정 결과 테스트`(number1: Int, number2: Int, number3: Int, outputResult: String) {
+        outputView.judgementResult(inputNumbers, listOf<Int>(number1, number2, number3))
+        Assertions.assertThat(outputStream.toString().trim()).isEqualTo(outputResult)
     }
 }
