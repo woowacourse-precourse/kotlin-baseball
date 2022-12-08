74ad21e6a4cbce7ea6b856775e17e048655a319b
윤동현 <donghyunyoon81@gmail.com>
2022-12-08 13:39:38 +0900

    refactor<OutputView> : baseballGame에 inputNumbers 필드 제거
    기능의 필드수를 최대한 줄이기 위해서

--- src/main/kotlin/baseball/OutputView.kt
+++ src/main/kotlin/baseball/OutputView.kt
@@ -1,8 +1,8 @@
 package baseball
 
 class OutputView {
-    private val judgment:Judgment = Judgment()
-    private val numberGenerator : NumberGenerator = NumberGenerator()
+    private val judgment: Judgment = Judgment()
+    private val numberGenerator: NumberGenerator = NumberGenerator()
     private val inputView: InputView = InputView()
 
     init {
@@ -15,9 +15,8 @@ class OutputView {
         val createNumbers = numberGenerator.createBaseballNumbers()
         while (true) {
             println("숫자를 입력해주세요 : ")
-            val inputNumbers = inputView.inputBaseballNumbers()
-            judgementResult(inputNumbers, createNumbers)
-            if (isThreeStrike(inputNumbers, createNumbers)) {
+            judgementResult(inputView.inputBaseballNumbers(), createNumbers)
+            if (isThreeStrike(inputView.inputBaseballNumbers(), createNumbers)) {
                 println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
                 break
             }
@@ -27,8 +26,9 @@ class OutputView {
     private fun restartGame() {
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         val restartNumber = inputView.inputRestartNumber()
-        if(restartNumber == 1) baseballGame()
+        if (restartNumber == 1) baseballGame()
     }
+
     fun isThreeStrike(inputNumbers: List<Int>, createNumbers: List<Int>) =
         judgment.countStrike(inputNumbers, createNumbers) == 3
 
@@ -37,9 +37,9 @@ class OutputView {
         val ball = judgment.countBall(inputNumbers, createNumbers)
         val strike = judgment.countStrike(inputNumbers, createNumbers)
         var compareResult = ""
-        if(strike+ball == 0) compareResult = "낫싱"
-        if(ball != 0) compareResult += "${ball}볼 "
-        if(strike != 0)  compareResult += "${strike}스트라이크"
+        if (strike + ball == 0) compareResult = "낫싱"
+        if (ball != 0) compareResult += "${ball}볼 "
+        if (strike != 0) compareResult += "${strike}스트라이크"
         println(compareResult)
     }
 }
