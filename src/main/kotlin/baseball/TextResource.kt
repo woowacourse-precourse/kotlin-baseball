ca798da0c412344ab5ba376c2a9f6a5d9ecce428
윤동현 <donghyunyoon81@gmail.com>
2022-12-08 13:49:36 +0900

    feat<TextResource> : OutputMessage 기능 구현
    outputView에서 출력할 메세지들을 상수로 선언

--- src/main/kotlin/baseball/TextResource.kt
+++ src/main/kotlin/baseball/TextResource.kt
@@ -5,3 +5,10 @@ enum class ErrorMessage(val message: String) {
     DUPLICATE_BASEBALL_NUMBERS("숫자의 중복이 없도록 입력해주세요."),
     RESTART_NUMBER("1 또는 2의 숫자를 하나만 입력해주세요")
 }
+
+enum class OutputMessage(val message: String) {
+    INPUT_NUMBERS("숫자를 입력해주세요 : "),
+    THREE_STRIKE("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+    QUESTION_RETRY("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+    GAME_START("숫자 야구 게임을 시작합니다.")
+}
