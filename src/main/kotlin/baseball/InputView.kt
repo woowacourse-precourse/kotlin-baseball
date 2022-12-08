662d23eeb8ef217ca902787c61090ce09124c0ec
윤동현 <donghyunyoon81@gmail.com>
2022-12-08 13:31:58 +0900

    refactor<InputView> : println 삭제 print 내용을 OutputView에서 구현하기 위해서 삭제합니다.

--- src/main/kotlin/baseball/InputView.kt
+++ src/main/kotlin/baseball/InputView.kt
@@ -6,7 +6,6 @@ class InputView {
     private val inputValidate: InputValidate = InputValidate()
 
     fun inputBaseballNumbers(): List<Int> {
-        println("숫자를 입력해주세요 : ")
         val inputBaseballNumbers = Console.readLine()
         inputValidate.baseballNumbersForm(inputBaseballNumbers)
         inputValidate.duplicateBaseballNumbers(inputBaseballNumbers)
@@ -14,7 +13,6 @@ class InputView {
     }
 
     fun inputRestartNumber(): Int {
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         val inputRestartNumber = Console.readLine()
         inputValidate.restartNumber(inputRestartNumber)
         return inputRestartNumber.first().digitToInt()
