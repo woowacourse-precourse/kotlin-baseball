03aafb83561822ca63d6360aee01215da6ba82ef
윤동현 <donghyunyoon81@gmail.com>
2022-12-08 15:26:48 +0900

    refactor<NumberGenerator> : 하드코딩된 숫자 데이터 수정

--- src/main/kotlin/baseball/NumberGenerator.kt
+++ src/main/kotlin/baseball/NumberGenerator.kt
@@ -6,13 +6,19 @@ class NumberGenerator {
 
     fun createBaseballNumbers(): List<Int> {
         val baseballNumbers = mutableListOf<Int>()
-        while (baseballNumbers.size < 3) {
-            val randomNumber = Randoms.pickNumberInRange(1, 9)
+        while (baseballNumbers.size < BASEBALL_NUMBERS_SIZE) {
+            val randomNumber = Randoms.pickNumberInRange(BASEBALL_NUMBERS_MIN_NUMBER, BASEBALL_NUMBERS_MAX_NUMBER)
             if (!baseballNumbers.contains(randomNumber)) {
                 baseballNumbers.add(randomNumber)
             }
         }
         return baseballNumbers
+    }
+
+    companion object {
+        const val BASEBALL_NUMBERS_SIZE = 3
+        const val BASEBALL_NUMBERS_MIN_NUMBER = 1
+        const val BASEBALL_NUMBERS_MAX_NUMBER = 9
     }
 
 }
