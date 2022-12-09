abe03f9887a1fd70585ddd9b91f811843e6a15a3
윤동현 <donghyunyoon81@gmail.com>
2022-12-09 13:44:55 +0900

    test<BaseBallGameActivityTest> : BaseBallGameActivityTest 클래스 생성
    테스트할 기능을 담을 baseballActivity 필드를 선언합니다.
    BeforeEach에서 인스턴스화한 클래스를 담아야 테스트때 각자 다른
    객체로서 기능합니다
    테스트에 사용할 inputNumbers 필드를 선언합니다.
    출력 문자를 담을 outputStream 필드를 선언합니다.

---
+++ src/test/kotlin/baseball/BaseBallGameActivityTest.kt
@@ -0,0 +1,14 @@
+package baseball
+
+import baseball.controller.BaseballActivity
+import org.junit.jupiter.api.BeforeEach
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
+import java.io.PrintStream
+
+class BaseBallGameActivityTest {
+    private lateinit var baseballActivity: BaseballActivity
+    private val inputNumbers = listOf<Int>(1, 2, 3)
+    private val outputStream: OutputStream = ByteArrayOutputStream()
+
+}
