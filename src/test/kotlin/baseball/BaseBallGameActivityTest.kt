62856a7925efac0a5b48e6f651233d1e66c615b4
윤동현 <donghyunyoon81@gmail.com>
2022-12-09 14:12:22 +0900

    test<BaseBallGameActivity>: 숫자 야구 게임 판정 결과 테스트 기능 구현
    테스트 결과 볼의 개수만 있을시에 볼만 있는 결과에 공백이 생겨 테스트 실패

--- src/test/kotlin/baseball/BaseBallGameActivityTest.kt
+++ src/test/kotlin/baseball/BaseBallGameActivityTest.kt
@@ -1,7 +1,10 @@
 package baseball
 
 import baseball.controller.BaseballActivity
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import java.io.ByteArrayOutputStream
 import java.io.OutputStream
 import java.io.PrintStream
@@ -15,6 +18,20 @@ class BaseBallGameActivityTest {
     fun setup() {
         baseballActivity = BaseballActivity()
         System.setOut(PrintStream(outputStream))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1,2,3,3스트라이크",
+        "1,6,5,1스트라이크",
+        "3,1,2,3볼",
+        "3,7,9,1볼",
+        "1,3,4,1볼 1스트라이크",
+        "6,9,5,낫싱"
+    )
+    fun `숫자 야구 게임 판정 결과 테스트`(number1: Int, number2: Int, number3: Int, testResult: String) {
+        val judgementResult = baseballActivity.getJudgementResult(inputNumbers, listOf<Int>(number1, number2, number3))
+        Assertions.assertThat(judgementResult).isEqualTo(testResult)
     }
 
 
