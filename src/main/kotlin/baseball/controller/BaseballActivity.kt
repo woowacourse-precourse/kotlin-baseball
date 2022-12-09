b04e77c3ee491dc59ecc476abf59eed5e1bf0b2e
윤동현 <donghyunyoon81@gmail.com>
2022-12-09 14:42:06 +0900

    refactor<BaseballActivity,Judgment> : isThreeStrike, getJudgementResult 기능 이전
    BaseballActivity 클래스에서 Judgment 클래스로 이전
    데이터의 형태로 반환되는 기능이기 때문에 model 레이어의 Judgment 클래스에
    더 어울리는 기능이라 생각해서 이전합니다.

--- src/main/kotlin/baseball/controller/BaseballActivity.kt
+++ src/main/kotlin/baseball/controller/BaseballActivity.kt
@@ -27,8 +27,8 @@ class BaseballActivity {
         while (gameLoop) {
             outputView.inputNumbers()
             val inputNumbers = inputView.inputBaseballNumbers()
-            outputView.judgementResult(getJudgementResult(inputNumbers, createNumbers))
-            gameLoop = !isThreeStrike(inputNumbers, createNumbers)
+            outputView.judgementResult(judgment.getJudgementResult(inputNumbers, createNumbers))
+            gameLoop = !judgment.isThreeStrike(inputNumbers, createNumbers)
         }
     }
 
@@ -38,31 +38,7 @@ class BaseballActivity {
         if (restartNumber == RESTART_NUMBER) baseballGame()
     }
 
-    fun isThreeStrike(inputNumbers: List<Int>, createNumbers: List<Int>): Boolean {
-        if (judgment.countStrike(inputNumbers, createNumbers) == MAX_COUNT) {
-            outputView.winGame()
-            return true
-        }
-        return false
-    }
-
-    fun getJudgementResult(inputNumbers: List<Int>, createNumbers: List<Int>) : String {
-        val ball = judgment.countBall(inputNumbers, createNumbers)
-        val strike = judgment.countStrike(inputNumbers, createNumbers)
-        var compareResult = INITIAL_COMPARE_RESULT
-        if (strike + ball == NO_COUNT) compareResult = NOTHING
-        if (ball != NO_COUNT) compareResult += "$ball$BALL_TEXT"
-        if (strike != NO_COUNT) compareResult += "$strike$STRIKE_TEXT"
-        return compareResult
-    }
-
     companion object {
-        const val NOTHING = "낫싱"
-        const val INITIAL_COMPARE_RESULT = ""
-        const val BALL_TEXT = "볼 "
-        const val STRIKE_TEXT = "스트라이크"
-        const val NO_COUNT = 0
-        const val MAX_COUNT = 3
         const val RESTART_NUMBER = 1
     }
 }
--- src/main/kotlin/baseball/model/Judgment.kt
+++ src/main/kotlin/baseball/model/Judgment.kt
@@ -18,8 +18,27 @@ class Judgment {
         return ballCount
     }
 
-    companion object {
-        const val INITIALIZE_COUNT = 0
+    fun isThreeStrike(inputNumbers: List<Int>, createNumbers: List<Int>): Boolean =
+        countStrike(inputNumbers, createNumbers) == MAX_COUNT
+
+    fun getJudgementResult(inputNumbers: List<Int>, createNumbers: List<Int>): String {
+        val ball = countBall(inputNumbers, createNumbers)
+        val strike = countStrike(inputNumbers, createNumbers)
+        var compareResult = INITIAL_COMPARE_RESULT
+        if (strike + ball == NO_COUNT) compareResult = NOTHING
+        if (ball != NO_COUNT) compareResult += "$ball${BALL_TEXT}"
+        if (strike != NO_COUNT) compareResult += "$strike${STRIKE_TEXT}"
+        return compareResult
     }
 
+
+    companion object {
+        const val INITIALIZE_COUNT = 0
+        const val NOTHING = "낫싱"
+        const val INITIAL_COMPARE_RESULT = ""
+        const val BALL_TEXT = "볼 "
+        const val STRIKE_TEXT = "스트라이크"
+        const val NO_COUNT = 0
+        const val MAX_COUNT = 3
+    }
 }
