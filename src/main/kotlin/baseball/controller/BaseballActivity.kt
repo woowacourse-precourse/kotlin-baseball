ea0463ae8612c5300a0d8ebc4b6d1a9dbdbb8de3
윤동현 <donghyunyoon81@gmail.com>
2022-12-09 12:30:53 +0900

    feat<BaseballActivity> : 판정 결과를 출력하는 getJudgementResult 기능 구현

--- src/main/kotlin/baseball/controller/BaseballActivity.kt
+++ src/main/kotlin/baseball/controller/BaseballActivity.kt
@@ -14,4 +14,24 @@ class BaseballActivity {
     private fun gameStart(){
         outputView.gameStart()
     }
+
+
+
+    private fun getJudgementResult(inputNumbers: List<Int>, createNumbers: List<Int>) : String {
+        val ball = judgment.countBall(inputNumbers, createNumbers)
+        val strike = judgment.countStrike(inputNumbers, createNumbers)
+        var compareResult = INITIAL_COMPARE_RESULT
+        if (strike + ball == NO_COUNT) compareResult = NOTHING
+        if (ball != NO_COUNT) compareResult += "$ball$BALL_TEXT"
+        if (strike != NO_COUNT) compareResult += "$strike$STRIKE_TEXT"
+        return compareResult
+    }
+
+    companion object {
+        const val NOTHING = "낫싱"
+        const val INITIAL_COMPARE_RESULT = ""
+        const val BALL_TEXT = "볼 "
+        const val STRIKE_TEXT = "스트라이크"
+        const val NO_COUNT = 0
+    }
 }
