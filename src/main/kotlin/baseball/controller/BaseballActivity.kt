f96a3506fb7359c1b81cb9623548a566b5351d39
윤동현 <donghyunyoon81@gmail.com>
2022-12-09 11:51:09 +0900

    feat<BaseballActivity> : model의 클래스와 view의 클래스 인스턴스화
    model 클래스와 view 클래스를 인스턴스화 해서 사용하기 위해서
    변수로 선언합니다.

--- src/main/kotlin/baseball/controller/BaseballActivity.kt
+++ src/main/kotlin/baseball/controller/BaseballActivity.kt
@@ -1,12 +1,13 @@
 package baseball.controller
 
+import baseball.model.Judgment
+import baseball.model.NumberGenerator
+import baseball.view.io.InputView
 import baseball.view.io.OutputView
 
 class BaseballActivity {
     private val outputView: OutputView = OutputView()
-    init {
-        outputView.gameStart()
-        outputView.baseballGame()
-        outputView.restartGame()
-    }
+    private val inputView: InputView = InputView()
+    private val judgment: Judgment = Judgment()
+    private val numberGenerator = NumberGenerator()
 }
