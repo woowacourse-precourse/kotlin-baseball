2ce01485b597a367d8ccc44db4c55ce2183b1e0e
윤동현 <donghyunyoon81@gmail.com>
2022-12-10 13:27:25 +0900

    test<OutputViewTest> : 각 테스트에 사용할 변수 초기화하는 setup 기능 구현
    outputView 필드에 인스턴스화 한 OutputView를 대입
    outputStream에 출력 내용을 대입

--- src/test/kotlin/baseball/OutputViewTest.kt
+++ src/test/kotlin/baseball/OutputViewTest.kt
@@ -1,7 +1,19 @@
 package baseball
 
 import baseball.view.io.OutputView
+import org.junit.jupiter.api.BeforeEach
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
+import java.io.PrintStream
 
 class OutputViewTest {
+    private lateinit var outputView: OutputView
+    private val outputStream: OutputStream = ByteArrayOutputStream()
+
+    @BeforeEach
+    fun `setup`(){
+        outputView = OutputView()
+        System.setOut(PrintStream(outputStream))
+    }
 
 }
