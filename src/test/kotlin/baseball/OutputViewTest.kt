546878fc6534222c762ea5c69cb679ae3afc8e88
윤동현 <donghyunyoon81@gmail.com>
2022-12-10 13:39:39 +0900

    test<OutputViewTest> : 게임을 승리할시 안내문을 출력하는지 테스트

--- src/test/kotlin/baseball/OutputViewTest.kt
+++ src/test/kotlin/baseball/OutputViewTest.kt
@@ -1,7 +1,11 @@
 package baseball
 
 import baseball.view.io.OutputView
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 import java.io.ByteArrayOutputStream
 import java.io.OutputStream
 import java.io.PrintStream
@@ -11,9 +15,19 @@ class OutputViewTest {
     private val outputStream: OutputStream = ByteArrayOutputStream()
 
     @BeforeEach
-    fun `setup`(){
+    fun `setup`() {
         outputView = OutputView()
         System.setOut(PrintStream(outputStream))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "true,3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+        "false,''"
+    )
+    fun `게임을 승리할시 안내문을 출력하는지 테스트`(isWinGame: Boolean, expectValue: String) {
+        outputView.winGame(isWinGame)
+        Assertions.assertThat(outputStream.toString().trim()).isEqualTo(expectValue)
     }
 
 }
