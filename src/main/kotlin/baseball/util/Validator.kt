0432e55e9b66838c35125bbab2a3d6fd1f89929e
sojk401 <sojk401@gmail.com>
2022-12-12 23:12:05 +0900

    fix: 중복된 숫자를 입력한 경우 수정

--- src/main/kotlin/baseball/util/Validator.kt
+++ src/main/kotlin/baseball/util/Validator.kt
@@ -2,19 +2,30 @@ package baseball.util
 
 object Validator {
     fun check3digitNum(num: String): Boolean {
+        var number = num.toMutableList()
+
+        // 3자리 수가 아닌 경우 예외 처리
+        if (num.length != 3) {
+            throw IllegalArgumentException("3자리 숫자를 입력해주세요.")
+        }
         // 숫자 형태가 아닌 경우 예외 처리
         if (num.toIntOrNull() == null) {
             throw IllegalArgumentException("숫자 형태로 입력해주세요.")
         }
-        // 1~9 사이 숫자가 아닌 경우 예외 처리
-        for (i in num) {
-            if (i !in '1'..'9') {
-                throw IllegalArgumentException("1~9 사이 숫자를 입력해주세요.")
+        // 중복된 숫자가 있는 경우 예외 처리
+        while (number.size != 0) {
+            var first = number.removeFirst()
+            println(first)
+            println(number)
+            if (first in number) {
+                throw IllegalArgumentException("중복되지 않은 숫자를 입력해주세요.")
             }
         }
-        // 3자리 수가 아닌 경우 또는 중복된 숫자가 있는 경우 예외 처리
-        if (setOf(num.toList()).size != 3) {
-            throw IllegalArgumentException("중복되지 않은 3자리 숫자를 입력해주세요.")
+        // 1~9 사이 숫자가 아닌 경우 예외 처리
+        for (i in num) {
+            if ('0' in num) {
+                throw IllegalArgumentException("1~9 사이 숫자를 입력해주세요.")
+            }
         }
         return true
     }
